01fb866835658aaaaabbc70cb2ccc958de91d055
Laurent Cozic <laurent@cozic.net>
2017-07-13 23:20:08 +0100

    Better rendering of markdown notes

--- ReactNativeClient/lib/components/screens/note.js
+++ ReactNativeClient/lib/components/screens/note.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, TextInput, WebView, Text } from 'react-native';
+import { View, Button, TextInput, WebView, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux'
 import { Log } from 'lib/log.js'
 import { Note } from 'lib/models/note.js'
@@ -8,6 +8,12 @@ import { ScreenHeader } from 'lib/components/screen-header.js';
 import { Checkbox } from 'lib/components/checkbox.js'
 import { _ } from 'lib/locale.js';
 import marked from 'lib/marked.js';
+
+const styles = StyleSheet.create({
+	webView: {
+		fontSize: 10,
+	},
+});
 
 class NoteScreenComponent extends React.Component {
 	
@@ -135,9 +141,50 @@ class NoteScreenComponent extends React.Component {
 
 		let bodyComponent = null;
 		if (this.state.mode == 'view') {
+
+			// https://necolas.github.io/normalize.css/
+			const normalizeCss = `
+				html{line-height:1.15;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}
+				article,aside,footer,header,nav,section{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0;overflow:visible}
+				pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent;-webkit-text-decoration-skip:objects}
+				b,strong{font-weight:bolder}small{font-size:80%}img{border-style:none}
+			`;
+			const css = `
+				body {
+					font-size: 14px;
+					margin: 1em;
+				}
+				h1 {
+					font-size: 1.2em;
+					font-weight: bold;
+				}
+				h2 {
+					font-size: 1em;
+					font-weight: bold;
+				}
+				li {
+					
+				}
+				ul {
+					padding-left: 1em;
+				}
+				.checkbox {
+					font-size: 1.4em;
+					position: relative;
+					top: 0.1em;
+				}
+			`;
+
+			let body = note.body;
+			body = body.replace(/- \[ \]/g, '☐');
+			body = body.replace(/- \[X\]/g, '☑');
+
 			const source = {
-				html: note ? marked(note.body, { gfm: true, breaks: true }) : '',
+				html: note ? '<style>' + normalizeCss + "\n" + css + '</style>' + marked(body, { gfm: true, breaks: true }) : '',
 			};
+
+			source.html = source.html.replace(/☐/g, '<span class="checkbox">☐</span>')
+			source.html = source.html.replace(/☑/g, '<span class="checkbox">☑</span>')
 
 			bodyComponent = (
 				<View style={{flex:1}}>
