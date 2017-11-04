f1e1db6744f7905630e6e00cdc8a1af153472c47
Laurent Cozic <laurent@cozic.net>
2017-11-04 11:50:58 +0000

    Clean up

--- CliClient/update-package-md5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-MD5="$(cat "$ROOT_DIR/package.json" | md5sum | cut -d' ' -f 1)"
-echo -n $MD5 > "$ROOT_DIR/package.json.md5"
