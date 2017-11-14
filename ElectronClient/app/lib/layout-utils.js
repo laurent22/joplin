c8385488317931b2aa5d247ca5b6ee9376fb1658
Laurent Cozic <laurent@cozic.net>
2017-11-14 20:58:40 +0000

    Unversioning build files

--- ElectronClient/app/lib/layout-utils.js
+++
@@ -1,9 +0,0 @@
-const layoutUtils = {};
-
-layoutUtils.size = function(prefered, min, max) {
-	if (prefered < min) return min;
-	if (typeof max !== 'undefined' && prefered > max) return max;
-	return prefered;
-}
-
-module.exports = layoutUtils;
--- ElectronClient/app/lib/urlUtils.js
+++
@@ -1,9 +0,0 @@
-const urlUtils = {};
-
-urlUtils.hash = function(url) {
-	const s = url.split('#');
-	if (s.length <= 1) return '';
-	return s[s.length - 1];
-}
-
-module.exports = urlUtils;
