e382231d7dbe55136684bccb957ffac34b82ac9a
Laurent Cozic <laurent@cozic.net>
2017-11-16 17:55:01 +0000

    Update for iOS

--- ReactNativeClient/ios/Joplin/AppDelegate.m
+++ ReactNativeClient/ios/Joplin/AppDelegate.m
@@ -18,10 +18,10 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"AwesomeProject"
+                                                      moduleName:@"Joplin"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
--- ReactNativeClient/ios/JoplinTests/JoplinTests.m
+++ ReactNativeClient/ios/JoplinTests/JoplinTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface JoplinTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation JoplinTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
