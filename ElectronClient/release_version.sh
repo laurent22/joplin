94a800aa3ca9f759f6e03a7507e2017e18a7f0ef
Laurent Cozic <laurent@cozic.net>
2017-11-16 19:21:23 +0000

    Electron release v0.10.18

--- ElectronClient/release_version.sh
+++ ElectronClient/release_version.sh
@@ -5,6 +5,8 @@ APP_DIR="$ROOT_DIR/app"
 cd "$APP_DIR"
 VERSION="$(npm version patch)"
 git add -A
-git commit -m "Version $VERSION"
+git commit -m "Electron release $VERSION"
 git tag $VERSION
 git push && git push --tags
+
+echo "Open https://github.com/laurent22/joplin/tags and create a draft release for tag $VERSION"
