5340fb8af9f13130f79d3f8e0acdfc5e33de4adc
Laurent Cozic <laurent@cozic.net>
2017-11-22 19:29:49 +0000

    Electron: Disable Control+Click

--- ElectronClient/app/main-html.js
+++ ElectronClient/app/main-html.js
@@ -33,11 +33,16 @@ Setting.setConstant('appType', 'desktop');
 
 // Disable drag and drop of links inside application (which would
 // open it as if the whole app was a browser)
-// document.addEventListener('dragover', event => event.preventDefault());
-// document.addEventListener('drop', event => event.preventDefault());
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
 
 // Disable middle-click (which would open a new browser window, but we don't want this)
 document.addEventListener('auxclick', event => event.preventDefault());
+
+// Each link (rendered as a button or list item) has its own custom click event
+// so disable the default. In particular this will disable Ctrl+Clicking a link
+// which would open a new browser window.
+document.addEventListener('click', (event) => event.preventDefault());
 
 shimInit();
 
