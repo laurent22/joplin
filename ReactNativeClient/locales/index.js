8838017830f5db366a1f2b5d65b9ccac1330fdc3
Laurent Cozic <laurent@cozic.net>
2017-12-06 23:55:22 +0000

    All: Updated translations

--- ReactNativeClient/locales/index.js
+++ ReactNativeClient/locales/index.js
@@ -1,5 +1,6 @@
 var locales = {};
 locales['en_GB'] = require('./en_GB.json');
+locales['de_DE'] = require('./de_DE.json');
 locales['es_CR'] = require('./es_CR.json');
 locales['fr_FR'] = require('./fr_FR.json');
 module.exports = { locales: locales };
