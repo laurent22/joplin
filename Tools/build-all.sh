71bd39a8a3f8631fdd813b2a8ec74a42e5578ee1
Laurent Cozic <laurent@cozic.net>
2018-05-02 15:31:17 +0100

    Update website

---
+++ Tools/build-all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+node "$ROOT_DIR/update-readme-download.js" && node "$ROOT_DIR/build-release-stats.js" && node "$ROOT_DIR/build-website.js" && git add -A && git commit -m "Update website" && git pull && git push
