7c99ab994786ca1510d529de9a8c90b5de145058
Laurent Cozic <laurent@cozic.net>
2018-05-14 12:41:45 +0100

    Update Readme for portable app

--- Tools/update-readme-download.js
+++ Tools/update-readme-download.js
@@ -1,9 +1,12 @@
+require('app-module-path').addPath(__dirname + '/../ReactNativeClient');
+
 'use strict';
 
 const fs = require('fs-extra');
 const https = require('https');
 const request = require('request');
 
+const { fileExtension } = require('lib/path-utils.js');
 const url = 'https://api.github.com/repos/laurent22/joplin/releases/latest';
 const readmePath = __dirname + '/../README.md';
 
@@ -34,16 +37,23 @@ async function gitHubLatestRelease() {
 	});
 }
 
-function downloadUrl(release, os) {
+function downloadUrl(release, os, portable = false) {
 	if (!release || !release.assets || !release.assets.length) return null;
 
 	for (let i = 0; i < release.assets.length; i++) {
 		const asset = release.assets[i];
 		const name = asset.name;
+		const ext = fileExtension(name);
 
-		if (name.indexOf('.dmg') > 0 && os === 'macos') return asset.browser_download_url; 
-		if (name.indexOf('.exe') > 0 && name.indexOf('-Setup') >= 0 && os === 'windows') return asset.browser_download_url; 
-		if (name.indexOf('.AppImage') > 0 && os === 'linux') return asset.browser_download_url; 
+		if (ext === 'dmg' && os === 'macos') return asset.browser_download_url; 
+		if (ext === 'exe' && os === 'windows') {
+			if (portable) {
+				if (name === 'JoplinPortable.exe') return asset.browser_download_url;
+			} else {
+				if (name.match(/^Joplin-Setup-[\d.]+\.exe$/)) return asset.browser_download_url;
+			}
+		}
+		if (ext === 'AppImage' && os === 'linux') return asset.browser_download_url; 
 	}
 }
 
@@ -76,16 +86,19 @@ async function main(argv) {
 	}
 
 	const winUrl = downloadUrl(release, 'windows');
+	const winPortableUrl = downloadUrl(release, 'windows', true);
 	const macOsUrl = downloadUrl(release, 'macos');
 	const linuxUrl = downloadUrl(release, 'linux');
 
 	console.info('Windows: ', winUrl);
+	console.info('Windows Portable: ', winPortableUrl);
 	console.info('macOS: ', macOsUrl);
 	console.info('Linux: ', linuxUrl);
 
 	let content = readmeContent();
 
-	if (winUrl) content = content.replace(/(https:\/\/github.com\/laurent22\/joplin\/releases\/download\/.*?\.exe)/, winUrl);
+	if (winUrl) content = content.replace(/(https:\/\/github.com\/laurent22\/joplin\/releases\/download\/.*?\/Joplin-Setup-.*?\.exe)/, winUrl);
+	if (winPortableUrl) content = content.replace(/(https:\/\/github.com\/laurent22\/joplin\/releases\/download\/.*?\/JoplinPortable.exe)/, winPortableUrl);
 	if (macOsUrl) content = content.replace(/(https:\/\/github.com\/laurent22\/joplin\/releases\/download\/.*?\.dmg)/, macOsUrl);
 	if (linuxUrl) content = content.replace(/(https:\/\/github.com\/laurent22\/joplin\/releases\/download\/.*?\.AppImage)/, linuxUrl);
 
