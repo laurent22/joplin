b3ba5b77479a9024e436d3da2d9e8590842ffef7
Laurent Cozic <laurent@cozic.net>
2018-06-21 19:00:20 +0100

    All: Revert #554 to try to fix #624: WebDAV error when syncing with SeaFile

--- ReactNativeClient/lib/file-api-driver-webdav.js
+++ ReactNativeClient/lib/file-api-driver-webdav.js
@@ -43,8 +43,10 @@ class FileApiDriverWebDav {
 		const propStat = this.api().arrayFromJson(resource, ['d:propstat']);
 		if (!Array.isArray(propStat)) throw new Error('Invalid WebDAV resource format: ' + JSON.stringify(resource));
 
-		const httpStatusLine = this.api().stringFromJson(resource, ['d:propstat',0,'d:status', 0]);
-		if ( typeof httpStatusLine === 'string' && httpStatusLine.indexOf('404') >= 0 ) throw  new JoplinError(resource, 404);
+		// Disabled for now to try to fix this: https://github.com/laurent22/joplin/issues/624
+		//
+		// const httpStatusLine = this.api().stringFromJson(resource, ['d:propstat',0,'d:status', 0]);
+		// if ( typeof httpStatusLine === 'string' && httpStatusLine.indexOf('404') >= 0 ) throw  new JoplinError(resource, 404);
 
 		const resourceTypes = this.api().resourcePropByName(resource, 'array', 'd:resourcetype');
 		let isDir = false;
