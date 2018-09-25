bc09d2c640ec2321725cbda2621aafb90023160c
Laurent Cozic <laurent@cozic.net>
2018-09-25 21:19:54 +0100

    Clipper: Fixes #817: Added support for PICTURE tags, which will fix issues with certain pages from which images were not being imported

--- CliClient/tests/HtmlToMd.js
+++ CliClient/tests/HtmlToMd.js
@@ -36,7 +36,7 @@ describe('HtmlToMd', function() {
 			const htmlPath = basePath + '/' + htmlFilename;
 			const mdPath = basePath + '/' + filename(htmlFilename) + '.md';
 
-			// if (htmlFilename !== 'code_1.html') continue;
+			// if (htmlFilename !== 'anchor_with_url_with_spaces.html') continue;
 
 			const html = await shim.fsDriver().readFile(htmlPath);
 			const expectedMd = await shim.fsDriver().readFile(mdPath);
