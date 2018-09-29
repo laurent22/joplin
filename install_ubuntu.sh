b01a32211bc4c4ceef7b0d4c963245eb13ebfb07
Andros Fenollosa <tanrax@users.noreply.github.com>
2018-09-29 10:24:49 +0200

    [Fixbug] Two icons in gnome #813
    Theoretically it shouldn't happen again... it's best to wait for feedback.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -16,13 +16,13 @@ rm -f ~/.joplin/*.AppImage ~/.local/share/applications/joplin.desktop
 # Creates the folder where the binary will be stored
 mkdir -p ~/.joplin/
 # Download the latest version
-wget -O ~/.joplin/Joplin-$version-x86_64.AppImage https://github.com/laurent22/joplin/releases/download/v$version/Joplin-$version-x86_64.AppImage 
+wget -O ~/.joplin/Joplin.AppImage https://github.com/laurent22/joplin/releases/download/v$version/Joplin-$version-x86_64.AppImage 
 # Gives execution privileges
-chmod +x ~/.joplin/Joplin-$version-x86_64.AppImage
+chmod +x ~/.joplin/Joplin.AppImage
 # Download icon
 wget -O ~/.joplin/Icon512.png https://joplin.cozic.net/images/Icon512.png
 # Create icon for Gnome
-echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=Joplin\nExec=/home/$USER/.joplin/Joplin-$version-x86_64.AppImage\nIcon=/home/$USER/.joplin/Icon512.png\nType=Application\nCategories=Application;" >> ~/.local/share/applications/joplin.desktop
+echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=Joplin\nExec=/home/$USER/.joplin/Joplin.AppImage\nIcon=/home/$USER/.joplin/Icon512.png\nType=Application\nCategories=Application;" >> ~/.local/share/applications/joplin.desktop
 # Informs the user that it has been installed and cleans variables
 echo 'Joplin installed in the version' $version
 unset version
