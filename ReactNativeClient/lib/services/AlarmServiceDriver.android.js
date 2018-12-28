59402cf198b9d8d1f556a095c41f51ee2612fded
Laurent Cozic <laurent@cozic.net>
2018-12-28 20:38:40 +0100

    Android: Fixes #1062: Don t display Play Service alert when Play Service unavailable

--- ReactNativeClient/lib/services/AlarmServiceDriver.android.js
+++ ReactNativeClient/lib/services/AlarmServiceDriver.android.js
@@ -1,8 +1,14 @@
+// Note: currently, if Play Services aren't available, notifications will not work at all
+// There won't be any warning or error message.
+
 import firebase from 'react-native-firebase';
 
 class AlarmServiceDriver {
 
 	constructor() {
+		this.playServiceAvailable_ = firebase.utils().playServicesAvailability.isAvailable;
+		if (!this.playServiceAvailable_) return;
+
 		this.channel_ = new firebase.notifications.Android.Channel('net.cozic.joplin.notification', 'Joplin Alarm',firebase.notifications.Android.Importance.Max)
 			.setDescription('Displays a notification for alarms associated with to-dos.');
 		firebase.notifications().android.createChannel(this.channel_);
@@ -21,10 +27,14 @@ class AlarmServiceDriver {
 	}
 
 	async clearNotification(id) {
+		if (!this.playServiceAvailable_) return;
+
 		return firebase.notifications().cancelNotification(this.firebaseNotificationId_(id))
 	}
 	
 	async scheduleNotification(notification) {
+		if (!this.playServiceAvailable_) return;
+
 		const firebaseNotification = new firebase.notifications.Notification()
 		firebaseNotification.setNotificationId(this.firebaseNotificationId_(notification.id));
 		firebaseNotification.setTitle(notification.title)	
