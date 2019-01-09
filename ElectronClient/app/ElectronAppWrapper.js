88a9d5e8023e9425108998d838a293ccfcf5d365
Caleb John <calebjohn4@gmail.com>
2019-01-09 10:05:28 -0700

    Fix window manager icon on linux (#1110)

--- ElectronClient/app/ElectronAppWrapper.js
+++ ElectronClient/app/ElectronAppWrapper.js
@@ -58,7 +58,7 @@ class ElectronAppWrapper {
 
 		// Linux icon workaround for bug https://github.com/electron-userland/electron-builder/issues/2098
 		// Fix: https://github.com/electron-userland/electron-builder/issues/2269
-		if (shim.isLinux()) windowOptions.icon = __dirname + '/build/icons/128x128.png';
+		if (shim.isLinux()) windowOptions.icon = path.join(__dirname, '..', 'build/icons/128x128.png');
 
 		require('electron-context-menu')({
 			shouldShowMenu: (event, params) => {
