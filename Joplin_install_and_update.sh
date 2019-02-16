714c406e7d200d5c741e6937fd619829b3d4dc1e
Andros Fenollosa <tanrax@users.noreply.github.com>
2019-02-16 11:07:29 +0100

    Less verbose and visually more informative

--- Joplin_install_and_update.sh
+++ Joplin_install_and_update.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -e
 # Title
-echo "       _             _         _           _        _ _           "
-echo "      | |           (_)       (_)         | |      | | |          "
-echo "      | | ___  _ __  _ _ __    _ _ __  ___| |_ __ _| | | ___ _ __ "
-echo "  _   | |/ _ \\\| '_ \| | '_ \\  | | '_ \\\/ __| __/ _\` | | |/ _ \ '__|"
-echo " | |__| | (_) | |_) | | | | | | | | | \__ \ || (_| | | |  __/ |   "
-echo "  \____/ \___/| .__/|_|_| |_| |_|_| |_|___/\__\__,_|_|_|\___|_|   "
-echo "              | |                                                 "
-echo "              |_|                                                 "
+echo "     _             _ _       "
+echo "    | | ___  _ __ | (_)_ __  "
+echo " _  | |/ _ \| '_ \| | | '_ \ "
+echo "| |_| | (_) | |_) | | | | | |"
+echo " \___/ \___/| .__/|_|_|_| |_|"
+echo "            |_|"
 echo ""
+echo "Linux installer and Updater"
+
+#-----------------------------------------------------
+# Variables
+#-----------------------------------------------------
+COLOR_RED=`tput setaf 1`
+COLOR_GREEN=`tput setaf 2`
+COLOR_RESET=`tput sgr0`
 
 # Check and warn if running as root.
 if [[ $EUID = 0 ]] ; then
   if [[ $* != *--allow-root* ]] ; then
-    echo "It is not recommended (nor necessary) to run this script as root. To do so anyway, please use '--allow-root'"
+    echo "${COLOR_RED}It is not recommended (nor necessary) to run this script as root. To do so anyway, please use '--allow-root'${COLOR_RESET}"
     exit 1
   fi
 fi
@@ -29,6 +35,7 @@ version=$(curl --silent "https://api.github.com/repos/laurent22/joplin/releases/
 # Check if it's in the latest version
 if [[ $(< ~/.joplin/VERSION) != "$version" ]]; then
 
+    echo 'Download Joplin.'
     # Delete previous version
     rm -f ~/.joplin/*.AppImage ~/.local/share/applications/joplin.desktop ~/.joplin/VERSION
     
@@ -36,17 +43,21 @@ if [[ $(< ~/.joplin/VERSION) != "$version" ]]; then
     mkdir -p ~/.joplin/
     
     # Download the latest version
-    wget -O ~/.joplin/Joplin.AppImage https://github.com/laurent22/joplin/releases/download/v$version/Joplin-$version-x86_64.AppImage 
+    wget -nv -O ~/.joplin/Joplin.AppImage https://github.com/laurent22/joplin/releases/download/v$version/Joplin-$version-x86_64.AppImage 
     
     # Gives execution privileges
     chmod +x ~/.joplin/Joplin.AppImage
+    
+    echo "${COLOR_GREEN}OK${COLOR_RESET}"
     
     #-----------------------------------------------------
     # Icon
     #-----------------------------------------------------
     
     # Download icon
-    wget -O ~/.joplin/Icon512.png https://joplin.cozic.net/images/Icon512.png
+    echo 'Download icon.'
+    wget -nv -O ~/.joplin/Icon512.png https://joplin.cozic.net/images/Icon512.png
+    echo "${COLOR_GREEN}OK${COLOR_RESET}"
     
     # Detect desktop environment  
     if [ "$XDG_CURRENT_DESKTOP" = "" ]
@@ -58,20 +69,23 @@ if [[ $(< ~/.joplin/VERSION) != "$version" ]]; then
     desktop=${desktop,,}  # convert to lower case
 
     # Create icon for Gnome
+    echo 'Create Desktop icon.'
     if [[ $desktop =~ .*gnome.* ]] || [[ $desktop =~ .*kde.* ]] 
     then
        echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=Joplin\nExec=/home/$USER/.joplin/Joplin.AppImage\nIcon=/home/$USER/.joplin/Icon512.png\nType=Application\nCategories=Application;" >> ~/.local/share/applications/joplin.desktop
     fi
+    echo "${COLOR_GREEN}OK${COLOR_RESET}"
     
     #-----------------------------------------------------
     # Finish
     #-----------------------------------------------------
     
     # Informs the user that it has been installed and cleans variables
-    echo 'Joplin installed in the version' $version
+    echo "${COLOR_GREEN}Joplin installed in the version${COLOR_RESET}" $version
     # Add version
     echo $version > ~/.joplin/VERSION
 else
-    echo 'You are now in the latest version.'
+    echo "${COLOR_GREEN}You are now in the latest version.${COLOR_RESET}"
 fi
+echo 'Bye!'
 unset version
