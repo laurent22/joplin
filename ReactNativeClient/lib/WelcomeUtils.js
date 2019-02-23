b5dff09c28c4de823abf9d2e98d3207d2bb149da
Laurent Cozic <laurent@cozic.net>
2019-02-23 15:47:29 +0000

    All: Simplify Welcome notes and folders to avoid sync problems. Duplicate items will have to be manually deleted.

--- ReactNativeClient/lib/WelcomeUtils.js
+++ ReactNativeClient/lib/WelcomeUtils.js
@@ -12,88 +12,51 @@ const { pregQuote } = require('lib/string-utils');
 class WelcomeUtils {
 
 	static async createWelcomeItems() {
-		const overwriteExisting = Setting.value('env') === 'dev';
-
 		const output = {
 			defaultFolderId: null,
 		};
 
-		const noteAssets = welcomeAssets.notes;
+		const appType = Setting.value('appType');
+
 		const folderAssets = welcomeAssets.folders;
 		const tempDir = Setting.value('resourceDir');
+		const timestamp = welcomeAssets.timestamp;
 
 		for (let i = 0; i < folderAssets.length; i++) {
 			const folderAsset = folderAssets[i];
-			const folderId = folderAsset.id;
-
-			if (!output.defaultFolderId) output.defaultFolderId = folderId;
-
-			let existingFolder = await Folder.load(folderId);
-
-			if (existingFolder && overwriteExisting) {
-				await Folder.delete(existingFolder.id);
-				existingFolder = null;
-			}
-
-			if (existingFolder) continue;
-
-			await Folder.save({
-				id: folderId,
-				title: folderAsset.title,
-			}, { isNew: true });
+			const folder = await Folder.save({ title: folderAsset.title  + ' (' + appType + ')'});
+			if (!output.defaultFolderId) output.defaultFolderId = folder.id;
 		}
+
+		const noteAssets = welcomeAssets.notes;
 
 		for (let i =  noteAssets.length - 1; i >= 0; i--) {
 			const noteAsset = noteAssets[i];
-
-			const noteId = noteAsset.id;
-
-			let existingNote = await Note.load(noteId);
-
-			if (existingNote && overwriteExisting) {
-				await Note.delete(existingNote.id);
-				existingNote = null;
-			}
-
-			if (existingNote) continue;
 
 			let noteBody = noteAsset.body;
 
 			for (let resourceUrl in noteAsset.resources) {
 				if (!noteAsset.resources.hasOwnProperty(resourceUrl)) continue;
 				const resourceAsset = noteAsset.resources[resourceUrl];
-				const resourceId = resourceAsset.id;
-
-				let existingResource = await Resource.load(resourceId);
-
-				if (existingResource && overwriteExisting) {
-					await Resource.delete(resourceId);
-					existingResource = null;
-				}
-
-				if (!existingResource) {
-					const ext = fileExtension(resourceUrl);
-					const tempFilePath = tempDir + '/' + uuid.create() + '.tmp.' + ext;
-					await shim.fsDriver().writeFile(tempFilePath, resourceAsset.body, 'base64');
-					await shim.createResourceFromPath(tempFilePath, {
-						id: resourceId,
-						title: basename(resourceUrl),
-					});
-					await shim.fsDriver().remove(tempFilePath);
-				}
+				const ext = fileExtension(resourceUrl);
+				const tempFilePath = tempDir + '/' + uuid.create() + '.tmp.' + ext;
+				await shim.fsDriver().writeFile(tempFilePath, resourceAsset.body, 'base64');
+				const resource = await shim.createResourceFromPath(tempFilePath, {
+					title: basename(resourceUrl),
+				});
+				await shim.fsDriver().remove(tempFilePath);
 
 				const regex = new RegExp(pregQuote('(' + resourceUrl + ')'), 'g');
-				noteBody = noteBody.replace(regex, '(:/' + resourceId + ')');				
+				noteBody = noteBody.replace(regex, '(:/' + resource.id + ')');				
 			}
 
-			await Note.save({
-				id: noteId,
-				parent_id: noteAsset.parent_id,
+			const note = await Note.save({
+				parent_id: output.defaultFolderId,
 				title: noteAsset.title,
 				body: noteBody,
-			}, { isNew: true });
+			});
 
-			if (noteAsset.tags) await Tag.setNoteTagsByTitles(noteId, noteAsset.tags);
+			if (noteAsset.tags) await Tag.setNoteTagsByTitles(note.id, noteAsset.tags);
 		}
 
 		return output;
--- Tools/build-welcome.js
+++ Tools/build-welcome.js
@@ -8,10 +8,12 @@ const markdownUtils = require('lib/markdownUtils.js');
 const rootDir = dirname(__dirname);
 const welcomeDir = rootDir + '/readme/welcome';
 
+const createdDate = new Date('2018-06-22T12:00:00Z')
+
 const itemMetadata_ = {
 	'1_welcome_to_joplin.md': {
 		id: '8a1556e382704160808e9a7bef7135d3',
-		tags: 'markdown,organizing',
+		tags: 'markdown,organising',
 	},
 	'2_importing_and_exporting_notes.md': {
 		id: 'b863cbc514cb4cafbae8dd6a4fcad919',
@@ -19,7 +21,7 @@ const itemMetadata_ = {
 	},
 	'3_synchronising_your_notes.md': {
 		id: '25b656aac0564d1a91ab98295aa3cc58',
-		tags: 'synchronizing',
+		tags: 'synchronising',
 	},
 	'4_tips.md': {
 		id: '2ee48f80889447429a3cccb04a466072',
@@ -29,6 +31,14 @@ const itemMetadata_ = {
 	'SubNotebooks.png': { id: '3a851ab0c0e849b7bc9e8cd5c4feb34a' },
 	'folder_Welcome': { id: '9bb5d498aba74cc6a047cfdc841e82a1' },
 	'WebClipper.png': { id: '30cf9214f5054c4da3b23eed7211a6e0' },
+
+	'markdown': { id: '79cc5ef0f6c24f138033ce48928c2cba' },
+	'organising': { id: 'c83be0495b5d4f1ab655c5c6dfed6804' },
+	'importing': { id: 'b5adb734bb0044f2a572a729266b610d' },
+	'exporting': { id: 'bed34e2e3ab74b45af8ba473a05f56f9' },
+	'synchronising': { id: 'c442fa3b2b2b4389b160c15eb73f35c9' },
+	'attachment': { id: '22c94167b6e94a92b560ffc31a7f4b1d' },
+	'search': { id: '83eae47427df4805905103d4a91727b7' },
 };
 
 function itemMetadata(path) {
@@ -86,6 +96,7 @@ async function parseNoteFile(filePath) {
 
 async function main() {
 	const notes = [];
+	const tagIdsToTag = {};
 	const filenames = fs.readdirSync(welcomeDir);
 
 	const rootFolder = {
@@ -100,14 +111,30 @@ async function main() {
 		if (ext === 'md') {
 			const note = await parseNoteFile(welcomeDir + '/' + f);
 			note.parent_id = rootFolder.id;
+
+			for (let j = 0; j < note.tags.length; j++) {
+				const tagTitle = note.tags[j];
+				const tagId = itemIdFromPath(tagTitle);
+				if (!tagIdsToTag[tagId]) tagIdsToTag[tagId] = {
+					id: tagId,
+					title: tagTitle,
+				};
+			}
+
 			notes.push(note);
 		}
+	}
+
+	const tags = [];
+	for (let n in tagIdsToTag) {
+		if (!tagIdsToTag.hasOwnProperty(n)) continue;
+		tags.push(tagIdsToTag[n]);
 	}
 
 	const folders = [];
 	folders.push(rootFolder);
 
-	const content = { notes: notes, folders: folders }
+	const content = { notes: notes, folders: folders, tags: tags, timestamp: createdDate.getTime() }
 	const jsonContent = JSON.stringify(content, null, 4);
 	const jsContent = 'module.exports = ' + jsonContent;
 	fs.writeFileSync(rootDir + '/ReactNativeClient/lib/welcomeAssets.js', jsContent, { encoding: 'utf8' });
