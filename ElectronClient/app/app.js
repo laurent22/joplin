cf448b553394d2914d986563805cdf7fe1364916
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2019-03-16 18:57:38 -0400

    macOS: make the menu more like a macOS menu

--- ElectronClient/app/app.js
+++ ElectronClient/app/app.js
@@ -308,6 +308,7 @@ class Application extends BaseApplication {
 
 		const importItems = [];
 		const exportItems = [];
+		const preferencesItems = [];
 		const ioService = new InteropService();
 		const ioModules = ioService.modules();
 		for (let i = 0; i < ioModules.length; i++) {
@@ -385,6 +386,50 @@ class Application extends BaseApplication {
 			}
 		});
 
+		preferencesItems.push({
+			label: _('General Options'),
+			accelerator: 'CommandOrControl+,',
+			click: () => {
+				this.dispatch({
+					type: 'NAV_GO',
+					routeName: 'Config',
+				});
+			}
+		}, {
+			label: _('Encryption options'),
+			click: () => {
+				this.dispatch({
+					type: 'NAV_GO',
+					routeName: 'EncryptionConfig',
+				});
+			}
+		}, {
+			label: _('Web clipper options'),
+			click: () => {
+				this.dispatch({
+					type: 'NAV_GO',
+					routeName: 'ClipperConfig',
+				});
+			}
+		});
+
+		function _checkForUpdates(ctx) {
+			bridge().checkForUpdates(false, bridge().window(), ctx.checkForUpdateLoggerPath(), { includePreReleases: Setting.value('autoUpdate.includePreReleases') });
+		}
+
+		function _showAbout() {
+			const p = packageInfo;
+			let message = [
+				p.description,
+				'',
+				'Copyright © 2016-2019 Laurent Cozic',
+				_('%s %s (%s, %s)', p.name, p.version, Setting.value('env'), process.platform),
+			];
+			bridge().showInfoMessageBox(message.join('\n'), {
+				icon: bridge().electronApp().buildDir() + '/icons/32x32.png',
+			});
+		}
+
 		const template = [
 			{
 				label: _('&File'),
@@ -393,6 +438,24 @@ class Application extends BaseApplication {
 				 * opens the first menu on Alt press if no hotkey assigned.
 				 * Issue: https://github.com/laurent22/joplin/issues/934 */
 				submenu: [{
+					label: _('About Joplin'),
+					visible: shim.isMac() ? true : false,
+					click: () => _showAbout()
+				}, {
+					type: 'separator',
+					visible: shim.isMac() ? true : false
+				}, {
+					label: 'Preferences...',
+					visible: shim.isMac() ? true : false,
+					submenu: preferencesItems
+				}, {
+					label: _('Check for updates...'),
+					visible: shim.isMac() ? true : false,
+					click: () => _checkForUpdates(this)
+				}, {
+					type: 'separator',
+					visible: shim.isMac() ? true : false
+				}, {
 					label: _('New note'),
 					accelerator: 'CommandOrControl+N',
 					screens: ['Main'],
@@ -439,6 +502,15 @@ class Application extends BaseApplication {
 						this.dispatch({
 							type: 'WINDOW_COMMAND',
 							name: 'synchronize',
+						});
+					}
+				}, {
+					label: _('Synchronisation status'),
+					visible: shim.isMac() ? true : false,
+					click: () => {
+						this.dispatch({
+							type: 'NAV_GO',
+							routeName: 'Status',
 						});
 					}
 				}, {
@@ -628,6 +700,7 @@ class Application extends BaseApplication {
 				}],
 			}, {
 				label: _('&Tools'),
+				visible: shim.isMac() ? false : true,
 				submenu: [{
 					label: _('Synchronisation status'),
 					click: () => {
@@ -676,26 +749,16 @@ class Application extends BaseApplication {
 					click () { bridge().openExternal('https://joplin.cozic.net/donate') }
 				}, {
 					label: _('Check for updates...'),
-					click: () => {
-						bridge().checkForUpdates(false, bridge().window(), this.checkForUpdateLoggerPath(), { includePreReleases: Setting.value('autoUpdate.includePreReleases') });
-					}
+					visible: shim.isMac() ? false : true,
+					click: () => _checkForUpdates(this)
 				}, {
 					type: 'separator',
+					visible: shim.isMac() ? false : true,
 					screens: ['Main'],
 				}, {
 					label: _('About Joplin'),
-					click: () => {
-						const p = packageInfo;
-						let message = [
-							p.description,
-							'',
-							'Copyright © 2016-2019 Laurent Cozic',
-							_('%s %s (%s, %s)', p.name, p.version, Setting.value('env'), process.platform),
-						];
-						bridge().showInfoMessageBox(message.join('\n'), {
-							icon: bridge().electronApp().buildDir() + '/icons/32x32.png',
-						});
-					}
+					visible: shim.isMac() ? false : true,
+					click: () => _showAbout()
 				}]
 			},
 		];
