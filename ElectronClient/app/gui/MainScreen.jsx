29582623b0b5489522f692a0c03037c920ae368b
Laurent Cozic <laurent@cozic.net>
2019-04-29 07:28:16 +0100

    Revert "Desktop: Improved: Removed gaps between note list and vertical resizers (#1464)"
    This reverts commit d6e59c5238da98b86ba4d89f5e7dcb1f340830a1.

--- ElectronClient/app/gui/MainScreen.jsx
+++ ElectronClient/app/gui/MainScreen.jsx
@@ -334,8 +334,6 @@ class MainScreenComponent extends React.Component {
 			height: rowHeight,
 			display: 'inline-block',
 			verticalAlign: 'top',
-			marginLeft: -5,
-			marginRight: -5,
 		};
 
 		this.styles_.noteText = {
