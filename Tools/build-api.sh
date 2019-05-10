440be3d920246a66f7a68072a6f60806bebe2752
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2019-05-09 23:01:49 -0400

    remove unintentionally committed files again

--- Tools/build-api.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-"$ROOT_DIR/../CliClient/run.sh" apidoc > "$ROOT_DIR/../readme/api.md" 
--- Tools/build-web.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-node "$ROOT_DIR/build-website.js" 
-#&& git add -A && git commit -m "Update website" && git pull && git push
