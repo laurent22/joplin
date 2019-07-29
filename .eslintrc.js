f41524ee01306cf41cdedf4761256a816e74a760
Laurent Cozic <laurent@cozic.net>
2019-07-29 22:00:35 +0200

    Removed prettier due to https://github.com/prettier/prettier/pull/4765

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
 		'es6': true,
 		'node': true,
 	},
-	'extends': ['eslint:recommended', 'prettier'],
+	'extends': ['eslint:recommended'],
 	'globals': {
 		'Atomics': 'readonly',
 		'SharedArrayBuffer': 'readonly'
@@ -24,13 +24,19 @@ module.exports = {
 		"no-unused-vars": ["error", { "argsIgnorePattern": ".*" }],
 		"no-constant-condition": 0,
 		"no-prototype-builtins": 0,
-		"prettier/prettier": "error",
+		"space-in-parens": ["error", "never"],
+		"semi": ["error", "always"],
+		"eol-last": ["error", "always"],
+		"quotes": ["error", "single"],
+		"indent": ["error", "tab"],
+		"comma-dangle": ["error", "always-multiline"],
+		// "prettier/prettier": "error",
 		// Uncomment this to automatically remove unused requires:
 		// "autofix/no-unused-vars": "error",
 	},
 	"plugins": [
 		"react",
-		"prettier",
+		// "prettier",
 		"autofix",
 	],
 };
