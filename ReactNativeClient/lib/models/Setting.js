b2eadffde01e5d5865235ba9cbb7d25e88b126ef
Laurent Cozic <laurent@cozic.net>
2019-07-29 11:51:09 +0200

    Using British English by default for consistency

--- ReactNativeClient/lib/models/Setting.js
+++ ReactNativeClient/lib/models/Setting.js
@@ -103,8 +103,8 @@ class Setting extends BaseModel {
 				let output = {};
 				output[Setting.THEME_LIGHT] = _('Light');
 				output[Setting.THEME_DARK] = _('Dark');
-				output[Setting.THEME_SOLARIZED_LIGHT] = _('Solarized Light');
-				output[Setting.THEME_SOLARIZED_DARK] = _('Solarized Dark');
+				output[Setting.THEME_SOLARIZED_LIGHT] = _('Solarised Light');
+				output[Setting.THEME_SOLARIZED_DARK] = _('Solarised Dark');
 				return output;
 			}},
 			'uncompletedTodosOnTop': { value: true, type: Setting.TYPE_BOOL, section: 'note', public: true, appTypes: ['cli'], label: () => _('Uncompleted to-dos on top') },
