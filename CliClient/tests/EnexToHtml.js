8380b6e6bc0ed944b727f722544d5bfadc132b98
Devon Zuegel <devonzuegel@github.com>
2019-08-10 00:44:03 +0200

    Clean up test

--- CliClient/tests/EnexToHtml.js
+++ CliClient/tests/EnexToHtml.js
@@ -2,27 +2,27 @@
 
 require('app-module-path').addPath(__dirname);
 
-const os = require('os');
-const { time } = require('lib/time-utils.js');
 const { filename } = require('lib/path-utils.js');
-const { asyncTest, fileContentEqual, setupDatabase, setupDatabaseAndSynchronizer, db, synchronizer, fileApi, sleep, clearDatabase, switchClient, syncTargetId, objectsEqual, checkThrowAsync } = require('test-utils.js');
-const Folder = require('lib/models/Folder.js');
-const Note = require('lib/models/Note.js');
-const BaseModel = require('lib/BaseModel.js');
+const { asyncTest, setupDatabaseAndSynchronizer, switchClient } = require('test-utils.js');
 const { shim } = require('lib/shim');
 const { enexXmlToHtml } = require('lib/import-enex-html-gen.js');
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 60 * 1000; // Can run for a while since everything is in the same test unit
 
 process.on('unhandledRejection', (reason, p) => {
-	console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+	console.warn('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
 
+const fileWithPath = (filename) =>
+	`${__dirname}/enex_to_html/${filename}`;
+
 /**
- * Tests the importer for a single note.
+ * Tests the importer for a single note, checking that the result of
+ * processing the given `.enex` input file matches the contents of the given
+ * `.html` file.
  *
- * In other words, this does not test the importing of an entire exported
- * `.enex` archive, but rather a node of such a file. Thus, the test data files
+ * Note that this does not test the importing of an entire exported `.enex`
+ * archive, but rather a single node of such a file. Thus, the test data files
  * (e.g. `./enex_to_html/code1.enex`) correspond to the contents of a single
  * `<note>...</note>` node in an `.enex` file already extracted from
  * `<content><![CDATA[...]]</content>`.
@@ -52,15 +52,11 @@ const compareOutputToExpected = (options) => {
 };
 
 describe('EnexToHtml', function() {
-
 	beforeEach(async (done) => {
 		await setupDatabaseAndSynchronizer(1);
 		await switchClient(1);
 		done();
 	});
-
-	const fileWithPath = (filename) =>
-		`${__dirname}/enex_to_html/${filename}`;
 
 	compareOutputToExpected({
 		inputFile: fileWithPath('checklist-list.enex'),
@@ -79,5 +75,4 @@ describe('EnexToHtml', function() {
 			title: '',
 		}],
 	});
-
 });
