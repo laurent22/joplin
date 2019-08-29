9a4f4cbb652aa32f4be97fc42d2bb7c47e621665
Caleb John <calebjohn4@gmail.com>
2019-08-29 10:34:54 -0600

    Desktop: Change template prompt to sans and sort templates (#1806)
    * Change template prompt to sans and sort templates
    
    * Sort templates by filename to ensure order

--- ReactNativeClient/lib/TemplateUtils.js
+++ ReactNativeClient/lib/TemplateUtils.js
@@ -41,6 +41,10 @@ TemplateUtils.loadTemplates = async function(filePath) {
 			throw error;
 		}
 
+		// Make sure templates are always in the same order
+		// sensitivity ensures that the sort will ignore case
+		files.sort((a, b) => { return a.path.localeCompare(b.path, undefined, {sensitivity: 'accent'}); });
+
 		files.forEach(async file => {
 			if (file.path.endsWith('.md')) {
 				try {
