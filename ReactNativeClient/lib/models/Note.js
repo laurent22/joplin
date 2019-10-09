257247d481b7bcf9900ca1b41e8ab9408d182063
Alan M Silva <alanfortlink@gmail.com>
2019-10-09 15:57:30 +0100

    Adding duplicate button on screen-header.js when selecting notes; Adding 'duplicateMultipleNotes' function on Note.js;

--- ReactNativeClient/lib/models/Note.js
+++ ReactNativeClient/lib/models/Note.js
@@ -481,6 +481,25 @@ class Note extends BaseItem {
 		return note;
 	}
 
+	static async duplicateMultipleNotes(noteIds, options = null){
+		/*
+			if options.uniqueTitle is true, a unique title for the duplicated file will be assigned.
+		 */
+		const uniqueTitle = options && options.uniqueTitle;
+
+		noteIds.forEach(async function(noteId){
+			const noteOptions = {};
+
+			//If uniqueTitle is truthy, set the original note's name as root for the unique title.
+			if(uniqueTitle){
+				const originalNote = await Note.load(noteId);
+				noteOptions.uniqueTitle = originalNote.title;
+			}
+
+			await Note.duplicate(noteId, noteOptions);
+		});
+	}
+
 	static async duplicate(noteId, options = null) {
 		const changes = options && options.changes;
 		const uniqueTitle = options && options.uniqueTitle;
