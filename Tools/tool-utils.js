a5f17fad5826d14586dfeb86d51bb4fe83e00a0e
Laurent Cozic <laurent@cozic.net>
2019-10-13 23:19:15 +0200

    Android release v1.0.308

--- Tools/tool-utils.js
+++ Tools/tool-utils.js
@@ -4,7 +4,7 @@ toolUtils.execCommand = function(command) {
 	const exec = require('child_process').exec;
 
 	return new Promise((resolve, reject) => {
-		const child = exec(command, (error, stdout) => {
+		exec(command, (error, stdout) => {
 			if (error) {
 				if (error.signal == 'SIGTERM') {
 					resolve('Process was killed');
@@ -15,9 +15,6 @@ toolUtils.execCommand = function(command) {
 				resolve(stdout.trim());
 			}
 		});
-
-		child.stdout.pipe(process.stdout);
-		child.stderr.pipe(process.stderr);
 	});
 };
 
