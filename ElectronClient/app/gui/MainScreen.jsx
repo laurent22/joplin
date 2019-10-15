cebc473da01ad1588e4d3280dc3bdcf0d3ab2f5b
Tommy Bergeron <tom@brainpad.org>
2019-10-15 12:49:12 -0400

    hiding verticalResizer to fully hide note list and its extra border #1988

--- ElectronClient/app/gui/MainScreen.jsx
+++ ElectronClient/app/gui/MainScreen.jsx
@@ -391,6 +391,7 @@ class MainScreenComponent extends React.Component {
 		if (isNoteListVisible === false) {
 			this.styles_.noteList.width = 0;
 			this.styles_.noteList.display = 'none';
+			this.styles_.verticalResizer.display = 'none';
 		}
 
 		this.styles_.noteText = {
