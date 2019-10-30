c3b1018929803fa9495635ab5e123c705dce72a1
Daniel Landau <daniel@landau.fi>
2019-10-30 17:40:59 +0200

    Desktop: Resolves #215: Fill X primary selection from the editor on text selection (#2029)
    This fixes the Joplin->"other apps" half of
    https://github.com/laurent22/joplin/issues/215.

--- ElectronClient/app/gui/NoteText.jsx
+++ ElectronClient/app/gui/NoteText.jsx
@@ -239,6 +239,14 @@ class NoteTextComponent extends React.Component {
 				this.selectionRange_ = null;
 			} else {
 				this.selectionRange_ = ranges[0];
+				if (process.platform === 'linux') {
+					const textRange = this.textOffsetSelection();
+					if (textRange.start != textRange.end) {
+						clipboard.writeText(this.state.note.body.slice(
+							Math.min(textRange.start, textRange.end),
+							Math.max(textRange.end, textRange.start)), 'selection');
+					}
+				}
 			}
 		};
 
