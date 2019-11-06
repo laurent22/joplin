fd51aa015eb5631fc69186cb85ea1f916d460d6f
Michael Hart <michael.hart@eckoh.com>
2019-11-06 19:02:57 +0000

    Remove openDevTools on env = dev

--- ElectronClient/app/ElectronAppWrapper.js
+++ ElectronClient/app/ElectronAppWrapper.js
@@ -81,9 +81,6 @@ class ElectronAppWrapper {
 			slashes: true,
 		}));
 
-		// Uncomment this to view errors if the application does not start
-		if (this.env_ === 'dev') this.win_.webContents.openDevTools();
-
 		this.win_.on('close', (event) => {
 			// If it's on macOS, the app is completely closed only if the user chooses to close the app (willQuitApp_ will be true)
 			// otherwise the window is simply hidden, and will be re-open once the app is "activated" (which happens when the
