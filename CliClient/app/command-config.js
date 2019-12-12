78f811fa60efb3e1b5e141510c25c5ad4701b6df
Marcus <marcushill12@gmail.com>
2019-12-12 09:26:19 +0530

    Add --export, --import, and --import-file flags to joplin config

--- CliClient/app/command-config.js
+++ CliClient/app/command-config.js
@@ -1,6 +1,7 @@
 const { BaseCommand } = require('./base-command.js');
 const { _, setLocale } = require('lib/locale.js');
 const { app } = require('./app.js');
+const fs = require('fs-extra');
 const Setting = require('lib/models/Setting.js');
 
 class Command extends BaseCommand {
@@ -13,18 +14,48 @@ class Command extends BaseCommand {
 	}
 
 	options() {
-		return [['-v, --verbose', _('Also displays unset and hidden config variables.')]];
+		return [['-v, --verbose', _('Also displays unset and hidden config variables.')],
+			['--export', _('Writes the variables to STDOUT in plain text including secure variables.')],
+			['--import', _('Reads the variables from STDIN in plain text in the format [name]=[value] and sets them as if you had individually called config [name] [value].')],
+			['--import-file <file>', _('Reads the variables from <file> in plain text in the format [name]=[value] and sets them as if you had individually called config [name] [value].')]];
 	}
+	async __importSettings(fileStream) {
+		return new Promise((resolve) => {
+			const readline = require('readline');
 
+			const rl = readline.createInterface({
+				input: fileStream,
+			});
+
+			rl.on('line', (line) => {
+				line = line ? line.trim() : '';
+				if (!line) {
+					return;
+				}
+				const [name, value] = line.split('=');
+				Setting.setValue(name, value);
+			})
+				.on('close', () => {
+					resolve();
+				});
+		});
+	}
 	async action(args) {
 		const verbose = args.options.verbose;
+		const isExport = args.options.export;
+		const isImport = args.options.import || args.options.importFile;
+		const importFile = args.options.importFile;
 
 		const renderKeyValue = name => {
 			const md = Setting.settingMetadata(name);
 			let value = Setting.value(name);
 			if (typeof value === 'object' || Array.isArray(value)) value = JSON.stringify(value);
-			if (md.secure && value) value = '********';
+			if (md.secure && value && !isExport) value = '********';
 
+			if (isExport) {
+				// No spaces as this will cause problems
+				return _('%s=%s', name, value);
+			}
 			if (Setting.isEnum(name)) {
 				return _('%s = %s (%s)', name, value, Setting.enumOptionsDoc(name));
 			} else {
@@ -32,7 +63,7 @@ class Command extends BaseCommand {
 			}
 		};
 
-		if (!args.name && !args.value) {
+		if (!isImport && !args.name && !args.value) {
 			let keys = Setting.keys(!verbose, 'cli');
 			keys.sort();
 			for (let i = 0; i < keys.length; i++) {
@@ -49,7 +80,7 @@ class Command extends BaseCommand {
 			return;
 		}
 
-		if (args.name && !args.value) {
+		if (!isImport && args.name && !args.value) {
 			this.stdout(renderKeyValue(args.name));
 			app()
 				.gui()
@@ -60,7 +91,16 @@ class Command extends BaseCommand {
 			return;
 		}
 
-		Setting.setValue(args.name, args.value);
+		if (isImport) {
+			let fileStream = process.stdin;
+			if (importFile) {
+				fileStream = fs.createReadStream(importFile, {autoClose: true});
+			}
+			await this.__importSettings(fileStream);
+		} else {
+			Setting.setValue(args.name, args.value);
+		}
+
 
 		if (args.name == 'locale') {
 			setLocale(Setting.value('locale'));
