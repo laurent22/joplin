b5bc5d6e6d6b5ae73f44f974c90eba421a479aaa
devonzuegel <devonzuegel@gmail.com>
2019-12-22 23:01:52 -0300

    wip: Works as long as I also load the image in a separate img too (so probably the issue is with the loader, not the renderer)

--- ReactNativeClient/lib/renderers/MdToHtml/rules/image.js
+++ ReactNativeClient/lib/renderers/MdToHtml/rules/image.js
@@ -5,17 +5,33 @@ const htmlUtils = require('lib/htmlUtils.js');
 function installRule(markdownIt, mdOptions, ruleOptions) {
 	const defaultRender = markdownIt.renderer.rules.image;
 
+	console.log('hello!');
+
 	markdownIt.renderer.rules.image = (tokens, idx, options, env, self) => {
 		const token = tokens[idx];
 		const src = utils.getAttr(token.attrs, 'src');
 		const title = utils.getAttr(token.attrs, 'title');
 
-		if (!Resource.isResourceUrl(src) || ruleOptions.plainResourceRendering) return defaultRender(tokens, idx, options, env, self);
+		// const imageRegex = /<img(.*?)src=["'](.*?)["'](.*?)>/gi;
+
+		console.log({src, split: src.split('%7C')});
+		// if (src.match(imageRegex)) {
+		// 	console.log('match!')
+		// }
+
+		if (!Resource.isResourceUrl(src) || ruleOptions.plainResourceRendering) {
+			return defaultRender(tokens, idx, options, env, self);
+		}
 
 		const r = utils.imageReplacement(src, ruleOptions.resources, ruleOptions.resourceBaseUrl);
 		if (typeof r === 'string') return r;
 		if (r) return `<img data-from-md ${htmlUtils.attributesHtml(Object.assign({}, r, { title: title }))}/>`;
 
+		return defaultRender(tokens, idx, options, env, self);
+	};
+	markdownIt.renderer.rules.foooooo = (tokens, idx, options, env, self) => {
+
+		console.log('in fooooo');
 		return defaultRender(tokens, idx, options, env, self);
 	};
 }
