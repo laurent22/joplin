e15abc809d298ef2e252b959022d8e8ea4449868
devonzuegel <devonzuegel@gmail.com>
2019-12-30 21:29:38 -0300

    Extract QuickActions

--- ReactNativeClient/root.js
+++ ReactNativeClient/root.js
@@ -6,7 +6,6 @@ const {
 	BackHandler,
 	Platform,
 	Animated,
-	DeviceEventEmitter,
 } = require('react-native');
 const { SafeAreaView } = require('react-navigation');
 const { connect, Provider } = require('react-redux');
@@ -89,9 +88,7 @@ const EncryptionService = require('lib/services/EncryptionService');
 const MigrationService = require('lib/services/MigrationService');
 
 import PluginAssetsLoader from './PluginAssetsLoader';
-
-import QuickActions from 'react-native-quick-actions';
-const { _ } = require('lib/locale.js');
+import setUpQuickActions from './lib/QuickActions';
 
 let storeDispatch = function() {};
 
@@ -545,32 +542,7 @@ async function initialize(dispatch) {
 			});
 		}
 
-		QuickActions.setShortcutItems([
-			{type: 'New note', title: _('New note'), icon: 'Compose'},
-			{type: 'New to-do', title: _('New to-do'), icon: 'Add'},
-			// TODO: Add Search
-			// {type: 'Search', icon: 'Search'},
-		]);
-
-		DeviceEventEmitter.addListener('quickActionShortcut', data => {
-			if (data.type === 'New note') {
-				dispatch({
-					type: 'NAV_GO',
-					noteId: null,
-					routeName: 'Note',
-					itemType: 'note',
-				});
-			}
-
-			if (data.type === 'New to-do') {
-				dispatch({
-					type: 'NAV_GO',
-					noteId: null,
-					routeName: 'Note',
-					itemType: 'todo',
-				});
-			}
-		});
+		setUpQuickActions(dispatch);
 	} catch (error) {
 		alert(`Initialization error: ${error.message}`);
 		reg.logger().error('Initialization error:', error);
