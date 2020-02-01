307918dd0b9a7049c59f74704718b76e1c052188
bedwardly-down <entertainment@brandongrows.me>
2020-02-01 06:16:13 -0600

    Save Icon / Notebook to Database (Broken)

--- ElectronClient/app/gui/NotebookPropertiesDialog.tsx
+++ ElectronClient/app/gui/NotebookPropertiesDialog.tsx
@@ -17,35 +17,56 @@ export default function NotebookPropertiesDialog(props: NotebookPropertiesDialog
 	console.info('Render NotebookPropertiesDialog');
 
 	const [notebook, setNotebook] = useState<any>({});
-	const [icon, setIcon] = useState<string>('');
+	const [icon, setIcon] = useState<string>('fa fa-book');
+	const [loaded, setLoaded] = useState<boolean>(false);
 
 	const theme = themeStyle(props.theme);
 
 	useEffect(() => {
 		async function fetchNotebook() {
-			const n = await Folder.load(props.folderId);
-			setNotebook(n);
+			if (!props.folderId) {
+				setNotebook({notebook: null});
+			} else {
+				const n = await Folder.load(props.folderId);
+				const nb = formNotebook(n);
+				setNotebook(nb);
+			}
 		}
 
 		fetchNotebook();
 	}, [props.folderId]);
+
+	const formNotebook = (targetNotebook:any) => {
+		if (!loaded) {
+			const nb = Object.assign({}, targetNotebook, {
+				icon: targetNotebook.icon,
+			});
+			setIcon(nb.icon);
+			setLoaded(true);
+			return nb;
+		}
+	};
 
 	const buttonRow_click = () => {
 		props.onClose();
 	};
 
 	// having issues wrapping my head around saving folder information to the database
-	const saveIcon = async () => {
-		console.info('Saving Icon Now');
-		notebook.icon = icon;
-		await Folder.save(notebook, { fields: ['icon'], userSideValidation: true });
-		setNotebook(notebook);
+	const saveNotebook = async () => {
+		console.info('Saving Notebook Now');
+		if (!notebook) {
+			console.error('Notebook saving failed in the NotebookPropertiesDialog');
+		} else {
+			const nb = await Folder.save(notebook, {userSideValidation: true});
+			setNotebook(nb);
+		}
+		props.onClose();
 	};
 
 	const rootStyle = Object.assign({}, theme.dialogBox);
 	rootStyle.width = '50%';
 
-	console.info('Icon: %s', notebook.icon);
+	console.info('Icon: %s', icon);
 
 	return (
 		<div style={theme.dialogModalLayer}>
@@ -53,8 +74,8 @@ export default function NotebookPropertiesDialog(props: NotebookPropertiesDialog
 				<div style={theme.dialogTitle}>{_('Notebook Properties')}</div>
 				<label style={theme.textStyle}>{_('Icon: ')}</label>
 				<input defaultValue={notebook.icon} onChange={event => setIcon(event.target.value)} />
-				<button onClick={saveIcon}>{_('Save')}</button>
-				<DialogButtonRow theme={props.theme} onClick={buttonRow_click} okButtonShow={true} cancelButtonLabel={_('Cancel')}/>
+				<button onClick={saveNotebook}>{_('Save')}</button>
+				<DialogButtonRow theme={props.theme} onClick={buttonRow_click} okButtonShow={false} cancelButtonLabel={_('Cancel')}/>
 			</div>
 		</div>
 	);
