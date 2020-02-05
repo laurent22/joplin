a50b0e57b028dc183cb970318272782369f7aede
bedwardly-down <entertainment@brandongrows.me>
2020-02-05 10:02:22 -0600

    Code Fixes + More Style Work
    Styling and Formatting Work
    
    If FolderID null, Throw Error

--- ElectronClient/app/gui/NotebookPropertiesDialog.tsx
+++ ElectronClient/app/gui/NotebookPropertiesDialog.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 
 const { _ } = require('lib/locale.js');
-const { themeStyle } = require('../theme.js');
+const { themeStyle, buildStyle } = require('../theme.js');
 const DialogButtonRow = require('./DialogButtonRow.min');
 
 const Folder = require('lib/models/Folder.js');
@@ -13,17 +13,58 @@ interface FolderPropertiesDialogProps {
 	onClose: Function,
 }
 
+function styles_(props:FolderPropertiesDialogProps) {
+	return buildStyle('FolderPropertiesDialog', props.theme, (theme: any) => {
+		return {
+			folderLabel: {
+				...theme.textStyle,
+				flex: 1,
+				display: 'flex',
+				fontWeight: 'bold',
+				color: theme.color,
+			},
+			folderSaveButton: {
+				background: 'none',
+				border: 'none',
+			},
+			folderSaveButtonIcon: {
+				color: theme.color,
+				fontSize: '1.4em',
+			},
+			titleForm: {
+				display: 'inline-block',
+				color: theme.color,
+				backgroundColor: theme.backgroundColor,
+				border: '1px solid',
+				borderColor: theme.dividerColor,
+				marginLeft: '3em',
+				width: '6em',
+			},
+			iconForm: {
+				display: 'inline-block',
+				color: theme.color,
+				backgroundColor: theme.backgroundColor,
+				border: '1px solid',
+				borderColor: theme.dividerColor,
+				marginLeft: '3.6em',
+				width: '6em',
+			},
+		};
+	});
+}
+
 export default function FolderPropertiesDialog(props: FolderPropertiesDialogProps) {
 	console.info('Render NotebookPropertiesDialog');
 
 	const [formFolder, setFormFolder] = useState<any>({});
 
 	const theme = themeStyle(props.theme);
+	const styles = styles_(props);
 
 	useEffect(() => {
 		async function fetchFolder() {
 			if (!props.folderId) {
-				setFormFolder({folder: null});
+				throw new Error('FolderID is not set. This Notebook is not valid!!!');
 			} else {
 				setFormFolder(await Folder.load(props.folderId));
 			}
@@ -44,17 +85,50 @@ export default function FolderPropertiesDialog(props: FolderPropertiesDialogProp
 	const rootStyle = Object.assign({}, theme.dialogBox);
 	rootStyle.width = '50%';
 
+	let titleComp = (
+		<label
+			style={styles.folderLabel}>
+			{_('Name: ')}
+			<input
+				defaultValue={formFolder.title}
+				onChange={event => formFolder.title = event.target.value}
+				style={styles.titleForm} />
+			<button
+				onClick={saveFolder}
+				style={styles.folderSaveButton}>
+				<i
+					style={styles.folderSaveButtonIcon}
+					className={'fa fa-save'}>
+				</i>
+			</button>
+		</label>
+	);
+
+	let iconComp = (
+		<label
+			style={styles.folderLabel}>
+			{_('Icon: ')}
+			<input
+				defaultValue={formFolder.icon}
+				onChange={event => formFolder.icon = event.target.value}
+				style={styles.iconForm} />
+			<button
+				onClick={saveFolder}
+				style={styles.folderSaveButton}>
+				<i
+					style={styles.folderSaveButtonIcon}
+					className={'fa fa-save'}>
+				</i>
+			</button>
+		</label>
+	);
+
 	return (
 		<div style={theme.dialogModalLayer}>
 			<div style={rootStyle}>
 				<div style={theme.dialogTitle}>{_('Notebook Properties')}</div>
-				<label style={theme.textStyle}>{_('Name: ')}</label>
-				<input defaultValue={formFolder.title} onChange={event => formFolder.title = event.target.value} />
-				<button onClick={saveFolder}>{_('Save')}</button>
-				<br />
-				<label style={theme.textStyle}>{_('Icon: ')}</label>
-				<input defaultValue={formFolder.icon} onChange={event => formFolder.icon = event.target.value} />
-				<button onClick={saveFolder}>{_('Save')}</button>
+				{titleComp}
+				{iconComp}
 				<DialogButtonRow theme={props.theme} onClick={buttonRow_click} okButtonShow={false} cancelButtonLabel={_('Close')}/>
 			</div>
 		</div>
