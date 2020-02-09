d8ef15a1e2e579c67d16f6d75803dce0e489916e
Laurent Cozic <laurent@cozic.net>
2020-02-09 20:21:52 +0000

    Mobile: Upgrading to React Native to 0.61.5

--- ReactNativeClient/android/app/src/main/java/net/cozic/joplin/MainApplication.java
+++ ReactNativeClient/android/app/src/main/java/net/cozic/joplin/MainApplication.java
@@ -1,10 +1,8 @@
 package net.cozic.joplin;
 
 import android.app.Application;
-import android.util.Log;
+import android.content.Context;
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -15,29 +13,33 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.Field;
 // import com.alinz.parkerdan.shareextension.SharePackage;
 import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import android.database.CursorWindow;
 
 public class MainApplication extends Application implements ReactApplication {
 
-	private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-		@Override
-		public boolean getUseDeveloperSupport() {
-			return BuildConfig.DEBUG;
-		}
+	private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
 
-		@Override
-		protected List<ReactPackage> getPackages() {
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          return packages;
+        }
 
-			// Was added in RN 60.x:
-			//
-			@SuppressWarnings("UnnecessaryLocalVariable")
-			List<ReactPackage> packages = new PackageList(this).getPackages();
-			// Packages that cannot be autolinked yet can be added manually here, for example:
-			// packages.add(new MyReactNativePackage());
-			return packages;
-		}
-	};
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
 
 	@Override
 	public ReactNativeHost getReactNativeHost() {
@@ -60,5 +62,31 @@ public class MainApplication extends Application implements ReactApplication {
 		}
 
 		SoLoader.init(this, /* native exopackage */ false);
-	}
+		initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+  }
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
