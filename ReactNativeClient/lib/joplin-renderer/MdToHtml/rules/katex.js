018222a1f4588f26b316e0ee9c10d026393a1634
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2020-02-11 17:53:12 -0500

    Desktop, Mobile: Fixes #2477: Add new Katex fonts to improve rendering (#2478)
    * add katex font KaTeX_Size4-Regular.woff2
    
    fixes #2477
    
    * deleted:    ElectronClient/app/gui/note-viewer/pluginAssets/katex/fonts/KaTeX_Size4-Regular.woff2
    
    * add font to buildAssets.js
    
    * register font in katex.js
    
    * prepare files in case we need other fonts in the future
    
    * use all fonts

--- ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/katex.js
+++ ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/katex.js
@@ -192,10 +192,25 @@ module.exports = function(context) {
 		context.pluginAssets['katex'] = [
 			{ name: 'katex.css' },
 			{ name: 'fonts/KaTeX_Main-Regular.woff2' },
+			{ name: 'fonts/KaTeX_Main-Bold.woff2' },
+			{ name: 'fonts/KaTeX_Main-BoldItalic.woff2' },
+			{ name: 'fonts/KaTeX_Main-Italic.woff2' },
 			{ name: 'fonts/KaTeX_Math-Italic.woff2' },
+			{ name: 'fonts/KaTeX_Math-BoldItalic.woff2' },
 			{ name: 'fonts/KaTeX_Size1-Regular.woff2' },
 			{ name: 'fonts/KaTeX_Size2-Regular.woff2' },
+			{ name: 'fonts/KaTeX_Size3-Regular.woff2' },
+			{ name: 'fonts/KaTeX_Size4-Regular.woff2' },
 			{ name: 'fonts/KaTeX_AMS-Regular.woff2' },
+			{ name: 'fonts/KaTeX_Caligraphic-Bold.woff2' },
+			{ name: 'fonts/KaTeX_Caligraphic-Regular.woff2' },
+			{ name: 'fonts/KaTeX_Fraktur-Bold.woff2' },
+			{ name: 'fonts/KaTeX_Fraktur-Regular.woff2' },
+			{ name: 'fonts/KaTeX_SansSerif-Bold.woff2' },
+			{ name: 'fonts/KaTeX_SansSerif-Italic.woff2' },
+			{ name: 'fonts/KaTeX_SansSerif-Regular.woff2' },
+			{ name: 'fonts/KaTeX_Script-Regular.woff2' },
+			{ name: 'fonts/KaTeX_Typewriter-Regular.woff2' },
 		];
 	};
 
--- ReactNativeClient/lib/joplin-renderer/Tools/buildAssets.js
+++ ReactNativeClient/lib/joplin-renderer/Tools/buildAssets.js
@@ -16,10 +16,25 @@ async function main() {
 
 	await copyFile(`${rootDir}/node_modules/katex/dist/katex.min.css`, 'katex/katex.css');
 	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Main-Regular.woff2`, 'katex/fonts/KaTeX_Main-Regular.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Main-Bold.woff2`, 'katex/fonts/KaTeX_Main-Bold.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Main-BoldItalic.woff2`, 'katex/fonts/KaTeX_Main-BoldItalic.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Main-Italic.woff2`, 'katex/fonts/KaTeX_Main-Italic.woff2');
 	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Math-Italic.woff2`, 'katex/fonts/KaTeX_Math-Italic.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Math-BoldItalic.woff2`, 'katex/fonts/KaTeX_Math-BoldItalic.woff2');
 	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Size1-Regular.woff2`, 'katex/fonts/KaTeX_Size1-Regular.woff2');
 	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Size2-Regular.woff2`, 'katex/fonts/KaTeX_Size2-Regular.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Size3-Regular.woff2`, 'katex/fonts/KaTeX_Size3-Regular.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Size4-Regular.woff2`, 'katex/fonts/KaTeX_Size4-Regular.woff2');
 	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_AMS-Regular.woff2`, 'katex/fonts/KaTeX_AMS-Regular.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Caligraphic-Bold.woff2`, 'katex/fonts/KaTeX_Caligraphic-Bold.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Caligraphic-Regular.woff2`, 'katex/fonts/KaTeX_Caligraphic-Regular.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Fraktur-Bold.woff2`, 'katex/fonts/KaTeX_Fraktur-Bold.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Fraktur-Regular.woff2`, 'katex/fonts/KaTeX_Fraktur-Regular.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_SansSerif-Bold.woff2`, 'katex/fonts/KaTeX_SansSerif-Bold.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_SansSerif-Italic.woff2`, 'katex/fonts/KaTeX_SansSerif-Italic.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_SansSerif-Regular.woff2`, 'katex/fonts/KaTeX_SansSerif-Regular.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Script-Regular.woff2`, 'katex/fonts/KaTeX_Script-Regular.woff2');
+	await copyFile(`${rootDir}/node_modules/katex/dist/fonts/KaTeX_Typewriter-Regular.woff2`, 'katex/fonts/KaTeX_Typewriter-Regular.woff2');
 
 	await copyFile(`${rootDir}/node_modules/highlight.js/styles/atom-one-light.css`, 'highlight.js/atom-one-light.css');
 	await copyFile(`${rootDir}/node_modules/highlight.js/styles/atom-one-dark-reasonable.css`, 'highlight.js/atom-one-dark-reasonable.css');
