fe5ee47330873a3d7e0ae0c9c9540c4f6691fddd
Laurent Cozic <laurent@cozic.net>
2020-02-20 20:17:09 +0000

    Fixed pod build

--- ReactNativeClient/tools/podInstall.js
+++ ReactNativeClient/tools/podInstall.js
@@ -2,7 +2,14 @@ const execa = require('execa');
 
 module.exports = async function() {
 	if (process.platform !== 'darwin') return Promise.resolve();
-	const promise = execa('pod', ['install'], { cwd: `${__dirname}/../ios` });
-	promise.stdout.pipe(process.stdout);
-	return promise;
+
+	try {
+		const promise = execa('pod', ['install'], { cwd: `${__dirname}/../ios` });
+		promise.stdout.pipe(process.stdout);
+		await promise;
+	} catch (error) {
+		console.warn('Could not run pod install', error);
+	}
+
+	return Promise.resolve();
 };
