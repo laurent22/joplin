1f803662d399abf3dc255c8332a7b9aa57deb913
mic704b <mic704b@gmail.com>
2020-02-21 22:48:53 +1100

    Tools: Improve test building (#2546)

--- CliClient/run_test.sh
+++ CliClient/run_test.sh
@@ -4,9 +4,9 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD_DIR="$ROOT_DIR/tests-build"
 FILTER="$1"
 
-rsync -a --exclude "node_modules/" "$ROOT_DIR/tests/" "$BUILD_DIR/"
-rsync -a "$ROOT_DIR/../ReactNativeClient/lib/" "$BUILD_DIR/lib/"
-rsync -a "$ROOT_DIR/../ReactNativeClient/locales/" "$BUILD_DIR/locales/"
+rsync -a --delete --exclude "lib/" --exclude "locales/" --exclude "node_modules/" "$ROOT_DIR/tests/" "$BUILD_DIR/"
+rsync -a --delete "$ROOT_DIR/../ReactNativeClient/lib/" "$BUILD_DIR/lib/"
+rsync -a --delete "$ROOT_DIR/../ReactNativeClient/locales/" "$BUILD_DIR/locales/"
 mkdir -p "$BUILD_DIR/data"
 
 function finish {
