5988c8347299cba8f32182956b6a8cbfecb1bf96
Laurent Cozic <laurent@cozic.net>
2020-02-22 12:37:22 +0000

    Added support for Joplin source blocks

--- ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/katex.js
+++ ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/katex.js
@@ -241,7 +241,7 @@ module.exports = function(context) {
 		var katexInline = function(latex) {
 			options.displayMode = false;
 			try {
-				return `<span class="joplin-katex-inline joplin-editable"><pre class="joplin-source" data-joplin-source-open="$" data-joplin-source-close="$">${latex}</pre>${renderToStringWithCache(latex, options)}</span>`;
+				return `<span class="joplin-editable"><pre class="joplin-source" data-joplin-source-open="$" data-joplin-source-close="$">${latex}</pre>${renderToStringWithCache(latex, options)}</span>`;
 			} catch (error) {
 				console.error('Katex error for:', latex, error);
 				return latex;
@@ -256,7 +256,7 @@ module.exports = function(context) {
 		var katexBlock = function(latex) {
 			options.displayMode = true;
 			try {
-				return `<div class="joplin-katex-block joplin-editable"><pre class="joplin-source" data-joplin-source-open="$$&#10;" data-joplin-source-close="&#10;$$">${latex}</pre>${renderToStringWithCache(latex, options)}</div>`;
+				return `<div class="joplin-editable"><pre class="joplin-source" data-joplin-source-open="$$&#10;" data-joplin-source-close="&#10;$$">${latex}</pre>${renderToStringWithCache(latex, options)}</div>`;
 			} catch (error) {
 				console.error('Katex error for:', latex, error);
 				return latex;
