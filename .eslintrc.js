82f0c2dec1344787a28051933a42730e922c3556
apshada <49001649+apshada@users.noreply.github.com>
2020-02-25 23:35:37 +0530

    updated eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,8 @@ module.exports = {
 	    "sourceType": "module",
 	},
 	'rules': {
+		"react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+   		"react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
 		// -------------------------------
 		// Code correctness
 		// -------------------------------
