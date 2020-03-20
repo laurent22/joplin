0d10505c994267f4a72060a6fbf64dd193bb9b28
Naveen M V <naveenmv7@gmail.com>
2020-03-20 20:32:02 +0530

    Add test for search in feature_NoteHistory

--- CliClient/tests/feature_NoteHistory.js
+++ CliClient/tests/feature_NoteHistory.js
@@ -1,7 +1,22 @@
 require('app-module-path').addPath(__dirname);
 const { asyncTest, id, ids, createNTestFolders, createNTestNotes, TestApp } = require('test-utils.js');
+const BaseModel = require('lib/BaseModel.js');
+const { uuid } = require('lib/uuid.js');
 
-const { time } = require('lib/time-utils.js');
+
+
+//
+// The integration tests are to test the integration of the core system, comprising the
+// base application with middleware, reducer and models in response to dispatched events.
+//
+// The general strategy for each integration test is:
+//  - create a starting application state,
+//  - inject the event to be tested
+//  - check the resulting application state
+//
+// Important: TestApp.wait() must be used after TestApp dispatch to allow the async
+// processing to complete
+//
 
 
 let testApp = null;
@@ -33,54 +48,54 @@ describe('integration_ForwardBackwardNoteHistory', function() {
 	it('should save history when navigating through notes', asyncTest(async () => {
 		// setup
 		const folders = await createNTestFolders(2);
-		await time.msleep(100);
+		await testApp.wait();
 		const notes0 = await createNTestNotes(5, folders[0]);
 		// let notes1 = await createNTestNotes(5, folders[1]);
-		await time.msleep(100);
+		await testApp.wait();
 
 		testApp.dispatch({ type: 'FOLDER_SELECT', id: id(folders[0]) });
-		await time.msleep(100);
+		await testApp.wait();
 
 		let state = testApp.store().getState();
 		expect(state.backwardHistoryNotes).toEqual([]);
 		expect(state.forwardHistoryNotes).toEqual([]);
 
 		testApp.dispatch({ type: 'NOTE_SELECT', id: notes0[3].id, historyAction: 'goto' });
-		await time.msleep(100);
+		await testApp.wait();
 		testApp.dispatch({ type: 'NOTE_SELECT', id: notes0[2].id, historyAction: 'goto' });
-		await time.msleep(100);
+		await testApp.wait();
 		testApp.dispatch({ type: 'NOTE_SELECT', id: notes0[1].id, historyAction: 'goto' });
-		await time.msleep(100);
+		await testApp.wait();
 		testApp.dispatch({ type: 'NOTE_SELECT', id: notes0[0].id, historyAction: 'goto' });
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4], notes0[3], notes0[2], notes0[1]]));
 		expect(ids(state.forwardHistoryNotes)).toEqual([]);
 
 		goBackWard(state);
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4], notes0[3], notes0[2]]));
 		expect(ids(state.forwardHistoryNotes)).toEqual(ids([notes0[0]]));
 
 		goBackWard(state);
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4], notes0[3]]));
 		expect(ids(state.forwardHistoryNotes)).toEqual(ids([notes0[0], notes0[1]]));
 
 		goForward(state);
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4], notes0[3], notes0[2]]));
 		expect(ids(state.forwardHistoryNotes)).toEqual(ids([notes0[0]]));
 
 		testApp.dispatch({ type: 'NOTE_SELECT', id: notes0[4].id, historyAction: 'goto' });
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4], notes0[3], notes0[2], notes0[1]]));
@@ -92,47 +107,99 @@ describe('integration_ForwardBackwardNoteHistory', function() {
 
 	it('should save history when navigating through notebooks', asyncTest(async () => {
 		const folders = await createNTestFolders(2);
-		await time.msleep(100);
+		await testApp.wait();
 		const notes0 = await createNTestNotes(5, folders[0]);
 		const notes1 = await createNTestNotes(5, folders[1]);
-		await time.msleep(100);
+		await testApp.wait();
 
 		testApp.dispatch({ type: 'FOLDER_SELECT', id: id(folders[0]) });
-		await time.msleep(100);
+		await testApp.wait();
 
 		let state = testApp.store().getState();
 		expect(state.backwardHistoryNotes).toEqual([]);
 		expect(state.forwardHistoryNotes).toEqual([]);
 
 		testApp.dispatch({ type: 'FOLDER_SELECT', id: id(folders[1]), historyAction: 'goto' });
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4]])); // notes0[4] was last created
 		expect(ids(state.forwardHistoryNotes)).toEqual([]);
 
 		testApp.dispatch({ type: 'FOLDER_SELECT', id: id(folders[0]), historyAction: 'goto' });
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4], notes1[4]]));
 		expect(state.forwardHistoryNotes).toEqual([]);
 
 		goBackWard(state);
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4]]));
 		expect(ids(state.forwardHistoryNotes)).toEqual(ids([notes0[4]]));
 
 		goForward(state);
-		await time.msleep(100);
+		await testApp.wait();
 
 		state = testApp.store().getState();
 		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4], notes1[4]]));
 		expect(state.forwardHistoryNotes).toEqual([]);
+	}));
+
+
+	it('should save history when searching for a note', asyncTest(async () => {
+		const folders = await createNTestFolders(2);
+		await testApp.wait();
+		const notes0 = await createNTestNotes(5, folders[0]);
+		const notes1 = await createNTestNotes(5, folders[1]);
+		await testApp.wait();
+
+		testApp.dispatch({ type: 'FOLDER_SELECT', id: id(folders[0]) });
+		await testApp.wait();
+
+		let state = testApp.store().getState();
+		expect(state.backwardHistoryNotes).toEqual([]);
+		expect(state.forwardHistoryNotes).toEqual([]);
+
+		testApp.dispatch({ type: 'FOLDER_SELECT', id: id(folders[1]), historyAction: 'goto' });
+		await testApp.wait();
+
+		state = testApp.store().getState();
+		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4]])); // notes0[4] was last created
+		expect(ids(state.forwardHistoryNotes)).toEqual([]);
+
+		const searchId = uuid.create();
+		testApp.dispatch({
+			type: 'SEARCH_UPDATE',
+			search: {
+				id: searchId,
+				title: notes0[0].title,
+				query_pattern: notes0[0].title,
+				query_folder_id: null,
+				type: BaseModel.TYPE_SEARCH,
+			},
+		});
+
+		await testApp.wait();
+
+		testApp.dispatch({
+			type: 'SEARCH_SELECT',
+			id: searchId,
+		});
+
+		await testApp.wait();
+
+		state = testApp.store().getState();
+		expect(ids(state.backwardHistoryNotes)).toEqual(ids([notes0[4], notes1[4]]));
+		expect(ids(state.forwardHistoryNotes)).toEqual([]);
+
 
 
 	}));
+
+
+
 
 });
