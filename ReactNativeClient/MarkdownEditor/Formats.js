96acd5565a9a3d5a10ed7998276bd426d1d0ea58
devonzuegel <devonzuegel@gmail.com>
2020-03-20 22:47:59 -0700

    Update button order

--- ReactNativeClient/MarkdownEditor/Formats.js
+++ ReactNativeClient/MarkdownEditor/Formats.js
@@ -6,6 +6,9 @@ import applyWebLinkFormat from './applyWebLinkFormat';
 export default [
 	{ key: 'B', title: 'B', wrapper: '**', onPress: applyWrapFormat, style: { fontWeight: 'bold' } },
 	{ key: 'I', title: 'I', wrapper: '*', onPress: applyWrapFormat, style: { fontStyle: 'italic' } },
+	{ key: 'Link', title: 'Link', onPress: applyWebLinkFormat },
+	{ key: 'List', title: 'List', prefix: '-', onPress: applyListFormat },
+	{ key: '</>', title: '</>', wrapper: '`', onPress: applyWrapFormat },
 	{
 		key: 'S',
 		title: 'S',
@@ -13,10 +16,7 @@ export default [
 		onPress: applyWrapFormat,
 		style: { textDecorationLine: 'line-through' },
 	},
-	{ key: 'Code', title: 'Code', wrapper: '`', onPress: applyWrapFormat },
 	{ key: 'Pre', title: 'Pre', wrapper: '```', onPress: applyWrapFormatNewLines },
-	{ key: 'List', title: 'List', prefix: '-', onPress: applyListFormat },
-	{ key: 'Link', title: 'Link', onPress: applyWebLinkFormat },
 	{ key: 'H1', title: 'H1', prefix: '#', onPress: applyListFormat },
 	{ key: 'H2', title: 'H2', prefix: '##', onPress: applyListFormat },
 	{ key: 'H3', title: 'H3', prefix: '###', onPress: applyListFormat },
