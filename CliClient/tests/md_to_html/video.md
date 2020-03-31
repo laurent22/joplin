22132e63cff94681fa30ff0d52952ed5a60c335a
Rishabh Malhotra <Rishabhmalhotraa01@gmail.com>
2020-03-31 11:13:22 +0530

    Adding video embedding functionality

---
+++ CliClient/tests/md_to_html/video.md
@@ -0,0 +1,4 @@
+[](http://www.youtube.com/watch?v=iwGFalTRHDA)
+[](https://www.youtube.com/watch?v=iwGFalTRHDA&feature=related)
+[](http://www.youtube.com/attribution_link?u=/watch?v=aGmiw_rrNxk&feature=sharea=9QlmP1yvjcllp0h3l0NwuA)
+[google](https://www.google.com/)
---
+++ ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/video.js
@@ -0,0 +1,22 @@
+// LOGIC: Extract the & UUID of the youtube video and render iframe
+
+module.exports = function installRule(markdownIt) {
+
+	const defaultRender = markdownIt.renderer.rules.link_open;
+	const videoIDRegex = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+	// videoIDRegex extracts the unique 11 alpha-numeric code which is unique to every youtube video
+
+	markdownIt.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+		const token = tokens[idx];
+		const link = token.attrGet('href');
+		const videoID = link.match(videoIDRegex);
+
+		if (!videoID) return defaultRender(tokens, idx, options, env, self);
+
+		if (videoID[2]) {
+			return `<div><iframe  width="560" height="315" src="https://www.youtube.com/embed/${videoID[2]}" frameborder="0" allowfullscreen></iframe></div>`;
+		}
+
+		return defaultRender(tokens, idx, options, env, self);
+	};
+};
