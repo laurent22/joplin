479d075fe5ced0f4eb65653763daedc0928b0343
Roman Musin <musinrr@gmail.com>
2020-04-06 16:20:24 +0100

    Remove noData opt from DocumentPicker as it's only relevant for ImagePicker

--- ReactNativeClient/lib/components/screens/note.js
+++ ReactNativeClient/lib/components/screens/note.js
@@ -377,7 +377,7 @@ class NoteScreenComponent extends BaseScreenComponent {
 
 	async pickDocument() {
 		return new Promise((resolve) => {
-			DocumentPicker.show({ filetype: [DocumentPickerUtil.allFiles()], noData: true }, (error, res) => {
+			DocumentPicker.show({ filetype: [DocumentPickerUtil.allFiles()] }, (error, res) => {
 				if (error) {
 					// Also returns an error if the user doesn't pick a file
 					// so just resolve with null.
