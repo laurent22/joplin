200b8c8b61e6600c2c72367130903949a80cef43
Rishabh Malhotra <Rishabhmalhotraa01@gmail.com>
2020-04-13 19:57:29 +0530

    making requested changes

--- ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/video.js
+++ ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/video.js
@@ -1,17 +1,19 @@
-// LOGIC: Extract the & UUID of the youtube video and render it in iframe
+// LOGIC: Extract the & UUID of the youtube video and the host name from the href link
+// and render it in iframe if both of the are valid.
+const url = require('url');
 const videoCss = () => {
 	return [
 		{
 			inline: true,
 			mime: 'text/css',
 			text: `
-                .video-container {
+                .jop-video-container {
                     position: relative;
                     width: 100%;
                     height: 0;
                     padding-bottom: 56.25%;
                 }
-                .video-iframe {
+                .jop-video-iframe {
                     position: absolute;
                     top: 0;
                     left: 0;
@@ -30,24 +32,30 @@ function addContextAssets(context) {
 
 function installRule(markdownIt, mdOptions, ruleOptions, context) {
 	const defaultRender = markdownIt.renderer.rules.link_open;
+
 	const videoIDRegex = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+	const validHostName = ['youtu.be', 'www.youtube.com','www.youtube-nocookie.com'];
 	// videoIDRegex extracts the unique 11 digit code which is unique to every youtube video
 
 	markdownIt.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+
+		const renderLink = defaultRender(tokens, idx, options, env, self);
 		const token = tokens[idx];
 		const link = token.attrGet('href');
 		const videoID = link.match(videoIDRegex);
+		const hostname = url.parse(link).hostname;
+		const checkHostName = validHostName.includes(hostname);
 
-		if (!videoID) return defaultRender(tokens, idx, options, env, self);
+		if (!videoID || !checkHostName) {
+			return renderLink;
+		}
 
-		if (videoID[2]) {
+		if (videoID[2] && checkHostName) {
 			if (!('video' in context.pluginAssets)) {
 				addContextAssets(context);
 			}
-			return `<div class="video-container"><iframe  class="video-iframe"  width="560" height="315" src="https://www.youtube.com/embed/${videoID[2]}"  frameborder="0" allowfullscreen></iframe></div>`;
-
+			return `<div class="jop-video-container"><iframe class="jop-video-iframe" src="https://www.youtube.com/embed/${videoID[2]}" frameborder="0" allowfullscreen></iframe></div>${renderLink}`;
 		}
-		return defaultRender(tokens, idx, options, env, self);
 	};
 }
 
--- ReactNativeClient/lib/models/Setting.js
+++ ReactNativeClient/lib/models/Setting.js
@@ -387,7 +387,7 @@ class Setting extends BaseModel {
 			'markdown.plugin.emoji': { value: false, type: Setting.TYPE_BOOL, section: 'plugins', public: true, appTypes: ['mobile', 'desktop'], label: () => `${_('Enable markdown emoji')}${wysiwygNo}` },
 			'markdown.plugin.insert': { value: false, type: Setting.TYPE_BOOL, section: 'plugins', public: true, appTypes: ['mobile', 'desktop'], label: () => `${_('Enable ++insert++ syntax')}${wysiwygNo}` },
 			'markdown.plugin.multitable': { value: false, type: Setting.TYPE_BOOL, section: 'plugins', public: true, appTypes: ['mobile', 'desktop'], label: () => `${_('Enable multimarkdown table extension')}${wysiwygNo}` },
-			'markdown.plugin.video': { value: false, type: Setting.TYPE_BOOL, section: 'plugins', public: true, appTypes: ['desktop'], label: () => `${_('Enable video embedding')}${wysiwygNo}` },
+			'markdown.plugin.video': { value: false, type: Setting.TYPE_BOOL, section: 'plugins', public: true, appTypes: ['mobile','desktop'], label: () => `${_('Enable video embedding')}${wysiwygNo}` },
 
 			// Tray icon (called AppIndicator) doesn't work in Ubuntu
 			// http://www.webupd8.org/2017/04/fix-appindicator-not-working-for.html
