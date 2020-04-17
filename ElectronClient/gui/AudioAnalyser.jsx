084ecd268b2f963f8a1197206b5ffaf6746c7667
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2020-04-17 11:08:55 -0500

    fixed formatting and spacing

--- ElectronClient/gui/AudioAnalyser.jsx
+++ ElectronClient/gui/AudioAnalyser.jsx
@@ -4,38 +4,38 @@ const React = require('react');
 const AudioVisualiser = require('./AudioVisualiser.min');
 
 class AudioAnalyser extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { audioData: new Uint8Array(0) };
-    this.tick = this.tick.bind(this);
-    this.mediaRecorder = null;
-  }
+	constructor(props) {
+		super(props);
+		this.state = { audioData: new Uint8Array(0) };
+		this.tick = this.tick.bind(this);
+		this.mediaRecorder = null;
+	}
 
-  componentDidMount() {
-    this.audioContext = new (window.AudioContext ||
-      window.webkitAudioContext)();
-    this.analyser = this.audioContext.createAnalyser();
-    this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
-    this.source = this.audioContext.createMediaStreamSource(this.props.audio);
-    this.source.connect(this.analyser);
-    this.rafId = requestAnimationFrame(this.tick);
-  }
+	componentDidMount() {
+		this.audioContext = new (window.AudioContext ||
+			window.webkitAudioContext)();
+		this.analyser = this.audioContext.createAnalyser();
+		this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+		this.source = this.audioContext.createMediaStreamSource(this.props.audio);
+		this.source.connect(this.analyser);
+		this.rafId = requestAnimationFrame(this.tick);
+	}
 
-  tick() {
-    this.analyser.getByteTimeDomainData(this.dataArray);
-    this.setState({ audioData: this.dataArray });
-    this.rafId = requestAnimationFrame(this.tick);
-  }
+	tick() {
+		this.analyser.getByteTimeDomainData(this.dataArray);
+		this.setState({ audioData: this.dataArray });
+		this.rafId = requestAnimationFrame(this.tick);
+	}
 
-  componentWillUnmount() {
-    cancelAnimationFrame(this.rafId);
-    this.analyser.disconnect();
-    this.source.disconnect();
-  }
+	componentWillUnmount() {
+		cancelAnimationFrame(this.rafId);
+		this.analyser.disconnect();
+		this.source.disconnect();
+	}
 
-  render() {
-    return <AudioVisualiser audioData={this.state.audioData} />;
-  }
+	render() {
+		return <AudioVisualiser audioData={this.state.audioData} />;
+	}
 }
 
 module.exports = AudioAnalyser;
