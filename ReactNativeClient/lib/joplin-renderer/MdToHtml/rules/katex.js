b7ff93e7b47f5d0e68e5e35c0aed534b0762975e
zkesarani@gmail.com <zkesarani@gmail.com>
2020-04-19 21:00:41 +0530

    Fixed issue: #3078

--- ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/katex.js
+++ ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/katex.js
@@ -17,8 +17,28 @@ katex = mhchemModule(katex);
 function katexStyle() {
 	return [
 		{ name: 'katex.css' },
-		// Note: Katex also requires a number of fonts but they don't need to be specified here
-		// since they will be loaded as needed from the CSS.
+		{ name: 'fonts/KaTeX_AMS-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Caligraphic-Bold.woff2' },
+		{ name: 'fonts/KaTeX_Caligraphic-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Fraktur-Bold.woff2' },
+		{ name: 'fonts/KaTeX_Fraktur-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Main-Bold.woff2' },
+		{ name: 'fonts/KaTeX_Main-BoldItalic.woff2' },
+		{ name: 'fonts/KaTeX_Main-Italic.woff2' },
+		{ name: 'fonts/KaTeX_Main-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Math-BoldItalic.woff2' },
+		{ name: 'fonts/KaTeX_Math-Italic.woff2' },
+		{ name: 'fonts/KaTeX_SansSerif-Bold.woff2' },
+		{ name: 'fonts/KaTeX_SansSerif-Italic.woff2' },
+		{ name: 'fonts/KaTeX_SansSerif-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Script-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Size1-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Size2-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Size3-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Size4-Regular.woff2' },
+		{ name: 'fonts/KaTeX_Typewriter-Regular.woff2' },
+		// Note: Katex also requires a number of fonts and they need to be specified here
+		// since they will be required while exporting to HTML or PDF.
 	];
 }
 
