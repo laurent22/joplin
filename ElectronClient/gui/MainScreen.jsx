fc470dcb36cd7ab039c3f81a1b3b89883a4d1138
Siddhant Sehgal <sehgal.siddhant999@gmail.com>
2020-04-20 16:20:08 +0530

    Added Folder::sortFolderTree

--- ElectronClient/gui/MainScreen.jsx
+++ ElectronClient/gui/MainScreen.jsx
@@ -242,6 +242,7 @@ class MainScreenComponent extends React.Component {
 				},
 			});
 		} else if (command.name === 'moveToFolder') {
+			const folders = await Folder.sortFolderTree();
 			const startFolders = [];
 			const maxDepth = 15;
 
@@ -252,7 +253,8 @@ class MainScreenComponent extends React.Component {
 					if (folder.children) addOptions(folder.children, (depth + 1) < maxDepth ? depth + 1 : maxDepth);
 				}
 			};
-			addOptions(await Folder.allAsTree(), 0);
+
+			addOptions(folders, 0);
 
 			this.setState({
 				promptOptions: {
@@ -627,7 +629,7 @@ class MainScreenComponent extends React.Component {
 				color: theme.color,
 				backgroundColor: theme.backgroundColor,
 			},
-			this.props.style
+			this.props.style,
 		);
 		const promptOptions = this.state.promptOptions;
 		const folders = this.props.folders;
