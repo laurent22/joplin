24a90036b71715f2787f66819bf8111c18430ad4
Caleb John <calebjohn4@gmail.com>
2020-05-09 04:23:42 -0600

    Linux: Fixes #2906 : Update install script to remove command -v check (#2926)
    * Linux: update install script to remove command -v check
    
    * Formatting change
    
    * spacing

--- Joplin_install_and_update.sh
+++ Joplin_install_and_update.sh
@@ -83,21 +83,19 @@ fi
 #-----------------------------------------------------
 showLogo
 
+#-----------------------------------------------------
 print "Checking architecture..."
-# Architecture check
-if ! [[ -x "$(command -v uname)" ]] ; then
-	print "${COLOR_YELLOW}WARNING: Can't get system architecture, skipping check${COLOR_RESET}"
-else
-  ## this actually gives more information than needed, but it contains all architectures (hardware and software)
-	ARCHITECTURE=$(uname -a)
+## uname actually gives more information than needed, but it contains all architectures (hardware and software)
+ARCHITECTURE=$(uname -a || echo "NO CHECK")
 
-	if [[ $ARCHITECTURE =~ .*aarch.*|.*arm.* ]] ; then
-		showHelp "Arm systems are not officially supported by Joplin, please search the forum (https://discourse.joplinapp.org/) for more information"
-		exit 1
-	elif [[ $ARCHITECTURE =~ .*i.86.* ]] ; then
-		showHelp "32-bit systems are not supported by Joplin, please search the forum (https://discourse.joplinapp.org/) for more information"
-		exit 1
-	fi
+if [[ $ARCHITECTURE = "NO CHECK" ]] ; then
+  print "${COLOR_YELLOW}WARNING: Can't get system architecture, skipping check${COLOR_RESET}"
+elif [[ $ARCHITECTURE =~ .*aarch.*|.*arm.* ]] ; then
+  showHelp "Arm systems are not officially supported by Joplin, please search the forum (https://discourse.joplinapp.org/) for more information"
+  exit 1
+elif [[ $ARCHITECTURE =~ .*i386.*|.*i686.* ]] ; then
+  showHelp "32-bit systems are not supported by Joplin, please search the forum (https://discourse.joplinapp.org/) for more information"
+  exit 1
 fi
 
 #-----------------------------------------------------
