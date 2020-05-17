db622b1879f1ed771ec762657a69e9f4315e2c0f
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2020-05-17 17:58:32 +0100

    Refactored SideBar.jsx to use same rendering code for folders and nested tags

--- ElectronClient/gui/SideBar/SideBar.jsx
+++ ElectronClient/gui/SideBar/SideBar.jsx
@@ -64,6 +64,7 @@ class SideBarComponent extends React.Component {
 
 				const folderIds = JSON.parse(dt.getData('text/x-jop-folder-ids'));
 				for (let i = 0; i < folderIds.length; i++) {
+					if (folderId === folderIds[i]) continue;
 					await Folder.moveToFolder(folderIds[i], folderId);
 				}
 			}
@@ -101,6 +102,7 @@ class SideBarComponent extends React.Component {
 				const tagIds = JSON.parse(dt.getData('text/x-jop-tag-ids'));
 				try {
 					for (let i = 0; i < tagIds.length; i++) {
+						if (tagId === tagIds[i]) continue;
 						await Tag.moveTag(tagIds[i], tagId);
 					}
 				} catch (error) {
@@ -395,15 +397,46 @@ class SideBarComponent extends React.Component {
 		return <div style={this.style().noteCount}>({count})</div>;
 	}
 
-	folderItem(folder, selected, hasChildren, depth) {
-		let style = Object.assign({}, this.style().listItem);
-		if (folder.id === Folder.conflictFolderId()) style = Object.assign(style, this.style().conflictFolder);
+	renderItem(itemType, item, selected, hasChildren, depth) {
+		let itemTitle = '';
+		let collapsedIds = null;
+		const jsxItemIdAttribute = {};
+		let anchorRef = null;
+		let noteCount = '';
+		let onDragStart = null;
+		let onDragOver = null;
+		let onDrop = null;
+		let onItemClick = null;
+		let onItemToggleClick = null;
+		if (itemType === BaseModel.TYPE_FOLDER) {
+			itemTitle = Folder.displayTitle(item);
+			collapsedIds = this.props.collapsedFolderIds;
+			jsxItemIdAttribute.folderid = item.id;
+			anchorRef = this.anchorItemRef('folder', item.id);
+			noteCount = item.note_count ? this.noteCountElement(item.note_count) : '';
+			onDragStart = this.onFolderDragStart_;
+			onDragOver = this.onFolderDragOver_;
+			onDrop = this.onFolderDrop_;
+			onItemClick = this.folderItem_click.bind(this);
+			onItemToggleClick = this.onFolderToggleClick_;
+		} else {
+			itemTitle = Tag.displayTitle(item);
+			collapsedIds = this.props.collapsedTagIds;
+			jsxItemIdAttribute.tagid = item.id;
+			anchorRef = this.anchorItemRef('tag', item.id);
+			noteCount = Setting.value('showNoteCounts') ? this.noteCountElement(item.note_count) : '';
+			onDragStart = this.onTagDragStart_;
+			onDragOver = this.onTagDragOver_;
+			onDrop = this.onTagDrop_;
+			onItemClick = this.tagItem_click.bind(this);
+			onItemToggleClick = this.onTagToggleClick_;
+		}
 
-		const itemTitle = Folder.displayTitle(folder);
+		let style = Object.assign({}, this.style().listItem);
+		if (item.id === Folder.conflictFolderId()) style = Object.assign(style, this.style().conflictFolder);
 
 		let containerStyle = Object.assign({}, this.style().listItemContainer);
 		if (selected) containerStyle = Object.assign(containerStyle, this.style().listItemSelected);
-
 		containerStyle.paddingLeft = 8 + depth * 15;
 
 		const expandLinkStyle = Object.assign({}, this.style().listItemExpandIcon);
@@ -411,86 +444,34 @@ class SideBarComponent extends React.Component {
 			visibility: hasChildren ? 'visible' : 'hidden',
 		};
 
-		const iconName = this.props.collapsedFolderIds.indexOf(folder.id) >= 0 ? 'fa-chevron-right' : 'fa-chevron-down';
+		const iconName = collapsedIds.indexOf(item.id) >= 0 ? 'fa-chevron-right' : 'fa-chevron-down';
 		const expandIcon = <i style={expandIconStyle} className={`fas ${iconName}`}></i>;
 		const expandLink = hasChildren ? (
-			<a style={expandLinkStyle} href="#" folderid={folder.id} onClick={this.onFolderToggleClick_}>
+			<a style={expandLinkStyle} href="#" {...jsxItemIdAttribute} onClick={onItemToggleClick}>
 				{expandIcon}
 			</a>
 		) : (
 			<span style={expandLinkStyle}>{expandIcon}</span>
 		);
 
-		const anchorRef = this.anchorItemRef('folder', folder.id);
-		const noteCount = folder.note_count ? this.noteCountElement(folder.note_count) : '';
-
 		return (
-			<div className={`list-item-container list-item-depth-${depth}`} style={containerStyle} key={folder.id} onDragStart={this.onFolderDragStart_} onDragOver={this.onFolderDragOver_} onDrop={this.onFolderDrop_} draggable={true} folderid={folder.id}>
+			<div className={`list-item-container list-item-depth-${depth}`} style={containerStyle} key={item.id} onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop} draggable={true} {...jsxItemIdAttribute}>
 				{expandLink}
 				<a
 					ref={anchorRef}
 					className="list-item"
 					href="#"
-					data-id={folder.id}
-					data-type={BaseModel.TYPE_FOLDER}
+					data-id={item.id}
+					data-type={itemType}
 					onContextMenu={event => this.itemContextMenu(event)}
 					style={style}
-					folderid={folder.id}
+					{...jsxItemIdAttribute}
 					onClick={() => {
-						this.folderItem_click(folder);
+						onItemClick(item);
 					}}
-					onDoubleClick={this.onFolderToggleClick_}
+					onDoubleClick={onItemToggleClick}
 				>
 					{itemTitle} {noteCount}
-				</a>
-			</div>
-		);
-	}
-
-	tagItem(tag, selected, hasChildren, depth) {
-		let style = Object.assign({}, this.style().listItem);
-
-		const containerStyle = Object.assign({}, this.style(depth).listItemContainer);
-		if (selected) style = Object.assign(style, this.style().listItemSelected);
-
-		const expandLinkStyle = Object.assign({}, this.style().listItemExpandIcon);
-		const expandIconStyle = {
-			visibility: hasChildren ? 'visible' : 'hidden',
-			paddingLeft: 8 + depth * 10,
-		};
-
-		const iconName = this.props.collapsedTagIds.indexOf(tag.id) >= 0 ? 'fa-plus-square' : 'fa-minus-square';
-		const expandIcon = <i style={expandIconStyle} className={`fa ${iconName}`}></i>;
-		const expandLink = hasChildren ? (
-			<a style={expandLinkStyle} href="#" tagid={tag.id} onClick={this.onTagToggleClick_}>
-				{expandIcon}
-			</a>
-		) : (
-			<span style={expandLinkStyle}>{expandIcon}</span>
-		);
-
-
-		const anchorRef = this.anchorItemRef('tag', tag.id);
-		const noteCount = Setting.value('showNoteCounts') ? this.noteCountElement(tag.note_count) : '';
-
-		return (
-			<div className="list-item-container" style={containerStyle} key={tag.id} onDragStart={this.onTagDragStart_} onDragOver={this.onTagDragOver_} onDrop={this.onTagDrop_} draggable={true} tagid={tag.id}>
-				{expandLink}
-				<a
-					ref={anchorRef}
-					className="list-item"
-					href="#"
-					data-id={tag.id}
-					data-type={BaseModel.TYPE_TAG}
-					onContextMenu={event => this.itemContextMenu(event)}
-					style={style}
-					tagid={tag.id}
-					onClick={() => {
-						this.tagItem_click(tag);
-					}}
-					onDoubleClick={this.onTagToggleClick_}
-				>
-					{Tag.displayTitle(tag)} {noteCount}
 				</a>
 			</div>
 		);
@@ -720,7 +701,7 @@ class SideBarComponent extends React.Component {
 		);
 
 		if (this.props.folders.length) {
-			const result = shared.renderFolders(this.props, this.folderItem.bind(this));
+			const result = shared.renderFolders(this.props, this.renderItem.bind(this, BaseModel.TYPE_FOLDER));
 			const folderItems = result.items;
 			this.folderItemsOrder_ = result.order;
 			items.push(
@@ -738,7 +719,7 @@ class SideBarComponent extends React.Component {
 		);
 
 		if (this.props.tags.length) {
-			const result = shared.renderTags(this.props, this.tagItem.bind(this));
+			const result = shared.renderTags(this.props, this.renderItem.bind(this, BaseModel.TYPE_TAG));
 			const tagItems = result.items;
 			this.tagItemsOrder_ = result.order;
 
