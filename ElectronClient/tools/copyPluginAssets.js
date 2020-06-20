e38794171a70cda55c2490996e7645e2d5538e32
Laurent Cozic <laurent@cozic.net>
2020-06-20 01:18:46 +0100

    Tools: Fix desktop build issue on Windows

--- ElectronClient/tools/copyPluginAssets.js
+++ ElectronClient/tools/copyPluginAssets.js
@@ -10,9 +10,16 @@ async function main() {
 
 	for (const destDir of destDirs) {
 		console.info(`Copying to ${destDir}`);
-		await fs.remove(destDir);
-		await fs.mkdirp(destDir);
-		await fs.copy(sourceDir, destDir);
+
+		try {
+			await fs.remove(destDir);
+			await fs.copy(sourceDir, destDir);
+		} catch (error) {
+			// These calls randomly fail on Windows when the folders are being
+			// watch by TypeScript. As these files aren't always needed for
+			// development, only print a warning.
+			console.warn(`Could not copy to ${destDir}`, error);
+		}
 	}
 }
 
