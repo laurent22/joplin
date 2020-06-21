d05484a05a6c738e5b11a86ab559367c0858ba42
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2020-06-21 14:56:39 -0500

    added ts-ignore line

--- ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/audio.ts
+++ ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/audio.ts
@@ -1,6 +1,7 @@
 const utils = require('../../utils');
 
-function installRule(markdownIt:any, ruleOptions:any) {
+// @ts-ignore: Keep the function signature as-is despite unusued arguments
+function installRule(markdownIt:any, mdOptions:any, ruleOptions:any, context:any) {
 	const defaultRender = markdownIt.renderer.rules.link_open;
 
 	markdownIt.renderer.rules.link_open = (tokens: { [x: string]: any; }, idx: string | number, options: any, env: any, self: any) => {
@@ -15,13 +16,15 @@ function installRule(markdownIt:any, ruleOptions:any) {
 		if (typeof r === 'string') return r;
 		if (r && r.type === 'audio') return `<audio controls><source src='${r.src}'></audio><a href=# onclick=ipcProxySendToHost('joplin://${src.substring(2)}')>`;
 		if (r && r.type === 'video') return `<video style="width:100%" controls><source src='${r.src}'></video>`;
-		
+
+		console.log(context);
+
 		return defaultRender(tokens, idx, options, env, self);
 	};
 }
 
-export default function(ruleOptions: any) {
-	return function(md: any) {
-		installRule(md, ruleOptions);
+export default function(context:any,ruleOptions:any) {
+	return function(md:any, mdOptions:any) {
+		installRule(md, mdOptions, ruleOptions, context);
 	};
-};
+}
