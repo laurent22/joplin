6068727afdeefae8973db04b3a2d9355da757ea7
CalebJohn <calebjohn4@gmail.com>
2020-06-23 14:24:11 -0600

    switch to mkdirp

--- Tools/gulp/utils.js
+++ Tools/gulp/utils.js
@@ -126,7 +126,7 @@ utils.mkdir = async function(dir) {
 	if (utils.isWindows()) {
 		return utils.execCommand(`if not exist "${utils.toSystemSlashes(dir)}" mkdir "${utils.toSystemSlashes(dir)}"`);
 	} else {
-		return fs.ensureDir(dir);
+		return fs.mkdirp(dir);
 	}
 };
 
