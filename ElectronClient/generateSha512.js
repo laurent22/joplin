6da2e7f4048645f825bd6c792a6c0fd91f535ac8
Deep Majumder <deep.majumder2019@gmail.com>
2020-06-26 20:19:19 +0530

    Refectored the logic

--- ElectronClient/generateSha512.js
+++ ElectronClient/generateSha512.js
@@ -1,40 +1,33 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 const sha512 = require('js-sha512');
 
 const generateChecksumFile = () => {
+	if (os.platform() != 'linux') {
+		return []; // SHA-512 is only for AppImage
+	}
 	const distDirName = 'dist';
 	const distPath = path.join(__dirname, distDirName);
-	try {
-		let appImageName = '';
-		const files = fs.readdirSync(distPath);
-		for (const key in files) {
-			const filename = files[key];
-			if (filename.includes('AppImage')) {
-				appImageName = filename;
-				break;
-			}
+	let appImageName = '';
+	const files = fs.readdirSync(distPath);
+	for (const key in files) {
+		const filename = files[key];
+		if (filename.includes('AppImage')) {
+			appImageName = filename;
+			break;
 		}
-		if (appImageName === '') {
-			// That means we are not on Linux
-			return [];
-		}
-		try {
-			const appImagePath = path.join(distPath, appImageName);
-			const appImageContent = fs.readFileSync(appImagePath);
-			const checksum = sha512.sha512(appImageContent);
-			const sha512FileName = `${appImageName}.sha512`;
-			const sha512FilePath = path.join(distPath, sha512FileName);
-			fs.writeFileSync(sha512FilePath, checksum);
-			return [sha512FilePath];
-		} catch (e) {
-			console.error('Could not generate checksum file');
-			return [];
-		}
-	} catch (e) {
-		console.error('Please run "npm run dist" first');
-		return [];
 	}
+	if (appImageName == '') {
+		throw 'AppImage not found!';
+	}
+	const appImagePath = path.join(distPath, appImageName);
+	const appImageContent = fs.readFileSync(appImagePath);
+	const checksum = sha512.sha512(appImageContent);
+	const sha512FileName = `${appImageName}.sha512`;
+	const sha512FilePath = path.join(distPath, sha512FileName);
+	fs.writeFileSync(sha512FilePath, checksum);
+	return [sha512FilePath];
 };
 
 exports.default = generateChecksumFile;
