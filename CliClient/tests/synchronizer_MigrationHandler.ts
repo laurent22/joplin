14d5620e871fdb23e36944705c8493f940c232f2
Laurent Cozic <laurent@cozic.net>
2020-07-12 16:44:50 +0100

    test

--- CliClient/tests/synchronizer_MigrationHandler.ts
+++ CliClient/tests/synchronizer_MigrationHandler.ts
@@ -5,6 +5,9 @@ require('app-module-path').addPath(__dirname);
 
 const { asyncTest, fileApi, synchronizer, setupDatabaseAndSynchronizer, switchClient, expectThrow, expectNotThrow } = require('test-utils.js');
 const Setting = require('lib/models/Setting');
+const Folder = require('lib/models/Folder');
+const Note = require('lib/models/Note');
+const { shim } = require('lib/shim');
 
 let lockHandler_:LockHandler = null;
 let migrationHandler_:MigrationHandler = null;
@@ -19,6 +22,50 @@ function migrationHandler(clientId:string = 'abcd'):MigrationHandler {
 	if (migrationHandler_) return migrationHandler_;
 	migrationHandler_ = new MigrationHandler(fileApi(), lockHandler(), 'desktop', clientId);
 	return migrationHandler_;
+}
+
+async function createTestNotes() {
+	const struct:any = {
+		folder1: {
+			subFolder1: {},
+			subFolder2: {
+				note1: {
+					resource: true,
+					tags: ['tag1'],
+				},
+				note2: {},
+			},
+			note3: {
+				tags: ['tag1', 'tag2'],
+			},
+			note4: {
+				tags: ['tag2'],
+			},
+		},
+		folder2: {},
+		folder3: {
+			note5: {
+				resource: true,
+				tags: ['tag2'],
+			},
+		},
+	};
+
+	async function recurseStruct(s:any, parentId:string = '') {
+		for (const n in s) {
+			if (n.toLowerCase().includes('folder')) {
+				const folder = await Folder.save({ title: n, parent_id: parentId });
+				await recurseStruct(s[n], folder.id);
+			} else {
+				const note = await Note.save({ title: n, parent_id: parentId });
+				if (s[n].resource) {
+					await shim.attachFileToNote(note, `${__dirname}/../tests/support/photo.jpg`);
+				}
+			}
+		}
+	}
+
+	await recurseStruct(struct);
 }
 
 describe('synchronizer_MigrationHandler', function() {
@@ -50,7 +97,16 @@ describe('synchronizer_MigrationHandler', function() {
 		expectThrow(async () => await migrationHandler().checkCanSync(), 'outdatedClient');
 	}));
 
+	// Create a helper function that create notes, folders, resources and tags
+	// Sync with previous sync format
+	// Upgrade
+	// Sync with new sync format
+	// With encryption and without
+	// => Check we got back the same notes, folders and tags
+
 	it('should migrate (2)', asyncTest(async () => {
+		await createTestNotes();
+
 		Setting.setConstant('syncVersion', 1);
 
 		await synchronizer().start();
