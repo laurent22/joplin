28d423e1f58d954713423910760ed2fd9be845cc
CalebJohn <calebjohn4@gmail.com>
2020-07-30 20:07:34 -0600

    Modify the codemirror linter plugin to fix katex
    This stops the katex mode from trigger on a single $
    This also allows the user to escape $ characters

--- ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/utils/useJoplinMode.ts
+++ ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/utils/useJoplinMode.ts
@@ -1,13 +1,20 @@
-import 'codemirror/addon/mode/multiplex';
 import 'codemirror/mode/stex/stex';
+
+// We use require here because multiplex is a javascript file
+// multiplex is copied from codemirror/addon/mode/multiplex and is orginally js
+const { useMultiplexer } = require('./multiplex');
 
 // Joplin markdown is a the same as markdown mode, but it has configured defaults
 // and support for katex math blocks
 export default function useJoplinMode(CodeMirror: any) {
+	useMultiplexer(CodeMirror);
+
 	CodeMirror.defineMode('joplin-markdown', (config: any) => {
 		const stex = CodeMirror.getMode(config, { name: 'stex', inMathMode: true });
 		const blocks = [{ open: '$$', close: '$$', mode: stex, delimStyle: 'katex-marker' },
-			{ open: '$', close: '$', mode: stex, delimStyle: 'katex-marker' }];
+			// This regex states that an inline katex block must have a closing deliminator to be valid
+			// it also has some stipulations about the surrounding characters
+			{ openCheck: /\\?\$\S[\s\S]*?[^\\\s]\$(?:\s|$)/, open: '$', close: '$', mode: stex, delimStyle: 'katex-marker' }];
 
 		const markdownOptions = {
 			name: 'markdown',
