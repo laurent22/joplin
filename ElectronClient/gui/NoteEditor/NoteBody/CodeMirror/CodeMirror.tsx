5c5cb0f781a567525f0f5b9199af92d48d741199
Caleb John <calebjohn4@gmail.com>
2020-08-08 02:55:10 -0600

    Desktop: Implement scroll-past-end on CodeMirror editor (#3589)

--- ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/CodeMirror.tsx
+++ ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/CodeMirror.tsx
@@ -291,6 +291,12 @@ function CodeMirror(props: NoteBodyEditorProps, ref: any) {
 				position: absolute !important;
 				-webkit-box-shadow: none !important; // Some themes add a box shadow for some reason
 			}
+
+			.CodeMirror-lines {
+				/* This is used to enable the scroll-past end behaviour. The same height should */
+				/* be applied to the viewer. */
+				padding-bottom: 400px !important;
+			}
 			
 			.cm-header-1 {
 				font-size: 1.5em;
--- ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/utils/useScrollUtils.ts
+++ ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/utils/useScrollUtils.ts
@@ -1,19 +1,12 @@
 // Helper functions to sync up scrolling
 export default function useScrollUtils(CodeMirror: any) {
-	function getScrollHeight(cm: any) {
-		const info = cm.getScrollInfo();
-		const overdraw = cm.state.scrollPastEndPadding ? cm.state.scrollPastEndPadding : '0px';
-		return info.height - info.clientHeight - parseInt(overdraw, 10);
-	}
-
 	CodeMirror.defineExtension('getScrollPercent', function() {
 		const info = this.getScrollInfo();
-
-		return info.top / getScrollHeight(this);
+		return info.top / (info.height - info.clientHeight);
 	});
 
 	CodeMirror.defineExtension('setScrollPercent', function(p: number) {
-		this.scrollTo(null, p * getScrollHeight(this));
+		const info = this.getScrollInfo();
+		this.scrollTo(null, p * (info.height - info.clientHeight));
 	});
-
 }
