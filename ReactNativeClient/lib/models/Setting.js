d4d5b3d938c41e134e94e79830b76956062def9c
CalebJohn <calebjohn4@gmail.com>
2020-09-04 17:49:44 -0600

    Fully remove AceEditor

--- ReactNativeClient/lib/models/Setting.js
+++ ReactNativeClient/lib/models/Setting.js
@@ -392,14 +392,6 @@ class Setting extends BaseModel {
 				appTypes: ['desktop'],
 				label: () => _('Auto-pair braces, parenthesis, quotations, etc.'),
 			},
-			'editor.optOutCodeMirror': {
-				value: false,
-				type: Setting.TYPE_BOOL,
-				public: true,
-				section: 'note',
-				appTypes: ['desktop'],
-				label: () => 'Opt-out of the CodeMirror beta (this option will eventually be removed).',
-			},
 			'notes.sortOrder.reverse': { value: true, type: Setting.TYPE_BOOL, section: 'note', public: true, label: () => _('Reverse sort order'), appTypes: ['cli'] },
 			'folders.sortOrder.field': {
 				value: 'title',
