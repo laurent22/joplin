75e62b86a1341e701cb75008425fa7b68f30fc3f
Laurent Cozic <laurent@cozic.net>
2020-09-14 12:43:21 +0100

    Fixed search field hover styling

--- ElectronClient/gui/style/StyledInput.tsx
+++ ElectronClient/gui/style/StyledInput.tsx
@@ -1,7 +1,8 @@
 const styled = require('styled-components').default;
+const Color = require('color');
 
 const StyledInput = styled.input`
-	border: 1px solid ${(props:any) => props.theme.color3};
+	border: 1px solid ${(props:any) => Color(props.theme.color3).alpha(0.6)};
 	border-radius: 3px;
 	font-size: ${(props:any) => props.theme.fontSize}px;
 	color: ${(props:any) => props.theme.color};
@@ -9,14 +10,15 @@ const StyledInput = styled.input`
 	height: ${(props:any) => `${props.theme.toolbarHeight}px`};
 	max-height: ${(props:any) => `${props.theme.toolbarHeight}px`};
 	box-sizing: border-box;
-	background-color: ${(props:any) => props.theme.backgroundColor4};
+	background-color: ${(props:any) => Color(props.theme.backgroundColor4).alpha(0.5)};
 
 	&::placeholder {
 		color: ${(props:any) => props.theme.colorFaded};
 	}
 
 	&:focus {
-		background-color: ${(props:any) => props.theme.backgroundColorActive4};
+		background-color: ${(props:any) => props.theme.backgroundColor4};
+		border: 1px solid ${(props:any) => props.theme.color3};
 	}
 `;
 
