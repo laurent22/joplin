cc64614a88f3367d629ad0be354004b7e3c5a000
Carlos de Paula <me@carlosedp.com>
2020-09-21 13:36:00 -0300

    Add frequently used languages to markdown renderer

--- ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/Editor.tsx
+++ ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/Editor.tsx
@@ -53,6 +53,12 @@ const topLanguages = [
 	'css',
 	'xml',
 	'markdown',
+	'yaml',
+	'shell',
+	'dockerfile',
+	'diff',
+	'erlang',
+	'sql',
 ];
 // Load Top Modes
 for (let i = 0; i < topLanguages.length; i++) {
