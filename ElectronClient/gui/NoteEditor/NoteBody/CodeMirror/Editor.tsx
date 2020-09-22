5585a18d674e47fc0da65c21cf64b580f04c77fa
Laurent <laurent22@users.noreply.github.com>
2020-09-22 13:09:15 +0100

    Update Editor.tsx

--- ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/Editor.tsx
+++ ElectronClient/gui/NoteEditor/NoteBody/CodeMirror/Editor.tsx
@@ -28,7 +28,6 @@ const { shim } = require('lib/shim.js');
 const { reg } = require('lib/registry.js');
 
 // Based on http://pypl.github.io/PYPL.html
-// +XML (HTML) +CSS and Markdown added
 const topLanguages = [
 	'python',
 	'clike',
@@ -51,7 +50,9 @@ const topLanguages = [
 	'haskell',
 	'pascal',
 	'css',
-	'xml',
+	
+	// Additional languages, not in the PYPL list
+	'xml', // For HTML too
 	'markdown',
 	'yaml',
 	'shell',
