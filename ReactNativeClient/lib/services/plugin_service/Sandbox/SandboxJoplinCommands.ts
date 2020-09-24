4e80dbb7a741ee83089259fe349df21ac8b989d2
Laurent Cozic <laurent@cozic.net>
2020-09-24 15:53:37 +0100

    Allow tagging desktop-only feature in plugin doc

--- ReactNativeClient/lib/services/plugin_service/Sandbox/SandboxJoplinCommands.ts
+++ ReactNativeClient/lib/services/plugin_service/Sandbox/SandboxJoplinCommands.ts
@@ -2,10 +2,16 @@ import CommandService, { CommandDeclaration, CommandRuntime } from 'lib/services
 
 export default class SandboxJoplinCommands {
 
+	/**
+	 * <span class="platform-desktop">desktop</span> Executes the given command.
+	 */
 	execute(commandName: string, args: any) {
 		CommandService.instance().execute(commandName, args);
 	}
 
+	/**
+	 * <span class="platform-desktop">desktop</span> Registers the given command.
+	 */
 	register(declaration:CommandDeclaration, runtime:CommandRuntime) {
 		CommandService.instance().registerDeclaration(declaration);
 		CommandService.instance().registerRuntime(declaration.name, runtime);
