1ae4aee3c32fa1df2c24445f657d15a0d78841fd
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2020-10-17 13:28:21 -0400

    remove first 3 lines from version output when copying it to clipboard

--- ElectronClient/gui/MenuBar.tsx
+++ ElectronClient/gui/MenuBar.tsx
@@ -373,7 +373,7 @@ function useMenu(props:Props) {
 			});
 
 			if (copyToClipboard === 0) {
-				clipboard.writeText(v.message);
+				clipboard.writeText(v.message.split('\n').splice(3).join('\n'));
 			}
 		}
 
