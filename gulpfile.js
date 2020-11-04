b270d1010b6d260f4404558592156c2b8b1543aa
Laurent Cozic <laurent@cozic.net>
2020-11-04 17:02:50 +0000

    automate publish

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,25 @@ const gulp = require('gulp');
 const utils = require('./packages/tools/gulp/utils');
 
 const tasks = {
-	copyLib: require('./packages/tools/gulp/tasks/copyLib'),
-	tsc: require('./packages/tools/gulp/tasks/tsc'),
-	updateIgnoredTypeScriptBuild: require('./packages/tools/gulp/tasks/updateIgnoredTypeScriptBuild'),
-	deleteBuildDirs: require('./packages/tools/gulp/tasks/deleteBuildDirs'),
+	// copyLib: require('./packages/tools/gulp/tasks/copyLib'),
+	// tsc: require('./packages/tools/gulp/tasks/tsc'),
+	// updateIgnoredTypeScriptBuild: require('./packages/tools/gulp/tasks/updateIgnoredTypeScriptBuild'),
+	// deleteBuildDirs: require('./packages/tools/gulp/tasks/deleteBuildDirs'),
+	completePublishAll: {
+		fn: async () => {
+			await utils.execCommand('git pull');
+			await utils.execCommand('git add -A');
+			await utils.execCommand('git commit -m "Releasing sub-packages"');
+			await utils.execCommand('lerna publish from-package');
+			await utils.execCommand('git push');
+		},
+	},
 };
 
 utils.registerGulpTasks(gulp, tasks);
 
-gulp.task('build', gulp.series('copyLib', 'tsc', 'updateIgnoredTypeScriptBuild'));
+// gulp.task('build', gulp.series('copyLib', 'tsc', 'updateIgnoredTypeScriptBuild'));
 
-// The clean task removes build directories and copy back the library. This is useful
-// when switching from one branch to another.
-gulp.task('clean', gulp.series('deleteBuildDirs', 'copyLib'));
+// // The clean task removes build directories and copy back the library. This is useful
+// // when switching from one branch to another.
+// gulp.task('clean', gulp.series('deleteBuildDirs', 'copyLib'));
