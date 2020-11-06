25e70024df0469a40ce27cc40c2956d37ef20d43
Laurent Cozic <laurent@cozic.net>
2020-11-06 16:22:13 +0000

    Release script

--- packages/app-mobile/tools/prepareRelease.js
+++ packages/app-mobile/tools/prepareRelease.js
@@ -1,3 +1,8 @@
+// This is to replace the symlinks inside node_modules with the actual packages
+// as I assumed it was needed to build the final release. However it seems
+// Android `assembleRelease` handles symlinks properly so maybe this is not
+// needed after all ¯\_(ツ)_/¯
+
 const { copyDir } = require('@joplinapp/tools/gulp/utils');
 const { rootDir, deleteLink, toSystemSlashes } = require('@joplinapp/tools/tool-utils');
 const mobileDir = `${rootDir}/packages/app-mobile`;
