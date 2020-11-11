873184f69bac2af7b3fe2e648bea60e0c716c874
Laurent Cozic <laurent@cozic.net>
2020-11-11 14:22:42 +0000

    Desktop: Regression: Fix application name

--- packages/app-desktop/main.js
+++ packages/app-desktop/main.js
@@ -6,6 +6,14 @@ const { initBridge } = require('./bridge');
 const Logger = require('@joplin/lib/Logger').default;
 const FsDriverNode = require('@joplin/lib/fs-driver-node').default;
 const envFromArgs = require('@joplin/lib/envFromArgs');
+const packageInfo = require('./packageInfo.js');
+
+// Electron takes the application name from package.json `name` and
+// displays this in the tray icon toolip and message box titles, however in
+// our case it's a string like "@joplin/app-desktop". It's also supposed to
+// check the productName key but is not doing it, so here set the
+// application name to the right string.
+electronApp.name = packageInfo.name;
 
 process.on('unhandledRejection', (reason, p) => {
 	console.error('Unhandled promise rejection', p, 'reason:', reason);
