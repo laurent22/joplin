177dbaae774f44c2891e874dae8b206f32fdc95b
Laurent Cozic <laurent@cozic.net>
2020-11-14 23:26:07 +0000

    Add generator-joplin module

---
+++ packages/generator-joplin/generators/app/index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+	prompting() {
+		this.log(yosay(`Welcome to the fine ${chalk.red('generator-joplin')} generator!`));
+
+		const prompts = [
+			{
+				type: 'input',
+				name: 'pluginName',
+				message: 'Plugin name:',
+			},
+			{
+				type: 'input',
+				name: 'pluginDescription',
+				message: 'Plugin description:',
+			},
+			{
+				type: 'input',
+				name: 'pluginAuthor',
+				message: 'Author:',
+			},
+			{
+				type: 'input',
+				name: 'pluginHomepageUrl',
+				message: 'Homepage URL:',
+			},
+		];
+
+		return this.prompt(prompts).then(props => {
+			this.props = props;
+		});
+	}
+
+	writing() {
+		// Due to WONTFIX bug in npm, which treats .gitignore and pakage.json in a special way,
+		// we need to give them a different name in the templates dir and then rename them
+		// when installing.
+		// https://github.com/npm/npm/issues/3763
+
+		const files = [
+			'.gitignore_TEMPLATE',
+			'package_TEMPLATE.json',
+			'README.md',
+			'tsconfig.json',
+			'webpack.config.js',
+			'src/index.ts',
+			'src/manifest.json',
+		];
+
+		for (const file of files) {
+			const destFile = file.replace(/_TEMPLATE/, '');
+			this.fs.copyTpl(
+				this.templatePath(file),
+				this.destinationPath(destFile),
+				this.props
+			);
+		}
+
+		this.fs.copy(
+			this.templatePath('api'),
+			this.destinationPath('api')
+		);
+	}
+
+	install() {
+		this.installDependencies({
+			npm: true,
+			bower: false,
+			yarn: false,
+		});
+	}
+};
---
+++ packages/generator-joplin/updateTypes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+git pull
+
+if [[ -n $(git status --porcelain) ]]; then
+	echo "There are changes in the repo"
+	exit 1
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CLI_DIR="$SCRIPT_DIR/../joplin/packages/app-cli"
+LIB_DIR="$SCRIPT_DIR/../joplin/packages/lib"
+
+cd "$LIB_DIR"
+npm run generatePluginTypes
+
+cd "$SCRIPT_DIR"
+rsync -a --delete "$LIB_DIR/plugin_types/services/plugins/api/" "$SCRIPT_DIR/generators/app/templates/api/"
+cp "$LIB_DIR/services/plugins/api/types.ts" "$SCRIPT_DIR/generators/app/templates/api/"
+cp "$SCRIPT_DIR/generators/app/templates/api_index.ts" "$SCRIPT_DIR/generators/app/templates/api/index.ts"
+rm -f "$SCRIPT_DIR/generators/app/templates/api/types.d.ts"
+# rm -f "$SCRIPT_DIR/generators/app/templates/api/index.d.ts"
+
+npm link
+
+"$CLI_DIR/tests/support/plugins/updatePlugins.sh"
+
+git add -A
+git c -m "Updated types"
+git push
