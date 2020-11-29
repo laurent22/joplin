ce5d17c68c02d6009ea8a9426fae72a5cd1aec09
Laurent Cozic <laurent@cozic.net>
2020-11-29 14:41:53 +0000

    fixing search test

--- packages/lib/services/rest/utils/collectionToPaginatedResults.ts
+++ packages/lib/services/rest/utils/collectionToPaginatedResults.ts
@@ -10,6 +10,7 @@ import requestPaginationOptions from '../utils/requestPaginationOptions';
 // building complex SQL queries.
 export default function(items: any[], request: Request): ModelFeedPage {
 	const pagination = requestPaginationOptions(request);
+	console.info('requestPaginationOptions', request, pagination);
 	const startIndex = (pagination.page - 1) * pagination.limit;
 	const itemCount = Math.min(items.length - startIndex, pagination.limit);
 	const hasMore = itemCount >= pagination.limit;
