92fe5e23622fb2f6ba94fc41537afac0d7d64f22
Caleb John <calebjohn4@gmail.com>
2020-12-02 03:36:00 -0700

    Desktop: Fixes #4036: Focus editor after pressing toolbar buttons (#4037)

--- packages/lib/services/commands/ToolbarButtonUtils.ts
+++ packages/lib/services/commands/ToolbarButtonUtils.ts
@@ -33,6 +33,30 @@ export default class ToolbarButtonUtils {
 		return this.service_;
 	}
 
+	// Editor commands will focus the editor after they're executed
+	private isEditorCommand(commandName: string) {
+		return (commandName.indexOf('editor.') === 0 ||
+				// These commands are grandfathered in, but in the future
+				// all editor commands should start with "editor."
+				// WARNING: Some commands such as textLink are not defined here
+				// because they are more complex and handle focus manually
+				commandName === 'textCopy' ||
+				commandName === 'textCut' ||
+				commandName === 'textPaste' ||
+				commandName === 'textSelectAll' ||
+				commandName === 'textBold' ||
+				commandName === 'textItalic' ||
+				commandName === 'textCode' ||
+				commandName === 'attachFile' ||
+				commandName === 'textNumberedList' ||
+				commandName === 'textBulletedList' ||
+				commandName === 'textCheckbox' ||
+				commandName === 'textHeading' ||
+				commandName === 'textHorizontalRule' ||
+				commandName === 'insertDateTime'
+		);
+	}
+
 	private commandToToolbarButton(commandName: string, whenClauseContext: any): ToolbarButtonInfo {
 		const newEnabled = this.service.isEnabled(commandName, whenClauseContext);
 		const newTitle = this.service.title(commandName);
@@ -52,8 +76,11 @@ export default class ToolbarButtonUtils {
 			tooltip: this.service.label(commandName),
 			iconName: command.declaration.iconName,
 			enabled: newEnabled,
-			onClick: () => {
+			onClick: async () => {
 				void this.service.execute(commandName);
+				if (this.isEditorCommand(commandName)) {
+					void this.service.execute('editor.focus');
+				}
 			},
 			title: newTitle,
 		};
