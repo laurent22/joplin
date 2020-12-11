65cc6853bd01bedc057692e9d33b570b492d4ef6
Laurent Cozic <laurent@cozic.net>
2020-12-11 12:00:24 +0000

    Plugins: Added a way to execute commands from Markdown-it content scripts

--- packages/app-cli/tests/support/plugins/content_script/src/markdownItTestPlugin.js
+++ packages/app-cli/tests/support/plugins/content_script/src/markdownItTestPlugin.js
@@ -6,7 +6,13 @@ function plugin(markdownIt, _options) {
 	markdownIt.renderer.rules.fence = function(tokens, idx, options, env, self) {
 		const token = tokens[idx];
 		if (token.info !== 'justtesting') return defaultRender(tokens, idx, options, env, self);
-		return `<div class="just-testing">JUST TESTING: ${token.content}</div>`;
+		return `
+			<div class="just-testing">
+				<p>JUST TESTING: ${token.content}</p>
+				<p><a href="#" onclick="webviewApi.executeCommand('testCommand', 'one', 'two'); return false;">Click to send "testCommand" to plugin</a></p>
+				<p><a href="#" onclick="webviewApi.executeCommand('testCommandNoArgs'); return false;">Click to send "testCommandNoArgs" to plugin</a></p>
+			</div>
+		`;
 	};
 }
 
