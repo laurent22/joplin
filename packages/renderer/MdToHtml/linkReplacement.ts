8492a7ee268ef76fcff99b366d925cb59976d5f2
Roman Musin <musinrr@gmail.com>
2021-01-03 11:23:17 +0000

    Android: Fix images/resources long press - do not show menu on pinch zoom (#4275)

--- packages/renderer/MdToHtml/linkReplacement.ts
+++ packages/renderer/MdToHtml/linkReplacement.ts
@@ -91,7 +91,9 @@ export default function(href: string, options: Options = null): LinkReplacementR
 	if (options.enableLongPress && !!resourceId) {
 		const onClick = `${options.postMessageSyntax}(${JSON.stringify(href)})`;
 		const onLongClick = `${options.postMessageSyntax}("longclick:${resourceId}")`;
-		const touchStart = `t=setTimeout(()=>{t=null; ${onLongClick};}, ${utils.longPressDelay});`;
+		// if t is set when ontouchstart is called it means the user has already touched the screen once and this is the 2nd touch
+		// in this case we assume the user is trying to zoom and we don't want to show the menu
+		const touchStart = `if (typeof(t) !== "undefined" && !!t) { clearTimeout(t); t = null; } else { t = setTimeout(() => { t = null; ${onLongClick}; }, ${utils.longPressDelay}); }`;
 		const cancel = 'if (!!t) {clearTimeout(t); t=null;';
 		const touchEnd = `${cancel} ${onClick};}`;
 		js = `ontouchstart='${touchStart}' ontouchend='${touchEnd}' ontouchcancel='${cancel} ontouchmove="${cancel}'`;
