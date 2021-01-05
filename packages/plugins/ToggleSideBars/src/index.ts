158cf65c6d056c57dfde69741d9d8b9e593e4f2b
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2021-01-05 11:25:05 -0500

    actually toggle something ;-)

--- packages/plugins/ToggleSideBars/src/index.ts
+++ packages/plugins/ToggleSideBars/src/index.ts
@@ -2,6 +2,7 @@ import joplin from 'api';
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Test plugin started!');
+		await joplin.views.toolbarButtons.create('tsb', 'toggleSideBar', 'noteToolbar');
+		await joplin.views.toolbarButtons.create('tnl', 'toggleNoteList', 'noteToolbar');
 	},
 });
