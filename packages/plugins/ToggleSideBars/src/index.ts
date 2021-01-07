8a4c7a58c07155035271173de7daa0ffb5a5c5f1
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2021-01-07 12:06:20 -0500

    make ids a bit longer

--- packages/plugins/ToggleSideBars/src/index.ts
+++ packages/plugins/ToggleSideBars/src/index.ts
@@ -2,7 +2,7 @@ import joplin from 'api';
 
 joplin.plugins.register({
 	onStart: async function() {
-		await joplin.views.toolbarButtons.create('tsb', 'toggleSideBar', 'noteToolbar');
-		await joplin.views.toolbarButtons.create('tnl', 'toggleNoteList', 'noteToolbar');
+		await joplin.views.toolbarButtons.create('toggleSideBarButton', 'toggleSideBar', 'noteToolbar');
+		await joplin.views.toolbarButtons.create('toggleNoteListButton', 'toggleNoteList', 'noteToolbar');
 	},
 });
