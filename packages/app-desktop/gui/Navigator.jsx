49b2eeb134c4bc446da5706be26a2d187e99fcf3
asrient <asen182000@hotmail.com>
2021-01-20 00:35:35 +0530

    updated the title convension, avoided derived state

--- packages/app-desktop/gui/Navigator.jsx
+++ packages/app-desktop/gui/Navigator.jsx
@@ -5,31 +5,65 @@ const { connect } = require('react-redux');
 const bridge = require('electron').remote.require('./bridge').default;
 
 class NavigatorComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.prevTitle = null;
+	}
 	UNSAFE_componentWillReceiveProps(newProps) {
 		if (newProps.route) {
-			const screenInfo = this.props.screens[newProps.route.routeName];
-			const devMarker = Setting.value('env') === 'dev' ? ' (DEV)' : '';
 			const windowTitle = [];
-			if (screenInfo.title) {
-				windowTitle.push(screenInfo.title());
-			} else if (newProps.route.routeName == 'Main' && newProps.currentFolder && newProps.currentNote) {
-				const folderTitle = newProps.currentFolder.title.trim();
-				let noteTitle = newProps.currentNote.title.trim();
-				if (!noteTitle.length) {
-					noteTitle = 'Untitled';
-				}
-				windowTitle.push(`${noteTitle} (${folderTitle})`);
-			}
+			const txt = this.getWindowTitle(newProps);
+			if (txt) { windowTitle.push(txt); }
+			const devMarker = Setting.value('env') === 'dev' ? ' (DEV)' : '';
 			windowTitle.push(`Joplin${devMarker}`);
 			this.updateWindowTitle(windowTitle.join(' - '));
 		}
 	}
-
+	getWindowTitle(newProps) {
+		let currentNote = null;
+		let currentFolder = null;
+		if (newProps.selectedNoteIds.length) {
+			const noteId = newProps.selectedNoteIds[0];
+			const note = newProps.notes.find((_note) => _note.id == noteId);
+			if (note) {
+				currentNote = {
+					id: noteId,
+					title: note.title,
+				};
+			}
+		}
+		if (newProps.selectedFolderId) {
+			const folder = newProps.folders.find((_folder) => _folder.id == newProps.selectedFolderId);
+			if (folder) {
+				currentFolder = {
+					id: newProps.selectedFolderId,
+					title: folder.title,
+				};
+			}
+		}
+		const screenInfo = this.props.screens[newProps.route.routeName];
+		let txt = null;
+		if (screenInfo.title) {
+			txt = screenInfo.title();
+		} else if (newProps.route.routeName == 'Main' && currentFolder && currentNote) {
+			const folderTitle = currentFolder.title.trim();
+			let noteTitle = currentNote.title.trim();
+			if (!noteTitle.length) {
+				noteTitle = 'Untitled';
+			}
+			txt = `${folderTitle} > ${noteTitle}`;
+		}
+		return txt;
+	}
 	updateWindowTitle(title) {
-		try {
-			if (bridge().window()) bridge().window().setTitle(title);
-		} catch (error) {
-			console.warn('updateWindowTitle', error);
+		if (this.prevTitle != title) {
+			console.log('updating title',title);
+			try {
+				if (bridge().window()) bridge().window().setTitle(title);
+				this.prevTitle = title;
+			} catch (error) {
+				console.warn('updateWindowTitle', error);
+			}
 		}
 	}
 
@@ -55,29 +89,12 @@ class NavigatorComponent extends Component {
 }
 
 const Navigator = connect(state => {
-	let currentNote = null;
-	let currentFolder = null;
-	if (state.selectedNoteIds.length) {
-		const noteId = state.selectedNoteIds[0];
-		const note = state.notes.find((_note) => _note.id == noteId);
-		if (note) {
-			currentNote = {
-				id: noteId,
-				title: note.title,
-			};
-		}
-	}
-	if (state.selectedFolderId) {
-		const folder = state.folders.find((_folder) => _folder.id == state.selectedFolderId);
-		if (folder) {
-			currentFolder = {
-				id: state.selectedFolderId,
-				title: folder.title,
-			};
-		}
-	}
 	return {
-		route: state.route, currentNote, currentFolder,
+		route: state.route,
+		selectedNoteIds: state.selectedNoteIds,
+		selectedFolderId: state.selectedFolderId,
+		folders: state.folders,
+		notes: state.notes,
 	};
 })(NavigatorComponent);
 
