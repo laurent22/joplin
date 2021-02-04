7ade9b2948fe21b285a1bfa3887692f463208fd7
Laurent Cozic <laurent@cozic.net>
2021-02-04 21:57:58 +0000

    third user test

--- packages/server/src/routes/api/shares.test.ts
+++ packages/server/src/routes/api/shares.test.ts
@@ -149,12 +149,11 @@ describe('api_shares', function() {
 			cursor2 = page2.cursor;
 		}
 
-		await msleep(1);
-
 		// --------------------------------------------------------------------
 		// If file is changed on sharer side, sharee should see the changes
 		// --------------------------------------------------------------------
 
+		await msleep(1);
 		await updateFile(user1.id, sharerFile.id, 'from sharer');
 
 		{
@@ -176,17 +175,60 @@ describe('api_shares', function() {
 		// If file is changed on sharee side, sharer should see the changes
 		// --------------------------------------------------------------------
 
+		await msleep(1);
+		await updateFile(user2.id, shareeFile.id, 'from sharee');
+
+		{
+			const page1 = await getApi<PaginatedChanges>(session1.id, `files/${rootDirId1}/delta`, { query: { cursor: cursor1 } });
+			expect(page1.items.length).toBe(1);
+			expect(page1.items[0].item.id).toBe(sharerFile.id);
+			expect(page1.items[0].type).toBe(ChangeType.Update);
+			cursor1 = page1.cursor;
+
+			const page2 = await getApi<PaginatedChanges>(session2.id, `files/${rootDirId2}/delta`, { query: { cursor: cursor2 } });
+			expect(page2.items.length).toBe(1);
+			expect(page2.items[0].item.id).toBe(shareeFile.id);
+			expect(page2.items[0].type).toBe(ChangeType.Update);
+			cursor2 = page2.cursor;
+
+			// The updated_time properties don't necessarily match because first
+			// the sharer's file content is updated, and then the sharee's file
+			// metadata may be updated too.
+
+			// expect(page1.items[0].item.updated_time).toBe(page2.items[0].item.updated_time);
+		}
 	});
 
+	test('should see delta changes when a third user joins in', async function() {
+		// - User 1 shares a file with User 2
+		// - User 2 syncs and get a new delta cursor C2
+		// - User 3 shares a file with User 2
+		// - User 2 syncs **starting from C2**
+		// => The new changes from User 3 share should be visible
 
+		const { user: user1, session: session1 } = await createUserAndSession(1);
+		const { user: user2, session: session2 } = await createUserAndSession(2);
+		const { user: user3, session: session3 } = await createUserAndSession(3);
+		const rootDirId2 = await models().file({ userId: user2.id }).userRootFileId();
 
-	// TODO: test delta:
-	// - File is changed on sharer side
-	// - File is changed on sharee side
-	// - A third user shares a file - check delta is correct even when starting from an existing context
+		await shareWithUserAndAccept(session1.id, user1, session2.id, user2);
+		let cursor = null;
 
-	// test('shared file should have the same content', async function() {
+		{
+			const page = await getApi<PaginatedChanges>(session2.id, `files/${rootDirId2}/delta`);
+			cursor = page.cursor;
+		}
 
-	// });
+		const file3 = await createFile(user3.id, 'root:/test3.txt:', 'from user 3');
+		const { shareeFile } = await shareWithUserAndAccept(session3.id, user3, session2.id, user2, file3);
+
+		{
+			const page = await getApi<PaginatedChanges>(session2.id, `files/${rootDirId2}/delta`, { query: { cursor } });
+			cursor = page.cursor;
+			expect(page.items.length).toBe(1);
+			expect(page.items[0].type).toBe(ChangeType.Create);
+			expect(page.items[0].item.id).toBe(shareeFile.id);
+		}
+	});
 
 });
