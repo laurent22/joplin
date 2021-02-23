752c14d7265663e2723dd6e5fffa11db1a586499
James Wright <jrwrigh.iii@gmail.com>
2021-02-23 15:32:01 -0700

    Desktop: Resolves #4554: Improve mathmode syntax highlighting (#4580)

--- packages/app-desktop/gui/NoteEditor/NoteBody/CodeMirror/utils/useJoplinMode.ts
+++ packages/app-desktop/gui/NoteEditor/NoteBody/CodeMirror/utils/useJoplinMode.ts
@@ -20,8 +20,8 @@ export default function useJoplinMode(CodeMirror: any) {
 		const markdownMode = CodeMirror.getMode(config, markdownConfig);
 		const stex = CodeMirror.getMode(config, { name: 'stex', inMathMode: true });
 
-		const inlineKatexOpenRE = /(?<!\S)\$(?=[^\s$].*?[^\\\s$]\$(?!\S))/;
-		const inlineKatexCloseRE = /(?<![\\\s$])\$(?!\S)/;
+		const inlineKatexOpenRE = /(?<!\S)\$(?=[^\s$].*?[^\\\s$]\$(?![^\s!"#%&'()*+,\-.:;<=>?@[\]^_`{|}~]))/;
+		const inlineKatexCloseRE = /(?<![\\\s$])\$(?![^\s!"#%&'()*+,\-.:;<=>?@[\]^_`{|}~])/;
 		const blockKatexOpenRE = /(?<!\S)\$\$/;
 		const blockKatexCloseRE = /(?<![\\\s])\$\$/;
 
