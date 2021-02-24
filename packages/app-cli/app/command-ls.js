3ed7d4a72b610f739f426f01575474b31f68112b
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2021-02-24 01:23:13 -0500

    fix code typo and all source and target translations

--- packages/app-cli/app/command-ls.js
+++ packages/app-cli/app/command-ls.js
@@ -27,7 +27,7 @@ class Command extends BaseCommand {
 			['-n, --limit <num>', _('Displays only the first top <num> notes.')],
 			['-s, --sort <field>', _('Sorts the item by <field> (eg. title, updated_time, created_time).')],
 			['-r, --reverse', _('Reverses the sorting order.')],
-			['-t, --type <type>', _('Displays only the items of the specific type(s). Can be `n` for notes, `t` for to-dos, or `nt` for notes and to-dos (eg. `-tt` would display only the to-dos, while `-ttd` would display notes and to-dos.')],
+			['-t, --type <type>', _('Displays only the items of the specific type(s). Can be `n` for notes, `t` for to-dos, or `nt` for notes and to-dos (eg. `-tt` would display only the to-dos, while `-tnt` would display notes and to-dos.')],
 			['-f, --format <format>', _('Either "text" or "json"')],
 			['-l, --long', _('Use long list format. Format is ID, NOTE_COUNT (for notebook), DATE, TODO_CHECKED (for to-dos), TITLE')],
 		];
