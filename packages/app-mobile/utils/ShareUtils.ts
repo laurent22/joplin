0a24ecb4a89a98a28dce9f7f1ccabb0366525ade
Roman <musinrr@gmail.com>
2021-03-26 13:08:59 +0000

    Use existing method to get resource file name

--- packages/app-mobile/utils/ShareUtils.ts
+++ packages/app-mobile/utils/ShareUtils.ts
@@ -9,13 +9,7 @@ const DIR_NAME = 'sharedFiles';
  * Copy a file to be shared to cache, renaming it to its orignal name
  */
 export async function copyToCache(resource: ResourceEntity): Promise<string> {
-	let filename = resource.filename ?
-		`${resource.filename}.${resource.file_extension}` :
-		resource.title;
-
-	if (!filename) {
-		filename = ['untitled', resource.file_extension].join('.');
-	}
+	const filename = Resource.friendlySafeFilename(resource);
 
 	const targetDir = `${CachesDirectoryPath}/${DIR_NAME}`;
 	await shim.fsDriver().mkdir(targetDir);
