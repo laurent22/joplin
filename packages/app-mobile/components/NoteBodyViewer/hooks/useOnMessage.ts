04b29f5a2fd4073712891f873b95e0f8abd5ba7e
innkuika <innku.ika@gmail.com>
2021-04-01 22:45:15 -0700

    mobile bug fix, url contaning special characters will not break

--- packages/app-mobile/components/NoteBodyViewer/hooks/useOnMessage.ts
+++ packages/app-mobile/components/NoteBodyViewer/hooks/useOnMessage.ts
@@ -5,7 +5,10 @@ export default function useOnMessage(onCheckboxChange: Function, noteBody: strin
 	return useCallback((event: any) => {
 		// Since RN 58 (or 59) messages are now escaped twice???
 		const msg = unescape(unescape(event.nativeEvent.data));
-
+		// Maybe we can use this line instead?
+		// const msg = decodeURI(event.nativeEvent.data);
+		// This doc mentioned that unescape shouldn't be used in most cases, but I'm not sure about the context:
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/unescape
 		console.info('Got IPC message: ', msg);
 
 		if (msg.indexOf('checkboxclick:') === 0) {
@@ -22,7 +25,10 @@ export default function useOnMessage(onCheckboxChange: Function, noteBody: strin
 		} else if (msg.startsWith('error:')) {
 			console.error(`Webview injected script error: ${msg}`);
 		} else {
-			onJoplinLinkClick(msg);
+			// msg is broken at this point if it contained special characters
+			// see: https://github.com/laurent22/joplin/issues/4494
+			const decodedURI = decodeURI(event.nativeEvent.data);
+			onJoplinLinkClick(decodedURI);
 		}
 	}, [onCheckboxChange, noteBody, onMarkForDownload, onJoplinLinkClick, onResourceLongPress]);
 }
