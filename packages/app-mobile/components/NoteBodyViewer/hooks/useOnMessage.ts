e01b8344ee2543002985a290bf2c7953914ed295
innkuika <innku.ika@gmail.com>
2021-04-09 00:49:49 -0700

    removed encoding/decoding & related test cases

--- packages/app-mobile/components/NoteBodyViewer/hooks/useOnMessage.ts
+++ packages/app-mobile/components/NoteBodyViewer/hooks/useOnMessage.ts
@@ -4,11 +4,7 @@ const shared = require('@joplin/lib/components/shared/note-screen-shared');
 export default function useOnMessage(onCheckboxChange: Function, noteBody: string, onMarkForDownload: Function, onJoplinLinkClick: Function, onResourceLongPress: Function) {
 	return useCallback((event: any) => {
 		// Since RN 58 (or 59) messages are now escaped twice???
-		const msg = unescape(unescape(event.nativeEvent.data));
-		// Maybe we can use this line instead?
-		// const msg = decodeURI(event.nativeEvent.data);
-		// This doc mentioned that unescape shouldn't be used in most cases, but I'm not sure about the context:
-		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/unescape
+		let msg = unescape(unescape(event.nativeEvent.data));
 		console.info('Got IPC message: ', msg);
 
 		if (msg.indexOf('checkboxclick:') === 0) {
@@ -28,17 +24,8 @@ export default function useOnMessage(onCheckboxChange: Function, noteBody: strin
 			// msg is broken at this point if it contained special characters
 			// see: https://github.com/laurent22/joplin/issues/4494
 
-			// Had to encode URI before decoding it, since `decodeURI` only decodes URI encoded by `encodeURI`
-			// or by a similar routine. Thus, `decodeURI` is not able to correctly handle special characters
-			// in `event.nativeEvent.data`.
-			const encodedURI = encodeURI(event.nativeEvent.data);
-			try {
-				const decodedURI = decodeURI(encodedURI);
-				onJoplinLinkClick(decodedURI);
-			} catch (error) {
-				// catches a malformed URI
-				console.error('unable to decode URI', error);
-			}
+			msg = event.nativeEvent.data;
+			onJoplinLinkClick(msg);
 		}
 	}, [onCheckboxChange, noteBody, onMarkForDownload, onJoplinLinkClick, onResourceLongPress]);
 }
