016c2fc9ffdb3775d72b9951367379c37d5440f2
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-04-29 10:39:39 +0200

    revert 3be473d because no fts change needed

--- packages/lib/migrations/35.js
+++
@@ -1,9 +0,0 @@
-const SearchEngine = require('../services/searchengine/SearchEngine').default;
-
-const script = {};
-
-script.exec = async function() {
-	await SearchEngine.instance().rebuildIndex();
-};
-
-module.exports = script;
