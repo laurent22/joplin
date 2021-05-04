b5f01c005f48a5560be0d7404825b0e14eda691e
Laurent Cozic <laurent@cozic.net>
2021-05-04 11:05:20 +0200

    linter

--- packages/app-desktop/gui/NoteEditor/NoteBody/CodeMirror/utils/useCursorUtils.test.ts
+++ packages/app-desktop/gui/NoteEditor/NoteBody/CodeMirror/utils/useCursorUtils.test.ts
@@ -1,19 +1,19 @@
 import { modifyListLines } from './useCursorUtils';
 
 describe('useCursorUtils', () => {
-		
-	let listWithDashes = `- item1
+
+	const listWithDashes = `- item1
 - item2
 - item3`;
-	
-	let listNoDashes = `item1
+
+	const listNoDashes = `item1
 item2
 item3`;
-	
+
 	test('should remove "- " from beggining of each line of input string', () => {
 		expect(JSON.stringify(modifyListLines(listWithDashes.split('\n'), 0, '- '))).toBe(JSON.stringify(listNoDashes.split('\n')));
 	});
-	
+
 	test('should add "- " at the beggining of each line of the input string', () => {
 		expect(JSON.stringify(modifyListLines(listNoDashes.split('\n'), 0, '- '))).toBe(JSON.stringify(listWithDashes.split('\n')));
 	});
