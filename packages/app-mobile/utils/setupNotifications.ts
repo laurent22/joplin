9822eefe247d3ebb5ca63c120eef06d4a3840382
Roman <musinrr@gmail.com>
2021-05-18 21:24:27 +0100

    Address code review comments

--- packages/app-mobile/utils/setupNotifications.ts
+++ packages/app-mobile/utils/setupNotifications.ts
@@ -1,30 +1,37 @@
-import { NativeEventEmitter, NativeModules, Platform } from "react-native";
+import { Dispatch } from 'redux';
 
-export default async (dispatch: any) => {
-    if (Platform.OS === 'android') {
-        const { RNAlarmNotification } = NativeModules;
-        const RNAlarmEmitter = new NativeEventEmitter(RNAlarmNotification);
+const { NativeEventEmitter, NativeModules, Platform } = require('react-native');
 
-        const handleNotification = async (notification: any) => {
-            if (notification) {
-                const noteId = notification.noteId;
-                if (noteId) {
-                    dispatch({ type: 'NAV_BACK' });
-                    dispatch({ type: 'SIDE_MENU_CLOSE' });
-                    dispatch({
-                        type: 'NAV_GO',
-                        noteId: noteId,
-                        routeName: 'Note',
-                    });
-                }
-            }				
-        }
-
-        // receive notification click events when the app is running
-        RNAlarmEmitter.addListener('OnNotificationOpened', handleNotification);
-
-        // retirive notification info if the app was started after the user clicked notification
-        const notification = await RNAlarmNotification.getAlarmInfo();
-        await handleNotification(notification);
-    }    
+interface NotificationData {
+    joplinNotificationId: string;
+    noteId: string;
 }
+
+export default async (dispatch: Dispatch) => {
+	if (Platform.OS === 'android') {
+		const RNAlarmNotification = NativeModules.RNAlarmNotification;
+		const RNAlarmEmitter = new NativeEventEmitter(RNAlarmNotification);
+
+		const handleNotification = async (notification: NotificationData) => {
+			if (notification) {
+				const noteId = notification.noteId;
+				if (noteId) {
+					dispatch({ type: 'NAV_BACK' });
+					dispatch({ type: 'SIDE_MENU_CLOSE' });
+					dispatch({
+						type: 'NAV_GO',
+						noteId: noteId,
+						routeName: 'Note',
+					});
+				}
+			}
+		};
+
+		// receive notification click events when the app is running
+		RNAlarmEmitter.addListener('OnNotificationOpened', handleNotification);
+
+		// retrieve notification info if the app was started after the user clicked notification
+		const notification = await RNAlarmNotification.getAlarmInfo();
+		await handleNotification(notification);
+	}
+};
