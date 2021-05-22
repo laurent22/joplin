4760e5e8ba890b62d462a7b534ec72ae0a039c1a
mbalint <magyarb0201@gmail.com>
2021-05-22 19:30:11 +0200

    Desktop: Fixes #4864: Fixes panels overflowing window (#4991)

--- packages/app-desktop/gui/ResizableLayout/utils/useLayoutItemSizes.ts
+++ packages/app-desktop/gui/ResizableLayout/utils/useLayoutItemSizes.ts
@@ -3,6 +3,9 @@ import { LayoutItem, Size } from './types';
 
 const dragBarThickness = 5;
 
+export const itemMinWidth = 40;
+export const itemMinHeight = 40;
+
 export interface LayoutItemSizes {
 	[key: string]: Size;
 }
@@ -17,8 +20,8 @@ export function itemSize(item: LayoutItem, parent: LayoutItem | null, sizes: Lay
 	const bottomGap = !isContainer && (item.resizableBottom || parentResizableBottom) ? dragBarThickness : 0;
 
 	return {
-		width: ('width' in item ? item.width : sizes[item.key].width) - rightGap,
-		height: ('height' in item ? item.height : sizes[item.key].height) - bottomGap,
+		width: sizes[item.key].width - rightGap,
+		height: sizes[item.key].height - bottomGap,
 	};
 }
 
@@ -38,6 +41,10 @@ function calculateChildrenSizes(item: LayoutItem, parent: LayoutItem | null, siz
 	const noWidthChildren: any[] = [];
 	const noHeightChildren: any[] = [];
 
+	// The minimum space required for items with no defined size
+	let noWidthChildrenMinWidth = 0;
+	let noHeightChildrenMinHeight = 0;
+
 	for (const child of item.children) {
 		let w = 'width' in child ? child.width : null;
 		let h = 'height' in child ? child.height : null;
@@ -47,10 +54,43 @@ function calculateChildrenSizes(item: LayoutItem, parent: LayoutItem | null, siz
 		}
 
 		sizes[child.key] = { width: w, height: h };
+
 		if (w !== null) remainingSize.width -= w;
 		if (h !== null) remainingSize.height -= h;
-		if (w === null) noWidthChildren.push({ item: child, parent: item });
-		if (h === null) noHeightChildren.push({ item: child, parent: item });
+		if (w === null) {
+			noWidthChildren.push({ item: child, parent: item });
+			noWidthChildrenMinWidth += child.minWidth || itemMinWidth;
+		}
+		if (h === null) {
+			noHeightChildren.push({ item: child, parent: item });
+			noHeightChildrenMinHeight += child.minHeight || itemMinHeight;
+		}
+	}
+
+	while (remainingSize.width < noWidthChildrenMinWidth) {
+		// There is not enough space, the widest item will be made smaller
+		let widestChild = item.children[0].key;
+		for (const child of item.children) {
+			if (!child.visible) continue;
+			if (sizes[child.key].width > sizes[widestChild].width) widestChild = child.key;
+		}
+
+		const dw = Math.abs(remainingSize.width - noWidthChildrenMinWidth);
+		sizes[widestChild].width -= dw;
+		remainingSize.width += dw;
+	}
+
+	while (remainingSize.height < noHeightChildrenMinHeight) {
+		// There is not enough space, the tallest item will be made smaller
+		let tallestChild = item.children[0].key;
+		for (const child of item.children) {
+			if (!child.visible) continue;
+			if (sizes[child.key].height > sizes[tallestChild].height) tallestChild = child.key;
+		}
+
+		const dh = Math.abs(remainingSize.height - noHeightChildrenMinHeight);
+		sizes[tallestChild].height -= dh;
+		remainingSize.height += dh;
 	}
 
 	if (noWidthChildren.length) {
@@ -75,6 +115,24 @@ function calculateChildrenSizes(item: LayoutItem, parent: LayoutItem | null, siz
 	}
 
 	return sizes;
+}
+
+// Gives the maximum available space for this item that it can take up during resizing
+// availableSize = totalSize - ( [size of items with set size, except for the current item] + [minimum size of items with no set size] )
+export function calculateMaxSizeAvailableForItem(item: LayoutItem, parent: LayoutItem, sizes: LayoutItemSizes): Size {
+	const availableSize: Size = { ...sizes[parent.key] };
+
+	for (const sibling of parent.children) {
+		if (!sibling.visible) continue;
+
+		availableSize.width -= 'width' in sibling ? sizes[sibling.key].width : (sibling.minWidth || itemMinWidth);
+		availableSize.height -= 'height' in sibling ? sizes[sibling.key].height : (sibling.minHeight || itemMinHeight);
+	}
+
+	availableSize.width += sizes[item.key].width;
+	availableSize.height += sizes[item.key].height;
+
+	return availableSize;
 }
 
 export default function useLayoutItemSizes(layout: LayoutItem, makeAllVisible: boolean = false) {
