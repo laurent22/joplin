9b0113978c30e24d52f943d6eff0acc90c364d7a
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2021-06-04 00:27:51 -0400

    add global shortcut to show/hide Joplin

--- packages/app-desktop/ElectronAppWrapper.ts
+++ packages/app-desktop/ElectronAppWrapper.ts
@@ -3,7 +3,7 @@ import { PluginMessage } from './services/plugins/PluginRunner';
 import shim from '@joplin/lib/shim';
 import { isCallbackUrl } from '@joplin/lib/callbackUrlUtils';
 
-const { BrowserWindow, Tray, screen } = require('electron');
+const { BrowserWindow, Tray, screen, globalShortcut } = require('electron');
 const url = require('url');
 const path = require('path');
 const { dirname } = require('@joplin/lib/path-utils');
@@ -347,6 +347,15 @@ export default class ElectronAppWrapper {
 		return false;
 	}
 
+	toggleWindowVisilibity() {
+		if (this.win_.isVisible()) {
+			this.win_.hide();
+		} else {
+			this.win_.setVisibleOnAllWorkspaces(true);
+			this.win_.show();
+		}
+	}
+
 	async start() {
 		// Since we are doing other async things before creating the window, we might miss
 		// the "ready" event. So we use the function below to make sure that the app is ready.
@@ -357,8 +366,17 @@ export default class ElectronAppWrapper {
 
 		this.createWindow();
 
+		const registerGlobalShortcut = globalShortcut.register('CommandOrControl+Alt+J', () => {
+			this.toggleWindowVisilibity();
+		});
+
+		if (!registerGlobalShortcut) {
+			console.warn('Could not register global shortcut');
+		}
+
 		this.electronApp_.on('before-quit', () => {
 			this.willQuitApp_ = true;
+			globalShortcut.unregisterAll();
 		});
 
 		this.electronApp_.on('window-all-closed', () => {
