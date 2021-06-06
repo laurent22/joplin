3819ddc7f41ce3babec1363219c5ebc39a451249
Ahmed <ahmad.gasser@gmail.com>
2021-06-06 23:38:01 +0200

    Reset conflict_parent_id when moving or copying notes out of conflict folder.

--- packages/lib/models/Note.ts
+++ packages/lib/models/Note.ts
@@ -523,6 +523,7 @@ export default class Note extends BaseItem {
 			changes: {
 				parent_id: folderId,
 				is_conflict: 0, // Also reset the conflict flag in case we're moving the note out of the conflict folder
+				conflict_parent_id: '', // Reset parent id as well.
 			},
 		});
 	}
@@ -537,6 +538,7 @@ export default class Note extends BaseItem {
 			id: noteId,
 			parent_id: folderId,
 			is_conflict: 0,
+			conflict_parent_id: '',
 			updated_time: time.unixMs(),
 		};
 
