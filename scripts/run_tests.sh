7b81317d934c329fac1252bff0388366f992e632
Laurent Cozic <laurent@cozic.net>
2021-06-10 00:32:05 +0200

    test ci

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,6 +4,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "$SCRIPT_DIR/.."
 
+echo "Node $( node -v )"
+echo "Npm $( npm -v )"
+
 npm install
 
 npm run test-ci
