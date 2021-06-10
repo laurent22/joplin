f825452189cc3e3de0c3b139d57a171fb39b3018
Laurent Cozic <laurent@cozic.net>
2021-06-10 11:55:54 +0200

    test ci

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+echo "GITHUB_WORKFLOW=$GITHUB_WORKFLOW"
+echo "GITHUB_EVENT_NAME=$GITHUB_EVENT_NAME"
+
 cd "$SCRIPT_DIR/.."
 
 echo "Node $( node -v )"
