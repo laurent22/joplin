7c92015fe6b22c425169b7126fa5902d303f88a4
Laurent Cozic <laurent@cozic.net>
2021-06-21 19:30:20 +0100

    Desktop: Fixes #4983: Fixed readability of links in notification banners

--- packages/app-desktop/gui/MainScreen/MainScreen.tsx
+++ packages/app-desktop/gui/MainScreen/MainScreen.tsx
@@ -506,6 +506,30 @@ class MainScreenComponent extends React.Component<Props, State> {
 		return this.styles_;
 	}
 
+	private renderNotificationMessage(message: string, callForAction: string, callForActionHandler: Function, callForAction2: string = null, callForActionHandler2: Function = null) {
+		const theme = themeStyle(this.props.themeId);
+		const urlStyle: any = { color: theme.colorWarnUrl, textDecoration: 'underline' };
+
+		const cfa = (
+			<a href="#" style={urlStyle} onClick={() => callForActionHandler()}>
+				{callForAction}
+			</a>
+		);
+
+		const cfa2 = !callForAction2 ? null : (
+			<a href="#" style={urlStyle} onClick={() => callForActionHandler2()}>
+				{callForAction2}
+			</a>
+		);
+
+		return (
+			<span>
+				{message}{callForAction ? ' ' : ''}
+				{cfa}{callForAction2 ? ' / ' : ''}{cfa2}
+			</span>
+		);
+	}
+
 	renderNotification(theme: any, styles: any) {
 		if (!this.messageBoxVisible()) return null;
 
@@ -547,83 +571,57 @@ class MainScreenComponent extends React.Component<Props, State> {
 		let msg = null;
 
 		if (this.props.isSafeMode) {
-			msg = (
-				<span>
-					{_('Safe mode is currently active. Note rendering and all plugins are temporarily disabled.')}{' '}
-					<a href="#" onClick={() => onDisableSafeModeAndRestart()}>
-						{_('Disable safe mode and restart')}
-					</a>
-				</span>
+			msg = this.renderNotificationMessage(
+				_('Safe mode is currently active. Note rendering and all plugins are temporarily disabled.'),
+				_('Disable safe mode and restart'),
+				onDisableSafeModeAndRestart
 			);
 		} else if (this.props.shouldUpgradeSyncTarget) {
-			msg = (
-				<span>
-					{_('The sync target needs to be upgraded before Joplin can sync. The operation may take a few minutes to complete and the app needs to be restarted. To proceed please click on the link.')}{' '}
-					<a href="#" onClick={() => onRestartAndUpgrade()}>
-						{_('Restart and upgrade')}
-					</a>
-				</span>
+			msg = this.renderNotificationMessage(
+				_('The sync target needs to be upgraded before Joplin can sync. The operation may take a few minutes to complete and the app needs to be restarted. To proceed please click on the link.'),
+				_('Restart and upgrade'),
+				onRestartAndUpgrade
 			);
 		} else if (this.props.hasDisabledEncryptionItems) {
-			msg = (
-				<span>
-					{_('Some items cannot be decrypted.')}{' '}
-					<a href="#" onClick={() => onViewStatusScreen()}>
-						{_('View them now')}
-					</a>
-				</span>
+			msg = this.renderNotificationMessage(
+				_('Some items cannot be decrypted.'),
+				_('View them now'),
+				onViewStatusScreen
 			);
 		} else if (this.props.showNeedUpgradingMasterKeyMessage) {
-			msg = (
-				<span>
-					{_('One of your master keys use an obsolete encryption method.')}{' '}
-					<a href="#" onClick={() => onViewEncryptionConfigScreen()}>
-						{_('View them now')}
-					</a>
-				</span>
+			msg = this.renderNotificationMessage(
+				_('One of your master keys use an obsolete encryption method.'),
+				_('View them now'),
+				onViewEncryptionConfigScreen
 			);
 		} else if (this.props.showShouldReencryptMessage) {
-			msg = (
-				<span>
-					{_('The default encryption method has been changed, you should re-encrypt your data.')}{' '}
-					<a href="#" onClick={() => onViewEncryptionConfigScreen()}>
-						{_('More info')}
-					</a>
-				</span>
+			msg = this.renderNotificationMessage(
+				_('The default encryption method has been changed, you should re-encrypt your data.'),
+				_('More info'),
+				onViewEncryptionConfigScreen
 			);
 		} else if (this.showShareInvitationNotification(this.props)) {
 			const invitation = this.props.shareInvitations[0];
 			const sharer = invitation.share.user;
 
-			msg = (
-				<span>
-					{_('%s (%s) would like to share a notebook with you.', sharer.full_name, sharer.email)}{' '}
-					<a href="#" onClick={() => onInvitationRespond(invitation.id, true)}>
-						{_('Accept')}
-					</a>
-					{' / '}
-					<a href="#" onClick={() => onInvitationRespond(invitation.id,true)}>
-						{_('Reject')}
-					</a>
-				</span>
+			msg = this.renderNotificationMessage(
+				_('%s (%s) would like to share a notebook with you.', sharer.full_name, sharer.email),
+				_('Accept'),
+				() => onInvitationRespond(invitation.id, true),
+				_('Reject'),
+				() => onInvitationRespond(invitation.id, false)
 			);
 		} else if (this.props.hasDisabledSyncItems) {
-			msg = (
-				<span>
-					{_('Some items cannot be synchronised.')}{' '}
-					<a href="#" onClick={() => onViewStatusScreen()}>
-						{_('View them now')}
-					</a>
-				</span>
+			msg = this.renderNotificationMessage(
+				_('Some items cannot be synchronised.'),
+				_('View them now'),
+				onViewStatusScreen
 			);
 		} else if (this.props.showMissingMasterKeyMessage) {
-			msg = (
-				<span>
-					{_('One or more master keys need a password.')}{' '}
-					<a href="#" onClick={() => onViewEncryptionConfigScreen()}>
-						{_('Set the password')}
-					</a>
-				</span>
+			msg = this.renderNotificationMessage(
+				_('One or more master keys need a password.'),
+				_('Set the password'),
+				onViewEncryptionConfigScreen
 			);
 		}
 
