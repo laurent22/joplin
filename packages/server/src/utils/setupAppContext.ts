238cc86590a7b2ef50d7b796f5c188dc6c694d7d
Laurent Cozic <laurent@cozic.net>
2021-07-03 22:08:50 +0100

    Server: Fixed issue with user sessions being mixed up

--- packages/server/src/utils/setupAppContext.ts
+++ packages/server/src/utils/setupAppContext.ts
@@ -27,7 +27,9 @@ export default async function(appContext: AppContext, env: Env, dbConnection: Db
 	const models = newModelFactory(dbConnection, config());
 
 	appContext.joplin = {
-		...appContext.joplin,
+		owner: null,
+		account: null,
+		notifications: [],
 		env: env,
 		db: dbConnection,
 		models: models,
