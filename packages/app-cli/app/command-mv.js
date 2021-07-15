074c3cb216b03414a00b4eb4456e0a6bf7ed1f45
Kai <kai@edgesix.com>
2021-07-15 09:07:02 +0000

    MV command update

--- packages/app-cli/app/command-mv.js
+++ packages/app-cli/app/command-mv.js
@@ -7,25 +7,38 @@ const Note = require('@joplin/lib/models/Note').default;
 
 class Command extends BaseCommand {
 	usage() {
-		return 'mv <note> [notebook]';
+		return 'mv <note_or_notebook> [notebook]';
 	}
 
 	description() {
-		return _('Moves the notes matching <note> to [notebook].');
+		return _('Moves the notes matching <note_or_notebook> or single notebook to [notebook].');
 	}
 
 	async action(args) {
-		const pattern = args['note'];
+		const pattern = args['note_or_notebook'];
 		const destination = args['notebook'];
 
-		const folder = await Folder.loadByField('title', destination);
-		if (!folder) throw new Error(_('Cannot find "%s".', destination));
+		// TODO: allow partial ids
+		let folder = await Folder.loadByField('id', destination);
+		if (!folder) {
+			folder = await Folder.loadByField('title', destination);
+			if (!folder) throw new Error(_('Cannot find "%s".', destination));
+		}
 
-		const notes = await app().loadItems(BaseModel.TYPE_NOTE, pattern);
-		if (!notes.length) throw new Error(_('Cannot find "%s".', pattern));
-
-		for (let i = 0; i < notes.length; i++) {
-			await Note.moveToFolder(notes[i].id, folder.id);
+		let srcFolder = await Folder.loadByField('id', pattern);
+		if (!srcFolder) {
+			srcFolder = await Folder.loadByField('title', pattern);
+		}
+		if (srcFolder) {
+			// ../../lib/models/Folder.ts
+			// moveToFolder(folderId: string, targetFolderId: string)
+			await Folder.moveToFolder(srcFolder.id, folder.id);
+		} else {
+			const notes = await app().loadItems(BaseModel.TYPE_NOTE, pattern);
+			if (!notes.length) throw new Error(_('Cannot find "%s".', pattern));
+			for (let i = 0; i < notes.length; i++) {
+				await Note.moveToFolder(notes[i].id, folder.id);
+			}
 		}
 	}
 }
