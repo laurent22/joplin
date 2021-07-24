db0f9a5f54f5f7c5aab61c72be856e4c54c97d32
Kai <kai@edgesix.com>
2021-07-24 04:56:28 +0000

    MV command update - second attempt

--- packages/app-cli/app/command-mv.js
+++ packages/app-cli/app/command-mv.js
@@ -7,32 +7,39 @@ const Note = require('@joplin/lib/models/Note').default;
 
 class Command extends BaseCommand {
 	usage() {
-		return 'mv <note_or_notebook> [notebook]';
+		return 'mv <item> [notebook]';
 	}
 
 	description() {
-		return _('Moves the notes matching <note_or_notebook> or single notebook to [notebook].');
+		return _('Moves the given <item> (notes matching pattern in current notebook or one notebook) to [notebook]. If <item> is subnotebook and [notebook] is "root", will make <item> parent notebook');
 	}
 
 	async action(args) {
-		const pattern = args['note_or_notebook'];
+		const pattern = args['item'];
 		const destination = args['notebook'];
+		let folder = null;
 
-		// TODO: allow partial ids
-		let folder = await Folder.loadByField('id', destination);
-		if (!folder) {
-			folder = await Folder.loadByField('title', destination);
+		if (destination != 'root') {
+			folder = await app().loadItem(BaseModel.TYPE_FOLDER, destination);
 			if (!folder) throw new Error(_('Cannot find "%s".', destination));
 		}
 
-		let srcFolder = await Folder.loadByField('id', pattern);
-		if (!srcFolder) {
-			srcFolder = await Folder.loadByField('title', pattern);
+		const dstDups = await Folder.search({ titlePattern: destination, limit: 2 });
+		if (dstDups.length > 1) {
+			throw new Error(_('Ambiguous notebook "%s". Please use notebook id instead - look for parent id in metadata', destination));
 		}
-		if (srcFolder) {
-			// ../../lib/models/Folder.ts
-			// moveToFolder(folderId: string, targetFolderId: string)
-			await Folder.moveToFolder(srcFolder.id, folder.id);
+
+		const itemFolder = await app().loadItem(BaseModel.TYPE_FOLDER, pattern);
+		if (itemFolder) {
+			const srcDups = await Folder.search({ titlePattern: pattern, limit: 2 });
+			if (srcDups.length > 1) {
+				throw new Error(_('Ambiguous notebook "%s". Please use notebook id instead - look for parent id in metadata or use $b for current', pattern));
+			}
+			if (destination == 'root') {
+				await Folder.moveToFolder(itemFolder.id, '');
+			} else {
+				await Folder.moveToFolder(itemFolder.id, folder.id);
+			}
 		} else {
 			const notes = await app().loadItems(BaseModel.TYPE_NOTE, pattern);
 			if (!notes.length) throw new Error(_('Cannot find "%s".', pattern));
