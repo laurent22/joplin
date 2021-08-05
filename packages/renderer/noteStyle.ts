d3cd843fc8409e2c1353b4835bdfd3cc1c5d64d8
Laurent Cozic <laurent@cozic.net>
2021-08-05 12:50:41 +0100

    Desktop: Fixed header spacing (regression)

--- packages/renderer/noteStyle.ts
+++ packages/renderer/noteStyle.ts
@@ -87,6 +87,11 @@ export default function(theme: any) {
 			*/
 			unicode-bidi: plaintext;
 		}
+
+		h1, h2, h3, h4, h5, h6, ul, table {
+			margin-bottom: 0.65em;
+		}
+
 		h1, h2, h3, h4, h5, h6 {
 			line-height: 1.5em;
 		}
