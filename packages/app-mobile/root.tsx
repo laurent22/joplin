66e79ccb2385ee808185a601231933b8279d3b0f
Laurent Cozic <laurent@cozic.net>
2021-11-02 16:33:53 +0000

    Mobile: Upgraded React Native from 0.64 to 0.66

--- packages/app-mobile/root.tsx
+++ packages/app-mobile/root.tsx
@@ -505,9 +505,9 @@ async function initialize(dispatch: Function) {
 			Setting.setValue('sync.10.path', 'http://api.joplincloud.local:22300');
 			Setting.setValue('sync.10.userContentPath', 'http://joplinusercontent.local:22300');
 
-			Setting.setValue('sync.target', 10);
-			Setting.setValue('sync.10.username', 'user1@example.com');
-			Setting.setValue('sync.10.password', 'hunter1hunter2hunter3');
+			// Setting.setValue('sync.target', 10);
+			// Setting.setValue('sync.10.username', 'user1@example.com');
+			// Setting.setValue('sync.10.password', 'hunter1hunter2hunter3');
 		}
 
 		if (Setting.value('db.ftsEnabled') === -1) {
