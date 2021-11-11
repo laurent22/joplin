73137cfb270d5e5ffaf69841d38104ae5c26d8a4
Laurent Cozic <laurent@cozic.net>
2021-11-11 16:12:46 +0000

    Server: Hide credentials from the log

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -66,7 +66,7 @@ function markPasswords(o: Record<string, any>): Record<string, any> {
 	const output: Record<string, any> = {};
 
 	for (const k of Object.keys(o)) {
-		if (k.toLowerCase().includes('password')) {
+		if (k.toLowerCase().includes('password') || k.toLowerCase().includes('secret')) {
 			output[k] = '********';
 		} else {
 			output[k] = o[k];
