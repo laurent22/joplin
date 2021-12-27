c42355172327240d6103796650e7a040d3fa2027
Laurent Cozic <laurent@cozic.net>
2021-12-27 18:10:07 +0100

    Plugins: Added ModelType type

--- packages/lib/services/plugins/api/types.ts
+++ packages/lib/services/plugins/api/types.ts
@@ -1,5 +1,3 @@
-export { ModelType } from '../../../BaseModel';
-
 // =================================================================
 // Command API types
 // =================================================================
@@ -202,6 +200,25 @@ export interface Script {
 
 export interface Disposable {
 	// dispose():void;
+}
+
+export enum ModelType {
+	Note = 1,
+	Folder = 2,
+	Setting = 3,
+	Resource = 4,
+	Tag = 5,
+	NoteTag = 6,
+	Search = 7,
+	Alarm = 8,
+	MasterKey = 9,
+	ItemChange = 10,
+	NoteResource = 11,
+	ResourceLocalState = 12,
+	Revision = 13,
+	Migration = 14,
+	SmartFilter = 15,
+	Command = 16,
 }
 
 // =================================================================
