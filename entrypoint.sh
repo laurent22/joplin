8e4e1cacf7fff97ce2382cf99dcf5c5898f056b8
ifurther <55025025+ifurther@users.noreply.github.com>
2022-02-07 18:21:01 +0800

    remove dockerfile volume and add change uid/gid function

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,18 @@
 set -Eeo pipefail
 
 if [[ "$*" == node*dist/app.js* ]] && [ "$(id -u)" = '0' ]; then
+	USER_UID=${USER_UID:-1002}
+	USER_GID=${USER_GID:-1002}
+	Current_id=$(id -u)
+	Current_group_id=$(id -g)
+	if [ "$USER_UID" != "$Current_id" ]; then
+		groupmod -o -g "$USER_UID" openldap
+		echo 'user uid is changed'
+	fi
+	if [ "$USER_UID" != "$Current_group_id" ]; then
+		groupmod -o -g "$USER_UID" openldap
+		echo 'user gid is changed'
+	fi
 	find $Path \! -user USER -exec chown USER '{}' +
 	exec /usr/bin/tini -- /usr/sbin/gosu USER:USER "$@"
 fi
