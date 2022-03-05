4e7eb4c5f8d9a3468fbe4fdb47f7150129753a9c
asrient <asen182000@hotmail.com>
2022-03-06 02:29:07 +0530

    desktop context menu update

--- packages/app-desktop/gui/NoteEditor/utils/contextMenu.test.ts
+++ packages/app-desktop/gui/NoteEditor/utils/contextMenu.test.ts
@@ -1,4 +1,4 @@
-import { resourceInfo, ContextMenuOptions, ContextMenuItemType, svgUriToPng } from './contextMenuUtils';
+import { textToDataUri, ContextMenuOptions, ContextMenuItemType, svgUriToPng } from './contextMenuUtils';
 
 jest.mock('@joplin/lib/models/Resource');
 
@@ -22,8 +22,7 @@ describe('contextMenu', () => {
 
 		for (const testCase of testCases) {
 			const [inputObj, expectedText] = testCase;
-			const { getCopyPath } = await resourceInfo(inputObj);
-			expect(getCopyPath()).toBe(expectedText);
+			expect(textToDataUri(inputObj.textToCopy, inputObj.mime)).toBe(expectedText);
 		}
 	});
 
@@ -35,6 +34,17 @@ describe('contextMenu', () => {
 		for (const testCase of testCases) {
 			const png = await svgUriToPng(document, testCase);
 			expect(png).toBeInstanceOf(Uint8Array);
+		}
+	});
+
+	it('should throw error on invalid svg uri', async () => {
+		const testCases: Array<string> = [
+			'data:image/svg+xml;base64,error',
+			'invalid',
+		];
+
+		for (const testCase of testCases) {
+			expect(svgUriToPng(document, testCase)).rejects.toBeInstanceOf(Error);
 		}
 	});
 });
