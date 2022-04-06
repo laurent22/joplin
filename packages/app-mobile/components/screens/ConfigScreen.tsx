4cc2b54312ddb25353e9b44d5e84e44ae3c5dcb4
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-04-06 14:07:50 +0100

    Android: Fixes #6026: Long path in Export profile prevents tapping OK button

--- packages/app-mobile/components/screens/ConfigScreen.tsx
+++ packages/app-mobile/components/screens/ConfigScreen.tsx
@@ -531,7 +531,7 @@ class ConfigScreenComponent extends BaseScreenComponent {
 				const profileExportPrompt = (
 					<View style={this.styles().settingContainer} key="profileExport">
 						<Text style={{ ...this.styles().settingText, flex: 0 }}>Path:</Text>
-						<TextInput style={{ ...this.styles().textInput, paddingRight: 20, width: '75%' }} onChange={(event: any) => this.setState({ profileExportPath: event.nativeEvent.text })} value={this.state.profileExportPath} placeholder="/path/to/sdcard" keyboardAppearance={theme.keyboardAppearance} />
+						<TextInput style={{ ...this.styles().textInput, paddingRight: 20, width: '75%', marginRight: 'auto' }} onChange={(event: any) => this.setState({ profileExportPath: event.nativeEvent.text })} value={this.state.profileExportPath} placeholder="/path/to/sdcard" keyboardAppearance={theme.keyboardAppearance} />
 						<Button title="OK" onPress={this.exportProfileButtonPress2_} />
 					</View>
 				);
