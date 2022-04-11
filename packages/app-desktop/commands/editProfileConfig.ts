f6cd9889393fb0a30b539c14bd91f5bee2131379
Laurent Cozic <laurent@cozic.net>
2022-04-11 16:36:23 +0100

    disable edition if no multi profiles

--- packages/app-desktop/commands/editProfileConfig.ts
+++ packages/app-desktop/commands/editProfileConfig.ts
@@ -14,5 +14,6 @@ export const runtime = (): CommandRuntime => {
 		execute: async (_context: CommandContext) => {
 			await openFileWithExternalEditor(`${Setting.value('rootProfileDir')}/profiles.json`, bridge());
 		},
+		enabledCondition: 'hasMultiProfiles',
 	};
 };
