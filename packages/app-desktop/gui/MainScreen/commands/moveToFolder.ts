a301f30657a91e837bfd36cd65ee2592062d06e1
Bishoy <Bishoy.Magdy.Adeeb@gmail.com>
2022-04-12 19:23:04 +0200

    Update moveToFolder.ts

--- packages/app-desktop/gui/MainScreen/commands/moveToFolder.ts
+++ packages/app-desktop/gui/MainScreen/commands/moveToFolder.ts
@@ -8,31 +8,40 @@ export const declaration: CommandDeclaration = {
 	label: () => _('Move to notebook'),
 };
 
+export class AddOptions {
+
+	public startFolders: any[] = [];
+	private maxDepth = 15;
+
+	public addOptions(folders: any[], depth: number, path: string) {
+		for (let i = 0; i < folders.length; i++) {
+			const folder = folders[i];
+			// When NoteBook doesn't get a title
+			if (folder.title === '') {
+				folder.title = 'Untitled';
+			}
+			const new_path = path + folder.title;
+			this.startFolders.push({ key: folder.id, value: folder.id, label: folder.title, indentDepth: depth, path: new_path, val: folder.title });
+			if (folder.children) this.addOptions(folder.children, (depth + 1) < this.maxDepth ? depth + 1 : this.maxDepth, `${new_path}/`);
+		}
+	}
+}
+
 export const runtime = (comp: any): CommandRuntime => {
 	return {
 		execute: async (context: CommandContext, noteIds: string[] = null) => {
 			noteIds = noteIds || context.state.selectedNoteIds;
 
 			const folders: any[] = await Folder.sortFolderTree();
-			const startFolders: any[] = [];
-			const maxDepth = 15;
-
-			const addOptions = (folders: any[], depth: number) => {
-				for (let i = 0; i < folders.length; i++) {
-					const folder = folders[i];
-					startFolders.push({ key: folder.id, value: folder.id, label: folder.title, indentDepth: depth });
-					if (folder.children) addOptions(folder.children, (depth + 1) < maxDepth ? depth + 1 : maxDepth);
-				}
-			};
-
-			addOptions(folders, 0);
+			const options = new AddOptions();
+			options.addOptions(folders, 0, '');
 
 			comp.setState({
 				promptOptions: {
 					label: _('Move to notebook:'),
 					inputType: 'dropdown',
 					value: '',
-					autocomplete: startFolders,
+					autocomplete: options.startFolders,
 					onClose: async (answer: any) => {
 						if (answer) {
 							for (let i = 0; i < noteIds.length; i++) {
