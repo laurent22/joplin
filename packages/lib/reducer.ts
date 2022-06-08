40bc63e7ea535f1519d9b3aec23145706d6e0b71
Kenichi Kobayashi <ken1kob@users.noreply.github.com>
2022-06-08 18:34:08 +0900

    Suppress redundant NoteEditor re-rendering by removing non-changing updates of state.selectedNoteTags (#6470)

--- packages/lib/reducer.ts
+++ packages/lib/reducer.ts
@@ -1125,7 +1125,9 @@ const reducer = produce((draft: Draft<State> = defaultState, action: any) => {
 			break;
 
 		case 'SET_NOTE_TAGS':
-			draft.selectedNoteTags = action.items;
+			if (!fastDeepEqual(original(draft.selectedNoteTags), action.items)) {
+				draft.selectedNoteTags = action.items;
+			}
 			break;
 
 		case 'PLUGINLEGACY_DIALOG_SET':
