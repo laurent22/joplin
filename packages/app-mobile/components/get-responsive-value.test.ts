deed5539056d7d0afa6efdaa0cd03ca1350e1a06
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-06-17 17:01:50 +0100

    Write and test getResponsiveValue() part 2

--- packages/app-mobile/components/get-responsive-value.test.ts
+++ packages/app-mobile/components/get-responsive-value.test.ts
@@ -1,5 +1,16 @@
 import getResponsiveValue from './get-responsive-value';
 
+jest.mock('react-native', () => {
+	return {
+		__esModule: true,
+		Dimensions: {
+			get: jest.fn(() => {
+				return { width: 400 };
+			}),
+		},
+	};
+});
+
 describe('getResponsiveValue', () => {
 	test('Should return undefined if argument is an empty array', () => {
 		const result = getResponsiveValue([]);
