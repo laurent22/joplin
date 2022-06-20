8553087ea24ad923167439353b977006bc43d558
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-06-20 12:27:34 +0100

    Write and test getResponsiveValue() part 3

--- packages/app-mobile/components/get-responsive-value.ts
+++ packages/app-mobile/components/get-responsive-value.ts
@@ -6,7 +6,7 @@
 // md: >= 481
 // lg: >= 769px
 // xl: >= 1025px
-// xxl: >= 1200px
+// xxl: >= 1201px
 //
 // Eg. [ 10, 15, 20, 25, 30 ] means { sm: 10, md: 15, lg: 20, xl: 25, xxl: 30 }
 // [10] and [10, 15] are equivalent to { sm: 10 } and { sm: 10, md: 15 } respectively
@@ -23,10 +23,27 @@ interface ValueMap {
 
 export default function getResponsiveValue(valueMap: number[] | ValueMap): number {
 	const width = Dimensions.get('screen').width;
+	let value: number;
+	let sm: number,
+		md: number;
 
-	if (Array.isArray(valueMap)) { if (valueMap.length === 0) return undefined; }
+	if (Array.isArray(valueMap)) {
+		if (valueMap.length === 0) {
+			return undefined;
+		}
 
-	if (Object.keys(valueMap).length === 0) { return undefined; }
+		[sm, md] = valueMap;
+	} else if (Object.keys(valueMap).length === 0) {
+		return undefined;
+	} else {
+		({ sm, md } = valueMap as ValueMap);
+	}
 
-	return width;
+	value = sm;
+
+	if (width >= 481 && !!md) {
+		value = md;
+	}
+
+	return value;
 }
