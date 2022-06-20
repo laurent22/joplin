fb4b84522d3000a1f732235ff1e66afd26d1bc2b
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-06-20 12:27:34 +0100

    Write and test getResponsiveValue() part 3

--- packages/app-mobile/components/get-responsive-value.test.ts
+++ packages/app-mobile/components/get-responsive-value.test.ts
@@ -1,6 +1,23 @@
-const getResponsiveValue = require('./get-responsive-value').default;
+import getResponsiveValue from './get-responsive-value';
+import { Dimensions } from 'react-native';
 
 describe('getResponsiveValue', () => {
+	beforeEach(() => {
+		Dimensions.get = jest.fn()
+			.mockReturnValueOnce({ width: 400 })
+			.mockReturnValueOnce({ width: 480 })
+			.mockReturnValueOnce({ width: 481 })
+			.mockReturnValueOnce({ width: 600 })
+			.mockReturnValueOnce({ width: 768 })
+			.mockReturnValueOnce({ width: 769 })
+			.mockReturnValueOnce({ width: 900 })
+			.mockReturnValueOnce({ width: 1024 })
+			.mockReturnValueOnce({ width: 1025 })
+			.mockReturnValueOnce({ width: 1200 })
+			.mockReturnValueOnce({ width: 1201 })
+			.mockReturnValueOnce({ width: 1300 });
+	});
+
 	test('Should return undefined if argument is an empty array', () => {
 		const input: number[] = [];
 		const result = getResponsiveValue(input);
@@ -13,18 +30,58 @@ describe('getResponsiveValue', () => {
 		expect(result).toBe(undefined);
 	});
 
-	// test('Should use mocked Dimensions.get() value', () => {
-	// 	const result = getResponsiveValue([20]);
-	// 	expect(result).toBe(50);
-	// });
-
-	test('Should return correct value when only sm value is specified', () => {
+	test('Should return correct value when only sm value is specified (Object Syntax)', () => {
+		const correcOutput = 40;
 		const input = { sm: 40 };
 		let result: number;
 
-		for (let i = 0; i < 10; i++) {
-			result = getResponsiveValue(1);
-			expect(result).toBe(input.sm);
+		for (let i = 0; i < 12; i++) {
+			result = getResponsiveValue(input);
+			expect(result).toBe(correcOutput);
 		}
 	});
+
+	test('Should return correct value when only sm value is specified (Array Syntax)', () => {
+		const correcOutput = 40;
+		const input = [40];
+		let result: number;
+
+		for (let i = 0; i < 12; i++) {
+			result = getResponsiveValue(input);
+			expect(result).toBe(correcOutput);
+		}
+	});
+
+	test('Should return correct value when sm and md values are specified (Object Syntax)', () => {
+		const input = { sm: 40, md: 70 };
+		const correctOutputs = [40, 40, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70];
+		let result: number;
+
+		for (let i = 0; i < 12; i++) {
+			result = getResponsiveValue(input);
+			expect(result).toBe(correctOutputs[i]);
+		}
+	});
+
+	test('Should return correct value when sm and md values are specified (Array Syntax)', () => {
+		const input = [40, 70];
+		const correctOutputs = [40, 40, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70];
+		let result: number;
+
+		for (let i = 0; i < 12; i++) {
+			result = getResponsiveValue(input);
+			expect(result).toBe(correctOutputs[i]);
+		}
+	});
+
+	// test('Should return correct value when sm, md and lg values are specified (Object Syntax)', () => {
+	// 	const input = { sm: 40, md: 70, lg: 90 };
+	// 	const correctOutputs = [40, 40, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70];
+	// 	let result: number;
+
+	// 	for(let i = 0; i < 12; i++){
+	// 		result = getResponsiveValue(input);
+	// 		expect(result).toBe(correctOutputs[i]);
+	// 	}
+	// });
 });
