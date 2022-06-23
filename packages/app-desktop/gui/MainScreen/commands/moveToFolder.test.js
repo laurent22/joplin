df71ce550539e7466270f84bc6524a98b9318f3e
Bishoy <Bishoy.Magdy.Adeeb@gmail.com>
2022-06-23 07:02:19 +0200

    Update moveToFolder.test.js

--- packages/app-desktop/gui/MainScreen/commands/moveToFolder.test.js
+++ packages/app-desktop/gui/MainScreen/commands/moveToFolder.test.js
@@ -9,17 +9,33 @@ describe('AddOptions: Test The Path of Folders', () => {
 		options.startFolders = [];
 	});
 
-	it('Empty Notebook Title', () => {
-		const testCases = [{ id: 1, title: '' }];
+	it('Empty Folder Title', () => {
+		const testCases = [{
+			id: 1, title: '',
+			children: [{
+				id: 2, title: '',
+			}],
+		}];
 
 		options.addOptions(testCases, 0, '');
 		const data = options.startFolders;
-		expect(data[0].path).toEqual('Untitled');
-
+		expect(data[0].path).toEqual('');
 	});
 
-	it('Diffrent Paths', () => {
-		const testCases = [{ id: 8, title: 'my Note' }, { id: 2, title: 'first', children: [{ id: 3, title: 'second', children: [{ id: 4, title: 'third', children: [{ id: 5, title: 'fourth' }] }, { id: 6, title: 'fourth' }] }, { id: 7, title: 'fourth' }] }];
+	it('Different Paths', () => {
+		const testCases = [
+			{ id: 8, title: 'my Note' },
+			{
+				id: 2, title: 'first',
+				children: [{
+					id: 3, title: 'second',
+					children: [{
+						id: 4, title: 'third',
+						children: [{ id: 5, title: 'fourth' }],
+					}, { id: 6, title: 'fourth' }],
+				}, { id: 7, title: 'fourth' }],
+			}];
+
 		const answers = ['my Note', 'first', 'first/second', 'first/second/third', 'first/second/third/fourth', 'first/second/fourth', 'first/fourth'];
 
 		options.addOptions(testCases, 0, '');
@@ -31,7 +47,49 @@ describe('AddOptions: Test The Path of Folders', () => {
 	});
 
 	it('Long Path', () => {
-		const testCases = [{ id: 1, title: '1', children: [{ id: 2, title: '2', children: [{ id: 3, title: '3', children: [{ id: 4, title: '4', children: [{ id: 5, title: '5', children: [{ id: 6, title: '6', children: [{ id: 7, title: '7', children: [{ id: 8, title: '8', children: [{ id: 9, title: '9', children: [{ id: 10, title: '10', children: [{ id: 11, title: '11', children: [{ id: 12, title: '12', children: [{ id: 13, title: '13', children: [{ id: 14, title: '14', children: [{ id: 15, title: '15' }] }] }] }] }] }] }] }] }] }] }] }] }] }, { id: 16, title: '16' }] }];
+		const testCases = [{
+			id: 1, title: '1',
+			children: [{
+				id: 2, title: '2',
+				children: [{
+					id: 3, title: '3',
+					children: [{
+						id: 4, title: '4',
+						children: [{
+							id: 5, title: '5',
+							children: [{
+								id: 6, title: '6',
+								children: [{
+									id: 7, title: '7',
+									children: [{
+										id: 8, title: '8',
+										children: [{
+											id: 9, title: '9',
+											children: [{
+												id: 10, title: '10',
+												children: [{
+													id: 11, title: '11',
+													children: [{
+														id: 12, title: '12',
+														children: [{
+															id: 13, title: '13',
+															children: [{
+																id: 14, title: '14',
+																children: [{ id: 15, title: '15' }],
+															}],
+														}],
+													}],
+												}],
+											}],
+										}],
+									}],
+								}],
+							}],
+						}],
+					}],
+				}],
+			}, { id: 16, title: '16' }],
+		}];
 
 		options.addOptions(testCases, 0, '');
 		const data = options.startFolders;
