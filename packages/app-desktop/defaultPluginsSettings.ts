35363936750171b33452bac6c0f7550c0898dad8
mak2002 <makbond0902@gmail.com>
2022-06-29 23:10:35 +0530

    removed code for mkdir command

--- packages/app-desktop/defaultPluginsSettings.ts
+++ packages/app-desktop/defaultPluginsSettings.ts
@@ -1,7 +1,4 @@
 import Setting from '@joplin/lib/models/Setting';
-import shim from '@joplin/lib/shim';
-import path = require('path');
-
 interface SettingAndValue {
     [settingName: string]: string;
 }
@@ -10,24 +7,13 @@ interface InitialSettings {
     [pluginId: string]: SettingAndValue;
 }
 
-const makeBackupDir = async (backupDir: string) => {
-	if (!await shim.fsDriver().exists(backupDir)) {
-		await shim.fsDriver().mkdir(backupDir);
-	}
-};
-
 const initialSettings: InitialSettings = {
 	'io.github.jackgruber.backup': {
-		'password': '1234',
-		'passwordRepeat': '1234',
+		'path': '/JoplinBackup',
 	},
 };
 
 export default async function setSettingsForDefaultPlugins() {
-
-	const backupDir = path.join(`${Setting.value('profileDir')}`, '/testing-backup/');
-	await makeBackupDir(backupDir);
-	Setting.setValue('plugin-io.github.jackgruber.backup.path', backupDir);
 
 	Object.keys(initialSettings).forEach(pluginId => {
 		Object.keys(initialSettings[pluginId]).forEach((setting) => {
