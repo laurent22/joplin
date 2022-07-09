62b8cf53cde032a89e16fdf7361f285e6a0432e5
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-07-09 18:10:10 +0100

    Modify method in react-native-side-menu
    react-native-side-menu had an issue where it wasn't calculating
    the side menu's width properly, when a device's width changes
    , as a result the side menu wasn't rendering consistently with
    the openMenuOffset values being passed. To fix this, I overrided
    a method, with a slightly different version. The sidemenu now
    renders consistently when a device's width cahnges.

--- packages/app-mobile/components/sideMenu.ts
+++ packages/app-mobile/components/sideMenu.ts
@@ -1,7 +1,25 @@
 const { connect } = require('react-redux');
 const SideMenu_ = require('react-native-side-menu-updated').default;
+import { Dimensions } from 'react-native';
 
-class SideMenuComponent extends SideMenu_ {}
+type Event = {
+	nativeEvent: {
+		layout: {
+			width: number;
+			height: number;
+		};
+	};
+};
+
+class SideMenuComponent extends SideMenu_ {
+	onLayoutChange(e: Event) {
+		const { width, height } = e.nativeEvent.layout;
+		const openMenuOffsetPercentage = this.props.openMenuOffset / Dimensions.get('window').width;
+		const openMenuOffset = width * openMenuOffsetPercentage;
+		const hiddenMenuOffset = width * this.state.hiddenMenuOffsetPercentage;
+		this.setState({ width, height, openMenuOffset, hiddenMenuOffset });
+	}
+}
 
 const SideMenu = connect((state: any) => {
 	return {
