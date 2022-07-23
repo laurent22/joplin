13ecb8606f3ac1277aeeafd0f43b774512db2dd3
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-07-23 15:18:10 +0100

    Add notesbar 2

--- packages/app-mobile/components/NotesBar.tsx
+++ packages/app-mobile/components/NotesBar.tsx
@@ -1,9 +1,9 @@
 const React = require('react');
-const { View, Text, StyleSheet } = require('react-native');
+const { View, Text, StyleSheet, TextInput } = require('react-native');
 import { State } from '@joplin/lib/reducer';
 import { themeStyle } from './global-style';
 const { connect } = require('react-redux');
-// const Icon = require('react-native-vector-icons/Ionicons').default;
+const Icon = require('react-native-vector-icons/Ionicons').default;
 
 interface Props {
     themeId: string;
@@ -18,7 +18,58 @@ function NotesBarComponent(props: Props) {
 		let styles = {
 			container: {
 				width: 250,
-				backgroundColor: theme.backgroundColor3,
+				backgroundColor: theme.tableBackgroundColor,
+			},
+			horizontalFlex: {
+				flexDirection: 'row',
+			},
+			title: {
+				alignItems: 'center',
+			},
+			titleText: {
+				fontSize: 16,
+			},
+			closeIcon: {
+				fontSize: 32,
+			},
+			top: {
+				color: theme.color,
+			},
+			topContainer: {
+				width: '100%',
+				justifyContent: 'space-between',
+
+			},
+			padding: {
+				paddingLeft: theme.marginLeft,
+				paddingRight: theme.marginRight,
+				paddingTop: 12,
+				paddingBottom: 12,
+			},
+			titleIcon: {
+				fontSize: 22,
+				marginRight: 4,
+			},
+			divider: {
+				backgroundColor: theme.dividerColor,
+				height: 1,
+				width: '100%',
+			},
+			nativeInput: {
+				fontSize: theme.fontSize,
+				flex: 1,
+			},
+			searchIcon: {
+				fontSize: 22,
+			},
+			searchInput: {
+				alignItems: 'center',
+				backgroundColor: theme.backgroundColor,
+				paddingLeft: 8,
+				paddingRight: 8,
+				borderRadius: 4,
+				borderWidth: 1.5,
+				borderColor: theme.dividerColor,
 			},
 
 		};
@@ -28,10 +79,45 @@ function NotesBarComponent(props: Props) {
 		return styles;
 	}
 
+	const titleComp = (
+		<View style={[styles().title, styles().horizontalFlex]}>
+			<Icon name='md-document'style={[styles().top, styles().titleIcon]} />
+			<Text style={[styles().top, styles().titleText]}>Notes</Text>
+		</View>
+	);
+
+	const dividerComp = (
+		<View style={styles().divider}></View>
+	);
+
+	const topComp = (
+		<View>
+			<View style={[styles().topContainer, styles().horizontalFlex, styles().padding]}>
+				{titleComp}
+				<Icon name="close" style={[styles().top, styles().closeIcon]}/>
+			</View>
+			{dividerComp}
+		</View>
+	);
+
+	const searchInputComp = (
+		<View style={[styles().horizontalFlex, styles().searchInput]}>
+			<Icon name='search' style={[styles().top, styles().searchIcon]}/>
+			<TextInput style={styles().nativeInput} placeholder='Search' />
+		</View>
+	);
+
+	const inputGroupComp = (
+		<View style={styles().padding}>
+			{searchInputComp}
+		</View>
+	);
+
 
 	return (
 		<View style={styles().container}>
-			<Text style={{ color: '#fff' }}>Sample Code</Text>
+			{topComp}
+			{inputGroupComp}
 		</View>
 	);
 }
