2cfeb67073432645556d46e657ad3d2497bb90c9
Henry Heino <hheino@uw.edu>
2022-08-12 03:03:47 -0700

    Additional info in coloris.ts

--- packages/app-mobile/__mocks__/coloris.ts
+++ packages/app-mobile/__mocks__/coloris.ts
@@ -1,6 +1,6 @@
 
-// @melloware/coloris tries to use the HTML5Canvas on import. As such,
-// we need to mock it.
+// @melloware/coloris tries to use the HTML5Canvas on import, which is not supported by
+// JSDom (without installing additional packages). As such, we need to mock it.
 
 export const coloris = (_options: any): void => {
 };
