baadcd3acf7a87018ecc12e732f4482cbd74e893
Henry Heino <henryuheino@gmail.com>
2022-08-27 07:51:05 -0700

    Mobile: Tests: Force syntax tree generation after creating editor
    This fixes a race condition where some markdown commands that rely on
    the syntax tree's existence ran before the syntax tree was fully
    generated.

--- packages/app-mobile/components/NoteEditor/CodeMirror/createEditor.ts
+++ packages/app-mobile/components/NoteEditor/CodeMirror/createEditor.ts
@@ -1,13 +1,13 @@
 import { markdown } from '@codemirror/lang-markdown';
 import { GFM as GithubFlavoredMarkdownExt } from '@lezer/markdown';
-import { indentUnit } from '@codemirror/language';
+import { forceParsing, indentUnit } from '@codemirror/language';
 import { SelectionRange, EditorSelection, EditorState } from '@codemirror/state';
 import { EditorView } from '@codemirror/view';
 import { MarkdownMathExtension } from './markdownMathParser';
 
 // Creates and returns a minimal editor with markdown extensions
 const createEditor = (initialText: string, initialSelection: SelectionRange): EditorView => {
-	return new EditorView({
+	const editor = new EditorView({
 		doc: initialText,
 		selection: EditorSelection.create([initialSelection]),
 		extensions: [
@@ -18,6 +18,9 @@ const createEditor = (initialText: string, initialSelection: SelectionRange): Ed
 			EditorState.tabSize.of(4),
 		],
 	});
+
+	forceParsing(editor);
+	return editor;
 };
 
 export default createEditor;
