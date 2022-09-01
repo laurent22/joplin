0fd4a09c0fe774ffb05d993612d8c9507275b04d
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2022-09-01 11:16:05 +0100

    Fix notesbar toggle button bug
    Co-authored-by: Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>

--- packages/app-mobile/components/screens/Note.tsx
+++ packages/app-mobile/components/screens/Note.tsx
@@ -1414,7 +1414,11 @@ class NoteScreenComponent extends BaseScreenComponent {
 		// The note actions need to be draggable, because they could
 		// potentially obstruct some portion of a note's content
 		this.noteActionsDragResponder = PanResponder.create({
-			onMoveShouldSetPanResponder: () => true,
+			// Only start dragging after moving at least 10px — this prevents clicks from dragging instead
+			// of triggering onPress events
+			onMoveShouldSetPanResponder: (_evt, gestureState) => {
+				return Math.abs(gestureState.dx) > 10 || Math.abs(gestureState.dy) > 10;
+			},
 			onPanResponderMove: (_e: any, gestureState: any) => {
 				handleNoteActionsDrag(gestureState);
 			},
