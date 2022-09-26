9fa1b28ddbf6a579d1087ee8d7cd887ff56b1c26
wcyat <wcyat@wcyat.me>
2022-09-26 20:45:17 +0800

    Desktop: enable toolbar horizontal scroll only when child has a longer width than parent

--- packages/app-desktop/gui/ToolbarBase.tsx
+++ packages/app-desktop/gui/ToolbarBase.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import ToolbarButton from './ToolbarButton/ToolbarButton';
-import ToggleEditorsButton, { Value } from './ToggleEditorsButton/ToggleEditorsButton';
-const React = require('react');
-const { connect } = require('react-redux');
-const { themeStyle } = require('@joplin/lib/theme');
+import ToggleEditorsButton, {
+	Value,
+} from './ToggleEditorsButton/ToggleEditorsButton';
+import React = require('react');
+import { connect } from 'react-redux';
+import { themeStyle } from '@joplin/lib/theme';
 const ToolbarSpace = require('./ToolbarSpace.min.js');
 const HorizontalScroll = require('react-scroll-horizontal');
 
@@ -12,12 +15,39 @@ interface Props {
 	items: any[];
 }
 
-class ToolbarBaseComponent extends React.Component<Props, any> {
+function ToolbarBaseComponent(Props: Props) {
+	const parentRef = React.useRef<HTMLDivElement>(null);
+	const childRef = React.useRef<HTMLDivElement>(null);
+	const [horizScroll, setHorizScroll] = React.useState(false);
 
-	render() {
-		const theme = themeStyle(this.props.themeId);
+	React.useEffect(() => {
+		if (childRef.current?.clientWidth > parentRef.current?.clientWidth) {
+			setHorizScroll(true);
+		}
+		window?.addEventListener('resize', () => {
+			if (
+				childRef.current?.clientWidth > parentRef.current?.clientWidth
+			) {
+				setHorizScroll(true);
+			} else {
+				setHorizScroll(false);
+				if (!childRef.current) {
+					setTimeout(() => {
+						if (
+							childRef.current?.clientWidth >
+							parentRef.current?.clientWidth
+						) {
+							setHorizScroll(true);
+						}
+					}, 0);
+				}
+			}
+		});
+	}, []);
 
-		const style: any = Object.assign({
+	const theme = themeStyle(Props.themeId);
+	const style: any = Object.assign(
+		{
 			display: 'flex',
 			flexDirection: 'row',
 			boxSizing: 'border-box',
@@ -25,68 +55,75 @@ class ToolbarBaseComponent extends React.Component<Props, any> {
 			padding: theme.toolbarPadding,
 			paddingRight: theme.mainPadding,
 			overflow: 'hidden',
-		}, this.props.style);
-
-		const groupStyle: any = {
-			display: 'flex',
-			flexDirection: 'row',
-			boxSizing: 'border-box',
-		};
-
-		const leftItemComps: any[] = [];
-		const centerItemComps: any[] = [];
-		const rightItemComps: any[] = [];
-
-		if (this.props.items) {
-			for (let i = 0; i < this.props.items.length; i++) {
-				const o = this.props.items[i];
-				let key = o.iconName ? o.iconName : '';
-				key += o.title ? o.title : '';
-				key += o.name ? o.name : '';
-				const itemType = !('type' in o) ? 'button' : o.type;
-
-				if (!key) key = `${o.type}_${i}`;
-
-				const props = Object.assign(
-					{
-						key: key,
-						themeId: this.props.themeId,
-					},
-					o
-				);
-
-				if (o.name === 'toggleEditors') {
-					rightItemComps.push(<ToggleEditorsButton
+		},
+		Props.style
+	);
+	const groupStyle: any = {
+		display: 'flex',
+		flexDirection: 'row',
+		boxSizing: 'border-box',
+	};
+	const leftItemComps: any[] = [];
+	const centerItemComps: any[] = [];
+	const rightItemComps: any[] = [];
+	if (Props.items) {
+		for (let i = 0; i < Props.items.length; i++) {
+			const o = Props.items[i];
+			let key = o.iconName ? o.iconName : '';
+			key += o.title ? o.title : '';
+			key += o.name ? o.name : '';
+			const itemType = !('type' in o) ? 'button' : o.type;
+			if (!key) key = `${o.type}_${i}`;
+			const props = Object.assign(
+				{
+					key: key,
+					themeId: Props.themeId,
+				},
+				o
+			);
+			if (o.name === 'toggleEditors') {
+				rightItemComps.push(
+					<ToggleEditorsButton
 						key={o.name}
 						value={Value.Markdown}
-						themeId={this.props.themeId}
+						themeId={Props.themeId}
 						toolbarButtonInfo={o}
-					/>);
-				} else if (itemType === 'button') {
-					const target = ['historyForward', 'historyBackward', 'toggleExternalEditing'].includes(o.name) ? leftItemComps : centerItemComps;
-					target.push(<ToolbarButton {...props} />);
-				} else if (itemType === 'separator') {
-					centerItemComps.push(<ToolbarSpace {...props} />);
-				}
+					/>
+				);
+			} else if (itemType === 'button') {
+				const target = [
+					'historyForward',
+					'historyBackward',
+					'toggleExternalEditing',
+				].includes(o.name)
+					? leftItemComps
+					: centerItemComps;
+				target.push(<ToolbarButton {...props} />);
+			} else if (itemType === 'separator') {
+				centerItemComps.push(<ToolbarSpace {...props} />);
 			}
 		}
-
-		return (
-			<div className="editor-toolbar" style={style}>
-				<HorizontalScroll reverseScroll>
-					<div style={groupStyle}>
-						{leftItemComps}
-					</div>
-					<div style={groupStyle}>
-						{centerItemComps}
-					</div>
-					<div style={Object.assign({}, groupStyle, { flex: 1, justifyContent: 'flex-end' })}>
-						{rightItemComps}
-					</div>
-				</HorizontalScroll>
-			</div>
-		);
 	}
+	return (
+		<div className="editor-toolbar" ref={parentRef} style={style}>
+			{React.createElement(horizScroll ? HorizontalScroll : 'div', {
+				...(horizScroll ? { reverseScroll: true } : { ref: childRef }),
+				style: { display: 'flex' },
+				children: [
+					<div style={groupStyle}>{leftItemComps}</div>,
+					<div style={groupStyle}>{centerItemComps}</div>,
+					<div
+						style={Object.assign({}, groupStyle, {
+							flex: 1,
+							justifyContent: 'flex-end',
+						})}
+					>
+						{rightItemComps}
+					</div>,
+				],
+			})}
+		</div>
+	);
 }
 
 const mapStateToProps = (state: any) => {
