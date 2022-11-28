6ece7f4c1b83e6fe46df6b89a95f8b7f53ba5f27
Laurent Cozic <laurent@cozic.net>
2022-11-28 18:01:00 +0100

    Desktop: Fixes #7354: Open callback URLs from within the application

--- packages/app-desktop/gui/MainScreen/commands/openItem.ts
+++ packages/app-desktop/gui/MainScreen/commands/openItem.ts
@@ -16,8 +16,13 @@ export const runtime = (): CommandRuntime => {
 			if (!link) throw new Error('Link cannot be empty');
 
 			if (link.startsWith('joplin://') || link.startsWith(':/')) {
-				const { itemId, hash } = parseResourceUrl(link);
-				await openItemById(itemId, context.dispatch, hash);
+				const parsedUrl = parseResourceUrl(link);
+				if (parsedUrl) {
+					const { itemId, hash } = parsedUrl;
+					await openItemById(itemId, context.dispatch, hash);
+				} else {
+					void require('electron').shell.openExternal(link);
+				}
 			} else if (urlProtocol(link)) {
 				if (link.indexOf('file://') === 0) {
 					// When using the file:// protocol, openPath doesn't work (does
