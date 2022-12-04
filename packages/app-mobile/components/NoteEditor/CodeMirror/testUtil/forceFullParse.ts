b3affb70335fffc9530a731875e0697b8af46f47
Henry Heino <personalizedrefrigerator@gmail.com>
2022-12-04 13:41:42 -0800

    Attempt to work around failing test. Call ensureSyntaxTree at least once
    Attempts to work around https://github.com/laurent22/joplin/actions/runs/3614992406/jobs/6091799117#step:12:688

--- packages/app-mobile/components/NoteEditor/CodeMirror/testUtil/forceFullParse.ts
+++ packages/app-mobile/components/NoteEditor/CodeMirror/testUtil/forceFullParse.ts
@@ -4,10 +4,13 @@ import { EditorState } from '@codemirror/state';
 // Forces a full parse of a CodeMirror editor. This is intended for unit testing.
 // If not in a unit-test consider using ensureSyntaxTree or forceParsing.
 const forceFullParse = (editorState: EditorState) => {
-	while (!syntaxTreeAvailable(editorState)) {
+	// Call ensureSyntaxTree at least once. Not doing so seems to occasionally
+	// result in an outdated syntax tree being used.
+	do {
 		const timeout = 100; // ms
 		ensureSyntaxTree(editorState, editorState.doc.length, timeout);
 	}
+	while (!syntaxTreeAvailable(editorState));
 };
 
 export default forceFullParse;
