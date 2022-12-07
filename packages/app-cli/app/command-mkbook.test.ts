69edb20207de589cbe890505741b909fd74edf0b
k33pn3xtlvl <a.lifinzew@gmx.de>
2022-12-08 00:51:00 +0100

    Rework of the tests

--- packages/app-cli/app/command-mkbook.test.ts
+++ packages/app-cli/app/command-mkbook.test.ts
@@ -1,5 +1,6 @@
 import { setupDatabaseAndSynchronizer, switchClient } from '@joplin/lib/testing/test-utils';
 import { setupCommandForTesting, setupApplication } from './utils/testUtils';
+import Folder from '@joplin/lib/models/Folder';
 const Command = require('./command-mkbook');
 
 
@@ -15,20 +16,41 @@ describe('command-mkbook', function() {
 	it('should create a subfolder in first folder', async () => {
 		const command = setupCommandForTesting(Command);
 		await expect(command.action({ 'new-notebook': 'folder1' })).resolves.not.toThrowError();
-		await expect(command.action({ 'new-notebook': 'folder1.1', options: { s: true, sub: true } })).resolves.not.toThrowError();
+		await expect(command.action({ 'new-notebook': 'folder1_1' , options: { s: true, sub: true } })).resolves.not.toThrowError();
+
+		const folder1 = await Folder.loadByTitle('folder1');
+		const folder1_1 = await Folder.loadByTitle('folder1_1');
+
+		expect(folder1.title).toBe('folder1');
+		expect(folder1_1.parent_id).toBe(folder1.id);
 	});
 
 	it('should create a subfolder in the second destination folder', async () => {
 		const command = setupCommandForTesting(Command);
 		await expect(command.action({ 'new-notebook': 'folder2' })).resolves.not.toThrowError();
-		await expect(command.action({ 'new-notebook': 'folder2.1', 'notebook': 'folder2' })).resolves.not.toThrowError();
+		await expect(command.action({ 'new-notebook': 'folder2_1', 'notebook': 'folder2' })).resolves.not.toThrowError();
+
+		const folder2 = await Folder.loadByTitle('folder2');
+		const folder2_1 = await Folder.loadByTitle('folder2_1');
+
+		expect(folder2.title).toBe('folder2');
+		expect(folder2_1.parent_id).toBe(folder2.id);
 	});
 
 	it('should not be possible to create subfolder in ambiguous destination folder', async () => {
 		const command = setupCommandForTesting(Command);
 		await expect(command.action({ 'new-notebook': 'folder3' })).resolves.not.toThrowError();
 		await expect(command.action({ 'new-notebook': 'folder3' })).resolves.not.toThrowError();	// ambiguous folder
-		await expect(command.action({ 'new-notebook': 'folder3.1', 'notebook': 'folder3' })).rejects.toThrowError();
+		await expect(command.action({ 'new-notebook': 'folder3_1', 'notebook': 'folder3' })).rejects.toThrowError();
+
+		// check if duplicate entries have been created.
+		const folderAll = await Folder.all();
+		const folders3 = folderAll.filter(x => x.title === 'folder3');
+		expect(folders3.length).toBeGreaterThanOrEqual(2);
+
+		// check if something has been created in one of the duplicate entries.
+		expect(await Folder.childrenIds(folders3[0].id)).toEqual([]);
+		expect(await Folder.childrenIds(folders3[1].id)).toEqual([]);
 	});
 });
 
