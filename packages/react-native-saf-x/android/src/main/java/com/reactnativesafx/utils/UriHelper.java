7a3dfc7ed27cb9cb584c108b8480eb0186293d3c
Javad Mnjd <javad.mnjd@hotmail.com>
2023-01-19 23:48:57 +0330

    feat: improve saf-x module performance

--- packages/react-native-saf-x/android/src/main/java/com/reactnativesafx/utils/UriHelper.java
+++ packages/react-native-saf-x/android/src/main/java/com/reactnativesafx/utils/UriHelper.java
@@ -2,61 +2,67 @@ package com.reactnativesafx.utils;
 
 import android.content.ContentResolver;
 import android.net.Uri;
-import android.os.Build.VERSION_CODES;
+import android.os.Build;
+import android.provider.DocumentsContract;
 
 import androidx.annotation.RequiresApi;
 
-import java.util.regex.Pattern;
-
-@RequiresApi(api = VERSION_CODES.Q)
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class UriHelper {
-  public static final String CONTENT_URI_PREFIX = "content://";
-  public static final Pattern DOCUMENT_TREE_PREFIX =
-      Pattern.compile("^content://.*?/tree/.+?", Pattern.CASE_INSENSITIVE);
 
-  public static String getLastSegment(String uriString) {
+  public static String getFileName(String uriStr) {
+    // it should be safe because user cannot select sd root or primary root
+    // and any other path would have at least one '/' to provide a file name in a folder
+    String fileName = Uri.parse(Uri.decode(uriStr)).getLastPathSegment();
+    if (fileName.indexOf(':') != -1) {
+      throw new RuntimeException(
+        "Invalid file name: Could not extract filename from uri string provided");
+    }
 
-    return Uri.parse(Uri.decode(uriString)).getLastPathSegment();
-  }
-
-  public static boolean isContentDocumentTreeUri(String uriString) {
-    return DOCUMENT_TREE_PREFIX.matcher(uriString).matches();
+    return fileName;
   }
 
   public static String normalize(String uriString) {
-    if (isContentDocumentTreeUri(uriString)) {
+    return normalize(Uri.parse(uriString));
+  }
+
+  public static String normalize(Uri uri) {
+    if (DocumentsContract.isTreeUri(uri)) {
       // an abnormal uri example:
       // content://com.android.externalstorage.documents/tree/1707-3F0B%3Ajoplin/locks/2_2_fa4f9801e9a545a58f1a6c5d3a7cfded.json
       // normalized:
       // content://com.android.externalstorage.documents/tree/1707-3F0B%3Ajoplin%2Flocks%2F2_2_fa4f9801e9a545a58f1a6c5d3a7cfded.json
 
       // uri parts:
-      String[] parts = Uri.decode(uriString).split(":");
+      String[] parts = Uri.decode(uri.toString()).split(":");
       return parts[0] + ":" + parts[1] + Uri.encode(":" + parts[2]);
     }
-    return uriString;
+    return uri.toString();
   }
 
-  public static String denormalize(String uriString) {
-    if (isContentDocumentTreeUri(uriString)) {
+  public static String denormalize(Uri uri) {
+    if (DocumentsContract.isTreeUri(uri)) {
       // an normalized uri example:
       // content://com.android.externalstorage.documents/tree/1707-3F0B%3Ajoplin%2Flocks%2F2_2_fa4f9801e9a545a58f1a6c5d3a7cfded.json
       // denormalized:
       // content://com.android.externalstorage.documents/tree/1707-3F0B/Ajoplin/locks/2_2_fa4f9801e9a545a58f1a6c5d3a7cfded.json
 
-      return Uri.decode(normalize(uriString));
-    }
-    return uriString;
-  }
-
-  public static String getUnifiedUri(String uriString) throws IllegalArgumentException {
-    Uri uri = Uri.parse(uriString);
-    if (uri.getScheme() == null) {
-      uri = Uri.parse(ContentResolver.SCHEME_FILE + "://" + uriString);
-    } else if (!(uri.getScheme().equals(ContentResolver.SCHEME_FILE)
-        || uri.getScheme().equals(ContentResolver.SCHEME_CONTENT))) {
-      throw new IllegalArgumentException("Invalid Uri: Scheme not supported");
+      return Uri.decode(normalize(uri));
     }
     return uri.toString();
+  }
+
+  public static Uri getUnifiedUri(String unknownUriStr) {
+    if (unknownUriStr == null || unknownUriStr.equals("")) {
+      throw new IllegalArgumentException("Invalid Uri: No input was given");
+    }
+    Uri uri = Uri.parse(unknownUriStr);
+    if (uri.getScheme() == null) {
+      uri = Uri.parse(ContentResolver.SCHEME_FILE + "://" + unknownUriStr);
+    } else if (!(uri.getScheme().equals(ContentResolver.SCHEME_FILE)
+      || uri.getScheme().equals(ContentResolver.SCHEME_CONTENT))) {
+      throw new IllegalArgumentException("Invalid Uri: Scheme not supported");
+    }
+    return uri;
   }
 }
