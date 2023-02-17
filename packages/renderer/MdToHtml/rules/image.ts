057ac550bd57e9e6f752f63afb865279dbdd6879
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2023-02-17 05:13:28 -0800

    Chore: Renderer: Refactor and test long-press and click handlers (#7774)

--- packages/renderer/MdToHtml/rules/image.ts
+++ packages/renderer/MdToHtml/rules/image.ts
@@ -1,6 +1,7 @@
 import { RuleOptions } from '../../MdToHtml';
 import htmlUtils from '../../htmlUtils';
 import utils from '../../utils';
+import createEventHandlingAttrs from '../createEventHandlingAttrs';
 
 function plugin(markdownIt: any, ruleOptions: RuleOptions) {
 	const defaultRender = markdownIt.renderer.rules.image;
@@ -17,19 +18,14 @@ function plugin(markdownIt: any, ruleOptions: RuleOptions) {
 		const r = utils.imageReplacement(ruleOptions.ResourceModel, src, ruleOptions.resources, ruleOptions.resourceBaseUrl, ruleOptions.itemIdToUrl);
 		if (typeof r === 'string') return r;
 		if (r) {
-			let js = '';
-			if (ruleOptions.enableLongPress) {
-				const id = r['data-resource-id'];
+			const id = r['data-resource-id'];
 
-				const longPressHandler = `${ruleOptions.postMessageSyntax}('longclick:${id}')`;
-				// if t is set when ontouchstart is called it means the user has already touched the screen once and this is the 2nd touch
-				// in this case we assume the user is trying to zoom and we don't want to show the menu
-				const touchStart = `if (typeof(t) !== 'undefined' && !!t) { clearTimeout(t); t = null; } else { t = setTimeout(() => { t = null; ${longPressHandler}; }, ${utils.longPressDelay}); }`;
-				const cancel = 'if (!!t) clearTimeout(t); t=null';
+			const js = createEventHandlingAttrs(id, {
+				enableLongPress: ruleOptions.enableLongPress ?? false,
+				postMessageSyntax: ruleOptions.postMessageSyntax ?? 'void',
+			}, null);
 
-				js = ` ontouchstart="${touchStart}" ontouchend="${cancel}" ontouchcancel="${cancel}" ontouchmove="${cancel}"`;
-			}
-			return `<img data-from-md ${htmlUtils.attributesHtml(Object.assign({}, r, { title: title, alt: token.content }))}${js}/>`;
+			return `<img data-from-md ${htmlUtils.attributesHtml(Object.assign({}, r, { title: title, alt: token.content }))} ${js}/>`;
 		}
 		return defaultRender(tokens, idx, options, env, self);
 	};
