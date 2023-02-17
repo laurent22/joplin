2b5e70ae69234758c6587cd1a3ce357bef0e6924
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-02-17 21:56:09 +0200

    For #7766: use new notification icon

--- packages/app-mobile/services/AlarmServiceDriver.android.ts
+++ packages/app-mobile/services/AlarmServiceDriver.android.ts
@@ -53,7 +53,7 @@ export default class AlarmServiceDriver {
 			title: notification.title,
 			message: notification.body ? notification.body : '-', // Required
 			channel: 'net.cozic.joplin.notification',
-			small_icon: 'ic_launcher_foreground', // Android requires the icon to be transparent
+			small_icon: 'ic_notification', // Android requires the icon to be transparent
 			color: 'blue',
 			data: {
 				joplinNotificationId: notification.id,
