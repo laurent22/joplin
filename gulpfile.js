170295919affa5729e127bf3d52aad1fa1ea13ab
Laurent Cozic <laurent@cozic.net>
2023-02-26 12:17:17 +0000

    CI: Fixed publishall

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,6 @@ const tasks = {
 	buildCommandIndex: require('./packages/tools/gulp/tasks/buildCommandIndex'),
 	completePublishAll: {
 		fn: async () => {
-
-			await utils.execCommandVerbose('git', ['add', '-A']);
-			await utils.execCommandVerbose('git', ['commit', '-m', 'Releasing sub-packages']);
-
 			// Lerna does some unnecessary auth check that doesn't work with
 			// automation tokens, thus the --no-verify-access. Automation token
 			// is still used for access when publishing even with this flag
@@ -17,6 +13,9 @@ const tasks = {
 			// https://github.com/lerna/lerna/issues/2788
 			await utils.execCommandVerbose('lerna', ['publish', 'from-package', '-y', '--no-verify-access']);
 
+			await utils.execCommandVerbose('yarn', ['install']);
+			await utils.execCommandVerbose('git', ['add', '-A']);
+			await utils.execCommandVerbose('git', ['commit', '-m', 'Releasing sub-packages']);
 			await utils.execCommandVerbose('git', ['push']);
 		},
 	},
