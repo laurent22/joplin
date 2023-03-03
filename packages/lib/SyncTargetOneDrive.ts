a1ac3b7f2ce8d0befe12403df7875cacedaa2bb1
Self Not Found <wh201906@yandex.com>
2023-03-03 22:38:28 +0800

    Fix a bug

--- packages/lib/SyncTargetOneDrive.ts
+++ packages/lib/SyncTargetOneDrive.ts
@@ -106,7 +106,7 @@ export default class SyncTargetOneDrive extends BaseSyncTarget {
 		// the appDir might contain non-ASCII characters
 		// /[^\u0021-\u00ff]/ is used in Node.js to detect the unescaped characters.
 		// See https://github.com/nodejs/node/blob/bbbf97b6dae63697371082475dc8651a6a220336/lib/_http_client.js#L176
-		const baseDir = RegExp(/[^\u0021-\u00ff]/).exec() !== null ? encodeURI(appDir) : appDir
+		const baseDir = RegExp(/[^\u0021-\u00ff]/).exec(appDir) !== null ? encodeURI(appDir) : appDir
 		const fileApi = new FileApi(baseDir, new FileApiDriverOneDrive(this.api()));
 		fileApi.setSyncTargetId(this.syncTargetId());
 		fileApi.setLogger(this.logger());
