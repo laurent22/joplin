3a8d87d292ac438e0ae8580e63042f560a19f3f1
Laurent Cozic <laurent@cozic.net>
2023-03-19 17:31:37 +0000

    Tools: Export rootDir from utils

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,9 +1,13 @@
 import execCommand from './execCommand';
 import commandToString from './commandToString';
 import splitCommandString from './splitCommandString';
+import { dirname } from 'path';
+
+const rootDir = dirname(dirname(dirname(__dirname)));
 
 export {
 	execCommand,
 	commandToString,
 	splitCommandString,
+	rootDir,
 };
