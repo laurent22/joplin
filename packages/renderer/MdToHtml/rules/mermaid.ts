d576154a4b9b3a92fafff18ebcccf48a9e69f3f0
palerdot <palerdot@gmail.com>
2023-03-21 15:25:13 +0530

    feat(renderer): export button for mermaid graphs

--- packages/renderer/MdToHtml/rules/mermaid.ts
+++ packages/renderer/MdToHtml/rules/mermaid.ts
@@ -1,6 +1,6 @@
 export default {
 
-	assets: function() {
+	assets: function(theme: any) {
 		return [
 			{ name: 'mermaid.min.js' },
 			{ name: 'mermaid_render.js' },
@@ -12,13 +12,28 @@ export default {
 				text: '.mermaid { background-color: white; width: 640px; }',
 				mime: 'text/css',
 			},
+			{
+				inline: true,
+				// Export button in mermaid graph should be shown only on hovering the mermaid graph
+				// ref: https://github.com/laurent22/joplin/issues/6101
+				text: `
+				.mermaid-export-graph { visibility: hidden; } 
+				.joplin-editable:hover .mermaid-export-graph { visibility: visible !important; }
+				.mermaid-export-graph:hover {
+					background-color: ${theme.backgroundColorHover3} !important;
+				}
+				`.trim(),
+				mime: 'text/css',
+			},
 		];
 	},
 
-	plugin: function(markdownIt: any) {
+	plugin: function(markdownIt: any, ruleOptions: any) {
 		const defaultRender: Function = markdownIt.renderer.rules.fence || function(tokens: any[], idx: number, options: any, env: any, self: any) {
 			return self.renderToken(tokens, idx, options, env, self);
 		};
+
+		const exportButton = exportGraphButton(ruleOptions);
 
 		markdownIt.renderer.rules.fence = function(tokens: any[], idx: number, options: {}, env: any, self: any) {
 			const token = tokens[idx];
@@ -31,9 +46,39 @@ export default {
 			return `
 				<div class="joplin-editable">
 					<pre class="joplin-source" data-joplin-language="mermaid" data-joplin-source-open="\`\`\`mermaid&#10;" data-joplin-source-close="&#10;\`\`\`&#10;">${contentHtml}</pre>
+					${exportButton}
 					<pre class="mermaid">${contentHtml}</pre>
 				</div>
 			`;
 		};
 	},
 };
+
+function exportGraphButton(ruleOptions: any) {
+	const theme: any = ruleOptions.theme;
+	// Clicking on export button manually triggers a right click context menu event
+	const onClickHandler = `
+		const target = arguments[0].target;
+		const $mermaid_elem = target.nextElementSibling;
+		const rightClickEvent = new PointerEvent("contextmenu", {bubbles: true});
+		rightClickEvent.target = $mermaid_elem;
+		$mermaid_elem.dispatchEvent(rightClickEvent);
+		return false;
+	`.trim();
+	const js = `onclick='${onClickHandler}'`;
+	const style = `
+		display: block;	
+		margin-left: auto;
+		border-radius: ${theme.buttonStyle.borderRadius}px;
+		font-size: ${theme.fontSize}px;
+		color: ${theme.color};
+		background: ${theme.buttonStyle.backgroundColor};
+		border: ${theme.buttonStyle.border};
+		padding-top: ${theme.buttonStyle.paddingTop}px;
+		padding-bottom: ${theme.buttonStyle.paddingBottom}px;
+		padding-right: ${theme.buttonStyle.paddingRight}px;
+		padding-left: ${theme.buttonStyle.paddingLeft}px;
+	`.trim();
+
+	return `<button class="mermaid-export-graph" ${js} style="${style}">Export</button>`;
+}
