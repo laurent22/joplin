2584224026e9432e0686ca5ae2e2e6cb212f75c4
palerdot <palerdot@gmail.com>
2023-04-04 12:29:44 +0530

    chore: upgrade to @electron/notarize namespace

--- packages/app-desktop/tools/notarizeMacApp.js
+++ packages/app-desktop/tools/notarizeMacApp.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const electron_notarize = require('electron-notarize');
+const electron_notarize = require('@electron/notarize');
 const execCommand = require('./execCommand');
 
 function isDesktopAppTag(tagName) {
