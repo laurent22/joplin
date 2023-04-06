5c2640f88f76689d1d8c28e4e0f4a80bab21089a
Laurent Cozic <laurent@cozic.net>
2023-04-06 12:11:45 +0200

    Tools: Simplify root workspace build

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,52 @@
 const gulp = require('gulp');
-const { execCommand } = require('@joplin/utils');
+const execa = require('execa');
+const { stdout } = require('process');
+
+const execCommand = async (executableName, args, options = null) => {
+	options = {
+		showInput: true,
+		showStdout: true,
+		showStderr: true,
+		quiet: false,
+		...options,
+	};
+
+	if (options.quiet) {
+		options.showInput = false;
+		options.showStdout = false;
+		options.showStderr = false;
+	}
+
+	if (options.showInput) {
+		stdout.write(`> ${executableName} ${args.join(' ')}\n`);
+	}
+
+	const promise = execa(executableName, args);
+	if (options.showStdout && promise.stdout) promise.stdout.pipe(process.stdout);
+	if (options.showStderr && promise.stderr) promise.stderr.pipe(process.stderr);
+	const result = await promise;
+	return result.stdout.trim();
+};
+
 
 const tasks = {
 	completePublishAll: {
 		fn: async () => {
-			await execCommand(['git', 'add', '-A']);
-			await execCommand(['git', 'commit', '-m', 'Releasing sub-packages']);
+			await execCommand('git', ['add', '-A']);
+			await execCommand('git', ['commit', '-m', 'Releasing sub-packages']);
 
 			// Lerna does some unnecessary auth check that doesn't work with
 			// automation tokens, thus the --no-verify-access. Automation token
 			// is still used for access when publishing even with this flag
 			// (publishing would fail otherwise).
 			// https://github.com/lerna/lerna/issues/2788
-			await execCommand(['lerna', 'publish', 'from-package', '-y', '--no-verify-access']);
+			await execCommand('lerna', ['publish', 'from-package', '-y', '--no-verify-access']);
 
-			await execCommand(['yarn', 'install']);
-			await execCommand(['git', 'add', '-A']);
-			await execCommand(['git', 'commit', '-m', 'Lock file']);
+			await execCommand('yarn', ['install']);
+			await execCommand('git', ['add', '-A']);
+			await execCommand('git', ['commit', '-m', 'Lock file']);
 
-			await execCommand(['git', 'push']);
+			await execCommand('git', ['push']);
 		},
 	},
 	build: {
@@ -31,9 +59,9 @@ const tasks = {
 			// faster, especially when having to rebuild after adding a
 			// dependency.
 			if (process.env.BUILD_SEQUENCIAL === '1') {
-				await execCommand(['yarn', 'run', 'buildSequential']);
+				await execCommand('yarn', ['run', 'buildSequential']);
 			} else {
-				await execCommand(['yarn', 'run', 'buildParallel']);
+				await execCommand('yarn', ['run', 'buildParallel']);
 			}
 		},
 	},
