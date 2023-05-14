84852bc873afd2b20c1cd8f343ecc3f2323cb264
Hachemdr <120032388+Hachemdr@users.noreply.github.com>
2023-05-14 21:33:12 +0100

    Update gotoAnything.ts

--- packages/app-desktop/gui/MainScreen/commands/gotoAnything.ts
+++ packages/app-desktop/gui/MainScreen/commands/gotoAnything.ts
@@ -27,7 +27,35 @@ export const runtime = (): CommandRuntime => {
 			if (uiType === UiType.GotoAnything) {
 				menuItemById('gotoAnything').click();
 			} else if (uiType === UiType.CommandPalette) {
-				menuItemById('commandPalette').click();
+				const originalMenuItem = menuItemById('commandPalette');
+				const originalClickHandler = originalMenuItem.click;
+
+				// Replace the original click handler with a new one that preserves the original case
+				originalMenuItem.click = function() {
+					const originalRenderFunction = this.menu.render;
+					this.menu.render = function() {
+						const result = originalRenderFunction.apply(this, arguments);
+
+						// Preserve the original case of note titles
+						const search = result.querySelector('#search');
+						const resultsList = result.querySelector('#results-list');
+						if (search && resultsList) {
+							const searchValue = search.value.toLowerCase();
+							resultsList.querySelectorAll('li > a').forEach(link => {
+								link.textContent = link.title.replace(new RegExp(searchValue, 'ig'), (match) => {
+									const index = match.toLowerCase().indexOf(searchValue);
+									return match.slice(0, index) + match.slice(index, index + searchValue.length).toUpperCase() + match.slice(index + searchValue.length);
+								});
+							});
+						}
+
+						return result;
+					};
+
+					originalClickHandler.apply(this, arguments);
+				};
+
+				originalMenuItem.click();
 			} else if (uiType === UiType.ControlledApi) {
 				return new Promise((resolve: Function, reject: Function) => {
 					const menuItem = PluginManager.instance().menuItems().find((i: any) => i.id === 'controlledApi');
@@ -39,4 +67,5 @@ export const runtime = (): CommandRuntime => {
 			}
 		},
 	};
+
 };
