fb27ae991c3145a4416bc8f30e6b9353061dffa2
Laurent Cozic <laurent22@users.noreply.github.com>
2023-06-08 14:43:47 +0100

    Linux: Resolves #8289: Ask user to restart app after switching profile

--- packages/app-desktop/commands/switchProfile.ts
+++ packages/app-desktop/commands/switchProfile.ts
@@ -20,7 +20,7 @@ export const runtime = (): CommandRuntime => {
 			};
 
 			await saveProfileConfig(`${Setting.value('rootProfileDir')}/profiles.json`, newConfig);
-			await restart(false);
+			await restart();
 		},
 	};
 };
