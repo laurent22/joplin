a8821dfcb5a4afffc21957b06be993822ed53b9c
Laurent Cozic <laurent22@users.noreply.github.com>
2023-06-12 17:29:57 +0100

    test

--- packages/app-cli/tests/support/plugins/user_data/src/index.ts
+++ packages/app-cli/tests/support/plugins/user_data/src/index.ts
@@ -2,14 +2,20 @@ import joplin from 'api';
 
 joplin.plugins.register({
 	onStart: async function() {
-		const joplinData = (joplin.data);
+		const joplinData = (joplin.data as any);
+
+		const folder = await joplin.data.post(['folders'], null, { title: "test" });
+		const note = await joplin.data.post(['notes'], null, { title: "test", parent_id: folder.id });
+
+		console.info('Folder', folder);
+		console.info('Note', note);
 		
-		await joplinData.userDataSet('mykey', 'abcd');
+		await joplinData.userDataSet(note.id, 'mykey', 'abcd');
 
-		console.info('Got back user data:', await joplinData.userDataGet('mykey'));
+		console.info('Got back user data:', await joplinData.userDataGet(note.id, 'mykey'));
 
-		await joplinData.userDataDelete('mykey');
+		await joplinData.userDataDelete(note.id, 'mykey');
 	
-		console.info('Got back user data:', await joplinData.userDataGet('mykey'));
+		console.info('Got back user data:', await joplinData.userDataGet(note.id, 'mykey'));
 	},
 });
--- packages/app-desktop/testPluginDemo.sh
+++ packages/app-desktop/testPluginDemo.sh
@@ -5,7 +5,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TEMP_PATH=~/src/plugin-tests
-PLUGIN_PATH=~/src/user_data_plugin
+PLUGIN_PATH=~/src/joplin/packages/app-cli/tests/support/plugins/user_data
 
 mkdir -p "$TEMP_PATH"
 PLUGIN_NAME=$(echo "$PLUGIN_PATH" | awk -F/ '{print $NF}')
@@ -14,3 +14,5 @@ TEMP_PLUGIN_PATH="$TEMP_PATH/$PLUGIN_NAME"
 rsync -a --delete "$PLUGIN_PATH/" "$TEMP_PLUGIN_PATH/" 
 
 npm install --prefix="$TEMP_PLUGIN_PATH" && yarn start --dev-plugins "$TEMP_PLUGIN_PATH"
+
+# Add eg "--profile $HOME/.config/joplindev-desktop-1" to test with a different profile
