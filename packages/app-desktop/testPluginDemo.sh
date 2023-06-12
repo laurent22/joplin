b90cb89e0f7ee1d320519fda7f28d1ff556c1a81
Laurent Cozic <laurent22@users.noreply.github.com>
2023-06-12 17:02:49 +0100

    fix

--- packages/app-desktop/testPluginDemo.sh
+++ packages/app-desktop/testPluginDemo.sh
@@ -4,5 +4,13 @@
 # It could be used to develop plugins too.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PLUGIN_PATH=~/src/joplin/packages/app-cli/tests/support/plugins/user_data
-npm install --prefix="$PLUGIN_PATH" && yarn start --dev-plugins "$PLUGIN_PATH"
+TEMP_PATH=~/src/plugin-tests
+PLUGIN_PATH=~/src/user_data_plugin
+
+mkdir -p "$TEMP_PATH"
+PLUGIN_NAME=$(echo "$PLUGIN_PATH" | awk -F/ '{print $NF}')
+TEMP_PLUGIN_PATH="$TEMP_PATH/$PLUGIN_NAME" 
+
+rsync -a --delete "$PLUGIN_PATH/" "$TEMP_PLUGIN_PATH/" 
+
+npm install --prefix="$TEMP_PLUGIN_PATH" && yarn start --dev-plugins "$TEMP_PLUGIN_PATH"
