68fc749243fe2afb926b1fccdc8ec12591d8cdaa
Hubert <hubertfilho@users.noreply.github.com>
2023-06-30 09:17:12 -0300

    Improvements on the exception handling and implementation of the logic to call the 'rotational logs' logic.

--- packages/lib/RotatingLogs.ts
+++ packages/lib/RotatingLogs.ts
@@ -9,9 +9,12 @@ export default class RotatingLogs {
 	private logFileName: string = 'log.txt';
 	private logFileExpirationTimeInDays: number = 30;
 	private logFileMaximumSizeInBytes: number = 1024 * 1024 * 10;
+	private logFileRotetionalInterval: number = 43200 * 1000;
 
 	public constructor(logFileDir: string) {
 		this.setLogFileDir(logFileDir);
+		this.rotateLogsFiles();
+		this.rotateLogsFilesOnEachInterval();
 	}
 
 	public setLogFileDir(value: string) {
@@ -23,8 +26,41 @@ export default class RotatingLogs {
 		return RotatingLogs.fsDriver_;
 	}
 
-	public async cleanLogFile(): Promise<boolean> {
-		const stats = await this.fsDriver().stat(this.logFileFullpath());
+	private rotateLogsFiles() {
+		void this.cleanActiveLogFile(),
+		void this.deleteOldLogsFiles();
+	}
+
+	private rotateLogsFilesOnEachInterval() {
+		setInterval(() => {
+			this.rotateLogsFiles();
+		}, this.logFileRotetionalInterval);
+	}
+
+	private async cleanActiveLogFile() {
+		try {
+			await this.cleanActiveLogFileImplementation();
+		} catch (e) {
+			console.error(e.message);
+		}
+	}
+
+	private async deleteOldLogsFiles() {
+		try {
+			await this.deleteOldLogFilesImplementation();
+		} catch (e) {
+			console.error(e.message);
+		}
+	}
+
+	public async cleanActiveLogFileImplementation(): Promise<boolean> {
+		let stats: any = false;
+		try {
+			stats = await this.fsDriver().stat(this.logFileFullpath());
+		} catch (e) {
+			console.error(e.message);
+			throw new Error('Cannot read the stats of active log file.');
+		}
 		const sizeInBytes: number = stats.size;
 		if (this.isLogFileBiggerThanMaximumSizeInBytes(sizeInBytes)) {
 			const newLogFile: string = this.logFileFullpath(this.getNameToNonActiveLogFile());
@@ -42,17 +78,34 @@ export default class RotatingLogs {
 		return `log-${moment.now()}.txt`;
 	}
 
-	public async deleteOldLogFiles(): Promise<boolean> {
-		const files: Array<string> = await this.fsDriver().readDirStats(this.logFileDir);
+	public async deleteOldLogFilesImplementation(): Promise<boolean> {
+		let files: Array<string>;
+		try {
+			files = await this.fsDriver().readDirStats(this.logFileDir);
+		} catch (e) {
+			console.error(e.message);
+			throw new Error('Cannot read the stats of logs directory.');
+		}
 		const logs: Array<string> = this.getNonActiveLogs(files.map((file: any) => file.path));
 		let hasAnyFileBeenDeleted = false;
 		logs.forEach(async (log: string) => {
-			const stats: any = await this.fsDriver().stat(this.logFileFullpath(log));
+			let stats: any;
+			try {
+				stats = await this.fsDriver().stat(this.logFileFullpath(log));
+			} catch (e) {
+				console.error(e.message);
+				console.error('Cannot read the stats of the old log file.');
+			}
 			const birthtime: Date = stats.birthtime;
 			const diffInDays: number = moment().diff(birthtime, 'days');
 			if (this.isLogFileOlderThanExpirationTimeInDays(diffInDays)) {
-				this.fsDriver().remove(this.logFileFullpath(log));
-				hasAnyFileBeenDeleted = true;
+				try {
+					this.fsDriver().remove(this.logFileFullpath(log));
+					hasAnyFileBeenDeleted = true;
+				} catch (e) {
+					console.error(e.message);
+					throw new Error('Cannot delete the old log file.');
+				}
 			}
 		});
 		return hasAnyFileBeenDeleted;
