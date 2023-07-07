1034a93bd41614f9ac582d5abde2437ca644c347
Henry Heino <personalizedrefrigerator@gmail.com>
2023-07-07 06:04:07 -0700

    Update ShareUtils.test.ts with changes from feedback

--- packages/app-mobile/utils/ShareUtils.test.ts
+++ packages/app-mobile/utils/ShareUtils.test.ts
@@ -1,12 +1,12 @@
-import { describe, test, expect, jest, afterAll } from '@jest/globals';
-import { mkdtempSync, existsSync } from 'fs';
-import { rmdir } from 'fs/promises';
+import { describe, test, expect, jest, afterAll, beforeAll } from '@jest/globals';
+import { pathExists, mkdir, rmdir } from 'fs-extra';
 import { tmpdir } from 'os';
 import { join } from 'path';
+import uuid from '@joplin/lib/uuid';
 
 // react-native-fs's CachesDirectoryPath export doesn't work in a testing environment.
 // Use a temporary file instead.
-const tempDirectoryPath = mkdtempSync(join(tmpdir(), 'ShareUtilsTest'));
+const tempDirectoryPath = join(tmpdir(), `ShareUtilsTest-${uuid.createNano()}`);
 jest.mock('react-native-fs', () => {
 	return {
 		CachesDirectoryPath: tempDirectoryPath,
@@ -17,13 +17,17 @@ jest.mock('react-native-fs', () => {
 import { writeTextToCacheFile } from './ShareUtils';
 
 describe('ShareUtils', () => {
+	beforeAll(async () => {
+		await mkdir(tempDirectoryPath);
+	});
+
 	test('writeTextFileToCache should create a new, uniquely-named file', async () => {
 		const filePath1 = await writeTextToCacheFile('testing...');
-		expect(existsSync(filePath1)).toBe(true);
+		expect(await pathExists(filePath1)).toBe(true);
 
 		const filePath2 = await writeTextToCacheFile('test 2');
 		expect(filePath2).not.toBe(filePath1);
-		expect(existsSync(filePath2)).toBe(true);
+		expect(await pathExists(filePath2)).toBe(true);
 	});
 
 	afterAll(async () => {
