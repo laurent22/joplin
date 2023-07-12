e906a1db75401566bf1980e054970fc95bf0c550
Hubert <hubertfilho@users.noreply.github.com>
2023-07-12 14:46:59 -0300

    Simplification of create file function.

--- packages/lib/RotatingLogs.test.ts
+++ packages/lib/RotatingLogs.test.ts
@@ -1,17 +1,15 @@
-import { open, close, writeFile, readdir, remove } from 'fs-extra';
+import { writeFile, readdir, remove } from 'fs-extra';
 import { createTempDir, msleep } from './testing/test-utils';
 import RotatingLogs from './RotatingLogs';
 
-const createTestFile = async (dir: string, fileName: string) => {
-	const fd = await open(`${dir}/${fileName}`, 'w');
-	await writeFile(fd, 'some content');
-	await close(fd);
+const createLogFile = async (dir: string) => {
+	await writeFile(`${dir}/log.txt`, 'some content');
 };
 
 describe('RotatingLogs', () => {
 	test('should exists an log.txt file inside the folder', async () => {
 		const dir = await createTempDir();
-		await createTestFile(dir, 'log.txt');
+		await createLogFile(dir);
 		const files = await readdir(dir);
 		expect(files.find(file => file.match(/^log.txt$/gi))).toBeTruthy();
 		await remove(dir);
@@ -19,7 +17,7 @@ describe('RotatingLogs', () => {
 
 	test('should rename log.txt to log-TIMESTAMP.txt', async () => {
 		const dir = await createTempDir();
-		await createTestFile(dir, 'log.txt');
+		await createLogFile(dir);
 		const rotatingLogs = new RotatingLogs(dir, 1, 1);
 		await rotatingLogs.cleanActiveLogFile();
 		const files = await readdir(dir);
@@ -30,7 +28,7 @@ describe('RotatingLogs', () => {
 
 	test('should delete inactive log file older than 1ms', async () => {
 		const dir = await createTempDir();
-		await createTestFile(await createTempDir(), 'log.txt');
+		await createLogFile(dir);
 		const rotatingLogs = new RotatingLogs(dir, 1, 1);
 		await rotatingLogs.cleanActiveLogFile();
 		let files = await readdir(dir);
