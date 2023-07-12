b3308fa02cde1aabe188d7e6e32348325d45ab34
pedr <pedrlz.frn@gmail.com>
2023-07-12 15:49:06 -0300

    function to fetch inbox folder jop_id

---
+++ packages/lib/utils/inboxFolderFetcher.ts
@@ -0,0 +1,22 @@
+import SyncTargetRegistry from '../SyncTargetRegistry';
+import Setting from '../models/Setting';
+import { reg } from '../registry';
+
+const inboxFolderFetcher = async () => {
+
+	if (Setting.value('sync.target') !== SyncTargetRegistry.nameToId('joplinCloud')) {
+		return;
+	}
+
+	const syncTarget = reg.syncTarget();
+	const fileApi = await syncTarget.fileApi();
+	const api = fileApi.driver().api();
+
+	const owner = await api.exec('GET', `api/users/${api.userId}`);
+
+	if (owner.inbox) {
+		Setting.setValue('emailToNote.inboxJopId', owner.inbox.jop_id);
+	}
+};
+
+export default inboxFolderFetcher;
