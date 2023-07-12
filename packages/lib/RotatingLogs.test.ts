d9e96d199c72b8b3c573cb736fe7d044c3f3a861
Hubert <hubertfilho@users.noreply.github.com>
2023-07-12 12:17:37 -0300

    Removed the beforeEach and afterEach hooks.

--- packages/lib/RotatingLogs.test.ts
+++ packages/lib/RotatingLogs.test.ts
@@ -1,48 +1,44 @@
-import { open, close, writeFile, readdir, rm } from 'fs-extra';
+import { open, close, writeFile, readdir, remove } from 'fs-extra';
 import { createTempDir, msleep } from './testing/test-utils';
 import RotatingLogs from './RotatingLogs';
 
-const createTestFile = async (fileName: string) => {
-	const fd = await open(fileName, 'w');
+const createTestFile = async (dir: string, fileName: string) => {
+	const fd = await open(`${dir}/${fileName}`, 'w');
 	await writeFile(fd, 'some content');
 	await close(fd);
 };
 
 describe('RotatingLogs', () => {
-	let dir: string;
-	let files: string[];
-	let rotatingLogs: RotatingLogs;
-
-	beforeEach(async () => {
-		dir = await createTempDir();
-		rotatingLogs = new RotatingLogs(dir, 1, 1);
-		await createTestFile(`${dir}/log.txt`);
-	});
-
-	afterEach(async () => {
-		files = [];
-		await rm(dir, { recursive: true });
-	});
-
 	test('should exists an log.txt file inside the folder', async () => {
-		files = await readdir(dir);
+		const dir = await createTempDir();
+		await createTestFile(dir, 'log.txt');
+		const files = await readdir(dir);
 		expect(files.find(file => file.match(/^log.txt$/gi))).toBeTruthy();
+		await remove(dir);
 	});
 
 	test('should rename log.txt to log-TIMESTAMP.txt', async () => {
+		const dir = await createTempDir();
+		await createTestFile(dir, 'log.txt');
+		const rotatingLogs = new RotatingLogs(dir, 1, 1);
 		await rotatingLogs.cleanActiveLogFile();
-		files = await readdir(dir);
+		const files = await readdir(dir);
 		expect(files.find(file => file.match(/^log.txt$/gi))).toBeFalsy();
 		expect(files.find(file => file.match(/^log-[0-9]+.txt$/gi))).toBeTruthy();
+		await remove(dir);
 	});
 
 	test('should delete inactive log file older than 1ms', async () => {
+		const dir = await createTempDir();
+		await createTestFile(await createTempDir(), 'log.txt');
+		const rotatingLogs = new RotatingLogs(dir, 1, 1);
 		await rotatingLogs.cleanActiveLogFile();
-		files = await readdir(dir);
+		let files = await readdir(dir);
 		expect(files.find(file => file.match(/^log-[0-9]+.txt$/gi))).toBeTruthy();
 		await msleep(2);
 		await rotatingLogs.deleteNonActiveLogFiles();
 		files = await readdir(dir);
 		expect(files.find(file => file.match(/^log-[0-9]+.txt$/gi))).toBeFalsy();
+		await remove(dir);
 	});
 });
