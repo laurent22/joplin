8d817bc4373e5ce85cf6efe8adea8f3d5cd61a24
Hubert <hubertfilho@users.noreply.github.com>
2023-07-13 16:23:18 -0300

    Wrapping the test inside a try, finally block to clean the temp directory even when the test fail.

--- packages/lib/RotatingLogs.test.ts
+++ packages/lib/RotatingLogs.test.ts
@@ -4,22 +4,26 @@ import RotatingLogs from './RotatingLogs';
 
 describe('RotatingLogs', () => {
 	test('should rename log.txt to log-TIMESTAMP.txt and then delete it after 1ms', async () => {
-		const dir: string = await createTempDir();
-		await writeFile(`${dir}/log.txt`, 'some content');
-		let files: string[] = await readdir(dir);
-		expect(files.find(file => file.match(/^log.txt$/gi))).toBeTruthy();
-		expect(files.length).toBe(1);
-		const rotatingLogs: RotatingLogs = new RotatingLogs(dir, 1, 1);
-		await rotatingLogs.cleanActiveLogFile();
-		files = await readdir(dir);
-		expect(files.find(file => file.match(/^log.txt$/gi))).toBeFalsy();
-		expect(files.find(file => file.match(/^log-[0-9]+.txt$/gi))).toBeTruthy();
-		expect(files.length).toBe(1);
-		await msleep(2);
-		await rotatingLogs.deleteNonActiveLogFiles();
-		files = await readdir(dir);
-		expect(files.find(file => file.match(/^log-[0-9]+.txt$/gi))).toBeFalsy();
-		expect(files.length).toBe(0);
-		await remove(dir);
+		let dir: string;
+		try {
+			dir = await createTempDir();
+			await writeFile(`${dir}/log.txt`, 'some content');
+			let files: string[] = await readdir(dir);
+			expect(files.find(file => file.match(/^log.txt$/gi))).toBeTruthy();
+			expect(files.length).toBe(1);
+			const rotatingLogs: RotatingLogs = new RotatingLogs(dir, 1, 1);
+			await rotatingLogs.cleanActiveLogFile();
+			files = await readdir(dir);
+			expect(files.find(file => file.match(/^log.txt$/gi))).toBeFalsy();
+			expect(files.find(file => file.match(/^log-[0-9]+.txt$/gi))).toBeTruthy();
+			expect(files.length).toBe(1);
+			await msleep(2);
+			await rotatingLogs.deleteNonActiveLogFiles();
+			files = await readdir(dir);
+			expect(files.find(file => file.match(/^log-[0-9]+.txt$/gi))).toBeFalsy();
+			expect(files.length).toBe(0);
+		} finally {
+			await remove(dir);
+		}
 	});
 });
