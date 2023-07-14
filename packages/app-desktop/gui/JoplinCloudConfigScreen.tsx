88f6f2b40a141b48fcdcb8a9b64fd9bbfeb1e8fe
pedr <pedrlz.frn@gmail.com>
2023-07-14 17:34:38 -0300

    remove api call, since it already happens in app

--- packages/app-desktop/gui/JoplinCloudConfigScreen.tsx
+++ packages/app-desktop/gui/JoplinCloudConfigScreen.tsx
@@ -2,42 +2,16 @@ const { connect } = require('react-redux');
 import { AppState } from '../app.reducer';
 import { _ } from '@joplin/lib/locale';
 import { clipboard } from 'electron';
-import Setting from '@joplin/lib/models/Setting';
-import { reg } from '@joplin/lib/registry';
-import useAsyncEffect, { AsyncEffectEvent } from '@joplin/lib/hooks/useAsyncEffect';
 import Button from './Button/Button';
 
 type JoplinCloudConfigScreenProps = {
 	inboxEmail: string;
-	syncTarget: number;
-	joplinCloud: {
-		username: string;
-		password: string;
-		baseUrl: string;
-		userContentPath: string;
-	};
 };
 
 const JoplinCloudConfigScreen = (props: JoplinCloudConfigScreenProps) => {
 	const copyToClipboard = () => {
 		clipboard.writeText(props.inboxEmail);
 	};
-
-	useAsyncEffect(async (event: AsyncEffectEvent) => {
-		if (props.inboxEmail !== '') return;
-
-		const syncTarget = reg.syncTarget();
-		const fileApi = await syncTarget.fileApi();
-		const api = fileApi.driver().api();
-
-		const owner = await api.exec('GET', `api/users/${api.userId}`);
-
-		if (event.cancelled) return;
-
-		if (owner.inbox_email) {
-			Setting.setValue('emailToNote.inboxEmail', owner.inbox_email);
-		}
-	}, [props.inboxEmail, reg, Setting]);
 
 	return (
 		<div>
@@ -52,13 +26,6 @@ const JoplinCloudConfigScreen = (props: JoplinCloudConfigScreenProps) => {
 const mapStateToProps = (state: AppState) => {
 	return {
 		inboxEmail: state.settings['emailToNote.inboxEmail'],
-		syncTarget: state.settings['sync.target'],
-		joplinCloud: {
-			username: state.settings['sync.10.username'],
-			password: state.settings['sync.10.password'],
-			baseUrl: state.settings['sync.10.path'],
-			userContentPath: state.settings['sync.10.userContentPath'],
-		},
 	};
 };
 
