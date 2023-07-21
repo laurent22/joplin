e0fc40d256a1b809353a460f8730cd3304feaf0b
Henry Heino <personalizedrefrigerator@gmail.com>
2023-07-21 13:10:34 -0700

    Restore deleted function -- it will be necessary for leading spaces

--- packages/turndown/src/turndown.js
+++ packages/turndown/src/turndown.js
@@ -36,6 +36,7 @@ export default function TurndownService (options) {
     linkReferenceStyle: 'full',
     anchorNames: [],
     br: '  ',
+    nonbreakingSpace: '\u00A0', // unicode for &nbsp;
     disableEscapeContent: false,
     preformattedCode: false,
     blankReplacement: function (content, node) {
@@ -215,10 +216,15 @@ function replacementForNode (node) {
   var whitespace = node.flankingWhitespace
   if (whitespace.leading || whitespace.trailing) content = content.trim()
 
+  const replaceNonbreakingSpaces = space => {
+    // \u{00A0} is a nonbreaking space
+    return space.replace(/\u{00A0}/ug, this.options.nonbreakingSpace);
+  };
+
   return (
-    whitespace.leading +
-    rule.replacement(content, node, this.options) +
-    whitespace.trailing
+    replaceNonbreakingSpaces(whitespace.leading) +
+    replaceNonbreakingSpaces(rule.replacement(content, node, this.options)) +
+    replaceNonbreakingSpaces(whitespace.trailing)
   )
 }
 
