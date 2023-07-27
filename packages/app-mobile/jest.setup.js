cf5ab361ba6971e07d79c75a269af957f3936724
Henry Heino <personalizedrefrigerator@gmail.com>
2023-07-27 11:36:14 -0700

    Mobile: Tests: Fix error messages while running CodeMirror tests

--- packages/app-mobile/jest.setup.js
+++ packages/app-mobile/jest.setup.js
@@ -14,6 +14,23 @@ import { setImmediate } from 'timers';
 // so is removed by jsdom).
 window.setImmediate = setImmediate;
 
+// Prevents the CodeMirror error "getClientRects is undefined".
+// See https://github.com/jsdom/jsdom/issues/3002#issue-652790925
+document.createRange = () => {
+	const range = new Range();
+	range.getBoundingClientRect = jest.fn();
+	range.getClientRects = () => {
+		return {
+			length: 0,
+			item: () => null,
+			[Symbol.iterator]: jest.fn(),
+		};
+	};
+
+	return range;
+};
+
+
 shimInit({ nodeSqlite: sqlite3 });
 
 
