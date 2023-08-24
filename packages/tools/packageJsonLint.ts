d15118a931f786ad274f45339f9f1f60a2f88c57
Henry Heino <personalizedrefrigerator@gmail.com>
2023-08-24 09:05:51 -0700

    Chore: Fix packageJsonLint on some systems

--- packages/tools/packageJsonLint.ts
+++ packages/tools/packageJsonLint.ts
@@ -2,6 +2,10 @@ import { execCommand, getRootDir } from '@joplin/utils';
 import { chdir } from 'process';
 
 const main = async () => {
+	// Having no output seems to cause lint-staged to fail on some systems.
+	// Add a console.log statement to work around this issue.
+	console.log('Linting package.json files...');
+
 	const rootDir = await getRootDir();
 	chdir(rootDir);
 	await execCommand('yarn run npmPkgJsonLint --configFile .npmpackagejsonlintrc.json --quiet .');
