a03db28030a4fe412aadac21258ee334f2651eb3
Henry Heino <personalizedrefrigerator@gmail.com>
2023-08-25 12:00:53 -0700

    renameReleaseAssets: Fetch all releases

--- packages/app-desktop/tools/renameReleaseAssets.ts
+++ packages/app-desktop/tools/renameReleaseAssets.ts
@@ -3,31 +3,43 @@ import { parseArgs } from 'util';
 interface Context {
 	repo: string; // {owner}/{repo}
 	githubToken: string;
-	tag: string;
 }
 
 const apiBaseUrl = 'https://api.github.com/repos/';
 const defaultApiHeaders = (context: Context) => ({
-	'Authorization': `Bearer ${context.githubToken}`,
+	'Authorization': `token ${context.githubToken}`,
 	'X-GitHub-Api-Version': '2022-11-28',
 	'Accept': 'application/vnd.github+json',
 });
 
-const getTargetRelease = async (context: Context) => {
-	const result = await fetch(`${apiBaseUrl}${context.repo}/releases/tags/${context.tag}`, {
+const getTargetRelease = async (context: Context, targetTag: string) => {
+	console.log('Fetching releases...');
+
+	// Note: We need to fetch all releases, not just /releases/tag/tag-name-here.
+	// The latter doesn't include draft releases.
+
+	const result = await fetch(`${apiBaseUrl}${context.repo}/releases`, {
 		method: 'GET',
 		headers: defaultApiHeaders(context),
 	});
 
-	const json = await result.json();
+	const releases = await result.json();
 	if (!result.ok) {
-		throw new Error(`Error fetching release: ${JSON.stringify(json)}`);
+		throw new Error(`Error fetching release: ${JSON.stringify(releases)}`);
 	}
 
-	return json;
+	for (const release of releases) {
+		if (release.tag_name === targetTag) {
+			return release;
+		}
+	}
+
+	throw new Error(`No release with tag ${targetTag} found!`);
 };
 
 const updateReleaseAsset = async (context: Context, assetUrl: string, newName: string) => {
+	console.log('Updating asset with URL', assetUrl, 'to have name, ', newName);
+
 	// See https://docs.github.com/en/rest/releases/assets?apiVersion=2022-11-28#update-a-release-asset
 	const result = await fetch(assetUrl, {
 		method: 'PATCH',
@@ -61,13 +73,15 @@ const renameReleaseAssets = async () => {
 		].join('\n'));
 	}
 
+
 	const context: Context = {
 		repo: args.values.repo,
 		githubToken: args.values.token,
-		tag: args.values.tag,
 	};
 
-	const release = await getTargetRelease(context);
+	console.log('Renaming release assets for tag', args.values.tag, context.repo);
+
+	const release = await getTargetRelease(context, args.values.tag);
 
 	if (!release.assets) {
 		console.log(release);
