41dc96b44103adcc773e43075c84182689722e25
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2023-09-11 00:53:53 -0700

    Mobile: Resolves #8791: Revert to `react-native-sidemenu-updated` for navigation drawers (#8820)

--- packages/app-mobile/root.tsx
+++ packages/app-mobile/root.tsx
@@ -28,7 +28,7 @@ import SyncTargetJoplinCloud from '@joplin/lib/SyncTargetJoplinCloud';
 import SyncTargetOneDrive from '@joplin/lib/SyncTargetOneDrive';
 import initProfile from '@joplin/lib/services/profileConfig/initProfile';
 const VersionInfo = require('react-native-version-info').default;
-const { Keyboard, BackHandler, View, StatusBar, Platform, Dimensions } = require('react-native');
+const { Keyboard, BackHandler, Animated, View, StatusBar, Platform, Dimensions } = require('react-native');
 import { AppState as RNAppState, EmitterSubscription, Linking, NativeEventSubscription, Appearance } from 'react-native';
 import getResponsiveValue from './components/getResponsiveValue';
 import NetInfo from '@react-native-community/netinfo';
@@ -67,7 +67,7 @@ const { OneDriveLoginScreen } = require('./components/screens/onedrive-login.js'
 import EncryptionConfigScreen from './components/screens/encryption-config';
 const { DropboxLoginScreen } = require('./components/screens/dropbox-login.js');
 const { MenuContext } = require('react-native-popup-menu');
-import { Drawer } from 'react-native-drawer-layout';
+import SideMenu from './components/SideMenu';
 import SideMenuContent from './components/side-menu-content';
 const { SideMenuContentNote } = require('./components/side-menu-content-note.js');
 const { DatabaseDriverReactNative } = require('./utils/database-driver-react-native');
@@ -117,8 +117,8 @@ import ProfileEditor from './components/ProfileSwitcher/ProfileEditor';
 import sensorInfo, { SensorInfo } from './components/biometrics/sensorInfo';
 import { getCurrentProfile } from '@joplin/lib/services/profileConfig';
 import { getDatabaseName, getProfilesRootDir, getResourceDir, setDispatch } from './services/profiles';
-import { ReactNode } from 'react';
 import userFetcher, { initializeUserFetcher } from '@joplin/lib/utils/userFetcher';
+import { ReactNode } from 'react';
 import { parseShareCache } from '@joplin/lib/services/share/reducer';
 import autodetectTheme, { onSystemColorSchemeChange } from './utils/autodetectTheme';
 
@@ -748,6 +748,7 @@ class AppComponent extends React.Component {
 		super();
 
 		this.state = {
+			sideMenuContentOpacity: new Animated.Value(0),
 			sideMenuWidth: this.getSideMenuWidth(),
 			sensorInfo: null,
 		};
@@ -1041,20 +1042,18 @@ class AppComponent extends React.Component {
 
 		const mainContent = (
 			<View style={{ flex: 1, backgroundColor: theme.backgroundColor }}>
-				<Drawer
-					// Need to reset the key here based on menu position, otherwise
-					// the drawer will flash open on screen and close every time the
-					// drawer position switches (i.e. when opening or closing a note)
-					key={`main-drawer-${menuPosition}`}
-					open={this.props.showSideMenu}
-					onOpen={() => this.sideMenu_change(true)}
-					onClose={() => this.sideMenu_change(false)}
-					drawerPosition={menuPosition}
-					swipeEdgeWidth={15}
-					drawerStyle={{
-						width: this.state.sideMenuWidth,
+				<SideMenu
+					menu={sideMenuContent}
+					edgeHitWidth={20}
+					openMenuOffset={this.state.sideMenuWidth}
+					menuPosition={menuPosition}
+					onChange={(isOpen: boolean) => this.sideMenu_change(isOpen)}
+					onSliding={(percent: number) => {
+						this.props.dispatch({
+							type: 'SIDE_MENU_OPEN_PERCENT',
+							value: percent,
+						});
 					}}
-					renderDrawerContent={() => sideMenuContent}
 				>
 					<StatusBar barStyle={statusBarStyle} />
 					<MenuContext style={{ flex: 1 }}>
@@ -1071,7 +1070,7 @@ class AppComponent extends React.Component {
 							/> }
 						</SafeAreaView>
 					</MenuContext>
-				</Drawer>
+				</SideMenu>
 			</View>
 		);
 
