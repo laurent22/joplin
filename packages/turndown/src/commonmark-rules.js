e16fba5c77bd44915a81e82e8d5810cbcd88fb10
Henry Heino <personalizedrefrigerator@gmail.com>
2023-09-14 15:14:25 -0700

    Fix escapeContent return type

--- packages/turndown/src/commonmark-rules.js
+++ packages/turndown/src/commonmark-rules.js
@@ -316,8 +316,7 @@ rules.inlineLink = {
   escapeContent: function (node, _options) {
     // Disable escaping content (including '_'s) when the link has the same URL and href.
     // This prevents links from being broken by added escapes.
-    const doEscape = node.getAttribute('href') !== node.textContent;
-    return doEscape ? 'auto' : false;
+    return node.getAttribute('href') !== node.textContent;
   },
 
   replacement: function (content, node, options) {
