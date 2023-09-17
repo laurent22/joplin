96b2929846bb0a70ab1eedaae54ac68481463467
Laurent Cozic <laurent22@users.noreply.github.com>
2023-09-17 10:14:15 +0100

    Delete packages/app-cli/tests/support/updateTypes.sh

--- packages/app-cli/tests/support/updateTypes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-API_SOURCE_DIR="$SCRIPT_DIR/../../../../lib/services/plugins/api"
-
-for DIR in $SCRIPT_DIR/*/ ; do
-	[ -d "$DIR/api" ] && rsync -a "$API_SOURCE_DIR/" "$DIR/api/"
-done
