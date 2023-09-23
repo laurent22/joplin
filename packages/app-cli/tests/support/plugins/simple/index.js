a1821d607e8a7305500d9f112255a63f11b8f04a
Laurent Cozic <laurent22@users.noreply.github.com>
2023-09-23 17:50:24 +0100

    Desktop: Fixes #5955: Changing the currently opened note from plugins or the data API does not refresh the note content

--- packages/app-cli/tests/support/plugins/simple/index.js
+++ packages/app-cli/tests/support/plugins/simple/index.js
@@ -1,6 +1,21 @@
 joplin.plugins.register({
 	onStart: async function() {
-		const folder = await joplin.data.post(['folders'], null, { title: "my plugin folder" });
-		await joplin.data.post(['notes'], null, { parent_id: folder.id, title: "testing plugin!" });
+		// const folder = await joplin.data.post(['folders'], null, { title: "my plugin folder" });
+		// await joplin.data.post(['notes'], null, { parent_id: folder.id, title: "testing plugin!" });
+
+		await joplin.commands.register({
+			name: 'updateCurrentNote',
+			label: 'Update current note via the data API',
+			iconName: 'fas fa-music',
+			execute: async () => {
+				const noteIds = await joplin.workspace.selectedNoteIds();
+				const noteId = noteIds.length === 1 ? noteIds[0] : null;
+				if (!noteId) return;
+				console.info('Modifying current note...');
+				await joplin.data.put(['notes', noteId], null, { body: "New note body " + Date.now() });
+			},
+		});
+
+		await joplin.views.toolbarButtons.create('updateCurrentNoteButton', 'updateCurrentNote', 'editorToolbar');		
 	},
 });
