316ee5cbd8ec318d49284c059d54341bd066578b
wljince007 <liangwang007@gmail.com>
2023-09-29 14:50:12 +0800

    fix build err

--- packages/renderer/MdToHtml/rules/table_close.ts
+++ packages/renderer/MdToHtml/rules/table_close.ts
@@ -2,7 +2,7 @@ export default {
 	plugin: (markdownIt: any) => {
 		// const precedentRule = markdownIt.renderer.rules['table_close'];
 
-		markdownIt.renderer.rules.table_close = function(tokens: any[], idx: number, options: any, env: any, self: any) {
+		markdownIt.renderer.rules.table_close = function(tokens: any[], idx: number, options: any, _env: any, self: any) {
 			const cur = String(self.renderToken(tokens, idx, options));
 			return `${cur}</div>\n`;
 		};
