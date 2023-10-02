53c82434be69a68b5edf397d2dcdbc6769660f4b
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2023-10-02 11:14:08 -0700

    Mobile: Add share button to log screen (#8364)
    Co-authored-by: Laurent Cozic <laurent22@users.noreply.github.com>

--- packages/app-mobile/utils/ShareUtils.ts
+++ packages/app-mobile/utils/ShareUtils.ts
@@ -6,19 +6,34 @@ import { CachesDirectoryPath } from 'react-native-fs';
 // when refactoring this name, make sure to refactor the `SharePackage.java` (in android) as well
 const DIR_NAME = 'sharedFiles';
 
+const makeShareCacheDirectory = async () => {
+	const targetDir = `${CachesDirectoryPath}/${DIR_NAME}`;
+	await shim.fsDriver().mkdir(targetDir);
+
+	return targetDir;
+};
+
 // Copy a file to be shared to cache, renaming it to its orignal name
 export async function copyToCache(resource: ResourceEntity): Promise<string> {
 	const filename = Resource.friendlySafeFilename(resource);
 
-	const targetDir = `${CachesDirectoryPath}/${DIR_NAME}`;
-	await shim.fsDriver().mkdir(targetDir);
-
+	const targetDir = await makeShareCacheDirectory();
 	const targetFile = `${targetDir}/${filename}`;
 
 	await shim.fsDriver().copy(Resource.fullPath(resource), targetFile);
 
 	return targetFile;
 }
+
+// fileName should be unique -- any file with fileName will be overwritten if it already exists.
+export const writeTextToCacheFile = async (text: string, fileName: string): Promise<string> => {
+	const targetDir = await makeShareCacheDirectory();
+
+	const filePath = `${targetDir}/${fileName}`;
+	await shim.fsDriver().writeFile(filePath, text, 'utf8');
+
+	return filePath;
+};
 
 // Clear previously shared files from cache
 export async function clearSharedFilesCache(): Promise<void> {
