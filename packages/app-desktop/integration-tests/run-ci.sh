2738aa6e5602479e7da8c582b83cd44180307c06
Henry Heino <personalizedrefrigerator@gmail.com>
2023-10-09 23:18:12 -0700

    CI: Try running playwright test with xvfb on Linux

---
+++ packages/app-desktop/integration-tests/run-ci.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+if test "$RUNNER_OS" = "Linux" ; then
+	# The Ubuntu Github CI doesn't have a display server.
+	# Start a virtual one with xvfb-run. 
+	xvfb-run -- yarn run playwright test
+else
+	yarn run playwright test
+fi
