1e3e5cb9c9806c2cde07fc0ec96f9c6d01462def
Henry Heino <personalizedrefrigerator@gmail.com>
2023-10-11 07:51:47 -0700

    Prefer line test reporter in CI

--- packages/app-desktop/playwright.config.ts
+++ packages/app-desktop/playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 	workers: process.env.CI ? 1 : undefined,
 
 	// Reporter to use. See https://playwright.dev/docs/test-reporters
-	reporter: 'html',
+	reporter: process.env.CI ? 'line' : 'html',
 
 	// Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.
 	use: {
