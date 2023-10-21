8d32f5492c5e6618fb50c9dcbb944812d1342b45
Henry Heino <personalizedrefrigerator@gmail.com>
2023-10-21 12:11:00 -0700

    Desktop: Beta editor: Insert tabs at cursor rather than indenting in some cases

--- packages/editor/CodeMirror/markdown/markdownCommands.ts
+++ packages/editor/CodeMirror/markdown/markdownCommands.ts
@@ -12,6 +12,7 @@ import {
 	toggleInlineFormatGlobally, toggleRegionFormatGlobally, toggleSelectedLinesStartWith,
 	isIndentationEquivalent, stripBlockquote, tabsToSpaces,
 } from './markdownReformatter';
+import intersectsSyntaxNode from '../util/isInSyntaxNode';
 
 const startingSpaceRegex = /^(\s*)/;
 
@@ -416,6 +417,30 @@ export const increaseIndent: Command = (view: EditorView): boolean => {
 	view.dispatch(view.state.changeByRange((sel: SelectionRange) => {
 		return renumberList(view.state, sel);
 	}));
+
+	return true;
+};
+
+// Like `increaseIndent`, but may insert tabs in some instances.
+export const insertOrIncreaseIndent: Command = (view: EditorView): boolean => {
+	const selection = view.state.selection;
+	const mainSelection = selection.main;
+	if (selection.ranges.length !== 1 || !mainSelection.empty) {
+		return increaseIndent(view);
+	}
+
+
+	if (intersectsSyntaxNode(view.state, mainSelection, 'ListItem')) {
+		return increaseIndent(view);
+	}
+
+	const indentUnit = indentString(view.state, getIndentUnit(view.state));
+	view.dispatch({
+		changes: {
+			from: mainSelection.from,
+			insert: indentUnit,
+		},
+	});
 
 	return true;
 };
