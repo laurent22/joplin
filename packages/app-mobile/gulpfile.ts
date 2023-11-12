ddf716479d6620ffc33dcef3cd14beb90c2f38b0
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2023-11-12 07:07:30 -0800

    Chore: Partially resolves #9262: Mobile build: Convert bundle tasks to proper gulp tasks and increase output verbosity (#9266)

--- packages/app-mobile/gulpfile.ts
+++ packages/app-mobile/gulpfile.ts
@@ -1,23 +1,35 @@
 const gulp = require('gulp');
 const utils = require('@joplin/tools/gulp/utils');
-import { buildInjectedJS, watchInjectedJS } from './tools/buildInjectedJs';
+
+import gulpTasks from './tools/buildInjectedJs/gulpTasks';
 
 const tasks = {
 	encodeAssets: {
 		fn: require('./tools/encodeAssets'),
 	},
-	buildInjectedJs: {
-		fn: buildInjectedJS,
-	},
-	watchInjectedJs: {
-		fn: watchInjectedJS,
-	},
+	...gulpTasks,
 	podInstall: {
 		fn: require('./tools/podInstall'),
 	},
 };
 
 utils.registerGulpTasks(gulp, tasks);
+
+gulp.task('buildInjectedJs', gulp.series(
+	'beforeBundle',
+	'buildCodeMirrorEditor',
+	'buildJsDrawEditor',
+	'copyWebviewLib',
+));
+
+gulp.task('watchInjectedJs', gulp.series(
+	'beforeBundle',
+	'copyWebviewLib',
+	gulp.parallel(
+		'watchCodeMirrorEditor',
+		'watchJsDrawEditor',
+	),
+));
 
 gulp.task('build', gulp.series(
 	'buildInjectedJs',
