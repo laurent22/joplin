0cf24fe38dd185aa1d8b2107913ea0c4f281ba05
Henry Heino <personalizedrefrigerator@gmail.com>
2023-11-28 08:00:28 -0800

    Compile TypeScript using Gulp

--- packages/server/gulpfile.js
+++ packages/server/gulpfile.js
@@ -1,11 +1,37 @@
 const gulp = require('gulp');
 const utils = require('@joplin/tools/gulp/utils');
 const compilePackageInfo = require('@joplin/tools/compilePackageInfo');
+const { execCommand } = require('@joplin/utils');
 const fs = require('fs-extra');
+const path = require('path');
 
 const distDir = `${__dirname}/dist`;
 
+const tsConfigPaths = [
+	path.join(__dirname, 'tsconfig.json'),
+	path.join(__dirname, 'public', 'js', 'tsconfig.json'),
+];
+
 const tasks = {
+	tsc: {
+		fn: async () => {
+			for (const tsConfigPath of tsConfigPaths) {
+				await execCommand(['tsc', '--project', tsConfigPath]);
+			}
+		},
+	},
+
+	watch: {
+		fn: async () => {
+			const watchTasks = tsConfigPaths.map(tsConfigPath => {
+				return execCommand(
+					['tsc', '--watch', '--preserveWatchOutput', '--project', tsConfigPath],
+				);
+			});
+			await Promise.all(watchTasks);
+		},
+	},
+
 	compilePackageInfo: {
 		fn: async () => {
 			await fs.mkdirp(distDir);
