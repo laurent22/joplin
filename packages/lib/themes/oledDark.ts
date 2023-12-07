b9e5d5c40bda277b27f345d31a6ce089cb9e558e
Harshiths1 <136854105+Harshiths1@users.noreply.github.com>
2023-12-07 22:18:59 +0530

    Update oledDark.ts
    The selectedColor is now changed and should work on all devices

--- packages/lib/themes/oledDark.ts
+++ packages/lib/themes/oledDark.ts
@@ -7,14 +7,14 @@ const theme: Theme = {
 	color: '#dddddd',
 	colorFaded: '#777777',
 	dividerColor: '#3D444E',
-	selectedColor: '#333333',
+	selectedColor: '#B9B9B9',
 	urlColor: 'rgb(166,166,255)',
 	codeColor: '#ffffff',
 	raisedBackgroundColor: '#0F2051',
 	raisedColor: '#788BC3',
 	tableBackgroundColor: 'rgb(0, 0, 0)',
 	codeBackgroundColor: 'rgb(47, 48, 49)',
-	codeBorderColor: 'rgb(185,185,185)',
+	codeBorderColor: 'rgb(70,70,70)',
 	codeThemeCss: 'atom-one-dark-reasonable.css',
 };
 
