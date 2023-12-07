806c7e561afff54b58f407bdd939106ac83a0632
Henry Heino <personalizedrefrigerator@gmail.com>
2023-12-07 10:56:19 -0800

    Add additional simple backup test

--- packages/app-desktop/integration-tests/simpleBackup.spec.ts
+++ packages/app-desktop/integration-tests/simpleBackup.spec.ts
@@ -1,7 +1,7 @@
-import { test } from './util/test';
+import { test, expect } from './util/test';
 import MainScreen from './models/MainScreen';
 import SettingsScreen from './models/SettingsScreen';
-
+import activateMainMenuItem from './util/activateMainMenuItem';
 
 test.describe('simpleBackup', () => {
 	test('should have a section in settings', async ({ electronApp, mainWindow }) => {
@@ -17,6 +17,20 @@ test.describe('simpleBackup', () => {
 
 		const backupTab = settingsScreen.getTabLocator('Backup');
 		await backupTab.waitFor();
+	});
+
+	test('should create a backup', async ({ electronApp, mainWindow }) => {
+		const mainScreen = new MainScreen(mainWindow);
+		await mainScreen.waitFor();
+
+		expect(await activateMainMenuItem(electronApp, 'Create backup')).toBe(true);
+
+		const successDialog = mainWindow.locator('iframe[id$=backup-backupDialog]');
+		await successDialog.waitFor();
+
+		// Should report success
+		const dialogContentLocator = successDialog.frameLocator(':scope');
+		await dialogContentLocator.getByText('Backup completed').waitFor();
 	});
 });
 
