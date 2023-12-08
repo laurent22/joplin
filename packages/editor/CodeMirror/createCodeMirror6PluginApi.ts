d2405c3aafc9744275979ff481d23f2bfe79f37b
Henry Heino <personalizedrefrigerator@gmail.com>
2023-12-08 14:27:54 -0800

    Export lezer packages

--- packages/editor/CodeMirror/createCodeMirror6PluginApi.ts
+++ packages/editor/CodeMirror/createCodeMirror6PluginApi.ts
@@ -4,6 +4,9 @@ import * as codeMirrorState from '@codemirror/state';
 import * as codeMirrorLanguage from '@codemirror/language';
 import * as codeMirrorAutocomplete from '@codemirror/autocomplete';
 import * as codeMirrorCommands from '@codemirror/commands';
+import * as lezerHighlight from '@lezer/highlight';
+import * as lezerCommon from '@lezer/common';
+import * as lezerMarkdown from '@lezer/markdown';
 
 // CodeMirror 6 relies on objects/commands having a single instance. Bundling/including
 // the CodeMirror library twice or more times breaks this assumption.
@@ -14,12 +17,18 @@ const createCodeMirror6PluginApi = (view: EditorView) => {
 	return {
 		view,
 
-		codemirrorLib: {
+		codemirror: {
 			view: codeMirrorView,
 			state: codeMirrorState,
 			language: codeMirrorLanguage,
 			autocomplete: codeMirrorAutocomplete,
 			commands: codeMirrorCommands,
+		},
+
+		lezer: {
+			highlight: lezerHighlight,
+			common: lezerCommon,
+			markdown: lezerMarkdown,
 		},
 	};
 };
