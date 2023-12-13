19b68102b235cf32841ef26fcc335894a59f0f14
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2023-12-13 11:45:02 -0800

    Plugins: Resolves #9473: Expose common CodeMirror 6 packages to plugins (#9474)

---
+++ packages/app-cli/tests/support/plugins/codemirror6/api/JoplinInterop.d.ts
@@ -0,0 +1,17 @@
+import { ExportModule, ImportModule } from './types';
+/**
+ * Provides a way to create modules to import external data into Joplin or to export notes into any arbitrary format.
+ *
+ * [View the demo plugin](https://github.com/laurent22/joplin/tree/dev/packages/app-cli/tests/support/plugins/json_export)
+ *
+ * To implement an import or export module, you would simply define an object with various event handlers that are called
+ * by the application during the import/export process.
+ *
+ * See the documentation of the [[ExportModule]] and [[ImportModule]] for more information.
+ *
+ * You may also want to refer to the Joplin API documentation to see the list of properties for each item (note, notebook, etc.) - https://joplinapp.org/help/api/references/rest_api
+ */
+export default class JoplinInterop {
+    registerExportModule(module: ExportModule): Promise<void>;
+    registerImportModule(module: ImportModule): Promise<void>;
+}
