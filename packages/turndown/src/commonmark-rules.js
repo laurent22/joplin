74b0722f65db660a924795d1d06ef35a20048b73
Henry Heino <personalizedrefrigerator@gmail.com>
2023-12-21 07:05:12 -0800

    Commenting

--- packages/turndown/src/commonmark-rules.js
+++ packages/turndown/src/commonmark-rules.js
@@ -127,9 +127,13 @@ rules.subscript = {
   }
 }
 
+// Handles foreground color changes as created by the rich text editor.
+// We intentionally don't handle the general style="color: colorhere" case as
+// this may leave unwanted formatting when saving websites as markdown.
 rules.foregroundColor = {
   filter: function (node, options) {
-    return node.nodeName === 'SPAN' && node.getAttribute('joplin-color')?.match(/^\#[a-zA-Z0-9]+$/);
+    const hexColorRegex = /^\#[a-zA-Z0-9]+$/;
+    return node.nodeName === 'SPAN' && node.getAttribute('joplin-color')?.match(hexColorRegex);
   },
 
   replacement: function (content, node, options) {
