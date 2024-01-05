011837866aa19d2e35868a9757b11a9502e8b084
pedr <pedrlz.frn@gmail.com>
2024-01-04 22:25:14 -0300

    changing boolean conversion style

--- packages/lib/SyncTargetJoplinCloud.ts
+++ packages/lib/SyncTargetJoplinCloud.ts
@@ -50,7 +50,7 @@ export default class SyncTargetJoplinCloud extends BaseSyncTarget {
 			const fileApi = await this.fileApi();
 			const api = fileApi.driver().api();
 			const sessionId = await api.sessionId();
-			return Boolean(sessionId);
+			return !!sessionId;
 		} catch (error) {
 			return false;
 		}
