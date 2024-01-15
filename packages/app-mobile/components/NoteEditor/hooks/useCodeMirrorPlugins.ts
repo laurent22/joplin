47dd796307e0e28c9ca134042d2b3a6b68356fde
Henry Heino <henryuheino@gmail.com>
2024-01-14 18:01:15 -0800

    codeMirrorPlugins: Log warning when a CodeMirror content script fails to load, rather than crash

--- packages/app-mobile/components/NoteEditor/hooks/useCodeMirrorPlugins.ts
+++ packages/app-mobile/components/NoteEditor/hooks/useCodeMirrorPlugins.ts
@@ -3,7 +3,10 @@ import PluginService from '@joplin/lib/services/plugins/PluginService';
 import { ContentScriptType } from '@joplin/lib/services/plugins/api/types';
 import { PluginStates } from '@joplin/lib/services/plugins/reducer';
 import shim from '@joplin/lib/shim';
+import Logger from '@joplin/utils/Logger';
 import { useMemo } from 'react';
+
+const logger = Logger.create('useCodeMirrorPlugins');
 
 const useCodeMirrorPlugins = (pluginStates: PluginStates) => {
 	return useMemo(() => {
@@ -14,7 +17,17 @@ const useCodeMirrorPlugins = (pluginStates: PluginStates) => {
 		for (const pluginState of Object.values(pluginStates)) {
 			const pluginId = pluginState.id;
 			const contentScripts = pluginState.contentScripts[ContentScriptType.CodeMirrorPlugin] ?? [];
+
+			if (!pluginService.plugins[pluginId]) {
+				// This can happen after uninstalling a plugin -- the pluginState still exists but the plugin
+				// isn't regisered with the PluginService.
+				// TODO: Fix this.
+				logger.warn(`Plugin ${pluginId} not loaded but is present in contentScripts.`);
+				continue;
+			}
+
 			const plugin = pluginService.pluginById(pluginId);
+			logger.debug(`Loading content scripts for plugin ${pluginId}...`);
 
 			for (const contentScript of contentScripts) {
 				const contentScriptId = contentScript.id;
@@ -25,6 +38,7 @@ const useCodeMirrorPlugins = (pluginStates: PluginStates) => {
 						return await shim.fsDriver().readFile(contentScript.path);
 					},
 					postMessageHandler: async (message: any): Promise<any> => {
+						logger.debug(`Got message from plugin ${pluginId} content script ${contentScriptId}. Message:`, message);
 						return plugin.emitContentScriptMessage(contentScriptId, message);
 					},
 				});
