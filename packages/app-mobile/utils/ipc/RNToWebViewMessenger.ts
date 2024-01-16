59ddebc6b2e49da003f6a14c762c1f0815371eab
Henry Heino <henryuheino@gmail.com>
2024-01-16 15:06:51 -0800

    Slightly faster rendering

--- packages/app-mobile/utils/ipc/RNToWebViewMessenger.ts
+++ packages/app-mobile/utils/ipc/RNToWebViewMessenger.ts
@@ -8,6 +8,8 @@ import { RefObject } from 'react';
 export default class RNToWebViewMessenger<LocalInterface, RemoteInterface> extends RemoteMessenger<LocalInterface, RemoteInterface> {
 	public constructor(channelId: string, private webviewControl: WebViewControl|RefObject<WebViewControl>, localApi: LocalInterface) {
 		super(channelId, localApi);
+
+		this.onReadyToReceive();
 	}
 
 	protected override postMessage(message: SerializableData): void {
@@ -36,7 +38,9 @@ export default class RNToWebViewMessenger<LocalInterface, RemoteInterface> exten
 	};
 
 	public onWebViewLoaded = () => {
-		this.onReadyToReceive();
+		// TODO: Does this need to be re-enabled?
+		// Being ready to receive as soon as the messenger is created is helpful for speed (e.g. for the renderer).
+		// this.onReadyToReceive();
 	};
 
 	protected override onClose(): void {
