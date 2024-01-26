521c93d345287d32e26ca1b11c5a0f4cb6078e9e
Henry Heino <personalizedrefrigerator@gmail.com>
2024-01-26 10:21:32 -0800

    Chore: Desktop: Clean the defaultPlugins directory to fix slow startup issue

--- packages/app-desktop/gulpfile.ts
+++ packages/app-desktop/gulpfile.ts
@@ -4,6 +4,7 @@ const compileSass = require('@joplin/tools/compileSass');
 const compilePackageInfo = require('@joplin/tools/compilePackageInfo');
 import buildDefaultPlugins from '@joplin/default-plugins/commands/buildAll';
 import copy7Zip from './tools/copy7Zip';
+import { remove } from 'fs-extra';
 
 const tasks = {
 	compileScripts: {
@@ -34,6 +35,7 @@ const tasks = {
 	buildDefaultPlugins: {
 		fn: async () => {
 			const outputDir = `${__dirname}/build/defaultPlugins/`;
+			await remove(outputDir);
 			await buildDefaultPlugins(outputDir);
 		},
 	},
