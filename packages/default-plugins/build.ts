3fbf5ef5d015f60a69f8163b6f771684e716be55
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2024-01-28 10:53:13 -0800

    Chore: Default plugins: Rename patch to patch-plugin (#9797)

--- packages/default-plugins/build.ts
+++ packages/default-plugins/build.ts
@@ -15,7 +15,7 @@ const build = () => {
 			await buildAll(args.outputDir);
 			process.exit(0);
 		})
-		.command('patch <plugin>', 'Edit the patch file for the given plugin ID', (yargs: any) => {
+		.command('patch-plugin <plugin>', 'Edit the patch file for the given plugin ID', (yargs: any) => {
 			yargs.positional('plugin', {
 				type: 'string',
 				describe: 'ID of the plugin to patch',
