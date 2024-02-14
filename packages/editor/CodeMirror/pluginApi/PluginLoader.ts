5ff7fff9c8698f3dc1ed33a0186bb386772ca29b
Henry Heino <personalizedrefrigerator@gmail.com>
2024-02-13 21:51:33 -0800

    Fix PluginLoader tests

--- packages/editor/CodeMirror/pluginApi/PluginLoader.ts
+++ packages/editor/CodeMirror/pluginApi/PluginLoader.ts
@@ -129,7 +129,7 @@ export default class PluginLoader {
 				pluginId: plugin.pluginId,
 				contentScriptId: plugin.contentScriptId,
 			};
-			const loadedPlugin = exports.default(context);
+			const loadedPlugin = exports.default(context) ?? {};
 
 			loadedPlugin.plugin?.(this.editor);
 
