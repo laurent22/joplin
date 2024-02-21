00f0f17197c9328fd6ffabe0f4cfecb4915be098
Henry Heino <personalizedrefrigerator@gmail.com>
2024-02-21 09:35:00 -0800

    Simplify default language

--- packages/editor/CodeMirror/markdown/codeBlockLanguages/defaultLanguage.ts
+++ packages/editor/CodeMirror/markdown/codeBlockLanguages/defaultLanguage.ts
@@ -1,36 +1,18 @@
 import { LanguageDescription, LanguageSupport, StreamLanguage } from '@codemirror/language';
 
-// To allow auto-indent to work in an unrecognised language, we define a language
-// that provides just autoindent.
+// To allow auto-indent to work in an unrecognised language, we define an
+// empty language. Doing so seems to enable auto-indent in code blocks with
+// that language.
 const defaultLangauge = StreamLanguage.define({
-	startState: (indentUnit) => {
-		return {
-			indentUnit,
-			lastIndent: 0,
-			isLineStart: true,
-		};
-	},
-	token: (stream, state) => {
-		if (state.isLineStart) {
-			const indent = stream.eat(/\s*/) || '';
-			state.lastIndent = indent.length;
-			state.isLineStart = false;
-		} else {
-			const n = stream.next();
-			if (n === '\n') {
-				state.isLineStart = true;
-			}
-		}
+	token: (stream) => {
+		stream.next();
 		return null;
-	},
-	indent: (state) => {
-		return state.lastIndent;
 	},
 });
 
-const baseLanguageDescription = LanguageDescription.of({
+const defaultLanguageDescription = LanguageDescription.of({
 	name: 'default',
 	support: new LanguageSupport(defaultLangauge),
 });
 
-export default baseLanguageDescription;
+export default defaultLanguageDescription;
