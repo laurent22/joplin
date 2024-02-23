25edd4462028057d41a8b35dc20efa7b7c15a81e
Henry Heino <henryuheino@gmail.com>
2024-02-23 09:10:27 -0800

    Remove now-redundant style from NoteBodyViewer

--- packages/app-mobile/components/NoteBodyViewer/NoteBodyViewer.tsx
+++ packages/app-mobile/components/NoteBodyViewer/NoteBodyViewer.tsx
@@ -28,10 +28,6 @@ interface Props {
 	onLoadEnd?: ()=> void;
 }
 
-const webViewStyle = {
-	backgroundColor: 'transparent',
-};
-
 export default function NoteBodyViewer(props: Props) {
 	const dialogBoxRef = useRef(null);
 	const webviewRef = useRef<WebViewControl>(null);
@@ -103,7 +99,6 @@ export default function NoteBodyViewer(props: Props) {
 			<ExtendedWebView
 				ref={webviewRef}
 				webviewInstanceId='NoteBodyViewer'
-				style={webViewStyle}
 				html={html}
 				injectedJavaScript={injectedJs.join('\n')}
 				mixedContentMode="always"
