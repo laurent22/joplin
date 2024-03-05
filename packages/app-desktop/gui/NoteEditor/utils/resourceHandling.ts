ed92a71c1cff09107910f79ae69a887a2712df1e
Henry Heino <personalizedrefrigerator@gmail.com>
2024-03-05 14:11:19 -0800

    Fix copy/paste of images from the rich text editor into the rich text editor

--- packages/app-desktop/gui/NoteEditor/utils/resourceHandling.ts
+++ packages/app-desktop/gui/NoteEditor/utils/resourceHandling.ts
@@ -138,16 +138,10 @@ export async function getResourcesFromPasteEvent(event: any) {
 	return output;
 }
 
-export async function processPastedHtml(html: string, htmlToMd: HtmlToMarkdownHandler | null, mdToHtml: MarkupToHtmlHandler | null) {
+
+const processImagesInPastedHtml = async (html: string) => {
 	const allImageUrls: string[] = [];
 	const mappedResources: Record<string, string> = {};
-
-	// When copying text from eg. GitHub, the HTML might contain non-breaking
-	// spaces instead of regular spaces. If these non-breaking spaces are
-	// inserted into the TinyMCE editor (using insertContent), they will be
-	// dropped. So here we convert them to regular spaces.
-	// https://stackoverflow.com/a/31790544/561309
-	html = html.replace(/[\u202F\u00A0]/g, ' ');
 
 	htmlUtils.replaceImageUrls(html, (src: string) => {
 		allImageUrls.push(src);
@@ -200,6 +194,19 @@ export async function processPastedHtml(html: string, htmlToMd: HtmlToMarkdownHa
 
 	await Promise.all(downloadImages);
 
+	return htmlUtils.replaceImageUrls(html, (src: string) => mappedResources[src]);
+};
+
+export async function processPastedHtml(html: string, htmlToMd: HtmlToMarkdownHandler | null, mdToHtml: MarkupToHtmlHandler | null) {
+	// When copying text from eg. GitHub, the HTML might contain non-breaking
+	// spaces instead of regular spaces. If these non-breaking spaces are
+	// inserted into the TinyMCE editor (using insertContent), they will be
+	// dropped. So here we convert them to regular spaces.
+	// https://stackoverflow.com/a/31790544/561309
+	html = html.replace(/[\u202F\u00A0]/g, ' ');
+
+	html = await processImagesInPastedHtml(html);
+
 	// TinyMCE can accept any type of HTML, including HTML that may not be preserved once saved as
 	// Markdown. For example the content may have a dark background which would be supported by
 	// TinyMCE, but lost once the note is saved. So here we convert the HTML to Markdown then back
@@ -209,11 +216,7 @@ export async function processPastedHtml(html: string, htmlToMd: HtmlToMarkdownHa
 		html = (await mdToHtml(MarkupLanguage.Markdown, md, markupRenderOptions({ bodyOnly: true }))).html;
 	}
 
-	return extractHtmlBody(rendererHtmlUtils.sanitizeHtml(
-		htmlUtils.replaceImageUrls(html, (src: string) => {
-			return mappedResources[src];
-		}), {
-			allowedFilePrefixes: [Setting.value('resourceDir')],
-		},
-	));
+	return extractHtmlBody(rendererHtmlUtils.sanitizeHtml(html, {
+		allowedFilePrefixes: [Setting.value('resourceDir')],
+	}));
 }
