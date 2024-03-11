7ab4d90b4ea99d3a96fd0b50aabaf67b56a10a10
Henry Heino <personalizedrefrigerator@gmail.com>
2024-03-11 13:19:44 -0700

    Add additional readOnly check

--- packages/lib/models/utils/readOnly.test.ts
+++ packages/lib/models/utils/readOnly.test.ts
@@ -2,16 +2,24 @@ import { ModelType } from '../../BaseModel';
 import Folder from '../Folder';
 import ItemChange from '../ItemChange';
 import Note from '../Note';
-import { defaultState as defaultShareState } from '../../services/share/reducer';
+import { defaultState as defaultShareState, State as ShareState } from '../../services/share/reducer';
 import { ItemSlice, itemIsReadOnlySync } from './readOnly';
 import Resource from '../Resource';
 import shim from '../../shim';
-import { setupDatabaseAndSynchronizer, switchClient, tempFilePath } from '../../testing/test-utils';
+import { setupDatabaseAndSynchronizer, simulateReadOnlyShareEnv, switchClient, tempFilePath } from '../../testing/test-utils';
+import BaseItem from '../BaseItem';
 
 
-const checkUnsharedReadOnly = (itemType: ModelType, item: ItemSlice) => {
+const checkReadOnly = (itemType: ModelType, item: ItemSlice, shareData: ShareState = defaultShareState) => {
 	const syncUserId = '';
-	return itemIsReadOnlySync(itemType, ItemChange.SOURCE_UNSPECIFIED, item, syncUserId, defaultShareState);
+	return itemIsReadOnlySync(itemType, ItemChange.SOURCE_UNSPECIFIED, item, syncUserId, shareData);
+};
+
+const createTestResource = async () => {
+	const tempFile = tempFilePath('txt');
+	await shim.fsDriver().writeFile(tempFile, 'Test', 'utf8');
+	const note1 = await Note.save({ title: 'note' });
+	await shim.attachFileToNote(note1, tempFile);
 };
 
 describe('readOnly', () => {
@@ -24,8 +32,8 @@ describe('readOnly', () => {
 		let folder = await Folder.save({ title: 'Test' });
 		let note = await Note.save({ parent_id: folder.id, title: 'Test note' });
 
-		expect(checkUnsharedReadOnly(ModelType.Note, note as ItemSlice)).toBe(false);
-		expect(checkUnsharedReadOnly(ModelType.Folder, folder as ItemSlice)).toBe(false);
+		expect(checkReadOnly(ModelType.Note, note as ItemSlice)).toBe(false);
+		expect(checkReadOnly(ModelType.Folder, folder as ItemSlice)).toBe(false);
 
 		await Folder.delete(folder.id, { toTrash: true });
 
@@ -35,17 +43,25 @@ describe('readOnly', () => {
 		folder = await Folder.load(folder.id);
 		expect(folder.deleted_time).not.toBe(0);
 
-		expect(checkUnsharedReadOnly(ModelType.Note, note as ItemSlice)).toBe(true);
-		expect(checkUnsharedReadOnly(ModelType.Folder, folder as ItemSlice)).toBe(true);
+		expect(checkReadOnly(ModelType.Note, note as ItemSlice)).toBe(true);
+		expect(checkReadOnly(ModelType.Folder, folder as ItemSlice)).toBe(true);
 	});
 
-	test('should support checking if resources are read-only', async () => {
-		const tempFile = tempFilePath('txt');
-		await shim.fsDriver().writeFile(tempFile, 'Test', 'utf8');
-		const note1 = await Note.save({ title: 'note' });
-		await shim.attachFileToNote(note1, tempFile);
-
+	test('should support checking if resources are not read-only', async () => {
+		await createTestResource();
 		const resource = (await Resource.all())[0];
-		expect(checkUnsharedReadOnly(ModelType.Resource, resource)).toBe(false);
+		expect(checkReadOnly(ModelType.Resource, resource)).toBe(false);
+	});
+
+	test('should support checking that resources are read-only due to a share', async () => {
+		await createTestResource();
+
+		const share_id = '123456';
+		let resource = (await Resource.all())[0];
+		resource = await Resource.save({ ...resource, share_id });
+
+		const cleanup = simulateReadOnlyShareEnv(share_id);
+		expect(checkReadOnly(ModelType.Resource, resource, BaseItem.syncShareCache)).toBe(true);
+		cleanup();
 	});
 });
