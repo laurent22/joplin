14e4ccffe6345f907abbf42ba23d2cbefe03e4f3
chaNcharge <chancharge@gmail.com>
2024-03-15 23:07:35 -0700

    Complete move editor body focus to react effect

--- packages/app-desktop/gui/NoteEditor/utils/useFormNote.ts
+++ packages/app-desktop/gui/NoteEditor/utils/useFormNote.ts
@@ -64,7 +64,7 @@ function resourceInfosChanged(a: ResourceInfos, b: ResourceInfos): boolean {
 
 export default function useFormNote(dependencies: HookDependencies) {
 	const {
-		syncStarted, decryptionStarted, noteId, isProvisional, titleInputRef, editorRef, onBeforeLoad, onAfterLoad,
+		syncStarted, decryptionStarted, noteId, isProvisional, titleInputRef, onBeforeLoad, onAfterLoad,
 	} = dependencies;
 
 	const [formNote, setFormNote] = useState<FormNote>(defaultFormNote());
@@ -192,8 +192,6 @@ export default function useFormNote(dependencies: HookDependencies) {
 			requestAnimationFrame(() => {
 				if (Setting.value(focusSettingName) === 'title') {
 					if (titleInputRef.current) titleInputRef.current.focus();
-				} else {
-					if (editorRef.current) editorRef.current.execCommand({ name: 'editor.focus' });
 				}
 			});
 		}
