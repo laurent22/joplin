eb06ac673b7fe1ee4c59178069cd2c8a2fc88eee
Laurent Cozic <laurent22@users.noreply.github.com>
2024-03-20 10:50:11 +0000

    Revert "Desktop: Fixes #10036: Applied font family and font size to RTE (#10102)"
    This reverts commit 85d98f5254a601646e3df614350f0000c9f034cc.
    
    Reason: Introducing too many issues, some of them know, some of them unknown.

--- packages/app-desktop/gui/NoteEditor/NoteBody/TinyMCE/TinyMCE.tsx
+++ packages/app-desktop/gui/NoteEditor/NoteBody/TinyMCE/TinyMCE.tsx
@@ -565,8 +565,6 @@ const TinyMCE = (props: NoteBodyEditorProps, ref: any) => {
 	useEffect(() => {
 		if (!scriptLoaded) return;
 
-		const theme = themeStyle(props.themeId);
-
 		const loadEditor = async () => {
 			const language = closestSupportedLocale(props.locale, true, supportedLocales);
 
@@ -600,7 +598,6 @@ const TinyMCE = (props: NoteBodyEditorProps, ref: any) => {
 			];
 
 			const editors = await (window as any).tinymce.init({
-				content_style: `* {font-size: ${props.fontSize}px; font-family: "${props.fontFamily}"; line-height: ${theme.lineHeight};}`,
 				selector: `#${rootIdRef.current}`,
 				width: '100%',
 				body_class: 'jop-tinymce',
