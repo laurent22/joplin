9f507fccde1a6547f79a528ee1ad1998aee3f70c
Abdallah Elhadad <abdallah31009448@f-eng.tanta.edu.eg>
2024-03-24 16:13:15 +0200

    use async func

--- packages/lib/utils/chunkGenerator.ts
+++ packages/lib/utils/chunkGenerator.ts
@@ -1,19 +1,15 @@
-import * as fs from 'fs';
+import { read, open, stat } from 'fs';
+import { promisify } from 'util';
 
-function readBytes(fd: number, sharedBuffer: Buffer) {
-	return new Promise<void>((resolve, reject) => {
-		fs.read(fd, sharedBuffer, 0, sharedBuffer.length, null, (error) => {
-			if (error) {
-				return reject(error);
-			}
-			resolve();
-		});
-	});
-}
+const readPromisify = promisify(read);
+const statPromisify = promisify(stat);
+const openPromisify = promisify(open);
+
 
 async function* generateChunks(
 	filePath: string,
 	size: number,
+// eslint-disable-next-line no-undef
 ): AsyncGenerator<{ index: number; chunksNo: number; chunk: Buffer }> {
 	const sharedBuffer = Buffer.alloc(size);
 	let stats;
@@ -22,17 +18,18 @@ async function* generateChunks(
 	let end = size;
 
 	try {
-		stats = fs.statSync(filePath);
-		fd = fs.openSync(filePath, 'r');
-	} catch (_) {
+		stats = await statPromisify(filePath);
+		fd = await openPromisify(filePath, 'r');
+	} catch (error) {
 		throw new Error('Failed to open file');
 	}
 
 	const chunksNo = Math.ceil(stats.size / size);
 	for (let i = 0; i < chunksNo; i++) {
 		try {
-			await readBytes(fd, sharedBuffer);
-		} catch (_) {
+			// position = null means read from the first and auto increment
+			await readPromisify(fd, sharedBuffer, 0, size, i * size);
+		} catch (error) {
 			throw new Error('Failed to read bytes from file');
 		}
 
