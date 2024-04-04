500e2c65bea3ba10da1a280b1a85ee642dcbaa7f
Henry Heino <personalizedrefrigerator@gmail.com>
2024-04-04 15:36:54 -0700

    Chore: Refactor WarningBanner into a separate file
    This prerequisite to allowing shares to be accepted on mobile.

---
+++ packages/app-mobile/components/ScreenHeader/WarningBanner.test.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { WarningBannerComponent } from './WarningBanner';
+import Setting from '@joplin/lib/models/Setting';
+import NavService from '@joplin/lib/services/NavService';
+import { render, screen, userEvent } from '@testing-library/react-native';
+
+interface WrapperProps {
+	showMissingMasterKeyMessage?: boolean;
+	hasDisabledSyncItems?: boolean;
+	shouldUpgradeSyncTarget?: boolean;
+	showShouldUpgradeSyncTargetMessage?: boolean;
+	hasDisabledEncryptionItems?: boolean;
+	mustUpgradeAppMessage?: string;
+}
+
+const WarningBannerWrapper: React.FC<WrapperProps> = props => {
+	return <WarningBannerComponent
+		themeId={Setting.THEME_LIGHT}
+		showMissingMasterKeyMessage={props.showMissingMasterKeyMessage ?? false}
+		hasDisabledSyncItems={props.hasDisabledSyncItems ?? false}
+		shouldUpgradeSyncTarget={props.shouldUpgradeSyncTarget ?? false}
+		showShouldUpgradeSyncTargetMessage={props.showShouldUpgradeSyncTargetMessage ?? false}
+		hasDisabledEncryptionItems={props.hasDisabledEncryptionItems ?? false}
+		mustUpgradeAppMessage={props.mustUpgradeAppMessage ?? ''}
+	/>;
+};
+
+describe('WarningBanner', () => {
+	let navServiceMock: jest.Mock<(route: any)=> void>;
+	beforeEach(() => {
+		navServiceMock = jest.fn();
+		NavService.dispatch = navServiceMock;
+		jest.useFakeTimers();
+	});
+
+	test('the missing master key alert should link to the encryption config screen', async () => {
+		render(<WarningBannerWrapper showMissingMasterKeyMessage={true}/>);
+		expect(await screen.findAllByTestId('warning-box')).toHaveLength(1);
+
+		expect(navServiceMock).not.toHaveBeenCalled();
+
+		const masterKeyWarning = screen.getByText(/decryption password/);
+		const user = userEvent.setup();
+		await user.press(masterKeyWarning);
+
+		expect(navServiceMock.mock.lastCall).toMatchObject([{ routeName: 'EncryptionConfig' }]);
+	});
+});
---
+++ packages/app-mobile/components/ScreenHeader/WarningBanner.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { AppState } from '../../utils/types';
+import WarningBox from './WarningBox';
+import { _ } from '@joplin/lib/locale';
+import { showMissingMasterKeyMessage } from '@joplin/lib/services/e2ee/utils';
+import { localSyncInfoFromState } from '@joplin/lib/services/synchronizer/syncInfoUtils';
+import Setting from '@joplin/lib/models/Setting';
+
+interface Props {
+	themeId: number;
+	showMissingMasterKeyMessage: boolean;
+	hasDisabledSyncItems: boolean;
+	shouldUpgradeSyncTarget: boolean;
+	showShouldUpgradeSyncTargetMessage: boolean|undefined;
+	hasDisabledEncryptionItems: boolean;
+	mustUpgradeAppMessage: string;
+}
+
+
+export const WarningBannerComponent: React.FC<Props> = props => {
+	const warningComps = [];
+
+	const renderWarningBox = (screen: string, message: string) => {
+		return <WarningBox
+			key={screen}
+			themeId={props.themeId}
+			targetScreen={screen}
+			message={message}
+			testID='warning-box'
+		/>;
+	};
+
+	if (props.showMissingMasterKeyMessage) {
+		warningComps.push(renderWarningBox('EncryptionConfig', _('Press to set the decryption password.')));
+	}
+	if (props.hasDisabledSyncItems) {
+		warningComps.push(renderWarningBox('Status', _('Some items cannot be synchronised. Press for more info.')));
+	}
+	if (props.shouldUpgradeSyncTarget && props.showShouldUpgradeSyncTargetMessage !== false) {
+		warningComps.push(renderWarningBox('UpgradeSyncTarget', _('The sync target needs to be upgraded. Press this banner to proceed.')));
+	}
+	if (props.mustUpgradeAppMessage) {
+		warningComps.push(renderWarningBox('UpgradeApp', props.mustUpgradeAppMessage));
+	}
+	if (props.hasDisabledEncryptionItems) {
+		warningComps.push(renderWarningBox('Status', _('Some items cannot be decrypted.')));
+	}
+
+	return warningComps;
+};
+
+export default connect((state: AppState) => {
+	const syncInfo = localSyncInfoFromState(state);
+
+	return {
+		themeId: state.settings.themeId,
+		hasDisabledEncryptionItems: state.hasDisabledEncryptionItems,
+		noteSelectionEnabled: state.noteSelectionEnabled,
+		selectedFolderId: state.selectedFolderId,
+		notesParentType: state.notesParentType,
+		showMissingMasterKeyMessage: showMissingMasterKeyMessage(syncInfo, state.notLoadedMasterKeys),
+		hasDisabledSyncItems: state.hasDisabledSyncItems,
+		shouldUpgradeSyncTarget: state.settings['sync.upgradeState'] === Setting.SYNC_UPGRADE_STATE_SHOULD_DO,
+		mustUpgradeAppMessage: state.mustUpgradeAppMessage,
+	};
+})(WarningBannerComponent);
