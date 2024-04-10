310f0ac46bb062bf79da895a03f303b0ef095751
Henry Heino <henryuheino@gmail.com>
2024-04-10 11:00:22 -0700

    Fix alignment issues on iOS

--- packages/app-mobile/components/screens/ConfigScreen/plugins/PluginBox/index.tsx
+++ packages/app-mobile/components/screens/ConfigScreen/plugins/PluginBox/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Icon, Card, Chip, Text } from 'react-native-paper';
 import { _ } from '@joplin/lib/locale';
-import { Alert, Linking, StyleSheet, View } from 'react-native';
+import { Alert, Linking, View } from 'react-native';
 import { PluginItem } from '@joplin/lib/components/shared/config/plugins/types';
 import shim from '@joplin/lib/shim';
 import PluginService from '@joplin/lib/services/plugins/PluginService';
@@ -56,17 +56,9 @@ const onRecommendedPress = () => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
 const PluginIcon = (props: any) => <Icon {...props} source='puzzle'/>;
 
-const styles = StyleSheet.create({
-	versionText: {
-		opacity: 0.8,
-		marginLeft: 5,
-	},
-	titleWrapper: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'baseline',
-	},
-});
+const versionTextStyle = {
+	opacity: 0.8,
+};
 
 const PluginBox: React.FC<Props> = props => {
 	const manifest = props.item.manifest;
@@ -169,9 +161,9 @@ const PluginBox: React.FC<Props> = props => {
 
 	const updateStateIsIdle = props.updateState !== UpdateState.Idle;
 
-	const titleComponent = <View style={styles.titleWrapper}>
-		<Text variant='titleMedium'>{manifest.name}</Text><Text variant='bodySmall' style={styles.versionText}>v{manifest.version}</Text>
-	</View>;
+	const titleComponent = <>
+		{manifest.name} <Text variant='bodySmall' style={versionTextStyle}>v{manifest.version}</Text>
+	</>;
 	return (
 		<Card style={{ margin: 8, opacity: props.isCompatible ? undefined : 0.75 }} testID='plugin-card'>
 			<Card.Title
