aba3f0cc15f667f2aadba70e2798c3283ee519f1
pedr <pedrlz.frn@gmail.com>
2024-04-19 15:21:34 -0300

    fix error happening in one case of the loop stopping the whole process

--- packages/lib/services/rest/routes/notes.ts
+++ packages/lib/services/rest/routes/notes.ts
@@ -29,6 +29,7 @@ const { ErrorNotFound } = require('../utils/errors');
 import { fileUriToPath } from '@joplin/utils/url';
 import { NoteEntity, ResourceEntity } from '../../database/types';
 import { DownloadController } from '../../../downloadController';
+import { FetchBlobOptions } from '../../../types';
 
 const logger = Logger.create('routes/notes');
 
@@ -68,11 +69,11 @@ type RequestNote = {
 	stylesheets: any;
 };
 
-type FetchOptions = {
+interface FetchOptions extends FetchBlobOptions {
 	timeout?: number;
 	maxRedirects?: number;
 	downloadController?: DownloadController;
-};
+}
 
 
 type DownloadedMediaFile = {
@@ -315,20 +316,17 @@ async function downloadMediaFiles(urls: string[], fetchOptions?: FetchOptions, a
 }
 
 export async function createResourcesFromPaths(mediaFiles: DownloadedMediaFile[]) {
-	const resources: Promise<ResourceFromPath>[] = [];
-
-	for (const mediaFile of mediaFiles) {
+	const processFile = async (mediaFile: DownloadedMediaFile) => {
 		try {
-			resources.push(
-				shim.createResourceFromPath(mediaFile.path)
-					// eslint-disable-next-line
-					.then(resource => ({ ...mediaFile, resource }))
-			);
+			const resource = await shim.createResourceFromPath(mediaFile.path);
+			return { ...mediaFile, resource };
 		} catch (error) {
 			logger.warn(`Cannot create resource for ${mediaFile.originalUrl}`, error);
+			return { ...mediaFile, resource: null };
 		}
-	}
+	};
 
+	const resources = mediaFiles.map(processFile);
 	return Promise.all(resources);
 }
 
--- packages/lib/types.ts
+++ packages/lib/types.ts
@@ -1,3 +1,5 @@
+import { DownloadController } from './downloadController';
+
 export enum ApplicationPlatform {
 	Unknown = 0,
 	Windows = 1,
@@ -13,3 +15,13 @@ export enum ApplicationType {
 	Mobile = 2,
 	Cli = 3,
 }
+
+export type FetchBlobOptions = {
+	path?: string;
+	method?: string;
+	maxRedirects?: number;
+	timeout?: number;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
+	headers?: any;
+	downloadController?: DownloadController;
+};
