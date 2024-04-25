83b50aaa8e48261042c90fa7e985fa22de8caffb
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2024-04-25 05:31:48 -0700

    Chore: Migrate file system sync to TypeScript  (#10361)

--- packages/lib/shim.ts
+++ packages/lib/shim.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NoteEntity, ResourceEntity } from './services/database/types';
 import type FsDriverBase from './fs-driver-base';
+import type FileApiDriverLocal from './file-api-driver-local';
 
 export interface CreateResourceFromPathOptions {
 	resizeLargeImages?: 'always' | 'never' | 'ask';
@@ -260,8 +261,7 @@ const shim = {
 		throw new Error('Not implemented: fsDriver');
 	},
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
-	FileApiDriverLocal: null as any,
+	FileApiDriverLocal: null as typeof FileApiDriverLocal,
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
 	readLocalFileBase64: (_path: string): any => {
