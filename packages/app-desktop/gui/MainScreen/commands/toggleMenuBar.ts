17f2b7bb80e7830b2234e1afb93ecf0ed3c1bede
Alok Meshram <meshram.alok@gmail.com>
2024-04-27 14:39:39 -0700

    Removed isMac() check from the toggleMenuBar command implementation since the core implementation has moved

--- packages/app-desktop/gui/MainScreen/commands/toggleMenuBar.ts
+++ packages/app-desktop/gui/MainScreen/commands/toggleMenuBar.ts
@@ -1,5 +1,4 @@
 import { CommandDeclaration, CommandRuntime } from '@joplin/lib/services/CommandService';
-import shim from '@joplin/lib/shim';
 import Setting from '@joplin/lib/models/Setting';
 import { _ } from '@joplin/lib/locale';
 
@@ -11,9 +10,6 @@ export const declaration: CommandDeclaration = {
 export const runtime = (): CommandRuntime => {
 	return {
 		execute: async () => {
-			// Defensive code: macOS disallows hiding the menu bar, so ignore this Command
-			if (shim.isMac()) return;
-
 			Setting.toggle('showMenuBar');
 		},
 	};
