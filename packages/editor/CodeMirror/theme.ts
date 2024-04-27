1bb3632a70fc31174427ee27f90279e1b7bb36bd
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2024-04-27 03:43:37 -0700

    Mobile: Make editor styles closer to desktop (#10377)

--- packages/editor/CodeMirror/theme.ts
+++ packages/editor/CodeMirror/theme.ts
@@ -147,7 +147,7 @@ const createTheme = (theme: EditorTheme): Extension[] => {
 			borderColor: theme.colorFaded,
 			backgroundColor: 'rgba(155, 155, 155, 0.1)',
 
-			...(theme.isDesktop ? monospaceStyle : {}),
+			...monospaceStyle,
 		},
 
 		// CodeMirror wraps the existing inline span in an additional element.
@@ -161,7 +161,7 @@ const createTheme = (theme: EditorTheme): Extension[] => {
 			borderColor: isDarkTheme ? 'rgba(200, 200, 200, 0.5)' : 'rgba(100, 100, 100, 0.5)',
 			borderRadius: '4px',
 
-			...(theme.isDesktop ? monospaceStyle : {}),
+			...monospaceStyle,
 		},
 
 		'& .cm-mathBlock, & .cm-inlineMath': {
@@ -182,7 +182,7 @@ const createTheme = (theme: EditorTheme): Extension[] => {
 
 		// Override the default URL style when the URL is within a link
 		'& .tok-url.tok-link, & .tok-link.tok-meta, & .tok-link.tok-string': {
-			opacity: theme.isDesktop ? 0.6 : 1,
+			opacity: 0.6,
 		},
 
 		// Applying font size changes with CSS rather than the theme below works
@@ -244,7 +244,6 @@ const createTheme = (theme: EditorTheme): Extension[] => {
 		{
 			tag: tags.link,
 			color: theme.urlColor,
-			textDecoration: theme.isDesktop ? undefined : 'underline',
 		},
 		{
 			tag: [mathTag, inlineMathTag],
