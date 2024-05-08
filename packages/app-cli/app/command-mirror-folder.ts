2e711da050b30addc8850e3e040a6fde7f912d70
Henry Heino <personalizedrefrigerator@gmail.com>
2024-05-07 17:00:31 -0700

    CLI command for mirroring folders

---
+++ packages/app-cli/app/command-mirror-folder.ts
@@ -0,0 +1,31 @@
+import BaseCommand from './base-command';
+import BaseModel from '@joplin/lib/BaseModel';
+import app from './app';
+import { _ } from '@joplin/lib/locale';
+import FileMirroringService from '@joplin/lib/services/filesync/FileMirroringService';
+
+interface CommandArguments {
+	options: {
+		['no-watch']?: boolean;
+	};
+	folder?: string;
+	path: string;
+}
+
+class Command extends BaseCommand {
+	public override usage() {
+		return 'mirror-folder <path> [folder]';
+	}
+
+	public override description() {
+		return _('Syncs a local directory with the contents of [notebook]. If run from within Joplin, that directory is watched for changes.');
+	}
+
+	public override async action(args: CommandArguments) {
+		const folder = args.folder ? await app().loadItem(BaseModel.TYPE_FOLDER, args.folder) : { id: '' };
+		if (args.folder && !folder) throw new Error(_('Cannot find "%s".', args.folder));
+		await FileMirroringService.instance().mirrorFolder(args.path, folder.id);
+	}
+}
+
+module.exports = Command;
