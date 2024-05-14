3b6c53ca6357c0ca3100d1580e139145d1a5d8b6
Henry Heino <henryuheino@gmail.com>
2024-05-13 21:23:58 -0700

    WIP: Fix accepting encrypted shared notebooks on mobile

--- packages/app-mobile/services/e2ee/RSA.react-native.ts
+++ packages/app-mobile/services/e2ee/RSA.react-native.ts
@@ -4,6 +4,7 @@ const RnRSA = require('react-native-rsa-native').RSA;
 interface RSAKeyPair {
 	public: string;
 	private: string;
+	keySizeBytes: number;
 }
 
 const rsa: RSA = {
@@ -15,11 +16,78 @@ const rsa: RSA = {
 		if (!keys.private) throw new Error('No private key was generated');
 		if (!keys.public) throw new Error('No public key was generated');
 
-		return keys;
+		return rsa.loadKeys(keys.public, keys.private);
 	},
 
 	loadKeys: async (publicKey: string, privateKey: string): Promise<RSAKeyPair> => {
-		return { public: publicKey, private: privateKey };
+		const publicKeyContent = publicKey
+			// Remove header & footer
+			.replace(/(^|\n)----.*----(\n|$)/g, '')
+			.replace(/\n/g, '');
+		// RSA keys are formatted using ASN.1/DER.
+		// See https://www.rfc-editor.org/rfc/rfc3447.html#page-44,
+		//     https://crypto.stackexchange.com/a/18033, and
+		//     https://en.wikipedia.org/wiki/ASN.1
+		const publicKeyBytes = Buffer.from(publicKeyContent, 'base64');
+
+		// Note: Broken for mobile keys. Mobile keys use a different format:
+		//   0:d=0  hl=4 l= 290 cons: SEQUENCE
+		//   4:d=1  hl=2 l=  13 cons: SEQUENCE
+		//   6:d=2  hl=2 l=   9 prim: OBJECT            :rsaEncryption
+		//   17:d=2  hl=2 l=   0 prim: NULL
+		//   19:d=1  hl=4 l= 271 prim: BIT STRING
+		// (from cat pubKeyData | openssl asn1parse -inform PEM)
+
+		let cursor = 0;
+		const readTag = (expected: number) => {
+			// The last 5 bits give the tag type.
+			const tagValue = publicKeyBytes.readUInt8(cursor) & 0x1F;
+			cursor++;
+			if (tagValue !== expected) {
+				throw new Error(`Unexpected tag value at index ${cursor}. Was ${tagValue}, expected ${expected}`);
+			}
+			return tagValue;
+		};
+		const readLength = () => {
+			const lengthSpecifier = publicKeyBytes.readUInt8(cursor);
+			cursor++;
+
+			const longFormBit = !!(0x80 & lengthSpecifier);
+			let length;
+			if (longFormBit) {
+				// The byte length of the length field
+				const lengthFieldByteLength = 0x7F & lengthSpecifier;
+
+				if (lengthFieldByteLength === 1) {
+					length = publicKeyBytes.readUInt8(cursor);
+				} else if (lengthFieldByteLength === 2) {
+					length = publicKeyBytes.readUInt16BE(cursor);
+				} else if (lengthFieldByteLength === 3) {
+					length = publicKeyBytes.readUInt32BE(cursor);
+				} else {
+					throw new Error(`Unsupported length field size, ${lengthFieldByteLength}`);
+				}
+				cursor += lengthFieldByteLength;
+			} else {
+				length = lengthSpecifier;
+			}
+
+			return length;
+		};
+
+		readTag(16); // Sequence tag
+
+		// Length of the full key (including exponent).
+		readLength();
+
+		// Use a heuristic to determine the byte length used to generate the key.
+		// react-native-rsa-native only supports two key sizes: 256 and 512
+		// const byteLength = publicKeyContent.length < 400 ? 256 : 512;
+		readTag(2); // Integer tag
+
+		const length = readLength();
+
+		return { public: publicKey, private: privateKey, keySizeBytes: length };
 	},
 
 	encrypt: async (plaintextUtf8: string, rsaKeyPair: RSAKeyPair): Promise<string> => {
@@ -27,7 +95,36 @@ const rsa: RSA = {
 	},
 
 	decrypt: async (ciphertextBase64: string, rsaKeyPair: RSAKeyPair): Promise<string> => {
-		return RnRSA.decrypt(ciphertextBase64, rsaKeyPair.private);
+		// Multiply by 6/8: Each character corresponds to 6 bits, but there are 8 bits in a byte.
+		// Remove =s: Each = means "discard two bits" for up to 4 bits.
+		// See also https://en.wikipedia.org/wiki/Base64
+		const ciphertextLength = Math.floor(ciphertextBase64.replace(/=/g, '').length * 6 / 8);
+
+		const maximumEncryptedSize = rsaKeyPair.keySizeBytes; // Usually 256
+		if (ciphertextLength > maximumEncryptedSize) {
+			const ciphertextBuffer = Buffer.from(ciphertextBase64, 'base64');
+			// Use a numBlocks and blockSize that match node-rsa:
+			const numBlocks = Math.ceil(ciphertextBuffer.length / maximumEncryptedSize);
+			const blockSize = ciphertextBuffer.length / numBlocks;
+
+			const result: string[] = [];
+			for (let i = 0; i < numBlocks; i++) {
+				const cyphertextBlock = ciphertextBuffer.slice(
+					i * blockSize, Math.min(ciphertextBuffer.length, (i + 1) * blockSize),
+				);
+				const plainText = await RnRSA.decrypt(cyphertextBlock.toString('base64'), rsaKeyPair.private);
+
+				// On iOS, .decrypt fails without throwing or rejecting.
+				if (plainText === undefined) {
+					throw new Error('RN RSA: Decryption failed.');
+				}
+
+				result.push(plainText);
+			}
+			return result.join('');
+		} else {
+			return RnRSA.decrypt(ciphertextBase64, rsaKeyPair.private);
+		}
 	},
 
 	publicKey: (rsaKeyPair: RSAKeyPair): string => {
--- packages/lib/services/e2ee/ppkTestUtils.ts
+++ packages/lib/services/e2ee/ppkTestUtils.ts
@@ -30,6 +30,7 @@ export async function printTestData() {
 interface CheckTestDataOptions {
 	throwOnError?: boolean;
 	silent?: boolean;
+	testLabel?: string;
 }
 
 export async function checkTestData(data: TestData, options: CheckTestDataOptions = null) {
@@ -44,20 +45,25 @@ export async function checkTestData(data: TestData, options: CheckTestDataOption
 	let hasError = false;
 
 	const keyPair = await rsa().loadKeys(data.publicKey, data.privateKey);
-	const decrypted = await rsa().decrypt(data.ciphertext, keyPair);
-	if (decrypted !== data.plaintext) {
-		messages.push('RSA Tests: Data could not be decrypted');
-		messages.push('RSA Tests: Expected:', data.plaintext);
-		messages.push('RSA Tests: Got:', decrypted);
+	try {
+		const decrypted = await rsa().decrypt(data.ciphertext, keyPair);
+		if (decrypted !== data.plaintext) {
+			messages.push('RSA Tests: Data could not be decrypted');
+			messages.push('RSA Tests: Expected:', data.plaintext);
+			messages.push('RSA Tests: Got:', decrypted);
+			hasError = true;
+		} else {
+			messages.push('RSA Tests: Data could be decrypted');
+		}
+	} catch (error) {
 		hasError = true;
-	} else {
-		messages.push('RSA Tests: Data could be decrypted');
+		messages.push(`RSA Tests: Failed to decrypt data: Error: ${error}`);
 	}
 
 	// Then check that the public key can be used to encrypt new data, and then
 	// decrypt it with the private key.
 
-	{
+	try {
 		const encrypted = await rsa().encrypt('something else', keyPair);
 		const decrypted = await rsa().decrypt(encrypted, keyPair);
 		if (decrypted !== 'something else') {
@@ -68,10 +74,14 @@ export async function checkTestData(data: TestData, options: CheckTestDataOption
 		} else {
 			messages.push('RSA Tests: Data could be encrypted then decrypted');
 		}
+	} catch (error) {
+		hasError = true;
+		messages.push(`RSA Tests: Failed encrypt/decrypt of new data: ${error}`);
 	}
 
 	if (hasError && options.throwOnError) {
-		throw new Error(`Testing RSA failed: \n${messages.join('\n')}`);
+		const label = options.testLabel ? ` (test ${options.testLabel})` : '';
+		throw new Error(`Testing RSA failed${label}: \n${messages.join('\n')}`);
 	} else {
 		for (const msg of messages) {
 			if (hasError) {
@@ -95,11 +105,21 @@ const mobileData = {
 };
 
 // Data generated on desktop using node-rsa
-const desktopData = {
-	'publicKey': '-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAvLb8Lk0UBFEQ2UJsVMgKdbPYExhYqa87diBQiFBJglgNuZVi8/HX\nvpCVcH7BhdQKkA9Mh23SpNcYHR9JrzUTrn9Q21t1uj2J60+bfq1s0BA1wkS/xBPN\nrrLw2yCPpkZzNH8HcLx/MtMaOnOVfl5KqftXROzn+Vo3rrxNprd2ETLAxr+CC6SE\nTJiiP8ovUfH+TKZ3P2nkSyBy4oY24h4HA+wVnj12DspE8CMOXCyBUxlG2ki2c/sK\nDSDla3oEjB8QdpBKhIXD/Bb4MpLHfaby7O/eYjrteB8g6JU01JDsnQoomLe4FdCU\nnYK5sFNUQ89e05lMa3yxZWV3mXAVUi/mFwIDAQAB\n-----END RSA PUBLIC KEY-----',
-	'privateKey': '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAvLb8Lk0UBFEQ2UJsVMgKdbPYExhYqa87diBQiFBJglgNuZVi\n8/HXvpCVcH7BhdQKkA9Mh23SpNcYHR9JrzUTrn9Q21t1uj2J60+bfq1s0BA1wkS/\nxBPNrrLw2yCPpkZzNH8HcLx/MtMaOnOVfl5KqftXROzn+Vo3rrxNprd2ETLAxr+C\nC6SETJiiP8ovUfH+TKZ3P2nkSyBy4oY24h4HA+wVnj12DspE8CMOXCyBUxlG2ki2\nc/sKDSDla3oEjB8QdpBKhIXD/Bb4MpLHfaby7O/eYjrteB8g6JU01JDsnQoomLe4\nFdCUnYK5sFNUQ89e05lMa3yxZWV3mXAVUi/mFwIDAQABAoIBAQCMIm2djEsi8XfL\nfZGoW2u4/7WiaF/ekWtcSp7Cuqv7iJuYhiAW+i21KvRttxLJ6C130ISJxLm5Aqi7\nZ3J2ErnsyEoouf/wLqZuAI19QhcdYgwpmJe2aOZBpktIzSMe3A3Mm8/QnYjvGufN\nI+uNDUPwed3SJwITnjTfIqGe/XlFRtvCIurp7vDbh4kTASpg3M8kjXiznVMncC4D\nWNg0vRnj53zfiwRkxZwMubYa25qR2Kt/S703QJVh/ctccbuZ6GyRbtgBlGuxuwX8\n1aAMBScMBMFtU+Xpb55EgsFu6Snzs6yrFKXMybR6ea31CtzBZvjZdGKO1Yxh2Dlf\n7f1PWg9hAoGBAPpLMPXFIsUtq6iwh1slDXZ+IgIIYgs/JkYvDROFUbp9qrnGcQBi\nIC9Dnf7fYwVnYQ18+gz2Qcjn9e+5Y/4aBPW2PjAYurdBMNGlEEKMbl3Ocad9h8mL\nI2MRBFOpwZaVhm8PJBZkhhfkNouh13KRyr9vS4egTdEBOZGR43GSrZ9fAoGBAMEE\nZVqaTg3jAh5GJBcxKGjz77BN0X5wRkYO9OU4DYuBq+sz+JVytLTMMDTtdxbJy11b\nH3wOuz1SugtouuJZ4hmwfXuj+2AFh28tiBcz6nik2pQYgdgowP7eqXor4T+Nd0mP\nzEqa7+W62pNAVlA0DiR8obPmzKNwBm2OZXxR6wxJAoGAC6T95SFDydqjFtkHoxTp\nOG8L0/5h2VYZyMAdop/cOonoLHZwAW2PQ8OokRgBelnh6Qe8dmfqjZdFGN8OKN87\nBddxszkjTq1IwSglxoLUC6c0IG+1ponDnrNG+UF3kTLpqzcQHb6Vgn0KkJp59ImV\n3iwmXmv10th0vjIEW99QFo8CgYEAkqF/SdwtbdlF06/fXQsIMusV7K7Bdrdee3yD\nSNtTVub0ruK1dvtEEpGIEb1QmixE5TADdCBQ2B5Pnbk7OBemb3OncFU7809f+vLx\nDwdumaZLMvSHN6qGK1kGEPziyn/y3hxyyz52/uP7hp/6skVJdSiFQ4ETdxn0mCf0\nKwSkdpkCgYBDMXv2N3eF2IElZkN+8rQYHqWck1HqTcSnlrHS0i0uPQduQa+K/O+G\nVE2S8htp4/D0Gd3EwuDBIT3vUvnx7YBuMX1fXwU0m7oKKOyQqhxfrt9t7BEh2j5r\n5pvRU7dTKVbua78w1sQQMtYnWUyukBlaF/IpHPi5hwCjmDQR1EJY4w==\n-----END RSA PRIVATE KEY-----',
-	'plaintext': 'just testing',
-	'ciphertext': 'PRqiQjxnQMukoYPA9XtlGcgAjwuDJd24GtJ3iO2qhh0HnbPnx3c8ZaGWJyV1ejZCwIWv509js7sCTHtXqeGkZr//Db6oOIyi77VzRwvzPxReHPefF0rX62uMh+zTmQW7KSrFeAvtnpWiDcyynUtwycgrZcQCHZoEmSSyc3cyj09HgqEoSQb0BOc8daR0aXwOpgXsB8ypf3+m23U1gZmIyl0glymTN9h1jopV9dRtw5ufcc4ve/hHKp0gbaT2OaRKOLr6AXmbDGwkF5bsvjV+v4tTkj96OUjoG9qUMQh/JYRMl7mxJriUB3Jc6WHEKRVPQYAIZODfEOy3rkHwWAcYjA==',
+const desktopData: Record<string, TestData> = {
+	short: {
+		'publicKey': '-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAvLb8Lk0UBFEQ2UJsVMgKdbPYExhYqa87diBQiFBJglgNuZVi8/HX\nvpCVcH7BhdQKkA9Mh23SpNcYHR9JrzUTrn9Q21t1uj2J60+bfq1s0BA1wkS/xBPN\nrrLw2yCPpkZzNH8HcLx/MtMaOnOVfl5KqftXROzn+Vo3rrxNprd2ETLAxr+CC6SE\nTJiiP8ovUfH+TKZ3P2nkSyBy4oY24h4HA+wVnj12DspE8CMOXCyBUxlG2ki2c/sK\nDSDla3oEjB8QdpBKhIXD/Bb4MpLHfaby7O/eYjrteB8g6JU01JDsnQoomLe4FdCU\nnYK5sFNUQ89e05lMa3yxZWV3mXAVUi/mFwIDAQAB\n-----END RSA PUBLIC KEY-----',
+		'privateKey': '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAvLb8Lk0UBFEQ2UJsVMgKdbPYExhYqa87diBQiFBJglgNuZVi\n8/HXvpCVcH7BhdQKkA9Mh23SpNcYHR9JrzUTrn9Q21t1uj2J60+bfq1s0BA1wkS/\nxBPNrrLw2yCPpkZzNH8HcLx/MtMaOnOVfl5KqftXROzn+Vo3rrxNprd2ETLAxr+C\nC6SETJiiP8ovUfH+TKZ3P2nkSyBy4oY24h4HA+wVnj12DspE8CMOXCyBUxlG2ki2\nc/sKDSDla3oEjB8QdpBKhIXD/Bb4MpLHfaby7O/eYjrteB8g6JU01JDsnQoomLe4\nFdCUnYK5sFNUQ89e05lMa3yxZWV3mXAVUi/mFwIDAQABAoIBAQCMIm2djEsi8XfL\nfZGoW2u4/7WiaF/ekWtcSp7Cuqv7iJuYhiAW+i21KvRttxLJ6C130ISJxLm5Aqi7\nZ3J2ErnsyEoouf/wLqZuAI19QhcdYgwpmJe2aOZBpktIzSMe3A3Mm8/QnYjvGufN\nI+uNDUPwed3SJwITnjTfIqGe/XlFRtvCIurp7vDbh4kTASpg3M8kjXiznVMncC4D\nWNg0vRnj53zfiwRkxZwMubYa25qR2Kt/S703QJVh/ctccbuZ6GyRbtgBlGuxuwX8\n1aAMBScMBMFtU+Xpb55EgsFu6Snzs6yrFKXMybR6ea31CtzBZvjZdGKO1Yxh2Dlf\n7f1PWg9hAoGBAPpLMPXFIsUtq6iwh1slDXZ+IgIIYgs/JkYvDROFUbp9qrnGcQBi\nIC9Dnf7fYwVnYQ18+gz2Qcjn9e+5Y/4aBPW2PjAYurdBMNGlEEKMbl3Ocad9h8mL\nI2MRBFOpwZaVhm8PJBZkhhfkNouh13KRyr9vS4egTdEBOZGR43GSrZ9fAoGBAMEE\nZVqaTg3jAh5GJBcxKGjz77BN0X5wRkYO9OU4DYuBq+sz+JVytLTMMDTtdxbJy11b\nH3wOuz1SugtouuJZ4hmwfXuj+2AFh28tiBcz6nik2pQYgdgowP7eqXor4T+Nd0mP\nzEqa7+W62pNAVlA0DiR8obPmzKNwBm2OZXxR6wxJAoGAC6T95SFDydqjFtkHoxTp\nOG8L0/5h2VYZyMAdop/cOonoLHZwAW2PQ8OokRgBelnh6Qe8dmfqjZdFGN8OKN87\nBddxszkjTq1IwSglxoLUC6c0IG+1ponDnrNG+UF3kTLpqzcQHb6Vgn0KkJp59ImV\n3iwmXmv10th0vjIEW99QFo8CgYEAkqF/SdwtbdlF06/fXQsIMusV7K7Bdrdee3yD\nSNtTVub0ruK1dvtEEpGIEb1QmixE5TADdCBQ2B5Pnbk7OBemb3OncFU7809f+vLx\nDwdumaZLMvSHN6qGK1kGEPziyn/y3hxyyz52/uP7hp/6skVJdSiFQ4ETdxn0mCf0\nKwSkdpkCgYBDMXv2N3eF2IElZkN+8rQYHqWck1HqTcSnlrHS0i0uPQduQa+K/O+G\nVE2S8htp4/D0Gd3EwuDBIT3vUvnx7YBuMX1fXwU0m7oKKOyQqhxfrt9t7BEh2j5r\n5pvRU7dTKVbua78w1sQQMtYnWUyukBlaF/IpHPi5hwCjmDQR1EJY4w==\n-----END RSA PRIVATE KEY-----',
+		'plaintext': 'just testing',
+		'ciphertext': 'PRqiQjxnQMukoYPA9XtlGcgAjwuDJd24GtJ3iO2qhh0HnbPnx3c8ZaGWJyV1ejZCwIWv509js7sCTHtXqeGkZr//Db6oOIyi77VzRwvzPxReHPefF0rX62uMh+zTmQW7KSrFeAvtnpWiDcyynUtwycgrZcQCHZoEmSSyc3cyj09HgqEoSQb0BOc8daR0aXwOpgXsB8ypf3+m23U1gZmIyl0glymTN9h1jopV9dRtw5ufcc4ve/hHKp0gbaT2OaRKOLr6AXmbDGwkF5bsvjV+v4tTkj96OUjoG9qUMQh/JYRMl7mxJriUB3Jc6WHEKRVPQYAIZODfEOy3rkHwWAcYjA==',
+	},
+
+	// Desktop's node-rsa supports ECB-mode long data encryption. For now, mobile only needs to support decrypting this data.
+	long: {
+		'publicKey': '-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAieWC6SdHvTIbaqfs1fA8sFJKOEfXKE1Sm44AhwfGa7LjCaO0bZo2\n56NiV2ljGRAsVMUozmlwkMNOnhi1rBZXRIBb02xQUgIQWnSMRW44EUiFDNY6hQo0\nsJISDbi7Gq6aEEr952AcH7pacuilqyPLW1EiW0dHxksZXVlDjGwGIaBTzNDPIMbO\nJ1hBYWsZzzVsHq8YBnFG3lu5cL6ckbXUpw188k/HixQIPrqyofANDW0GP5AKxDd/\n6Kv+WfFgEhAuEbpNpavPFz4a3QCX2lfFpw3QBuPk1Ye+XByuDdwdN31KV07Fun6E\nyfvtCite0gJEcpjUS6iCPSAvf41ViRUXRwIDAQAB\n-----END RSA PUBLIC KEY-----',
+		'privateKey': '-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAieWC6SdHvTIbaqfs1fA8sFJKOEfXKE1Sm44AhwfGa7LjCaO0\nbZo256NiV2ljGRAsVMUozmlwkMNOnhi1rBZXRIBb02xQUgIQWnSMRW44EUiFDNY6\nhQo0sJISDbi7Gq6aEEr952AcH7pacuilqyPLW1EiW0dHxksZXVlDjGwGIaBTzNDP\nIMbOJ1hBYWsZzzVsHq8YBnFG3lu5cL6ckbXUpw188k/HixQIPrqyofANDW0GP5AK\nxDd/6Kv+WfFgEhAuEbpNpavPFz4a3QCX2lfFpw3QBuPk1Ye+XByuDdwdN31KV07F\nun6EyfvtCite0gJEcpjUS6iCPSAvf41ViRUXRwIDAQABAoIBAET+4BR2ge+JHays\n3tRggo0ab2Zfk6jnn1iToXHukvoaJH0TOtjIG4ak+jSQV3QX4oZ7q6IzoY3dGv4B\nkQzMlNeCfe94N2wA+lu4CuNGdak4JtI4cklUxO4/9+aDsqJ0EEPsscPOE+RQIqdx\n/kp1+27yVoVjUedGdid93U1qU1sJF9HcshwwJeieyhx+4qJEDsoN625UkjasItuS\npYPEK1L326HiENjI0u/biR/vzCvivkElylOtPcdVj42NcksyHAzNRoYT278ZyddA\nf1PNkfuZs1nK91JbEuE35YghBc8ZuN+ihyd2qmi2JDyEv4t8Dm6aY5VU8Vf4FZTc\nRS3/dXkCgYEAyQkpgg/tPPAuEVTnwPlMhVkjIvHQ8p4LFCoVo/WvYtH1RI5UArVK\nElOiwG5+m7xHfzDOSTK1kcvLOXfvRrzPv9spipAGzNHM8JqDAhdbsL19Sy/1G0C9\ntU1xx22Llz8TBD5uLLcA4Qcf5qgO3V93aVn8QarjV0iEbRb4wNxwvJUCgYEAr5kf\n6XL7avCBfOYiQAYQ3Xr976EbFRssnrBs9TfPFZlklXfG0OPsFFhDYm5VBDVir8mo\nFTuKPbVsrffmKqjamgTfCl4hOE3QlzrUSQYsNrEnSdiL3SCiHcJdDxz9AxmvO8IF\nZtKGzF7O5th1RV7fMBwEUZ0CyogcZjFC0Uk28WsCgYBhhcBNB8zeMuUmIzoJsuYc\nChGxcf0atPBK5993mkdqeM+yYhj+91LncQsSrodcpQlJ+jMX7zGFeIctabD5b76Y\nI4kTXqrt0RjJ3yqOaSZhpoqRXsJYZh+hQ+BeDhYZWM+wz1hC+MPEZeBj1ELhwN38\njVqBwJRoUxVqlio15LoLwQKBgHO3gOQitbQ/UdWxApkWZj7OMlGb5XD/O2RH90WR\nxZICR9qH2CkISicOEznmY3gmhiY5yd5UIkGQMVJjrvYl2AjvdNwsM9Aa138RcZu9\nyZ0xGSb84Q6T1sGtuA2hmMpWPrgun8imq9Y6FDbkJkPUxVe63s25noFBORXPGK06\nhk6JAoGBAKC/RNHfs9Fd8vA2fHujDRwzaTgKYkBTwDx4uLIUXCUxloQfILryeQI7\n9g1Xfh/ru2TpkKd9i2BZXYNCKBm0OUosH79kkqMMDZLpkYQ2rv+RZgZeCwVjpbJR\n/8NdeOUD9zrIxblqnYBQwoeF5syXSaM0P9VrsYsygqvN2Xp1mhdW\n-----END RSA PRIVATE KEY-----',
+		'plaintext': 'testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ testing... ⌨️ ',
+		'ciphertext': 'dfZDm+u0yV4jZpx9Q456bF2YDL6IMCsCeq4wmlpFLDsWwjY119q6pZVKMGj1ZG4/u13nlxhBxnTV69Zu4w7fNNefCdBl+ZdsIWrfX7zUON98yzjwFflWDX/vydztatHPA9qlwG15V68HU53q6V3hgKnm3Jv1azuwMkH3QupDd1df2JfkmQVVe9qR9Me6Bv+NSFt6Mccimaf2K8lqw83zKnLIJz1LoP/Koavzd5Xejp66E8Cv8WdAwgHDuWnWHyiaYFbW5uL65VyGTQFO4vQn7SJj8QE9PqvknJ5XPRVXWnjEbquMMdo8o48/M1l17KwbaES95vIOJKKO1EuUK7kT9l6bncSa4PlmzW4Bb5OcjzKBy8ovN+lf2a1B/z+Ob3Os4pv5FW7X9p1YZg1XluqolOqG3AuAbTmexMcuhkI6p3nC7FW472Wa2XbrIeDM0pWOxpKoDWDMT9vMbUVv2ZRVSj3Gk3KYwv3av/kkoNcmzRlRKgYG+zM1hYBVy+eqs55HHvnZYvx6f+PtXfPGMqxtxs2eXRJdmLuLyPIlhrLk+px9Z1QplxOovjgt5PtrJb9d/vNEjg1DIdpIpruEZQ6LbhwP94vSpGE8wbVmszakrQZpCsUn0vKhGDVBvAr4DkKET8PDfxclrlflN4GnWDN5Qal3P8gnMTAJ9NnAPCQydyxpeUDaVXMT8Ir7orrJzUCoFzltUkw/2deTPbnsM9GOAWFtjgUWg9YO7w23pUy1XYl1/eHSucOXh8Z8jLSS7lOk08X0NCYjDdeAzrlJANckTa5SJ9wWsUBUwIffJeK0oMk2qze1vu+oEb4VVbWxUEr/v8aLJSA5nvSIYknrLbHtqh7eoU0q5tfvhDJBxOQ8Xnuik+0p00SWCwoGJYM8RyWzPTUJ5QqAHA4b3gMPDHMXKTD1lyPJS7nknujaDb3+qFPqeKXJqhdlxfUdvNeuCVfEiqQFgy9Dxz0icpZdITuTNsJFn+hA1ni2rj/SldntxKizfPnTVxaMBLPCbzcX4BjZeHeL+DQqYsOM645/I8BPg3e6MsPbDOwkPq84VqorNsdNRyuh4rcdpLr5rHlyTFIGVzrJRvaiCQMuGW9pXN5vtsYgHpVYgH904iLOBxbu3dSMCSwSY5ciDM9M7qPD0trrLTiMTZLkgcmH6RQY9y/RpYOxbp4UbspOc5xNxnsdh341zuN5s8/bGS0EOMqkPNCtQ4B0EQg8/xIZqHdJMI0infNdkjmiMCQHJnDCdvImZPvrJsblXfGNqrWYxoyH68cXLl9Z4AE74vyexeT47yMEp9UG17W1/jBSlWUEj7uQBklhA/f6yWIYf4Mx8jbHon0+SVa+6ZPRe2OVZya7hnLJRg==',
+	},
 };
 
 // cSpell:enable
@@ -116,10 +136,13 @@ export const runIntegrationTests = async (silent = false) => {
 	log('RSA Tests: Running integration tests...');
 
 	log('RSA Tests: Decrypting and encrypting using desktop data...');
-	await checkTestData(desktopData, { silent, throwOnError: true });
+	for (const testLabel in desktopData) {
+		log(`RSA Tests: Running desktop test data case ${testLabel}...`);
+		await checkTestData(desktopData[testLabel], { silent, testLabel, throwOnError: true });
+	}
 
 	log('RSA Tests: Decrypting and encrypting using mobile data...');
-	await checkTestData(mobileData, { silent, throwOnError: true });
+	await checkTestData(mobileData, { silent, testLabel: 'mobile data', throwOnError: true });
 
 	log('RSA Tests: Decrypting and encrypting using local data...');
 	const newData = await createTestData();
