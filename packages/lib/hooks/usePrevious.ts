652add9af273bb447a5b4f7f24b67c3bd25f8e6c
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2024-05-20 17:28:19 -0700

    Chore: Desktop: Fix eslint issues and strengthen types in NoteEditor.tsx (#10449)

--- packages/lib/hooks/usePrevious.ts
+++ packages/lib/hooks/usePrevious.ts
@@ -2,9 +2,8 @@ import shim from '../shim';
 
 const { useRef, useEffect } = shim.react();
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
-const usePrevious = (value: any, initialValue: any = null) => {
-	const ref = useRef(initialValue);
+const usePrevious = <T> (value: T, initialValue: T = null) => {
+	const ref = useRef<T>(initialValue);
 	useEffect(() => {
 		ref.current = value;
 	});
