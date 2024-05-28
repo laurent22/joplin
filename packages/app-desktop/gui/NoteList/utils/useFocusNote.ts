c3b26f5ab9f5f344adc42bb080fdacaa3bab98a9
Henry Heino <personalizedrefrigerator@gmail.com>
2024-05-28 13:26:28 -0700

    Desktop: Fixes #10514: Fix useFocusNote

--- packages/app-desktop/gui/NoteList/utils/useFocusNote.ts
+++ packages/app-desktop/gui/NoteList/utils/useFocusNote.ts
@@ -1,11 +1,17 @@
 import shim from '@joplin/lib/shim';
 import { useRef, useCallback, MutableRefObject } from 'react';
 import { focus } from '@joplin/lib/utils/focusHandler';
+import { NoteEntity } from '@joplin/lib/services/database/types';
 
 export type FocusNote = (noteId: string)=> void;
 
-const useFocusNote = (itemRefs: MutableRefObject<Record<string, HTMLDivElement>>) => {
+type ItemRefs = MutableRefObject<Record<string, HTMLDivElement>>;
+type OnMakeIndexVisible = (i: number)=> void;
+const useFocusNote = (itemRefs: ItemRefs, notes: NoteEntity[], makeItemIndexVisible: OnMakeIndexVisible) => {
 	const focusItemIID = useRef(null);
+
+	const notesRef = useRef(notes);
+	notesRef.current = notes;
 
 	const focusNote: FocusNote = useCallback((noteId: string) => {
 		// - We need to focus the item manually otherwise focus might be lost when the
@@ -14,6 +20,11 @@ const useFocusNote = (itemRefs: MutableRefObject<Record<string, HTMLDivElement>>
 		//   of items might lag behind and so the ref is not yet available at this point.
 
 		if (!itemRefs.current[noteId]) {
+			const targetIndex = notesRef.current.findIndex(note => note.id === noteId);
+			if (targetIndex > -1) {
+				makeItemIndexVisible(targetIndex);
+			}
+
 			if (focusItemIID.current) shim.clearInterval(focusItemIID.current);
 			focusItemIID.current = shim.setInterval(() => {
 				if (itemRefs.current[noteId]) {
@@ -26,7 +37,7 @@ const useFocusNote = (itemRefs: MutableRefObject<Record<string, HTMLDivElement>>
 			if (focusItemIID.current) shim.clearInterval(focusItemIID.current);
 			focus('useFocusNote2', itemRefs.current[noteId]);
 		}
-	}, [itemRefs]);
+	}, [itemRefs, makeItemIndexVisible]);
 
 	return focusNote;
 };
