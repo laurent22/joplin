818fa602c61ab99cd61e5fd653b0ce637f36681a
Henry Heino <personalizedrefrigerator@gmail.com>
2024-05-29 10:44:42 -0700

    Tab indentation

--- packages/app-mobile/android/app/src/main/java/net/cozic/joplin/debug/DebugPackage.java
+++ packages/app-mobile/android/app/src/main/java/net/cozic/joplin/debug/DebugPackage.java
@@ -21,40 +21,40 @@ import java.util.Map;
 
 public class DebugPackage implements ReactPackage {
 
-    @NonNull
-    @Override
-    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-        return Collections.singletonList(new DebugModule(reactContext));
-    }
+	@NonNull
+	@Override
+	public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+		return Collections.singletonList(new DebugModule(reactContext));
+	}
 
-    @NonNull
-    @Override
-    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
-        return Collections.emptyList();
-    }
+	@NonNull
+	@Override
+	public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+		return Collections.emptyList();
+	}
 
-    public static class DebugModule extends ReactContextBaseJavaModule {
-        public DebugModule(@NonNull ReactApplicationContext context) {
-            super(context);
-        }
+	public static class DebugModule extends ReactContextBaseJavaModule {
+		public DebugModule(@NonNull ReactApplicationContext context) {
+			super(context);
+		}
 
-        @Override
+		@Override
 		public Map<String, Object> getConstants() {
-            final Map<String, Object> result = new HashMap<String, Object>();
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                PackageInfo webViewPackage = WebView.getCurrentWebViewPackage();
+			final Map<String, Object> result = new HashMap<String, Object>();
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+				PackageInfo webViewPackage = WebView.getCurrentWebViewPackage();
 
-                if (webViewPackage != null) {
-                    result.put("webViewVersion", webViewPackage.versionName);
-                }
-            }
-            return result;
-        }
+				if (webViewPackage != null) {
+					result.put("webViewVersion", webViewPackage.versionName);
+				}
+			}
+			return result;
+		}
 
-        @NonNull
-        @Override
-        public String getName() {
-            return "DebugModule";
-        }
-    }
+		@NonNull
+		@Override
+		public String getName() {
+			return "DebugModule";
+		}
+	}
 }
