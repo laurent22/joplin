1b5f16a4d195554043a73a714cbb0bbb7660cd53
Henry Heino <personalizedrefrigerator@gmail.com>
2024-06-04 14:17:39 -0700

    Fix tests

--- packages/app-mobile/plugins/loadPlugins.test.ts
+++ packages/app-mobile/plugins/loadPlugins.test.ts
@@ -1,13 +1,12 @@
 import Setting from '@joplin/lib/models/Setting';
 import PluginService, { defaultPluginSetting } from '@joplin/lib/services/plugins/PluginService';
-import Plugin from '@joplin/lib/services/plugins/Plugin';
 import { PluginManifest } from '@joplin/lib/services/plugins/utils/types';
 import { setupDatabaseAndSynchronizer, switchClient } from '@joplin/lib/testing/test-utils';
 import { writeFile } from 'fs-extra';
 import { join } from 'path';
 import loadPlugins, { Props as LoadPluginsProps } from './loadPlugins';
-import BasePluginRunner from '@joplin/lib/services/plugins/BasePluginRunner';
 import createMockReduxStore from '../utils/testing/createMockReduxStore';
+import MockPluginRunner from './testing/MockPluginRunner';
 
 const setPluginEnabled = (id: string, enabled: boolean) => {
 	const newPluginStates = {
@@ -30,22 +29,11 @@ const addPluginWithManifest = async (manifest: PluginManifest, enabled: boolean)
 			onStart: async function() { },
 		});
 	`;
-	const pluginPath = join(Setting.value('pluginDir'), 'plugin.js');
+	const pluginPath = join(Setting.value('pluginDir'), `${manifest.id}.js`);
 	await writeFile(pluginPath, pluginSource, 'utf-8');
 
 	setPluginEnabled(manifest.id, enabled);
 };
-
-class MockPluginRunner extends BasePluginRunner {
-	public runningPluginIds: string[] = [];
-	public override run = jest.fn(async (plugin: Plugin) => {
-		this.runningPluginIds.push(plugin.manifest.id);
-	});
-	public override stop = jest.fn(async (plugin: Plugin) => {
-		this.runningPluginIds = this.runningPluginIds.filter(id => id !== plugin.manifest.id);
-	});
-	public override async waitForSandboxCalls() {}
-}
 
 const defaultManifestProperties = {
 	manifest_version: 1,
@@ -58,10 +46,13 @@ describe('loadPlugins', () => {
 	beforeEach(async () => {
 		await setupDatabaseAndSynchronizer(1);
 		await switchClient(1);
+	});
 
+	afterEach(async () => {
 		for (const id of PluginService.instance().pluginIds) {
 			await PluginService.instance().unloadPlugin(id);
 		}
+		await PluginService.instance().destroy();
 	});
 
 	test('should load only enabled plugins', async () => {
@@ -70,9 +61,11 @@ describe('loadPlugins', () => {
 			id: 'this.is.a.test.1',
 			name: 'Disabled Plugin',
 		}, false);
+
+		const enabledPluginId = 'this.is.a.test.2';
 		await addPluginWithManifest({
 			...defaultManifestProperties,
-			id: 'this.is.a.test.2',
+			id: enabledPluginId,
 			name: 'Enabled Plugin',
 		}, true);
 
@@ -86,17 +79,21 @@ describe('loadPlugins', () => {
 			reloadAll: false,
 			cancelEvent: { cancelled: false },
 		};
+		expect(Object.keys(PluginService.instance().plugins)).toHaveLength(0);
 
 		await loadPlugins(loadPluginsOptions);
+		await pluginRunner.waitForAllToBeRunning([enabledPluginId]);
 
-		expect(pluginRunner.runningPluginIds).toMatchObject(['this.is.a.test.2']);
+		expect(pluginRunner.runningPluginIds).toMatchObject([enabledPluginId]);
 		// No plugins were running before, so none should be stopped.
-		expect(pluginRunner.stop).toHaveBeenCalledTimes(0);
-		expect(pluginRunner.run).toHaveBeenCalledTimes(1);
+		expect(pluginRunner.stopCalledTimes).toBe(0);
 
 		// Loading again should not re-run plugins
 		await loadPlugins(loadPluginsOptions);
-		expect(pluginRunner.run).toHaveBeenCalledTimes(1);
+
+		// Should have tried to stop at most the disabled plugin (which is a no-op).
+		expect(pluginRunner.stopCalledTimes).toBe(1);
+		expect(pluginRunner.runningPluginIds).toMatchObject([enabledPluginId]);
 	});
 
 	test('should reload all plugins when reloadAll is true', async () => {
@@ -104,7 +101,7 @@ describe('loadPlugins', () => {
 		for (let i = 0; i < enabledCount; i++) {
 			await addPluginWithManifest({
 				...defaultManifestProperties,
-				id: `test.plugin.${i}`,
+				id: `joplin.test.plugin.${i}`,
 				name: `Enabled Plugin ${i}`,
 			}, true);
 		}
@@ -113,7 +110,7 @@ describe('loadPlugins', () => {
 		for (let i = 0; i < disabledCount; i++) {
 			await addPluginWithManifest({
 				...defaultManifestProperties,
-				id: `test.plugin.disabled.${i}`,
+				id: `joplin.test.plugin.disabled.${i}`,
 				name: `Disabled Plugin ${i}`,
 			}, false);
 		}
@@ -128,19 +125,24 @@ describe('loadPlugins', () => {
 			cancelEvent: { cancelled: false },
 		};
 		await loadPlugins(loadPluginsOptions);
+		let expectedRunningIds = ['joplin.test.plugin.0', 'joplin.test.plugin.1', 'joplin.test.plugin.2'];
+		await pluginRunner.waitForAllToBeRunning(expectedRunningIds);
 
-		expect(pluginRunner.runningPluginIds.sort()).toMatchObject(['test.plugin.0', 'test.plugin.1', 'test.plugin.2']);
+		// No additional plugins should be running.
+		expect([...pluginRunner.runningPluginIds].sort()).toMatchObject(expectedRunningIds);
+
 		// No plugins were running before -- there were no plugins to stop
-		expect(pluginRunner.stop).toHaveBeenCalledTimes(0);
-		expect(pluginRunner.run).toHaveBeenCalledTimes(3);
+		expect(pluginRunner.stopCalledTimes).toBe(0);
+
+		// Enabling a plugin and reloading it should cause all plugins to load.
+		setPluginEnabled('joplin.test.plugin.disabled.2', true);
+		await loadPlugins({ ...loadPluginsOptions, pluginSettings: Setting.value('plugins.states') });
+		expectedRunningIds = ['joplin.test.plugin.0', 'joplin.test.plugin.1', 'joplin.test.plugin.2', 'joplin.test.plugin.disabled.2'];
+		await pluginRunner.waitForAllToBeRunning(expectedRunningIds);
 
 		// Reloading all should stop all plugins and rerun enabled plugins, even
 		// if not enabled previously.
-		setPluginEnabled('test.plugin.disabled.2', true);
-		await loadPlugins(loadPluginsOptions);
-
-		expect(pluginRunner.stop).toHaveBeenCalledTimes(3);
-		expect(pluginRunner.run).toHaveBeenCalledTimes(4);
-		expect(pluginRunner.runningPluginIds.includes('test.plugin.disabled.2')).toBe(true);
+		expect(pluginRunner.stopCalledTimes).toBe(disabledCount + enabledCount);
+		expect([...pluginRunner.runningPluginIds].sort()).toMatchObject(expectedRunningIds);
 	});
 });
