19f0b667b1a8dc25378a33b358574a4801314041
pedr <pedrlz.frn@gmail.com>
2024-06-04 05:55:57 -0300

    Server: Fixes #10118: Missing record validation before trying to add item to user (#10471)

--- packages/server/src/models/UserItemModel.ts
+++ packages/server/src/models/UserItemModel.ts
@@ -114,6 +114,9 @@ export default class UserItemModel extends BaseModel<UserItem> {
 
 	public async add(userId: Uuid, itemId: Uuid, options: SaveOptions = {}): Promise<void> {
 		const item = await this.models().item().load(itemId, { fields: ['id', 'name'] });
+		if (!item) {
+			throw new ErrorNotFound(`No such item: ${itemId}`);
+		}
 		await this.addMulti(userId, [item], options);
 	}
 
