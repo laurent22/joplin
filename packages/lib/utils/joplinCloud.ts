420bd565fd23f667351d1f96d0e04b646ded4320
pedr <pedrlz.frn@gmail.com>
2024-06-07 09:01:00 -0300

    adding Team to account type

--- packages/lib/utils/joplinCloud.ts
+++ packages/lib/utils/joplinCloud.ts
@@ -414,11 +414,13 @@ enum AccountType {
 	Default = 0,
 	Basic = 1,
 	Pro = 2,
+	Team = 3,
 }
 
 export function accountTypeToString(accountType: AccountType): string {
 	if (accountType === AccountType.Default) return 'Default';
 	if (accountType === AccountType.Basic) return 'Basic';
 	if (accountType === AccountType.Pro) return 'Pro';
+	if (accountType === AccountType.Team) return 'Team';
 	throw new Error(`Invalid type: ${accountType}`);
 }
