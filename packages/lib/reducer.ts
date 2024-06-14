8cf4ef88b50fba2365735500b591e299c40849a6
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2024-06-14 11:39:26 -0700

    Desktop: Fixes #10586: Don't re-order the note list when in search (#10587)

--- packages/lib/reducer.ts
+++ packages/lib/reducer.ts
@@ -11,10 +11,13 @@ import { FolderEntity, NoteEntity } from './services/database/types';
 import { getListRendererIds } from './services/noteList/renderers';
 import { ProcessResultsRow } from './services/search/SearchEngine';
 import { getDisplayParentId } from './services/trash';
+import Logger from '@joplin/utils/Logger';
 const fastDeepEqual = require('fast-deep-equal');
 const { ALL_NOTES_FILTER_ID } = require('./reserved-ids');
 const { createSelectorCreator, defaultMemoize } = require('reselect');
 const { createCachedSelector } = require('re-reselect');
+
+const logger = Logger.create('lib/reducer');
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
 const additionalReducers: any[] = [];
@@ -1045,7 +1048,11 @@ const reducer = produce((draft: Draft<State> = defaultState, action: any) => {
 		case 'NOTE_SORT':
 
 			{
-				draft.notes = Note.sortNotes(draft.notes, stateUtils.notesOrder(draft.settings), draft.settings.uncompletedTodosOnTop);
+				if (draft.notesParentType === 'Search') {
+					logger.debug('Not sorting the note list -- sorting should be done by search.');
+				} else {
+					draft.notes = Note.sortNotes(draft.notes, stateUtils.notesOrder(draft.settings), draft.settings.uncompletedTodosOnTop);
+				}
 				draft.noteListLastSortTime = Date.now();
 			}
 			break;
