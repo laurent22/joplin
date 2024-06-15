a4a4170d49b6b13d32930ffc44518b8e8e6ba3a7
tiberiusteng <tiberiusteng@users.noreply.github.com>
2024-06-15 16:58:23 +0800

    Mobile: Resolves #8639: implement callback url (#9803)
    Co-authored-by: Tib Teng <661892+tiberiusteng@users.noreply.github.com>

--- packages/app-mobile/components/screens/Note.tsx
+++ packages/app-mobile/components/screens/Note.tsx
@@ -55,6 +55,7 @@ import { join } from 'path';
 import { Dispatch } from 'redux';
 import { RefObject } from 'react';
 import { SelectionRange } from '../NoteEditor/types';
+import { getNoteCallbackUrl } from '@joplin/lib/callbackUrlUtils';
 import { AppState } from '../../utils/types';
 import restoreItems from '@joplin/lib/services/trash/restoreItems';
 import { getDisplayParentTitle } from '@joplin/lib/services/trash';
@@ -1083,6 +1084,11 @@ class NoteScreenComponent extends BaseScreenComponent<Props, State> implements B
 		Clipboard.setString(Note.markdownTag(note));
 	}
 
+	private copyExternalLink_onPress() {
+		const note = this.state.note;
+		Clipboard.setString(getNoteCallbackUrl(note.id));
+	}
+
 	public sideMenuOptions() {
 		const note = this.state.note;
 		if (!note) return [];
@@ -1293,6 +1299,12 @@ class NoteScreenComponent extends BaseScreenComponent<Props, State> implements B
 				title: _('Copy Markdown link'),
 				onPress: () => {
 					this.copyMarkdownLink_onPress();
+				},
+			});
+			output.push({
+				title: _('Copy external link'),
+				onPress: () => {
+					this.copyExternalLink_onPress();
 				},
 			});
 		}
