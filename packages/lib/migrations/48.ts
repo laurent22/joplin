331bbca24be91e5c5161a945926a5e4e043d0498
Henry Heino <personalizedrefrigerator@gmail.com>
2024-06-20 09:13:56 -0700

    Fix keychain support check not re-run on Linux

--- packages/lib/migrations/48.ts
+++ packages/lib/migrations/48.ts
@@ -9,7 +9,7 @@ const script: Script = {
 	exec: async () => {
 		// Keychain support was added for Electron+Linux through Electron safeStorage.
 		// The support check needs to be re-run.
-		if (Setting.value('keychain.supported') === -1) {
+		if (Setting.value('keychain.supported') === 0) {
 			Setting.setValue('keychain.supported', -1);
 		}
 		await KeychainService.instance().runMigration(48);
