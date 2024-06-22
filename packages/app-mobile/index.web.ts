2aa3e984b1bd64943c4160f4eee0bf337b08d9a7
Henry Heino <personalizedrefrigerator@gmail.com>
2024-06-22 14:28:10 -0700

    Try to handle cross origin isolation failure in a better way

--- packages/app-mobile/index.web.ts
+++ packages/app-mobile/index.web.ts
@@ -7,6 +7,15 @@ require('./web/rnVectorIconsSetup.js');
 AppRegistry.registerComponent('Joplin', () => Root as any);
 
 addEventListener('DOMContentLoaded', () => {
+	if (window.crossOriginIsolated === false) {
+		// Currently, reloading might help because a service worker loaded by index.html
+		// tries to enable crossOriginIsolated.
+		// TODO: Handle this in a better way.
+		document.body.appendChild(
+			document.createTextNode('Warning: crossOriginIsolated is false. SharedArrayBuffer and similar APIs may not work correctly. Try refreshing the page.'),
+		);
+	}
+
 	AppRegistry.runApplication('Joplin', {
 		rootTag: document.querySelector('#root'),
 	});
