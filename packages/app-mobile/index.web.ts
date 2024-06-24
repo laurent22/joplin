db9dbe853c4cd5b74a824f5e399350773f0e48c3
Henry Heino <henryuheino@gmail.com>
2024-06-24 14:34:53 -0700

    Request persistent storage
    Should show a prompt that may make browsers less likely to auto-delete site data

--- packages/app-mobile/index.web.ts
+++ packages/app-mobile/index.web.ts
@@ -6,6 +6,13 @@ require('./web/rnVectorIconsSetup.js');
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Necessary until Root doesn't extend `any`
 AppRegistry.registerComponent('Joplin', () => Root as any);
 
+// Should prevent the browser from auto-deleting background data.
+const requestPersistentStorage = async () => {
+	if (!(await navigator.storage.persisted())) {
+		await navigator.storage.persist();
+	}
+};
+
 addEventListener('DOMContentLoaded', () => {
 	if (window.crossOriginIsolated === false) {
 		// Currently, reloading might help because a service worker loaded by index.html
@@ -15,6 +22,8 @@ addEventListener('DOMContentLoaded', () => {
 			document.createTextNode('Warning: crossOriginIsolated is false. SharedArrayBuffer and similar APIs may not work correctly. Try refreshing the page.'),
 		);
 	}
+
+	void requestPersistentStorage();
 
 	AppRegistry.runApplication('Joplin', {
 		rootTag: document.querySelector('#root'),
