laurent22/joplin

commit f19b1c536439bc0a9f04253203371c15b60f65b3

All: Resolves #483: Add trash folder (#9671)

packages/app-mobile/components/side-menu-content.tsx
@@ -9,11 +9,15 @@ import NavService from '@joplin/lib/services/NavService';
 import { _ } from '@joplin/lib/locale';
 const { themeStyle } = require('./global-style.js');
 import { renderFolders } from '@joplin/lib/components/shared/side-menu-shared';
-import { FolderEntity, FolderIcon } from '@joplin/lib/services/database/types';
+import { FolderEntity, FolderIcon, FolderIconType } from '@joplin/lib/services/database/types';
 import { AppState } from '../utils/types';
 import Setting from '@joplin/lib/models/Setting';
 import { reg } from '@joplin/lib/registry';
 import { ProfileConfig } from '@joplin/lib/services/profileConfig/types';
+import { getTrashFolderIcon, getTrashFolderId } from '@joplin/lib/services/trash';
+import restoreItems from '@joplin/lib/services/trash/restoreItems';
+import { ModelType } from '@joplin/lib/BaseModel';
+const { substrWithEllipsis } = require('@joplin/lib/string-utils');
 
 // We need this to suppress the useless warning
 // https://github.com/oblador/react-native-vector-icons/issues/1465
@@ -142,58 +146,96 @@ const SideMenuContentComponent = (props: Props) => {
 
 		const folder = folderOrAll as FolderEntity;
 
-		const generateFolderDeletion = () => {
-			const folderDeletion = (message: string) => {
-				Alert.alert('', message, [
-					{
-						text: _('OK'),
-						onPress: () => {
-							void Folder.delete(folder.id);
+		if (folder && folder.id === getTrashFolderId()) return;
+
+		const menuItems: any[] = [];
+
+		if (folder && !!folder.deleted_time) {
+			menuItems.push({
+				text: _('Restore'),
+				onPress: async () => {
+					await restoreItems(ModelType.Folder, [folder.id]);
+				},
+				style: 'destructive',
+			});
+
+			// Alert.alert(
+			// 	'',
+			// 	_('Notebook: %s', folder.title),
+			// 	[
+			// 		{
+			// 			text: _('Restore'),
+			// 			onPress: async () => {
+			// 				await restoreItems(ModelType.Folder, [folder.id]);
+			// 			},
+			// 			style: 'destructive',
+			// 		},
+			// 		{
+			// 			text: _('Cancel'),
+			// 			onPress: () => {},
+			// 			style: 'cancel',
+			// 		},
+			// 	],
+			// 	{
+			// 		cancelable: false,
+			// 	},
+			// );
+		} else {
+			const generateFolderDeletion = () => {
+				const folderDeletion = (message: string) => {
+					Alert.alert('', message, [
+						{
+							text: _('OK'),
+							onPress: () => {
+								void Folder.delete(folder.id, { toTrash: true });
+							},
 						},
-					},
-					{
-						text: _('Cancel'),
-						onPress: () => { },
-						style: 'cancel',
-					},
-				]);
+						{
+							text: _('Cancel'),
+							onPress: () => { },
+							style: 'cancel',
+						},
+					]);
+				};
+
+				if (folder.id === props.inboxJopId) {
+					return folderDeletion(
+						_('Delete the Inbox notebook?\n\nIf you delete the inbox notebook, any email that\'s recently been sent to it may be lost.'),
+					);
+				}
+				return folderDeletion(_('Move notebook "%s" to the trash?\n\nAll notes and sub-notebooks within this notebook will also be moved to the trash.', substrWithEllipsis(folder.title, 0, 32)));
 			};
 
-			if (folder.id === props.inboxJopId) {
-				return folderDeletion(
-					_('Delete the Inbox notebook?\n\nIf you delete the inbox notebook, any email that\'s recently been sent to it may be lost.'),
-				);
-			}
-			return folderDeletion(_('Delete notebook "%s"?\n\nAll notes and sub-notebooks within this notebook will also be deleted.', folder.title));
-		};
+			menuItems.push({
+				text: _('Edit'),
+				onPress: () => {
+					props.dispatch({ type: 'SIDE_MENU_CLOSE' });
+
+					props.dispatch({
+						type: 'NAV_GO',
+						routeName: 'Folder',
+						folderId: folder.id,
+					});
+				},
+			});
+
+			menuItems.push({
+				text: _('Delete'),
+				onPress: generateFolderDeletion,
+				style: 'destructive',
+			});
+		}
+
+		menuItems.push({
+			text: _('Cancel'),
+			onPress: () => {},
+			style: 'cancel',
+		});
 
 		Alert.alert(
 			'',
 			_('Notebook: %s', folder.title),
-			[
-				{
-					text: _('Edit'),
-					onPress: () => {
-						props.dispatch({ type: 'SIDE_MENU_CLOSE' });
-
-						props.dispatch({
-							type: 'NAV_GO',
-							routeName: 'Folder',
-							folderId: folder.id,
-						});
-					},
-				},
-				{
-					text: _('Delete'),
-					onPress: generateFolderDeletion,
-					style: 'destructive',
-				},
-				{
-					text: _('Cancel'),
-					onPress: () => {},
-					style: 'cancel',
-				},
-			],
+			menuItems,
 			{
 				cancelable: false,
 			},
@@ -308,10 +350,12 @@ const SideMenuContentComponent = (props: Props) => {
 		if (actionDone === 'auth') props.dispatch({ type: 'SIDE_MENU_CLOSE' });
 	}, [performSync, props.dispatch]);
 
-	const renderFolderIcon = (theme: any, folderIcon: FolderIcon) => {
+	const renderFolderIcon = (folderId: string, theme: any, folderIcon: FolderIcon) => {
 		if (!folderIcon) {
 			if (alwaysShowFolderIcons) {
 				return <Icon name="folder-outline" style={styles_.emptyFolderIcon} />;
+			} else if (folderId === getTrashFolderId()) {
+				folderIcon = getTrashFolderIcon(FolderIconType.Emoji);
 			} else {
 				return null;
 			}
@@ -378,7 +422,7 @@ const SideMenuContentComponent = (props: Props) => {
 					}}
 				>
 					<View style={folderButtonStyle}>
-						{renderFolderIcon(theme, folderIcon)}
+						{renderFolderIcon(folder.id, theme, folderIcon)}
 						<Text numberOfLines={1} style={styles_.folderButtonText}>
 							{Folder.displayTitle(folder)}
 						</Text>

packages/lib/components/shared/side-menu-shared.ts
@@ -1,6 +1,7 @@
 import Folder from '../../models/Folder';
 import BaseModel from '../../BaseModel';
 import { FolderEntity, TagEntity } from '../../services/database/types';
+import { getDisplayParentId, getTrashFolderId } from '../../services/trash';
 
 interface Props {
 	folders: FolderEntity[];
@@ -11,26 +12,33 @@ interface Props {
 	tags?: TagEntity[];
 }
 
-type RenderFolderItem = (folder: FolderEntity, selected: boolean, hasChildren: boolean, depth: number)=> any;
-type RenderTagItem = (tag: TagEntity, selected: boolean)=> any;
+export type RenderFolderItem = (folder: FolderEntity, selected: boolean, hasChildren: boolean, depth: number)=> any;
+export type RenderTagItem = (tag: TagEntity, selected: boolean)=> any;
 
 function folderHasChildren_(folders: FolderEntity[], folderId: string) {
+	if (folderId === getTrashFolderId()) {
+		return !!folders.find(f => !!f.deleted_time);
+	}
+
 	for (let i = 0; i < folders.length; i++) {
 		const folder = folders[i];
-		if (folder.parent_id === folderId) return true;
+		const folderParentId = getDisplayParentId(folder, folders.find(f => f.id === folder.parent_id));
+		if (folderParentId === folderId) return true;
 	}
+
 	return false;
 }
 
-function folderIsVisible(folders: FolderEntity[], folderId: string, collapsedFolderIds: string[]) {
-	if (!collapsedFolderIds || !collapsedFolderIds.length) return true;
+function folderIsCollapsed(folders: FolderEntity[], folderId: string, collapsedFolderIds: string[]) {
+	if (!collapsedFolderIds || !collapsedFolderIds.length) return false;
 
 	while (true) {
-		const folder = BaseModel.byId(folders, folderId);
+		const folder: FolderEntity = BaseModel.byId(folders, folderId);
 		if (!folder) throw new Error(`No folder with id ${folder.id}`);
-		if (!folder.parent_id) return true;
-		if (collapsedFolderIds.indexOf(folder.parent_id) >= 0) return false;
-		folderId = folder.parent_id;
+		const folderParentId = getDisplayParentId(folder, folders.find(f => f.id === folder.parent_id));
+		if (!folderParentId) return false;
+		if (collapsedFolderIds.indexOf(folderParentId) >= 0) return true;
+		folderId = folderParentId;
 	}
 }
 
@@ -38,8 +46,11 @@ function renderFoldersRecursive_(props: Props, renderItem: RenderFolderItem, ite
 	const folders = props.folders;
 	for (let i = 0; i < folders.length; i++) {
 		const folder = folders[i];
-		if (!Folder.idsEqual(folder.parent_id, parentId)) continue;
-		if (!folderIsVisible(props.folders, folder.id, props.collapsedFolderIds)) continue;
+
+		const folderParentId = getDisplayParentId(folder, props.folders.find(f => f.id === folder.parent_id));
+
+		if (!Folder.idsEqual(folderParentId, parentId)) continue;
+		if (folderIsCollapsed(props.folders, folder.id, props.collapsedFolderIds)) continue;
 		const hasChildren = folderHasChildren_(folders, folder.id);
 		order.push(folder.id);
 		items.push(renderItem(folder, props.selectedFolderId === folder.id && props.notesParentType === 'Folder', hasChildren, depth));
@@ -75,7 +86,7 @@ export const renderTags = (props: Props, renderItem: RenderTagItem) => {
 		return a.title.toLowerCase() < b.title.toLowerCase() ? -1 : +1;
 	});
 	const tagItems = [];
-	const order = [];
+	const order: string[] = [];
 	for (let i = 0; i < tags.length; i++) {
 		const tag = tags[i];
 		order.push(tag.id);

commit a13c8293cc096827485f4ce7fab1491f03b1434d

Desktop, Mobile: Fixes #10674: Fix sidebar performance regression with many nested notebooks
Fixes a regression that seems to be introduced by
f19b1c536439bc0a9f04253203371c15b60f65b3. This seems related to
`folderHasChildren_` and related methods going from O(n) to O(n^2) (new
`folders.find(f => f.id === parent_id)` logic to find original parent
items).

This commit fixes the issue by caching parent items in a `Map`.

packages/app-desktop/gui/Sidebar/hooks/useSidebarListData.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { FolderListItem, HeaderId, HeaderListItem, ListItem, ListItemType, TagListItem } from '../types';
 import { FolderEntity, TagsWithNoteCountEntity } from '@joplin/lib/services/database/types';
-import { renderFolders, renderTags } from '@joplin/lib/components/shared/side-menu-shared';
+import { buildFolderTree, renderFolders, renderTags } from '@joplin/lib/components/shared/side-menu-shared';
 import { _ } from '@joplin/lib/locale';
 import CommandService from '@joplin/lib/services/CommandService';
 import Setting from '@joplin/lib/models/Setting';
@@ -35,10 +35,13 @@ const useSidebarListData = (props: Props): ListItem[] => {
 		});
 	}, [props.tags]);
 
+	const folderTree = useMemo(() => {
+		return buildFolderTree(props.folders);
+	}, [props.folders]);
 
 	const folderItems = useMemo(() => {
 		const renderProps = {
-			folders: props.folders,
+			folderTree,
 			collapsedFolderIds: props.collapsedFolderIds,
 		};
 		return renderFolders<ListItem>(renderProps, (folder, hasChildren, depth): FolderListItem => {
@@ -50,7 +53,7 @@ const useSidebarListData = (props: Props): ListItem[] => {
 				key: folder.id,
 			};
 		});
-	}, [props.folders, props.collapsedFolderIds]);
+	}, [folderTree, props.collapsedFolderIds]);
 
 	return useMemo(() => {
 		const foldersHeader: HeaderListItem = {

packages/app-mobile/components/side-menu-content.tsx
@@ -8,7 +8,7 @@ import Synchronizer from '@joplin/lib/Synchronizer';
 import NavService from '@joplin/lib/services/NavService';
 import { _ } from '@joplin/lib/locale';
 import { ThemeStyle, themeStyle } from './global-style';
-import { isFolderSelected, renderFolders } from '@joplin/lib/components/shared/side-menu-shared';
+import { buildFolderTree, isFolderSelected, renderFolders } from '@joplin/lib/components/shared/side-menu-shared';
 import { FolderEntity, FolderIcon, FolderIconType } from '@joplin/lib/services/database/types';
 import { AppState } from '../utils/types';
 import Setting from '@joplin/lib/models/Setting';
@@ -560,8 +560,20 @@ const SideMenuContentComponent = (props: Props) => {
 
 	items.push(renderSidebarButton('folder_header', _('Notebooks'), 'folder'));
 
+	const folderTree = useMemo(() => {
+		reg.logger().info('!!! Building folder tree');
+		const ft = buildFolderTree(props.folders);
+		reg.logger().info('!!! Done');
+
+		return ft;
+	}, [props.folders]);
+
 	if (props.folders.length) {
-		const result = renderFolders(props, renderFolderItem);
+		const result = renderFolders({
+			folderTree,
+			collapsedFolderIds: props.collapsedFolderIds,
+		}, renderFolderItem);
+
 		const folderItems = result.items;
 		items = items.concat(folderItems);
 	}

packages/lib/components/shared/side-menu-shared.ts
@@ -1,38 +1,9 @@
-import Folder from '../../models/Folder';
-import BaseModel from '../../BaseModel';
 import { FolderEntity, TagEntity, TagsWithNoteCountEntity } from '../../services/database/types';
-import { getDisplayParentId, getTrashFolderId } from '../../services/trash';
+import { getDisplayParentId } from '../../services/trash';
 import { getCollator } from '../../models/utils/getCollator';
 
 export type RenderFolderItem<T> = (folder: FolderEntity, hasChildren: boolean, depth: number)=> T;
 export type RenderTagItem<T> = (tag: TagsWithNoteCountEntity)=> T;
-
-function folderHasChildren_(folders: FolderEntity[], folderId: string) {
-	if (folderId === getTrashFolderId()) {
-		return !!folders.find(f => !!f.deleted_time);
-	}
-
-	for (let i = 0; i < folders.length; i++) {
-		const folder = folders[i];
-		const folderParentId = getDisplayParentId(folder, folders.find(f => f.id === folder.parent_id));
-		if (folderParentId === folderId) return true;
-	}
-
-	return false;
-}
-
-function folderIsCollapsed(folders: FolderEntity[], folderId: string, collapsedFolderIds: string[]) {
-	if (!collapsedFolderIds || !collapsedFolderIds.length) return false;
-
-	while (true) {
-		const folder: FolderEntity = BaseModel.byId(folders, folderId);
-		if (!folder) throw new Error(`No folder with id ${folder.id}`);
-		const folderParentId = getDisplayParentId(folder, folders.find(f => f.id === folder.parent_id));
-		if (!folderParentId) return false;
-		if (collapsedFolderIds.indexOf(folderParentId) >= 0) return true;
-		folderId = folderParentId;
-	}
-}
 
 interface FolderSelectedContext {
 	selectedFolderId: string;
@@ -48,21 +19,37 @@ type ItemsWithOrder<ItemType> = {
 	order: string[];
 };
 
-interface RenderFoldersProps {
+interface FolderTree {
 	folders: FolderEntity[];
+	parentIdToChildren: Map<string, FolderEntity[]>;
+	idToItem: Map<string, FolderEntity>;
+}
+
+interface RenderFoldersProps {
+	// All folders
+	folderTree: FolderTree;
 	collapsedFolderIds: string[];
 }
 
+function folderIsCollapsed(context: RenderFoldersProps, folderId: string) {
+	if (!context.collapsedFolderIds || !context.collapsedFolderIds.length) return false;
+
+	while (true) {
+		const folder = context.folderTree.idToItem.get(folderId);
+		const folderParentId = getDisplayParentId(folder, context.folderTree.idToItem.get(folder.parent_id));
+		if (!folderParentId) return false;
+		if (context.collapsedFolderIds.includes(folderParentId)) return true;
+		folderId = folderParentId;
+	}
+}
+
 function renderFoldersRecursive_<T>(props: RenderFoldersProps, renderItem: RenderFolderItem<T>, items: T[], parentId: string, depth: number, order: string[]): ItemsWithOrder<T> {
-	const folders = props.folders;
-	for (let i = 0; i < folders.length; i++) {
-		const folder = folders[i];
+	const folders = props.folderTree.parentIdToChildren.get(parentId ?? '');
+	const parentIdToChildren = props.folderTree.parentIdToChildren;
+	for (const folder of folders) {
+		if (folderIsCollapsed(props, folder.id)) continue;
 
-		const folderParentId = getDisplayParentId(folder, props.folders.find(f => f.id === folder.parent_id));
-
-		if (!Folder.idsEqual(folderParentId, parentId)) continue;
-		if (folderIsCollapsed(props.folders, folder.id, props.collapsedFolderIds)) continue;
-		const hasChildren = folderHasChildren_(folders, folder.id);
+		const hasChildren = parentIdToChildren.has(folder.id);
 		order.push(folder.id);
 		items.push(renderItem(folder, hasChildren, depth));
 		if (hasChildren) {
@@ -79,6 +66,24 @@ function renderFoldersRecursive_<T>(props: RenderFoldersProps, renderItem: Rende
 
 export const renderFolders = <T> (props: RenderFoldersProps, renderItem: RenderFolderItem<T>): ItemsWithOrder<T> => {
 	return renderFoldersRecursive_(props, renderItem, [], '', 0, []);
+};
+
+export const buildFolderTree = (folders: FolderEntity[]): FolderTree => {
+	const idToItem = new Map<string, FolderEntity>();
+	for (const folder of folders) {
+		idToItem.set(folder.id, folder);
+	}
+
+	const parentIdToChildren = new Map<string, FolderEntity[]>();
+	for (const folder of folders) {
+		const displayParentId = getDisplayParentId(folder, idToItem.get(folder.parent_id)) ?? '';
+		if (!parentIdToChildren.has(displayParentId)) {
+			parentIdToChildren.set(displayParentId, []);
+		}
+		parentIdToChildren.get(displayParentId).push(folder);
+	}
+
+	return { folders, parentIdToChildren, idToItem };
 };
 
 const sortTags = (tags: TagEntity[]) => {