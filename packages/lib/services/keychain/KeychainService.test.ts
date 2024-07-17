436a84fdd29982b08de0d46232ed9d8c10cc73b4
Henry Heino <personalizedrefrigerator@gmail.com>
2024-07-17 13:33:39 -0700

    Fix keychain support not enabled on Linux after safestorage becomes available

--- packages/lib/services/keychain/KeychainService.test.ts
+++ packages/lib/services/keychain/KeychainService.test.ts
@@ -2,6 +2,7 @@ import Setting from '../../models/Setting';
 import shim from '../../shim';
 import { switchClient, setupDatabaseAndSynchronizer } from '../../testing/test-utils';
 import KeychainService from './KeychainService';
+import KeychainServiceDriverDummy from './KeychainServiceDriver.dummy';
 import KeychainServiceDriverElectron from './KeychainServiceDriver.electron';
 import KeychainServiceDriverNode from './KeychainServiceDriver.node';
 
@@ -56,6 +57,7 @@ describe('KeychainService', () => {
 		await switchClient(0);
 		Setting.setValue('keychain.supported', 1);
 		shim.electronBridge = null;
+		shim.keytar = null;
 	});
 
 	test('should migrate keys from keytar to safeStorage', async () => {
@@ -100,5 +102,28 @@ describe('KeychainService', () => {
 
 		await Setting.load();
 		expect(Setting.value('encryption.masterPassword')).toBe('test-password');
+	});
+
+	test('should re-check for keychain support when a new driver is added', async () => {
+		mockKeytar();
+		mockSafeStorage({});
+		Setting.setValue('keychain.supported', -1);
+
+		await KeychainService.instance().initialize([
+			new KeychainServiceDriverDummy(Setting.value('appId'), Setting.value('clientId')),
+		]);
+		await KeychainService.instance().detectIfKeychainSupported();
+
+		expect(Setting.value('keychain.supported')).toBe(0);
+
+		// Should re-run the check after keytar and safeStorage are available.
+		await KeychainService.instance().initialize(makeDrivers());
+		await KeychainService.instance().detectIfKeychainSupported();
+		expect(Setting.value('keychain.supported')).toBe(1);
+
+		// Should re-run the check if safeStorage and keytar are both no longer available.
+		await KeychainService.instance().initialize([]);
+		await KeychainService.instance().detectIfKeychainSupported();
+		expect(Setting.value('keychain.supported')).toBe(0);
 	});
 });
--- packages/lib/services/keychain/KeychainService.ts
+++ packages/lib/services/keychain/KeychainService.ts
@@ -118,14 +118,22 @@ export default class KeychainService extends BaseService {
 		this.logger().info('KeychainService: checking if keychain supported');
 
 		const lastAvailableDrivers = Setting.value('keychain.lastAvailableDrivers');
-		const hasNewBackend = this.drivers_.some(driver => {
-			return !lastAvailableDrivers.includes(driver.driverId);
-		});
+		const hasDifferentBackends = (() => {
+			if (lastAvailableDrivers.length !== this.drivers_.length) return true;
+			return this.drivers_.some(driver => {
+				return !lastAvailableDrivers.includes(driver.driverId);
+			});
+		})();
 
 		const checkAlreadyDone = Setting.value('keychain.supported') >= 0;
-		if (checkAlreadyDone && !hasNewBackend) {
+		if (checkAlreadyDone && !hasDifferentBackends) {
 			this.logger().info('KeychainService: check was already done - skipping. Supported:', Setting.value('keychain.supported'));
 			return;
+		}
+
+		if (hasDifferentBackends) {
+			// Reset supported -- this allows the test .setPassword to work.
+			Setting.setValue('keychain.supported', -1);
 		}
 
 		const passwordIsSet = await this.setPassword('zz_testingkeychain', 'mytest');
