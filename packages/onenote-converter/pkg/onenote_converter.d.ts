ef4ffc72f64895a574eda3b4883295ae4b595133
pedr <pedrlz.frn@gmail.com>
2024-08-02 19:43:27 -0300

    removing generated files

--- packages/onenote-converter/pkg/onenote_converter.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @param {string} input
-* @param {string} output
-* @param {string} base_path
-*/
-export function oneNoteConverter(input: string, output: string, base_path: string): void;
