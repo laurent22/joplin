8db9c98220da4eca5070a38b428d877effe88acc
pedr <pedrlz.frn@gmail.com>
2024-08-02 19:51:03 -0300

    add simple script to build wasm files only on production

--- packages/onenote-converter/build.js
+++ packages/onenote-converter/build.js
@@ -1,8 +1,10 @@
 const { execSync } = require('child_process');
 
-const build = () => {
-	const profile = process.env.NODE_ENV === 'production' ? '--release' : '--debug';
-	return execSync(`wasm-pack build --target nodejs ${profile}`);
+const build = (env = process.env.NODE_ENV) => {
+	if (env === 'dev') return;
+
+	const profile = env === 'production' ? '--release' : '--debug';
+	return execSync(`wasm-pack build --scope joplin --target nodejs ${profile}`);
 };
 
 build();
