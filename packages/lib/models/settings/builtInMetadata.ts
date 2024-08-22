c19d0773b3e64d1888ea7edb69cb48e6dbf52b13
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2024-08-22 13:51:57 -0700

    Mobile,Desktop,Cli: Fixes #10896: Fix "Enable auto-updates" enabled by default and visible on unsupported platforms (#10897)

--- packages/lib/models/settings/builtInMetadata.ts
+++ packages/lib/models/settings/builtInMetadata.ts
@@ -1553,12 +1553,14 @@ const builtInMetadata = (Setting: typeof SettingType) => {
 		},
 
 		'featureFlag.autoUpdaterServiceEnabled': {
-			value: true,
+			value: false,
 			type: SettingItemType.Bool,
 			public: true,
 			storage: SettingStorage.File,
+			appTypes: [AppType.Desktop],
 			label: () => 'Enable auto-updates',
 			description: () => 'Enable this feature to receive notifications about updates and install them instead of manually downloading them. Restart app to start receiving auto-updates.',
+			show: () => shim.isWindows() || shim.isMac(),
 			section: 'application',
 			isGlobal: true,
 		},
