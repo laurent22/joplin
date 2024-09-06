8ff67121effdb28a6191e494d05d64779e84b2fa
Henry Heino <personalizedrefrigerator@gmail.com>
2024-09-06 14:11:47 -0700

    Move error handling into the deferred loadKey callback

--- packages/lib/services/e2ee/EncryptionService.ts
+++ packages/lib/services/e2ee/EncryptionService.ts
@@ -150,13 +150,17 @@ export default class EncryptionService {
 				return;
 			}
 
-			this.decryptedMasterKeys_.set(model.id, {
-				plainText: await this.decryptMasterKeyContent(model, password),
-				updatedTime: model.updated_time,
-			});
-			this.encryptedMasterKeys_.delete(model.id);
+			try {
+				this.decryptedMasterKeys_.set(model.id, {
+					plainText: await this.decryptMasterKeyContent(model, password),
+					updatedTime: model.updated_time,
+				});
+				this.encryptedMasterKeys_.delete(model.id);
 
-			if (makeActive) this.setActiveMasterKeyId(model.id);
+				if (makeActive) this.setActiveMasterKeyId(model.id);
+			} catch (error) {
+				logger.warn(`Cannot load master key ${model.id}. Invalid password?`, error);
+			}
 		};
 
 		if (!makeActive) {
