079c54499edd0a10fb8470aca90508dfa5a70a8a
pedr <pedrlz.frn@gmail.com>
2024-09-20 10:35:26 -0300

    read all rotation log files when creating the deletion log

--- packages/app-desktop/commands/exportDeletionLogs.ts
+++ packages/app-desktop/commands/exportDeletionLogs.ts
@@ -6,10 +6,36 @@ import { TargetType } from '@joplin/utils/Logger';
 import shim from '@joplin/lib/shim';
 import Setting from '@joplin/lib/models/Setting';
 import bridge from '../services/bridge';
+import { Stat } from '@joplin/lib/fs-driver-base';
 
 export const declaration: CommandDeclaration = {
 	name: 'exportDeletionLogs',
 	label: () => _('Export deletion log'),
+};
+
+// Return log files orderer from oldest to newest, with log.txt being the last one
+const logsOrderedByTimestamp = (logFiles: Stat[]) => {
+	return logFiles.sort((a, b) => {
+		const aTimestamp = a.path.match(/\d+/);
+		const bTimestamp = b.path.match(/\d+/);
+
+		if (!aTimestamp) return 1;
+		if (!bTimestamp) return -1;
+
+		return parseInt(aTimestamp[0], 10) - parseInt(bTimestamp[0], 10);
+	});
+};
+
+const getDeletionLines = async (filePath: string) => {
+	const logFile: string = await shim.fsDriver().readFile(`${Setting.value('profileDir')}/${filePath}`);
+
+	const deletionLines = logFile
+		.split('\n')
+		.filter(line => line.includes('DeleteAction'));
+
+	if (!deletionLines.length) return '';
+
+	return `${deletionLines.join('\n')}\n`;
 };
 
 export const runtime = (): CommandRuntime => {
@@ -25,16 +51,23 @@ export const runtime = (): CommandRuntime => {
 				throw new Error('Log file target not found');
 			}
 
-			const logFile: string = await shim.fsDriver().readFile(fileTarget.path);
+			const files = await shim.fsDriver().readDirStats(Setting.value('profileDir'));
+			// Get all log.txt and log-{timestamp}.txt files but ignore deletion_log.txt
+			const logFiles = files.filter(f => f.path.match(/^log(-\d+)?\.txt$/gi));
 
-			const deletionLog = logFile
-				.split('\n')
-				.filter(line => line.includes('DeleteAction'))
-				.join('\n');
+			const orderedLogs = logsOrderedByTimestamp(logFiles);
+
+			let allDeletionLines = '';
+			for (const file of orderedLogs) {
+
+				const deletionLines = await getDeletionLines(file.path);
+
+				allDeletionLines += deletionLines;
+			}
 
 			const deletionLogPath = `${Setting.value('profileDir')}/deletion_log.txt`;
 
-			await shim.fsDriver().writeFile(deletionLogPath, deletionLog, 'utf8');
+			await shim.fsDriver().writeFile(deletionLogPath, allDeletionLines, 'utf8');
 
 			await bridge().openItem(deletionLogPath);
 		},
