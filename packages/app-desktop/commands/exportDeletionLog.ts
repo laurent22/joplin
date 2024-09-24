ac47028a260db9e85c663ff932d17f5244219fa3
pedr <pedrlz.frn@gmail.com>
2024-09-24 18:03:41 -0300

    updating code according to latest review

--- packages/app-desktop/commands/exportDeletionLog.ts
+++ packages/app-desktop/commands/exportDeletionLog.ts
@@ -2,28 +2,13 @@
 import { CommandRuntime, CommandDeclaration } from '@joplin/lib/services/CommandService';
 import { _ } from '@joplin/lib/locale';
 import Logger from '@joplin/utils/Logger';
-import { TargetType } from '@joplin/utils/Logger';
 import shim from '@joplin/lib/shim';
 import Setting from '@joplin/lib/models/Setting';
 import bridge from '../services/bridge';
-import { Stat } from '@joplin/lib/fs-driver-base';
 
 export const declaration: CommandDeclaration = {
 	name: 'exportDeletionLog',
 	label: () => _('Export deletion log'),
-};
-
-// Return log files orderer from oldest to newest, with log.txt being the last one
-const logsOrderedByTimestamp = (logFiles: Stat[]) => {
-	return logFiles.sort((a, b) => {
-		const aTimestamp = a.path.match(/\d+/);
-		const bTimestamp = b.path.match(/\d+/);
-
-		if (!aTimestamp) return 1;
-		if (!bTimestamp) return -1;
-
-		return parseInt(aTimestamp[0], 10) - parseInt(bTimestamp[0], 10);
-	});
 };
 
 const getDeletionLines = async (filePath: string) => {
@@ -45,20 +30,14 @@ export const runtime = (): CommandRuntime => {
 				throw new Error('Could not find global logger');
 			}
 
-			const fileTarget = Logger.globalLogger.targets().find(t => t.type === TargetType.File);
-
-			if (!fileTarget || !fileTarget.path) {
-				throw new Error('Log file target not found');
-			}
-
 			const files = await shim.fsDriver().readDirStats(Setting.value('profileDir'));
 			// Get all log.txt and log-{timestamp}.txt files but ignore deletion_log.txt
 			const logFiles = files.filter(f => f.path.match(/^log(-\d+)?\.txt$/gi));
 
-			const orderedLogs = logsOrderedByTimestamp(logFiles);
+			const lastOneAndCurrent = logFiles.sort().slice(logFiles.length - 2);
 
 			let allDeletionLines = '';
-			for (const file of orderedLogs) {
+			for (const file of lastOneAndCurrent) {
 
 				const deletionLines = await getDeletionLines(file.path);
 
