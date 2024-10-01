9fcacb694928ebaccc4c562a5df566c6c33f1573
Henry Heino <personalizedrefrigerator@gmail.com>
2024-09-30 18:26:23 -0700

    spaces -> tabs

--- packages/app-mobile/android/app/src/main/java/net/cozic/joplin/audio/SpeechToTextPackage.kt
+++ packages/app-mobile/android/app/src/main/java/net/cozic/joplin/audio/SpeechToTextPackage.kt
@@ -13,74 +13,74 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class SpeechToTextPackage : ReactPackage {
-    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf<NativeModule>(SpeechToTextModule(reactContext))
-    }
+	override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+		return listOf<NativeModule>(SpeechToTextModule(reactContext))
+	}
 
-    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return emptyList()
-    }
+	override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+		return emptyList()
+	}
 
-    class SpeechToTextModule(
-        private var context: ReactApplicationContext,
-    ) : ReactContextBaseJavaModule(context), LifecycleEventListener {
-        private var environment: OrtEnvironment? = null
-        private val executorService: ExecutorService = Executors.newFixedThreadPool(1)
-        private val sessionManager = SpeechToTextSessionManager(executorService)
+	class SpeechToTextModule(
+		private var context: ReactApplicationContext,
+	) : ReactContextBaseJavaModule(context), LifecycleEventListener {
+		private var environment: OrtEnvironment? = null
+		private val executorService: ExecutorService = Executors.newFixedThreadPool(1)
+		private val sessionManager = SpeechToTextSessionManager(executorService)
 
-        override fun getName() = "SpeechToTextModule"
+		override fun getName() = "SpeechToTextModule"
 
-        override fun onHostResume() { }
-        override fun onHostPause() { }
-        override fun onHostDestroy() {
-            environment?.close()
-        }
+		override fun onHostResume() { }
+		override fun onHostPause() { }
+		override fun onHostDestroy() {
+			environment?.close()
+		}
 
-        @ReactMethod
-        fun openSession(modelPath: String, locale: String, promise: Promise) {
-            val appContext = context.applicationContext
-            // Initialize environment as late as possible:
-            val ortEnvironment = environment ?: OrtEnvironment.getEnvironment()
-            if (environment != null) {
-                environment = ortEnvironment
-            }
+		@ReactMethod
+		fun openSession(modelPath: String, locale: String, promise: Promise) {
+			val appContext = context.applicationContext
+			// Initialize environment as late as possible:
+			val ortEnvironment = environment ?: OrtEnvironment.getEnvironment()
+			if (environment != null) {
+				environment = ortEnvironment
+			}
 
-            try {
-                val sessionId = sessionManager.openSession(modelPath, locale, ortEnvironment, appContext)
-                promise.resolve(sessionId)
-            } catch (exception: Throwable) {
-                promise.reject(exception)
-            }
-        }
+			try {
+				val sessionId = sessionManager.openSession(modelPath, locale, ortEnvironment, appContext)
+				promise.resolve(sessionId)
+			} catch (exception: Throwable) {
+				promise.reject(exception)
+			}
+		}
 
-        @ReactMethod
-        fun startRecording(sessionId: Int, promise: Promise) {
-            sessionManager.startRecording(sessionId, promise)
-        }
+		@ReactMethod
+		fun startRecording(sessionId: Int, promise: Promise) {
+			sessionManager.startRecording(sessionId, promise)
+		}
 
-        @ReactMethod
-        fun getBufferLengthSeconds(sessionId: Int, promise: Promise) {
-            sessionManager.getBufferLengthSeconds(sessionId, promise)
-        }
+		@ReactMethod
+		fun getBufferLengthSeconds(sessionId: Int, promise: Promise) {
+			sessionManager.getBufferLengthSeconds(sessionId, promise)
+		}
 
-        @ReactMethod
-        fun dropFirstSeconds(sessionId: Int, duration: Double, promise: Promise) {
-            sessionManager.dropFirstSeconds(sessionId, duration, promise)
-        }
+		@ReactMethod
+		fun dropFirstSeconds(sessionId: Int, duration: Double, promise: Promise) {
+			sessionManager.dropFirstSeconds(sessionId, duration, promise)
+		}
 
-        @ReactMethod
-        fun expandBufferAndConvert(sessionId: Int, duration: Double, promise: Promise) {
-            sessionManager.expandBufferAndConvert(sessionId, duration, promise)
-        }
+		@ReactMethod
+		fun expandBufferAndConvert(sessionId: Int, duration: Double, promise: Promise) {
+			sessionManager.expandBufferAndConvert(sessionId, duration, promise)
+		}
 
-        @ReactMethod
-        fun convertAvailable(sessionId: Int, promise: Promise) {
-            sessionManager.convertAvailable(sessionId, promise)
-        }
+		@ReactMethod
+		fun convertAvailable(sessionId: Int, promise: Promise) {
+			sessionManager.convertAvailable(sessionId, promise)
+		}
 
-        @ReactMethod
-        fun closeSession(sessionId: Int, promise: Promise) {
-            sessionManager.closeSession(sessionId, promise)
-        }
-    }
+		@ReactMethod
+		fun closeSession(sessionId: Int, promise: Promise) {
+			sessionManager.closeSession(sessionId, promise)
+		}
+	}
 }
