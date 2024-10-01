d19eca221b56b8fe8782d00768a393900c9a2f6a
Henry Heino <personalizedrefrigerator@gmail.com>
2024-10-01 13:29:21 -0700

    Chore: Update KaTeX asset files

--- packages/app-mobile/pluginAssets/index.js
+++ packages/app-mobile/pluginAssets/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-hash:"d7216e17c44aad8a0b1c3cf2e01a0135", files: {
+hash:"addedbac5508e231800fe0f97c326075", files: {
 'highlight.js/atom-one-dark-reasonable.css': { data: require('./highlight.js/atom-one-dark-reasonable.css.base64.js'), mime: 'text/css', encoding: 'base64' },
 'highlight.js/atom-one-light.css': { data: require('./highlight.js/atom-one-light.css.base64.js'), mime: 'text/css', encoding: 'base64' },
 'katex/fonts/KaTeX_AMS-Regular.woff2': { data: require('./katex/fonts/KaTeX_AMS-Regular.woff2.base64.js'), mime: 'application/octet-stream', encoding: 'base64' },
