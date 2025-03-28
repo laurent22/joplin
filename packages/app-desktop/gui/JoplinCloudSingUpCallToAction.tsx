f23888c8746d5199cc5f71ab9e98f33676cbf90d
pedr <pedrlz.frn@gmail.com>
2025-03-28 10:34:38 -0300

    adding signup call to joplin cloud

---
+++ packages/app-desktop/gui/JoplinCloudSingUpCallToAction.tsx
@@ -0,0 +1,22 @@
+import { _ } from '@joplin/lib/locale';
+import * as React from 'react';
+import bridge from '../services/bridge';
+
+const JoplinCloudSignUpCallToAction = () => {
+
+	const onJoplinCloudSingUpClick = async () => {
+		await bridge().openExternal('https://joplinapp.org/plans/');
+	};
+
+	return <div className="joplin-cloud-sign-up">
+		<p>{_('Don\'t have a Joplin Cloud account? ')}
+			<a
+				href="#"
+				onClick={onJoplinCloudSingUpClick}
+			>{_('Sign-up here')}</a>
+		</p>
+	</div>;
+
+};
+
+export default JoplinCloudSignUpCallToAction;
