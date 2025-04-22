4c6d19e99e23863879dd46117b125eed82f85167
pedr <pedrlz.frn@gmail.com>
2025-04-22 10:50:55 -0300

    Update packages/app-desktop/gui/ErrorBoundary.tsx
    Co-authored-by: Laurent Cozic <laurent22@users.noreply.github.com>

--- packages/app-desktop/gui/ErrorBoundary.tsx
+++ packages/app-desktop/gui/ErrorBoundary.tsx
@@ -41,7 +41,7 @@ const SwitchToNewEditorBanner = (props: BannerProps) => <BannerContent
 	onAccept={() => Setting.setValue('editor.legacyMarkdown', false)}
 	visible={props.isVisible}
 >
-	{_('The legacy Markdown editor might be incompatible with a number of plugins. We recommend using the new editor.')}
+	{_('The legacy Markdown editor appears to have crashed due to an incompatibility with a plugin. We recommend using the new editor.')}
 	<br/>
 </BannerContent>;
 
