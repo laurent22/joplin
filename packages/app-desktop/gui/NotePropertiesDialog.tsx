2b52554d9acdab3d76b117b9129c3d3700fc804f
SAYAN02-DEV <sayan2003.dev@gmail.com>
2025-07-03 13:13:37 +0530

    Fix: Long URL in note properties no longer breaks the dialog layout (#12643)

--- packages/app-desktop/gui/NotePropertiesDialog.tsx
+++ packages/app-desktop/gui/NotePropertiesDialog.tsx
@@ -411,9 +411,9 @@ class NotePropertiesDialog extends React.Component<Props, State> {
 					const ll = this.latLongFromLocation(value);
 					url = Note.geoLocationUrlFromLatLong(ll.latitude, ll.longitude);
 				}
-				const urlStyle: React.CSSProperties = { ...theme.urlStyle, maxWidth: '180px', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' };
+				const urlStyle: React.CSSProperties = { ...theme.urlStyle, maxWidth: '100%', wordBreak: 'break-all', display: 'inline-block' };
 				controlComp = (
-					<a href="#" onClick={() => bridge().openExternal(url)} style={urlStyle}>
+					<a href="#" onClick={() => bridge().openExternal(url)} style={urlStyle} title ={url}>
 						{displayedValue}
 					</a>
 				);
