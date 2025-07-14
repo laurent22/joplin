63e7c7d3d8d225314e12d2de7383ec77201b2e72
pedr <pedrlz.frn@gmail.com>
2025-07-14 15:36:22 -0300

    restoring types to JoplinServerApi

--- packages/lib/JoplinServerApi.ts
+++ packages/lib/JoplinServerApi.ts
@@ -8,11 +8,41 @@ import personalizedUserContentBaseUrl from './services/joplinServer/personalized
 import { getHttpStatusMessage } from './net-utils';
 import { getApplicationInformation } from './services/joplinCloudUtils';
 const { stringify } = require('query-string');
-import { ExecOptions, ExecOptionsResponseFormat, ExecOptionsTarget, Options, ServerApiInterface, Session } from './types';
 
 const logger = Logger.create('JoplinServerApi');
 
-export default class JoplinServerApi implements ServerApiInterface {
+interface Session {
+	id: string;
+	user_id: string;
+}
+
+interface Options {
+	baseUrl(): string;
+	userContentBaseUrl(): string;
+	username(): string;
+	password(): string;
+	session(): Session | null;
+	env?: Env;
+}
+
+enum ExecOptionsResponseFormat {
+	Json = 'json',
+	Text = 'text',
+}
+
+enum ExecOptionsTarget {
+	String = 'string',
+	File = 'file',
+}
+
+interface ExecOptions {
+	responseFormat?: ExecOptionsResponseFormat;
+	target?: ExecOptionsTarget;
+	path?: string;
+	source?: string;
+}
+
+export default class JoplinServerApi {
 
 	private options_: Options;
 	private session_: Session;
--- packages/lib/types.ts
+++ packages/lib/types.ts
@@ -1,5 +1,4 @@
 import { DownloadController } from './downloadController';
-import { Env } from './models/Setting';
 
 export enum ApplicationPlatform {
 	Unknown = 0,
@@ -26,40 +25,3 @@ export type FetchBlobOptions = {
 	downloadController?: DownloadController;
 };
 
-export interface Session {
-	id: string;
-	user_id: string;
-}
-
-export interface Options {
-	baseUrl(): string;
-	userContentBaseUrl(): string;
-	username(): string;
-	password(): string;
-	session(): Session | null;
-	env?: Env;
-}
-
-export enum ExecOptionsResponseFormat {
-	Json = 'json',
-	Text = 'text',
-}
-
-export enum ExecOptionsTarget {
-	String = 'string',
-	File = 'file',
-}
-
-export interface ExecOptions {
-	responseFormat?: ExecOptionsResponseFormat;
-	target?: ExecOptionsTarget;
-	path?: string;
-	source?: string;
-}
-
-export interface ServerApiInterface {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
-	exec(method: string, path?: string, query?: Record<string, any>, body?: any, headers?: any, options?: ExecOptions | null): Promise<any>;
-}
-
-export type ServerApiClass = new (options: Options)=> ServerApiInterface;
