ebfb6ec69e7b331bee19815c4e9476295d5eab72
Laurent Cozic <laurent22@users.noreply.github.com>
2025-07-31 20:05:55 +0100

    update

--- packages/tools/buildServerDocker.ts
+++ packages/tools/buildServerDocker.ts
@@ -17,25 +17,21 @@ function parseArgv(): Argv {
 	return require('yargs')
 		.scriptName('yarn buildServerDocker')
 		.usage('$0 --repository OWNER/IMAGE [args]')
-		.option('r', {
-			alias: 'dockerFile',
+		.option('dockerFile', {
 			describe: 'Dockerfile - either Dockerfile.server or Dockerfile.transcribe',
 			demandOption: true,
 			type: 'string',
 		})
-		.option('r', {
-			alias: 'repository',
+		.option('repository', {
 			describe: 'Target image repository. Usually in format `OWNER/NAME`',
 			demandOption: true,
 			type: 'string',
 		})
-		.option('t', {
-			alias: 'tagName',
+		.option('tagName', {
 			describe: 'Base image tag. Usually should be in format `server-v1.2.3` or `server-v1.2.3-beta`. The latest `server-v*` git tag will be used by default.',
 			type: 'string',
 		})
-		.option('l', {
-			alias: 'addLatestTag',
+		.option('addLatestTag', {
 			describe: 'Add `latest` tag even for pre-release images.',
 			type: 'boolean',
 			default: false,
@@ -50,14 +46,12 @@ function parseArgv(): Argv {
 			type: 'string',
 			default: 'https://github.com/laurent22/joplin.git',
 		})
-		.option('p', {
-			alias: 'pushImages',
+		.option('pushImages', {
 			describe: 'Publish images to target repository.',
 			type: 'boolean',
 			default: false,
 		})
 		.option('dryRun', {
-			alias: 'dryRun',
 			describe: 'Do not call docker, just show command instead.',
 			type: 'boolean',
 			default: false,
