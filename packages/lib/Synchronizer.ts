ec683795eaa4e58fcea800fb6482837bd3538f76
mrjo118 <jo.118@hotmail.com>
2025-08-19 13:17:16 +0100

    Avoid interference with tests

--- packages/lib/Synchronizer.ts
+++ packages/lib/Synchronizer.ts
@@ -1190,11 +1190,17 @@ export default class Synchronizer {
 
 		if (errorToThrow) throw errorToThrow;
 
-		// If there are any un-synced outgoing changes made up to the point just before the sync completes, then trigger the sync again to reduce the likelihood
-		// that the user will close or minimise the app when there are un-synced changes, because they think the sync has completed
-		const result = await BaseItem.itemsThatNeedSync(syncTargetId);
-		options.context = outputContext;
+		if (!shim.isTestingEnv()) {
+			// If there are any un-synced outgoing changes made up to the point just before the sync completes, then trigger the sync again to reduce the likelihood
+			// that the user will close or minimise the app when there are un-synced changes, because they think the sync has completed
+			const result = await BaseItem.itemsThatNeedSync(syncTargetId);
+			options.context = outputContext;
 
-		return (result.items.length > 0) ? await this.start(options) : outputContext;
+			if (result.items.length > 0) {
+				return await this.start(options);
+			}
+		}
+
+		return outputContext;
 	}
 }
