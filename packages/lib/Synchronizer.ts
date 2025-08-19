c44b56c2140667da64162b12287416ee30542a93
mrjo118 <jo.118@hotmail.com>
2025-08-19 19:23:12 +0100

    Make call to this.cancelling in addition to checking the value before it is cleared

--- packages/lib/Synchronizer.ts
+++ packages/lib/Synchronizer.ts
@@ -1197,7 +1197,7 @@ export default class Synchronizer {
 
 		// If there are any un-synced outgoing changes made up to the point just before the sync completes, then trigger the sync again to reduce the likelihood
 		// that the user will close or minimise the app when there are un-synced changes, because they think the sync has completed
-		if (!shim.isTestingEnv() && !hasErrors && !cancelling) {
+		if (!shim.isTestingEnv() && !hasErrors && !cancelling && !this.cancelling()) {
 			// This logic must be bypassed by automated tests, because errors are only added to the progressReport when shim.isTestingEnv() is false, which means
 			// the hasErrors check wont work correctly in automated tests
 			const result = await BaseItem.itemsThatNeedSync(syncTargetId);
