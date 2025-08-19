36a457691dd2b8a5da82c0a40c3a7bd998dd1a8d
pedr <pedrlz.frn@gmail.com>
2025-08-19 11:39:36 -0300

    fix test from export frontmatter to md

--- packages/lib/services/interop/InteropService_Exporter_Md.ts
+++ packages/lib/services/interop/InteropService_Exporter_Md.ts
@@ -7,6 +7,7 @@ import Note from '../../models/Note';
 import { NoteEntity, ResourceEntity } from '../database/types';
 import { basename, dirname, friendlySafeFilename, safeFilename } from '../../path-utils';
 import { MarkupToHtml } from '@joplin/renderer';
+import { normalize } from '@joplin/utils/path';
 
 export default class InteropService_Exporter_Md extends InteropService_Exporter_Base {
 
@@ -106,7 +107,7 @@ export default class InteropService_Exporter_Md extends InteropService_Exporter_
 			}
 
 			// Strip the absolute path to export dir and keep only the relative paths
-			const destDir = this.destDir_;
+			const destDir = normalize(this.destDir_);
 			Object.keys(context.notePaths).map((id) => {
 				context.notePaths[id] = context.notePaths[id].substr(destDir.length + 1);
 			});
