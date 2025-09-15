6b8599868b95a300c7f4c8c81a6a7b9d511bf510
Laurent Cozic <laurent22@users.noreply.github.com>
2025-09-15 22:57:27 +0100

    Fixed test

--- packages/transcribe/src/services/FileStorage.test.ts
+++ packages/transcribe/src/services/FileStorage.test.ts
@@ -1,21 +1,43 @@
-import { readdir, copyFile, exists, remove } from 'fs-extra';
-import { join } from 'path';
+import { readdir, copyFile, exists, remove, mkdirp } from 'fs-extra';
+import { dirname, join } from 'path';
 import FileStorage from './FileStorage';
 import initiateLogger from './initiateLogger';
 import Logger from '@joplin/utils/Logger';
 
-const imagesFolderPath = join(process.cwd(), 'images');
+const transcribeDir = dirname(dirname(dirname(__dirname)));
+
+const imagesFolderPath = join(transcribeDir, 'images');
+const tempDir = join(transcribeDir, 'temp');
+
+const clearImageFolder = async () => {
+	const files = await readdir(imagesFolderPath);
+	for (const file of files) {
+		if (file === 'htr_sample.png') continue;
+		await remove(`${imagesFolderPath}/${file}`);
+	}
+};
 
 describe('FileStorage', () => {
 
-	beforeAll(() => {
+	beforeAll(async () => {
 		initiateLogger();
 		Logger.globalLogger.enabled = false;
 	});
 
+	beforeEach(async () => {
+		await clearImageFolder();
+		await remove(tempDir);
+		await mkdirp(tempDir);
+	});
+
+	afterEach(async () => {
+		await clearImageFolder();
+		await remove(tempDir);
+	});
+
 	it('should move file to storage folder', async () => {
 		const originalFilePath = join(imagesFolderPath, 'htr_sample.png');
-		const testFilePath = join(imagesFolderPath, 'test_file.png');
+		const testFilePath = join(tempDir, 'test_file.png');
 		await copyFile(originalFilePath, testFilePath);
 
 		const fs = new FileStorage();
@@ -31,7 +53,7 @@ describe('FileStorage', () => {
 
 	it('should remove the original file', async () => {
 		const originalFilePath = join(imagesFolderPath, 'htr_sample.png');
-		const testFilePath = join(imagesFolderPath, 'test_file.png');
+		const testFilePath = join(tempDir, 'test_file.png');
 		await copyFile(originalFilePath, testFilePath);
 
 		const fs = new FileStorage();
@@ -45,24 +67,24 @@ describe('FileStorage', () => {
 
 	it('should remove files that are older than the given date', async () => {
 		const originalFilePath = join(imagesFolderPath, 'htr_sample.png');
-		const mockedFilenames = [
+		const testFilenames = [
 			`${new Date('2025-03-01 17:44').getTime()}_should_delete`,
 			`${new Date('2025-03-02 17:44').getTime()}_should_delete`,
 			`${new Date('2025-03-04 17:44').getTime()}_not_deleted`,
 		];
-		const mockedFiles = mockedFilenames.map(name => join('images', name));
-		for (const file of mockedFiles) {
-			await copyFile(originalFilePath, file);
+
+		for (const testFilename of testFilenames) {
+			await copyFile(originalFilePath, `${imagesFolderPath}/${testFilename}`);
 		}
 
 		const fs = new FileStorage();
 		await fs.removeOldFiles(new Date('2025-03-03 12:00'));
 		const files = await readdir(imagesFolderPath);
 		expect(files.length).toBe(2);
-		expect(files.includes(mockedFilenames[2])).toBe(true);
+		expect(files.includes(testFilenames[2])).toBe(true);
 
-		for (const file of mockedFiles) {
-			await remove(join(imagesFolderPath, file));
+		for (const testFilename of testFilenames) {
+			await remove(`${imagesFolderPath}/${testFilename}`);
 		}
 	});
 });
