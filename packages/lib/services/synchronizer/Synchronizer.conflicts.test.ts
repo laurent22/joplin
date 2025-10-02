faa898217432efb0d5d494a60ccf2712a445bf9d
mrjo118 <jo.118@hotmail.com>
2025-10-02 23:31:03 +0100

    Include direct child of deleted folder in test

--- packages/lib/services/synchronizer/Synchronizer.conflicts.test.ts
+++ packages/lib/services/synchronizer/Synchronizer.conflicts.test.ts
@@ -170,7 +170,8 @@ describe('Synchronizer.conflicts', () => {
 	it('should handle conflict when remote folder is deleted then local folder is renamed', (async () => {
 		const folder1 = await Folder.save({ title: 'folder1' });
 		const folder2 = await Folder.save({ title: 'folder2', parent_id: folder1.id });
-		const note1 = await Note.save({ title: 'un', parent_id: folder2.id });
+		const note1 = await Note.save({ title: 'note1', parent_id: folder2.id });
+		const note2 = await Note.save({ title: 'note2', parent_id: folder1.id });
 		await synchronizerStart();
 
 		await switchClient(2);
@@ -195,10 +196,12 @@ describe('Synchronizer.conflicts', () => {
 		const remainingFolder1 = await Folder.load(folder1.id);
 		const remainingFolder2 = await Folder.load(folder2.id);
 		const remainingNote1 = await Note.load(note1.id);
+		const remainingNote2 = await Note.load(note2.id);
 
 		expect(remainingFolder1).toBeUndefined();
 		expect(remainingFolder2?.id).toBe(folder2.id);
 		expect(remainingNote1?.id).toBe(note1.id);
+		expect(remainingNote2?.id).toBe(note2.id);
 	}));
 
 	it('should not sync notes with conflicts', (async () => {
