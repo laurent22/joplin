f3558268e1c6b36f45a55bc588df97df691bf58f
Wohlstand <admin@wohlnet.ru>
2025-11-18 18:06:16 +0300

    Fix for #13254: Always fully encode URL for the Nextcloud API
    This is the similar fix to #7868 done for OneDrive, but it's
    fully suitable for the Nextcloud too, allowing Unicode paths
    used normally.

--- packages/lib/SyncTargetNextcloud.js
+++ packages/lib/SyncTargetNextcloud.js
@@ -42,8 +42,13 @@ class SyncTargetNextcloud extends BaseSyncTarget {
 	}
 
 	async initFileApi() {
+		let syncPath = Setting.value('sync.5.path');
+		// the syncPath might contain non-ASCII characters
+		// /[^\u0021-\u00ff]/ is used in Node.js to detect the unescaped characters.
+		// See https://github.com/nodejs/node/blob/bbbf97b6dae63697371082475dc8651a6a220336/lib/_http_client.js#L176
+		const syncPathUrl = RegExp(/[^\u0021-\u00ff]/).exec(syncPath) !== null ? encodeURI(syncPath) : syncPath;
 		const fileApi = await SyncTargetWebDAV.newFileApi_(SyncTargetNextcloud.id(), {
-			path: () => Setting.value('sync.5.path'),
+			path: () => syncPathUrl,
 			username: () => Setting.value('sync.5.username'),
 			password: () => Setting.value('sync.5.password'),
 			ignoreTlsErrors: () => Setting.value('net.ignoreTlsErrors'),
