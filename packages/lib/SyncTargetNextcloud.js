303ceab95ff309adbfa468c54d82fccb3776dcc7
Wohlstand <admin@wohlnet.ru>
2025-11-18 20:47:26 +0300

    Attempt to fix bot's warnings

--- packages/lib/SyncTargetNextcloud.js
+++ packages/lib/SyncTargetNextcloud.js
@@ -42,11 +42,12 @@ class SyncTargetNextcloud extends BaseSyncTarget {
 	}
 
 	async initFileApi() {
-		let syncPath = Setting.value('sync.5.path');
+		const syncPath = Setting.value('sync.5.path');
 		// the syncPath might contain non-ASCII characters
 		// /[^\u0021-\u00ff]/ is used in Node.js to detect the unescaped characters.
 		// See https://github.com/nodejs/node/blob/bbbf97b6dae63697371082475dc8651a6a220336/lib/_http_client.js#L176
-		const syncPathUrl = RegExp(/[^\u0021-\u00ff]/).exec(syncPath) !== null ? encodeURI(syncPath) : syncPath;
+		const charsReg = /[^\u0021-\u00ff]/;
+		const syncPathUrl = charsReg.exec(syncPath) !== null ? encodeURI(syncPath) : syncPath;
 		const fileApi = await SyncTargetWebDAV.newFileApi_(SyncTargetNextcloud.id(), {
 			path: () => syncPathUrl,
 			username: () => Setting.value('sync.5.username'),
