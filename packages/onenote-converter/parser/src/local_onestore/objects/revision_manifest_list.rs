ebcae7ff7437a2f715623ab407151ab71d94a2ce
Henry Heino <personalizedrefrigerator@gmail.com>
2025-12-03 10:49:25 -0800

    Revert unnecessary change

--- packages/onenote-converter/parser/src/local_onestore/objects/revision_manifest_list.rs
+++ packages/onenote-converter/parser/src/local_onestore/objects/revision_manifest_list.rs
@@ -77,11 +77,7 @@ impl RevisionManifestList {
                     })?;
                     let revision_ref = Rc::new(revision);
                     revisions.push(revision_ref.clone());
-                    let original = revisions_map.insert(revision_ref.id, revision_ref);
-
-                    if let Some(original) = original {
-                        log_warn!("Multiple revisions with the same ID! {:?}", original.id);
-                    }
+                    revisions_map.insert(revision_ref.id, revision_ref);
                 }
             }
 
@@ -89,7 +85,6 @@ impl RevisionManifestList {
             assert_ne!(index, last_index);
             last_index = index;
         }
-
         Ok(RevisionManifestList { revisions })
     }
 }
