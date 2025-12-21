e853c1776c13383626ef2f5fbf3dd291b98ff0c4
Wohlstand <admin@wohlnet.ru>
2025-12-21 18:28:00 +0300

    More code style fixes

--- packages/lib/utils/urlUtils.js
+++ packages/lib/utils/urlUtils.js
@@ -4,15 +4,14 @@ function safe_encodeURI(path) {
 	let begin = 0;
 	let i;
 
-	if (path == undefined || path === null || path.length === 0) {
+	if (path === undefined || path === null || path.length === 0) {
 		return path;
 	}
 
 	// Collecting ranges that should be encoded
-	for (i = 0; i < path.length; ) {
+	for (i = 0; i < path.length;) {
 		// If valid percent encoding is detected, don't encode it again!
-		if (path[i] === '%' && i + 3 < path.length && /[a-fA-F0-9]{2}/.test(path.substring(i + 1, i + 3)))
-		{
+		if (path[i] === '%' && i + 3 < path.length && /[a-fA-F0-9]{2}/.test(path.substring(i + 1, i + 3))) {
 			if (begin !== i) {
 				out += encodeURI(path.substring(begin, i));
 			}
