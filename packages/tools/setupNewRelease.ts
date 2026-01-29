fb2fba19f53f093b42a975f521f5c5c4ce7b9f6b
Henry Heino <personalizedrefrigerator@gmail.com>
2026-01-29 15:46:49 -0800

    Fixing package versions

--- packages/tools/setupNewRelease.ts
+++ packages/tools/setupNewRelease.ts
@@ -145,6 +145,7 @@ async function main() {
 	await updatePackageVersion(`${rootDir}/packages/default-plugins/package.json`, majorMinorVersion, options);
 	await updatePackageVersion(`${rootDir}/packages/editor/package.json`, majorMinorVersion, options);
 	await updatePackageVersion(`${rootDir}/packages/transcribe/package.json`, majorMinorVersion, options);
+	await updatePackageVersion(`${rootDir}/packages/whisper-voice-typing/package.json`, majorMinorVersion, options);
 
 	if (options.updateVersion) {
 		await updateGradleVersion(`${rootDir}/packages/app-mobile/android/app/build.gradle`, majorMinorVersion);
