b80005d34d312c80311fc497254808450dbe6b67
jun.kobayashi <jun.kobayashi@access-company.com>
2026-02-02 17:25:02 +0900

    fix error in console.

--- packages/web_viewer/app/layout.tsx
+++ packages/web_viewer/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import EmotionRegistry from "./components/EmotionRegistry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <EmotionRegistry>{children}</EmotionRegistry>
       </body>
     </html>
   );
