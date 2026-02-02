c16998b5e39be30954dfb0adadf165e129097733
jun.kobayashi <jun.kobayashi@access-company.com>
2026-02-02 09:23:46 +0900

    split pane example

--- packages/web_viewer/app/note/page.tsx
+++ packages/web_viewer/app/note/page.tsx
@@ -1,10 +1,20 @@
 import Link from 'next/link'
+import { Panel, Group, Separator } from "react-resizable-panels";
 
 export default function NotePage() {
   return (
-    <div>
-      <h1>hello world</h1>
-      <Link href="/" className="text-blue-600 underline hover:text-blue-800">Go to Home</Link>
+    <div className="h-screen">
+      <Group orientation="horizontal">
+        <Panel defaultSize={400} minSize={20} className="bg-gray-100 p-4">
+          <h1>hello world</h1>
+          <Link href="/" className="text-blue-600 underline hover:text-blue-800">Go to Home</Link>
+        </Panel>
+        <Separator className="w-2 bg-gray-300 hover:bg-gray-400 cursor-col-resize" />
+        <Panel className="bg-white p-4">
+          <h2>右ペイン</h2>
+          <p>コンテンツがここに表示されます</p>
+        </Panel>
+      </Group>
     </div>
   );
 }
