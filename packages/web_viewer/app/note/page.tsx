51cfb873493b8422e6914813aa2f2f095b308b71
jun.kobayashi <jun.kobayashi@access-company.com>
2026-02-02 09:18:08 +0900

    add  react-resizable-panels

--- packages/web_viewer/app/note/page.tsx
+++ packages/web_viewer/app/note/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function NotePage() {
   return (
     <div>
       <h1>hello world</h1>
+      <Link href="/" className="text-blue-600 underline hover:text-blue-800">Go to Home</Link>
     </div>
   );
 }
