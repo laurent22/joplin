c5d6d2e0a8c784149a7c933ba8a62ef8e767c910
Henry Heino <personalizedrefrigerator@gmail.com>
2026-02-02 13:43:28 -0800

    Switch KeyboardAvoidingView implementation back to upstream

--- packages/app-mobile/components/KeyboardAvoidingView.tsx
+++ packages/app-mobile/components/KeyboardAvoidingView.tsx
@@ -1,21 +1,10 @@
 import * as React from 'react';
-import { View, ViewProps } from 'react-native';
+import { KeyboardAvoidingViewProps, KeyboardAvoidingView as NativeKeyboardAvoidingView } from 'react-native';
 import useKeyboardState from '../utils/hooks/useKeyboardState';
-import { useMemo } from 'react';
 
-interface Props extends ViewProps {
-	enabled: boolean;
-}
+interface Props extends KeyboardAvoidingViewProps {}
 
-// To work around various issues, don't use React Native's KeyboardAvoidingView here.
-// Using a custom KeyboardAvoidingView implementation seems to be more reliable. As of early 2026,
-// - On an Android 10 emulator and iOS, React Native's KeyboardAvoiding view needs additional padding
-//   to prevent content from being covered by the keyboard. On an Android 16 emulator, it does not.
-// - On iPadOS, showing the floating keyboard causes the KeyboardAvoidingView to have a very small height
-//   (https://github.com/facebook/react-native/issues/29473).
-//
-// This view assumes that keyboards, if docked, are docked to the bottom of the screen.
-const KeyboardAvoidingView: React.FC<Props> = ({ children, style, enabled, ...forwardedProps }) => {
+const KeyboardAvoidingView: React.FC<Props> = ({ enabled, children, ...forwardedProps }) => {
 	const keyboardState = useKeyboardState();
 
 	enabled &&= (
@@ -23,23 +12,15 @@ const KeyboardAvoidingView: React.FC<Props> = ({ children, style, enabled, ...fo
 		// height. Don't use the KeyboardAvoidingView when the floating keyboard is enabled.
 		// See https://github.com/facebook/react-native/issues/29473
 		!keyboardState.isFloatingKeyboard
-
-		// Disable the keyboard avoiding view when the keyboard isn't visible. This may prevent the view's content
-		// from shifting if the view is loaded soon after closing the keyboard.
-		&& keyboardState.keyboardVisible
 	);
 
-	const dockedKeyboardHeight = keyboardState.dockedKeyboardHeight;
-	const paddingStyles = useMemo(() => {
-		return { paddingBottom: dockedKeyboardHeight };
-	}, [dockedKeyboardHeight]);
-
-	return <View
-		style={enabled ? [paddingStyles, style] : style}
+	return <NativeKeyboardAvoidingView
+		behavior='padding'
 		{...forwardedProps}
+		enabled={enabled}
 	>
 		{children}
-	</View>;
+	</NativeKeyboardAvoidingView>;
 };
 
 export default KeyboardAvoidingView;
