91b95b4db20f1031fd2143966e524b9a64417fb4
jun.kobayashi <jun.kobayashi@access-company.com>
2026-02-06 10:01:19 +0900

    add config settings

--- packages/web_viewer/config.ts
+++ packages/web_viewer/config.ts
@@ -1,4 +1,5 @@
 export class Config {
     public static readonly searchNoteLimit = 30;
-    public static readonly renderWaitTimeoutMs = 3000;
+    public static readonly renderWaitTimeoutMs = 10_000;
+    public static readonly stableMs = 100;
 }
