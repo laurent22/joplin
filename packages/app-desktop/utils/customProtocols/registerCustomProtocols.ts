12f54136ab1e2798dc514af81f1b9072b16ac27a
Henry Heino <personalizedrefrigerator@gmail.com>
2026-02-09 11:18:05 -0800

    Type safety, remove supportsFetchAPI for now

--- packages/app-desktop/utils/customProtocols/registerCustomProtocols.ts
+++ packages/app-desktop/utils/customProtocols/registerCustomProtocols.ts
@@ -1,11 +1,11 @@
 
-import { protocol } from 'electron';
+import { Privileges, protocol } from 'electron';
 import { contentProtocolName, pluginProtocolName } from './constants';
 
 // This must be called before Electron's onReady event.
 // handleCustomProtocols should be called separately, after onReady.
 const registerCustomProtocols = async () => {
-	const contentProtocolPrivileges = {
+	const contentProtocolPrivileges: Privileges = {
 		supportFetchAPI: true,
 
 		// Don't trigger mixed content warnings (see https://stackoverflow.com/a/75988466)
@@ -22,12 +22,9 @@ const registerCustomProtocols = async () => {
 	};
 	// The plugin protocol is currently only used for serving JS and doesn't require as
 	// broad privileges.
-	const pluginProtocolPrivileges = {
+	const pluginProtocolPrivileges: Privileges = {
 		secure: true,
 		standard: true,
-		// Support the fetch API to fix "fetch failed" errors for source map requests.
-		// (Note: Currently all source map requests will return 404)
-		supportFetchApi: true,
 	};
 	protocol.registerSchemesAsPrivileged([
 		{ scheme: contentProtocolName, privileges: contentProtocolPrivileges },
