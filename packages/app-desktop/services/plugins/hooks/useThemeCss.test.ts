a6fe618be88df7e2c92d11e96513111b670eb5e6
Laurent Cozic <laurent22@users.noreply.github.com>
2026-02-10 14:23:04 +0000

    fix tests

--- packages/app-desktop/services/plugins/hooks/useThemeCss.test.ts
+++ packages/app-desktop/services/plugins/hooks/useThemeCss.test.ts
@@ -1,6 +1,7 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks/pure';
 import useThemeCss from './useThemeCss';
 import Setting from '@joplin/lib/models/Setting';
+import { waitForHook } from '@joplin/lib/testing/test-utils';
 
 describe('useThemeCss', () => {
 	it('should return a different path when the theme changes', async () => {
@@ -8,13 +9,13 @@ describe('useThemeCss', () => {
 			initialProps: { pluginId: 'testid', themeId: Setting.THEME_DARK },
 		});
 
-		await hookResult.waitFor(() => {
+		await waitForHook(() => {
 			expect(hookResult.result.current).toContain(`plugin_testid_theme_${Setting.THEME_DARK}.css`);
 		});
 
 		hookResult.rerender({ pluginId: 'testid', themeId: Setting.THEME_LIGHT });
 
-		await hookResult.waitFor(() => {
+		await waitForHook(() => {
 			expect(hookResult.result.current).toContain(`plugin_testid_theme_${Setting.THEME_LIGHT}.css`);
 		});
 	});
