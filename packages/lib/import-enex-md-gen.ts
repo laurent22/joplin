9d8a7929f3102cebb4296ced40da2c12a859039c
yugalkaushik <yugalkaushik14@gmail.com>
2026-02-21 10:00:03 +0000

    Update functionality and added test case

--- packages/lib/import-enex-md-gen.ts
+++ packages/lib/import-enex-md-gen.ts
@@ -1435,15 +1435,12 @@ function renderLine(line: any) {
 		// ENEX notes sometimes have hidden tags. We could strip off these
 		// sections but in the spirit of preserving all data we wrap them in
 		// a hidden tag too.
-		const renderedHiddenContent = renderLines(line.lines);
-		const contentText = renderedHiddenContent.join('').replace(/\[\[NEWLINE\]\]|\[\[BLOCK_OPEN\]\]|\[\[BLOCK_CLOSE\]\]|\[\[SPACE\]\]|\[\[MERGED\]\]/g, '').replace(/\s+/g, '');
-
-		if (!contentText) {
-			return [];
-		}
+		const rendered = renderLines(line.lines);
+		const content = rendered.join('').replace(/\[\[.*?\]\]/g, '').replace(/\s+/g, '');
+		if (!content) return [];
 
 		let hiddenLines = ['<div style="display: none;">'];
-		hiddenLines = hiddenLines.concat(renderedHiddenContent);
+		hiddenLines = hiddenLines.concat(rendered);
 		hiddenLines.push('</div>');
 
 		// We need to add two new lines after the HTML block, or the Markdown
