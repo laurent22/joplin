a3e58cd46f42d0a9991f02d542ef2f4205034c88
jun.kobayashi <jun.kobayashi@access-company.com>
2026-02-26 16:46:48 +0900

    Revert "test"
    This reverts commit fef362f4952d5be517dd380989edb3144f89d1ee.

--- packages/web_viewer/lib/store.ts
+++ packages/web_viewer/lib/store.ts
@@ -16,4 +16,3 @@ export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>;
 export type AppDispatch = AppStore['dispatch'];
-// test
