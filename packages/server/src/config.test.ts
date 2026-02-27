7b9ff42c140fdbf14048e5007af57c1a157c1ac2
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-02-27 15:19:29 +0530

    Added extra test as suggested by code rabbit

--- packages/server/src/config.test.ts
+++ packages/server/src/config.test.ts
@@ -11,4 +11,17 @@ describe('config', () => {
 		expect(mailerSecurityForPort(465, MailerSecurity.Starttls)).toBe(MailerSecurity.Tls);
 	});
 
+	it('should keep starttls for port 587 when security is already starttls', () => {
+		expect(mailerSecurityForPort(587, MailerSecurity.Starttls)).toBe(MailerSecurity.Starttls);
+	});
+
+	it('should keep tls for port 465 when security is already tls', () => {
+		expect(mailerSecurityForPort(465, MailerSecurity.Tls)).toBe(MailerSecurity.Tls);
+	});
+
+	it('should not modify security for non-standard ports', () => {
+		expect(mailerSecurityForPort(25, MailerSecurity.Tls)).toBe(MailerSecurity.Tls);
+		expect(mailerSecurityForPort(2525, MailerSecurity.Starttls)).toBe(MailerSecurity.Starttls);
+	});
+
 });
