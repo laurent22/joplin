5514901761965ff85da12aeffcb01c6e4f3beba8
Rohit <dv6510@gmail.com>
2026-03-02 09:05:46 +0530

    refactor: added explanation for eslint-disable as requested

--- packages/lib/services/plugins/utils/validateManifest.ts
+++ packages/lib/services/plugins/utils/validateManifest.ts
@@ -2,6 +2,6 @@
 import manifestFromObject from './manifestFromObject';
 
 export default function validateManifest(manifest: unknown): void {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- manifestFromObject takes any
 	manifestFromObject(manifest as any);
 }
