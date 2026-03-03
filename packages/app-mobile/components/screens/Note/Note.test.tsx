fee2836b1a7c548b72c7a38805084d8dbaa4da83
yugalkaushik <yugalkaushik14@gmail.com>
2026-03-03 07:49:14 +0000

    Call unmount() in Note.test.tsx tests to suppress act() warnings

--- packages/app-mobile/components/screens/Note/Note.test.tsx
+++ packages/app-mobile/components/screens/Note/Note.test.tsx
@@ -35,10 +35,10 @@ interface WrapperProps {
 
 let store: Store<AppState>;
 
-const mockNavigation = { state: {} };
+const mockNavigation = { state: { } };
 const WrappedNoteScreen: React.FC<WrapperProps> = _props => {
 	return <TestProviderStack store={store}>
-		<NoteScreen navigation={mockNavigation} />
+		<NoteScreen navigation={mockNavigation}/>
 	</TestProviderStack>;
 };
 
