baf3145828b8ade43160e6d9966405a7bec516f0
Vinayreddy765 <vinayreddy7628@gmail.com>
2026-03-11 17:17:23 +0530

    Fix encryption enable flow when no master keys exist

--- packages/app-desktop/gui/EncryptionConfigScreen/EncryptionConfigScreen.tsx
+++ packages/app-desktop/gui/EncryptionConfigScreen/EncryptionConfigScreen.tsx
@@ -200,8 +200,24 @@ const EncryptionConfigScreen = (props: Props) => {
 			const answer = await dialogs.confirm(_('Disabling encryption means *all* your notes and attachments are going to be re-synchronised and sent unencrypted to the sync target. Do you wish to continue?'));
 			if (!answer) return;
 		} else {
-			if (!hasMasterPassword) {
+			if (props.masterKeys.length === 0) {
 				await CommandService.instance().execute('openMasterPasswordDialog');
+
+				const passwordAfterDialog = Setting.value('encryption.masterPassword') as string;
+
+				if (!passwordAfterDialog) return;
+
+				try {
+					await toggleAndSetupEncryption(
+						EncryptionService.instance(),
+						newEnabled,
+						masterKey,
+						passwordAfterDialog,
+					);
+				} catch (error) {
+					await dialogs.alert(error.message);
+				}
+
 				return;
 			}
 			const msg = enableEncryptionConfirmationMessages(masterKey, hasMasterPassword);
@@ -220,7 +236,7 @@ const EncryptionConfigScreen = (props: Props) => {
 		} catch (error) {
 			await dialogs.alert(error.message);
 		}
-	}, [props.masterPassword]);
+	}, [props.masterPassword, props.masterKeys.length]);
 
 	const renderEncryptionSection = () => {
 		const decryptedItemsInfo = <p>{decryptedStatText(stats)}</p>;
