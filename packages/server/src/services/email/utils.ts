79660c3a0e48ecd2d39356c8f32f70c580c508be
Mahathi-s154 <1bi22ri028@bit-bangalore.edu.in>
2026-03-31 21:43:48 +0530

    Use shared markdown renderer for email HTML

--- packages/server/src/services/email/utils.ts
+++ packages/server/src/services/email/utils.ts
@@ -1,14 +1,10 @@
-import MarkdownIt = require('markdown-it');
+import renderMarkdown from '../../utils/renderMarkdown';
 
 export function markdownBodyToPlainText(md: string): string {
 	// Just convert the links to plain URLs
 	return md.replace(/\[.*\]\((.*)\)/g, '$1');
 }
 
-// TODO: replace with renderMarkdown()
 export function markdownBodyToHtml(md: string): string {
-	const markdownIt = new MarkdownIt({
-		linkify: true,
-	});
-	return markdownIt.render(md);
+	return renderMarkdown(md);
 }
