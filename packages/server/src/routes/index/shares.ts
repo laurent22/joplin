f9738d83a8bf8b634188f31c32ddc434fb34adf3
Aissa Benfodda <aissabenfodda@protonmail.com>
2026-03-30 21:47:57 -0400

    Server: add access to nested published notes

--- packages/server/src/routes/index/shares.ts
+++ packages/server/src/routes/index/shares.ts
@@ -1,9 +1,9 @@
-import { SubPath, ResponseType, Response } from '../../utils/routeUtils';
+import { SubPath, ResponseType, Response, redirect } from '../../utils/routeUtils';
 import Router from '../../utils/Router';
 import { RouteType } from '../../utils/types';
 import { AppContext } from '../../utils/types';
 import { ErrorForbidden, ErrorNotFound } from '../../utils/errors';
-import { Item, Share } from '../../services/database/types';
+import { Item, Share, ShareType } from '../../services/database/types';
 import { ModelType } from '@joplin/lib/BaseModel';
 import { FileViewerResponse, renderItem as renderJoplinItem } from '../../utils/joplinUtils';
 import { friendlySafeFilename } from '@joplin/lib/path-utils';
@@ -38,6 +38,16 @@ router.get('shares/:id', async (path: SubPath, ctx: AppContext) => {
 
 	const user = await ctx.joplin.models.user().load(share.owner_id);
 	if (!user.enabled) throw new ErrorForbidden('This account has been disabled');
+
+	if (ctx.query.note_id && !share.recursive) {
+		const noteItem = await ctx.joplin.models.item().loadByJopId(share.owner_id, ctx.query.note_id as string);
+		if (!noteItem) throw new ErrorForbidden('This linked note has not been published');
+
+		const noteShare = await shareModel.itemShare(ShareType.Note, noteItem.id);
+		if (!noteShare) throw new ErrorForbidden('This linked note has not been published');
+
+		return redirect(ctx, shareModel.shareUrl(noteShare.owner_id, noteShare.id));
+	}
 
 	const itemModel = ctx.joplin.models.item();
 
