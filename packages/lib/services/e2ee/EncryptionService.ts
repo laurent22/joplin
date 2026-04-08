9fc02d028e22c982ba98fe46950ffc18833cae54
mrjo118 <jo.118@hotmail.com>
2026-04-08 20:32:22 +0100

    Update comment

--- packages/lib/services/e2ee/EncryptionService.ts
+++ packages/lib/services/e2ee/EncryptionService.ts
@@ -348,11 +348,11 @@ export default class EncryptionService {
 	public async checkMasterKeyPassword(model: MasterKeyEntity, password: string, performTestCipherCheck = false) {
 		const task = perfLogger.taskStart('EncryptionService/checkMasterKeyPassword');
 		try {
-			// On the mobile platform (but not on web), decrypting the master key with an incorrect password does not throw an exception, so to validate the
-			// master password is correct, the decrypted key is used to decrypt a previously stored cipher of a known value, to verify the key decrypts the
-			// correct value. If testCipher is not populated, further validation is skipped because there is no other way to verify the key is correct.
-			// Newly created master keys will have this set, while for pre-existing keys, the testCipher is populated the first time an encrypted item is
-			// successfully decrypted using the master key
+			// On the mobile platform (but not on web), decrypting the master key with an incorrect password (or double decrypting it) does not throw an
+			// exception, so to validate the master password is correct, the decrypted key is used to decrypt a previously stored cipher of a known value,
+			// to verify the key decrypts the correct value. If testCipher is not populated, further validation is skipped because there is no other way
+			// to verify the key is correct. Newly created master keys will have this set, while for pre-existing keys, the testCipher is populated the
+			// first time an encrypted item is successfully decrypted using the master key
 			if (performTestCipherCheck && model.testCipher) {
 				// Reselect the key to ensure it is in encrypted form
 				const mk = await MasterKey.load(model.id);
