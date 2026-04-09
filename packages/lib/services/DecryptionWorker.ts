cc846e35f431747de75d7389769710d457975248
mrjo118 <jo.118@hotmail.com>
2026-04-09 15:10:39 +0100

    Fix resource decryption issue

--- packages/lib/services/DecryptionWorker.ts
+++ packages/lib/services/DecryptionWorker.ts
@@ -246,6 +246,25 @@ export default class DecryptionWorker {
 							// but that will result in an infinite loop if the blob simply has not been downloaded yet.
 							// So skip the ID for now, and the service will try to decrypt the blob again the next time.
 							excludedIds.push(decryptedItem.id);
+						} else {
+							// If ItemClass.decrypt did not error, the item was successfully deserialized after decryption, which means the master password
+							// is correct, and therefore this key can be used to create a testCipher if the key does not have one, because it was pre-existing
+							// before testCipher was added
+							if (header?.masterKeyId && (!lastMasterKeyId || header.masterKeyId !== lastMasterKeyId)) {
+								lastMasterKeyId = header.masterKeyId;
+								const mk = masterKeyById(header.masterKeyId);
+
+								if (mk && !mk.testCipher) {
+									try {
+										mk.testCipher = await this.encryptionService().encryptString(mkTestText, { masterKeyId: mk.id });
+										mk.updated_time = Date.now();
+										await MasterKey.save(mk);
+										this.logger().info(`DecryptionWorker: Updated missing testCipher for master key with id ${header.masterKeyId}`);
+									} catch (exception) {
+										this.logger().info(`DecryptionWorker: Error populating testCipher for master key with id ${header.masterKeyId}`, exception);
+									}
+								}
+							}
 						}
 
 						if (decryptedItem.type_ === Resource.modelType() && !decryptedItem.encryption_blob_encrypted) {
@@ -254,25 +273,6 @@ export default class DecryptionWorker {
 
 						if (decryptedItem.type_ === Resource.modelType() && !decryptedItem.encryption_applied && !!decryptedItem.encryption_blob_encrypted) {
 							this.eventEmitter_.emit('resourceMetadataButNotBlobDecrypted', { id: decryptedItem.id });
-						}
-
-						// If ItemClass.decrypt did not error, the item was successfully serialised after decryption, which means the master password is correct,
-						// and therefore this key can be used to create a test cipher if the key does not have one, because it was pre-existing before test cipher
-						// was added
-						if (header?.masterKeyId && (!lastMasterKeyId || header.masterKeyId !== lastMasterKeyId)) {
-							lastMasterKeyId = header.masterKeyId;
-							const mk = masterKeyById(header.masterKeyId);
-
-							if (mk && !mk.testCipher) {
-								try {
-									mk.testCipher = await this.encryptionService().encryptString(mkTestText, { masterKeyId: mk.id });
-									mk.updated_time = Date.now();
-									await MasterKey.save(mk);
-									this.logger().info(`DecryptionWorker: Updated missing testCipher for master key with id ${header.masterKeyId}`);
-								} catch (exception) {
-									this.logger().info(`DecryptionWorker: Error populating testCipher for master key with id ${header.masterKeyId}`, exception);
-								}
-							}
 						}
 					} catch (error) {
 						excludedIds.push(item.id);
