c94491659833b7f211f9e563ed9537afb3846f98
Henry Heino <personalizedrefrigerator@gmail.com>
2026-04-13 13:36:14 -0700

    Commenting

--- packages/editor/CodeMirror/configFromSettings.ts
+++ packages/editor/CodeMirror/configFromSettings.ts
@@ -109,6 +109,8 @@ const configFromSettings = (settings: EditorSettings, context: RenderedContentCo
 		extensions.push(Prec.low(keymap.of(defaultKeymap)));
 	}
 
+	// Only enable in-editor rendering for Markdown notes. In-editor rendering can result in
+	// confusing output in HTML notes (e.g. some, but not most, tags hidden).
 	if (settings.inlineRenderingEnabled && settings.language === EditorLanguageType.Markdown) {
 		extensions.push(renderingExtension());
 	}
