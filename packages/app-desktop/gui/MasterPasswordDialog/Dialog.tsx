0a160be2667289b9fa224ecfade64f94bc0973b2
keshav0479 <keshav.rsk07@gmail.com>
2026-04-16 19:22:02 +0530

    Desktop: Fixes #15107: Defer sync after master password save to prevent race with encryption enable

--- packages/app-desktop/gui/MasterPasswordDialog/Dialog.tsx
+++ packages/app-desktop/gui/MasterPasswordDialog/Dialog.tsx
@@ -78,7 +78,7 @@ export default function(props: Props) {
 				} else {
 					throw new Error(`Unknown mode: ${mode}`);
 				}
-				void reg.waitForSyncFinishedThenSync();
+				void reg.scheduleSync();
 				onClose();
 			} catch (error) {
 				void shim.showErrorDialog(error.message);
