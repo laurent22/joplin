a937934bb115dd2feebc21309a9b0905a5b1c393
Laurent Cozic <laurent22@users.noreply.github.com>
2026-04-17 07:32:49 +0100

    update

--- packages/server/src/utils/metrics.test.ts
+++ packages/server/src/utils/metrics.test.ts
@@ -6,13 +6,10 @@ describe('metrics', () => {
 
 	beforeEach(() => {
 		clearMetrics();
-		jest.useFakeTimers({
-			// Timers need to auto-advance to support node-os-utils.
-			advanceTimers: true,
-		});
+		jest.useFakeTimers();
 	});
 
-	it('should generate a heartbeat message', async () => {
+	it('should generate a heartbeat message', () => {
 		const requestId1 = Math.random().toString();
 		const requestId2 = Math.random().toString();
 		const requestId3 = Math.random().toString();
@@ -24,16 +21,15 @@ describe('metrics', () => {
 
 		const regex = /Cpu: (.*?)%; Mem: (.*?) \/ (.*?) MB \((.*?)%\); Req: 3 \/ min; Active req: 2/;
 
-		const message = await heartbeatMessage();
+		const message = heartbeatMessage();
 
 		const match = message.match(regex);
 		expect(match.length).toBe(5);
-		expect(Number(match[1])).toBeGreaterThan(0);
 		expect(Number(match[2])).toBeLessThan(Number(match[3]));
 		expect(Number(match[3])).toBeGreaterThan(0);
 	});
 
-	it('should count the number of requests per minute', async () => {
+	it('should count the number of requests per minute', () => {
 		const mockRequest = () => {
 			const id = uuid.create();
 			onRequestStart(id);
@@ -44,18 +40,18 @@ describe('metrics', () => {
 			mockRequest();
 			jest.advanceTimersByTime(Second);
 		}
-		expect(await heartbeatMessage()).toMatch(/Req: 10 \/ min/);
+		expect(heartbeatMessage()).toMatch(/Req: 10 \/ min/);
 
 		jest.advanceTimersByTime(Minute * 15);
-		expect(await heartbeatMessage()).toMatch(/Req: 0 \/ min/);
+		expect(heartbeatMessage()).toMatch(/Req: 0 \/ min/);
 		mockRequest();
-		expect(await heartbeatMessage()).toMatch(/Req: 1 \/ min/);
+		expect(heartbeatMessage()).toMatch(/Req: 1 \/ min/);
 
 		jest.advanceTimersByTime(Minute * 2);
 		mockRequest();
 		jest.advanceTimersByTime(Second * 10);
 		mockRequest();
-		expect(await heartbeatMessage()).toMatch(/Req: 2 \/ min/);
+		expect(heartbeatMessage()).toMatch(/Req: 2 \/ min/);
 	});
 
 });
