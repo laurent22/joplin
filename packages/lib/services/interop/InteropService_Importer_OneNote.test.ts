b06a807682ecaac5309ff07c9f7595ece54bf7fb
Henry Heino <personalizedrefrigerator@gmail.com>
2026-04-23 10:12:18 -0700

    Simplify test snapshot

--- packages/lib/services/interop/InteropService_Importer_OneNote.test.ts
+++ packages/lib/services/interop/InteropService_Importer_OneNote.test.ts
@@ -389,6 +389,7 @@ describe('InteropService_Importer_OneNote', () => {
 
 	it('should import bold and italic in a way that can be converted to Markdown', async () => {
 		const notes = await importNote(`${supportDir}/onenote/bold_and_italic.one`);
-		expect(notesToMarkdownString(notes)).toMatchSnapshot();
+		const matchingNotes = notes.filter(n => n.title === 'Bold & italic');
+		expect(notesToMarkdownString(matchingNotes)).toMatchSnapshot();
 	});
 });
