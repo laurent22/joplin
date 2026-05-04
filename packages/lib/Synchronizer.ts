a5a543eef4596adb6ad0b987442ac919b2cd351d
mrjo118 <jo.118@hotmail.com>
2026-05-04 02:49:38 +0100

    Make the sync fail when an item with an updated_date over 1 day in the future causes the sync to exit

--- packages/lib/Synchronizer.ts
+++ packages/lib/Synchronizer.ts
@@ -634,7 +634,11 @@ export default class Synchronizer {
 						if (donePaths.indexOf(path) >= 0) {
 							const syncItem = await BaseItem.syncItem(syncTargetId, local.id, { fields: ['force_sync'] });
 							if (local.updated_time > time.unixMs()) {
-								throw new JoplinError(sprintf('Processing a path that has already been done: %s. Remote item has an updated_time in the future', path), 'processingPathTwice');
+								if (local.updated_time > time.unixMs() + 24 * 60 * 60 * 1000) {
+									throw new JoplinError(sprintf('Unexpected data: Remote item %s has an updated_time over 24 hours in the future', path), 'unexpectedData');
+								} else {
+									throw new JoplinError(sprintf('Processing a path that has already been done: %s. Remote item has an updated_time in the future', path), 'processingPathTwice');
+								}
 							} else if (syncItem.force_sync) {
 								throw new JoplinError(sprintf('Processing a path that has already been done: %s. Item was marked for sync using force_sync', path), 'processingPathTwice');
 							} else {
