c6e323e91b7803e7b6fdedc579060c8c25423ff4
Henry Heino <personalizedrefrigerator@gmail.com>
2026-05-12 19:59:44 -0700

    Coderabbit feedback: remoteDeletedItems: Handle the case where itemIds is empty

--- packages/lib/models/BaseItem.ts
+++ packages/lib/models/BaseItem.ts
@@ -393,8 +393,13 @@ export default class BaseItem extends BaseModel {
 		};
 	}
 
-	public static remoteDeletedItems(syncTarget: number, itemIds: string[]) {
-		return this.db().exec(`DELETE FROM deleted_items WHERE item_id IN (${this.escapeIdsForSql(itemIds)}) AND sync_target = ?`, [syncTarget]);
+	public static async remoteDeletedItems(syncTarget: number, itemIds: string[]) {
+		if (itemIds.length === 0) return;
+
+		await this.db().exec(
+			`DELETE FROM deleted_items WHERE item_id IN (${this.escapeIdsForSql(itemIds)}) AND sync_target = ?`,
+			[syncTarget],
+		);
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
