359a26ef0f888c745f911fd6b1cdbfbbe75a6267
Henry Heino <personalizedrefrigerator@gmail.com>
2026-05-21 07:31:55 -0700

    Fix focusing the Markdown editor when tapping near the end of the WebView

--- packages/app-mobile/components/NoteEditor/MarkdownEditor.tsx
+++ packages/app-mobile/components/NoteEditor/MarkdownEditor.tsx
@@ -28,6 +28,10 @@ function useCss(themeId: number): string {
 
 			body {
 				margin: 0;
+				/* The body should be at least as tall as the screen:
+				   - This allows the editor to focus when tapping anywhere in the body in an empty note.
+				   - This must be **at least** and not **exactly** 100vh to avoid scroll issues (see PR #15499) */
+				min-height: 100vh;
 				/* Prefer 100% -- 100vw shows an unnecessary horizontal scrollbar in Google Chrome (desktop). */
 				width: 100%;
 				box-sizing: border-box;
