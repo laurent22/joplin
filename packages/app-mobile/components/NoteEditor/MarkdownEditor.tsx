d2fb7add4ce2264a0724903d7d5f89485dffa973
Henry Heino <personalizedrefrigerator@gmail.com>
2026-05-21 08:04:24 -0700

    Web: Fix unnecessary scrollbar in new, empty notes

--- packages/app-mobile/components/NoteEditor/MarkdownEditor.tsx
+++ packages/app-mobile/components/NoteEditor/MarkdownEditor.tsx
@@ -30,14 +30,22 @@ function useCss(themeId: number): string {
 				margin: 0;
 				/* Prefer 100% -- 100vw shows an unnecessary horizontal scrollbar in Google Chrome (desktop). */
 				width: 100%;
-				box-sizing: border-box;
+				padding: 0;
 
+				font-size: 13pt;
+			}
+
+			body > .CodeMirror {
 				padding-left: 1px;
 				padding-right: 1px;
 				padding-bottom: 1px;
 				padding-top: 10px;
+				/* Padding contributes to the height */
+				box-sizing: border-box;
 
-				font-size: 13pt;
+				/* Tapping anywhere in an empty note should focus the editor. This style ensures that the
+				   editor wrapper is large enough for this to happen. */
+				min-height: 100vh;
 			}
 
 			* {
@@ -98,7 +106,7 @@ function useHtml(): string {
 				</style>
 			</head>
 			<body>
-				<div class="CodeMirror" style="min-height:100vh;" autocapitalize="on"></div>
+				<div class="CodeMirror" autocapitalize="on"></div>
 			</body>
 		</html>
 	`, []);
