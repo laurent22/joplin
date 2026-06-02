8b364a496e48efba5946fac46f2e0c268ee8113a
Aissa Benfodda <aissabenfodda@protonmail.com>
2026-06-02 05:02:09 -0400

    fix coderrabitai review

--- packages/lib/commands/convertNoteToMarkdown.test.ts
+++ packages/lib/commands/convertNoteToMarkdown.test.ts
@@ -29,13 +29,14 @@ describe('convertNoteToMarkdown', () => {
 
 		const refreshedNote = await Note.load(htmlNote.id);
 		const trashedNotes = await Note.previews(getTrashFolderId());
-		const backupNote = await Note.load(trashedNotes[0].id);
 
 		expect(refreshedNote.id).toBe(htmlNote.id);
 		expect(refreshedNote.deleted_time).toBe(0);
 		expect(refreshedNote.markup_language).toBe(MarkupLanguage.Markdown);
-
 		expect(trashedNotes).toHaveLength(1);
+
+		const backupNote = await Note.load(trashedNotes[0].id);
+
 		expect(backupNote.id).not.toBe(htmlNote.id);
 		expect(backupNote.deleted_time).not.toBe(0);
 		expect(backupNote.body).toBe(htmlNote.body);
