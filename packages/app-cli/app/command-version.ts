bc47eaa18688495ed5de64be8b2d2a78fd04bbfb
Henry Heino <personalizedrefrigerator@gmail.com>
2026-06-22 07:54:27 -0700

    Cli: Fix 'version' command when published to NPM

--- packages/app-cli/app/command-version.ts
+++ packages/app-cli/app/command-version.ts
@@ -1,6 +1,24 @@
 import BaseCommand from './base-command';
 import { _ } from '@joplin/lib/locale';
 import versionInfo from '@joplin/lib/versionInfo';
+import { pathExists } from 'fs-extra';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+// The package.json file is stored in a different locations depending on whether
+// the CLI app is published to NPM.
+const loadPackageJson = async () => {
+	const packageJsonPaths = ['./package.json', '../package.json'];
+	for (const path of packageJsonPaths) {
+		const fullPath = join(__dirname, path);
+		if (await pathExists(fullPath)) {
+			return JSON.parse(
+				await readFile(fullPath, 'utf-8'),
+			);
+		}
+	}
+	throw new Error('Unable to find package.json');
+};
 
 class Command extends BaseCommand {
 	public override usage() {
@@ -12,7 +30,9 @@ class Command extends BaseCommand {
 	}
 
 	public override async action() {
-		this.stdout(versionInfo(require('../package.json'), {}).message);
+		this.stdout(
+			versionInfo(await loadPackageJson(), {}).message,
+		);
 	}
 }
 
