b68f595bbcc3b4d7dc216ec604a96ce089acc851
Henry Heino <personalizedrefrigerator@gmail.com>
2026-06-22 08:03:49 -0700

    Alternate fix: Run the CLI app from `build/`

--- packages/app-cli/app/command-version.ts
+++ packages/app-cli/app/command-version.ts
@@ -1,24 +1,6 @@
 import BaseCommand from './base-command';
 import { _ } from '@joplin/lib/locale';
 import versionInfo from '@joplin/lib/versionInfo';
-import { pathExists } from 'fs-extra';
-import { readFile } from 'fs/promises';
-import { join } from 'path';
-
-// The package.json file is stored in a different locations depending on whether
-// the CLI app is published to NPM.
-const loadPackageJson = async () => {
-	const packageJsonPaths = ['./package.json', '../package.json'];
-	for (const path of packageJsonPaths) {
-		const fullPath = join(__dirname, path);
-		if (await pathExists(fullPath)) {
-			return JSON.parse(
-				await readFile(fullPath, 'utf-8'),
-			);
-		}
-	}
-	throw new Error('Unable to find package.json');
-};
 
 class Command extends BaseCommand {
 	public override usage() {
@@ -30,9 +12,7 @@ class Command extends BaseCommand {
 	}
 
 	public override async action() {
-		this.stdout(
-			versionInfo(await loadPackageJson(), {}).message,
-		);
+		this.stdout(versionInfo(require('./package.json'), {}).message);
 	}
 }
 
