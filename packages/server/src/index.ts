ca2a02a1bfb6b759d57b56a54217c01aa77ec261
Henry Heino <personalizedrefrigerator@gmail.com>
2026-06-24 13:19:05 -0700

    Disable only __proto__

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,33 +1,15 @@
-import Logger, { TargetType } from '@joplin/utils/Logger';
 import { fork } from 'node:child_process';
 import { join } from 'node:path';
 
 // This script starts the server with a customized environment. This is useful
 // for enabling global NodeJS hardening options.
 
-const logger = Logger.create('index');
-
-const setUpLogger = () => {
-	const globalLogger = new Logger();
-	globalLogger.addTarget(TargetType.Console);
-	Logger.initializeGlobalLogger(globalLogger);
-};
-
-const getHardeningLevel = () => {
-	const hardeningLevel = Number(process.env.JOPLIN_HARDENING_LEVEL || '0');
-	if (!isFinite(hardeningLevel)) throw new Error('Invalid environment: JOPLIN_HARDENING_LEVEL must be an integer or undefined');
-	return hardeningLevel;
-};
-
 const getServerEnv = () => {
-	const hardeningLevel = getHardeningLevel();
-	if (hardeningLevel === 0) return process.env;
-
-	logger.info('Starting with certain NodeJS hardening options enabled.');
-
 	const nodeOptions = [
 		// Hardening: Disallow code execution through 'eval' and 'new Function'
-		'--disallow-code-generation-from-strings',
+		// Disabled for now: Some libraries still use eval():
+		// '--disallow-code-generation-from-strings',
+
 		// Disable the __proto__ property:
 		// Ref: https://cheatsheetseries.owasp.org/cheatsheets/Prototype_Pollution_Prevention_Cheat_Sheet.html#nodejs-configuration-flag
 		'--disable-proto=delete',
@@ -35,6 +17,7 @@ const getServerEnv = () => {
 
 	return {
 		...process.env,
+
 		'NODE_OPTIONS': [
 			process.env.NODE_OPTIONS ?? '',
 			...nodeOptions,
@@ -43,8 +26,6 @@ const getServerEnv = () => {
 };
 
 const main = () => {
-	setUpLogger();
-
 	// Omit the NodeJS process name and file path arguments:
 	const argv = process.argv.slice(2);
 	const child = fork(join(__dirname, 'app.js'), argv, {
