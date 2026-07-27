9324eb22193736c2f1de9cfeaf7314cd117554ec
mrjo118 <jo.118@hotmail.com>
2026-07-27 13:03:14 +0100

    Update comment

--- packages/app-mobile/components/screens/Notes/TextWrapCalculator.tsx
+++ packages/app-mobile/components/screens/Notes/TextWrapCalculator.tsx
@@ -14,9 +14,9 @@ interface Props {
 }
 
 // This component can be used to estimate when text wrapping is required for a TextInput or Text element, to conditionally display a button to enable / disable
-// text wrapping, depending on whether the text is wide enough to require a toggle. Because the exact wrap point cannot be known precisely, this component uses
-// an over-approximation when updating 'multiline' (which can be mapped to the driving component's multiline prop). This causes the toggle to appear only after
-// wrapping has begun, avoiding early expansion and improving the editing experience.
+// text wrapping, depending on whether the text is wide enough to require a toggle. Note that there may be a small amount of over-approximation when updating
+// 'multiline' (which can be mapped to the driving component's multiline prop). This causes the toggle to appear only after wrapping has begun, avoiding early
+// expansion and improving the editing experience.
 // Even if already using a Text element, a separate hidden Text element must be used for text wrapping estimation, because if onTextLayout is used on a visible
 // component, it prevents text highlighting from working.
 const TextWrapCalculator: React.FC<Props> = props => {
