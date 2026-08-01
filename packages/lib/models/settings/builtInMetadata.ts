a4361fdc9978be4d802b314266cf1a3b9ac44052
Henry Heino <personalizedrefrigerator@gmail.com>
2026-07-31 22:37:05 -0700

    Disable the semantic search results flag for now

--- packages/lib/models/settings/builtInMetadata.ts
+++ packages/lib/models/settings/builtInMetadata.ts
@@ -2378,7 +2378,7 @@ const builtInMetadata = (Setting: typeof SettingType) => {
 		},
 
 		'featureFlag.enableSemanticSearch': {
-			value: true,
+			value: false,
 			type: SettingItemType.Bool,
 			public: true,
 			storage: SettingStorage.File,
