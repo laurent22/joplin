15f6a06c54e6541210bfcf76f340682bf65f3be3
Henry Heino <personalizedrefrigerator@gmail.com>
2026-08-01 00:27:40 -0700

    Add test for useSearchMarkers

--- packages/lib/services/search/SearchEngine.semantic.test.ts
+++ packages/lib/services/search/SearchEngine.semantic.test.ts
@@ -1,9 +1,6 @@
 import Note from '../../models/Note';
-import Setting from '../../models/Setting';
-import { db, setupDatabaseAndSynchronizer, switchClient } from '../../testing/test-utils';
-import AiService from '../ai/AiService';
-import EmbeddingIndexer from '../ai/EmbeddingIndexer';
-import TestEmbeddingProvider from '../ai/testing/TestEmbeddingProvider';
+import { setUpSemanticSearch, tearDownSemanticSearch, updateSearchIndex } from '../../testing/ai/semanticSearch';
+import { setupDatabaseAndSynchronizer, switchClient } from '../../testing/test-utils';
 import SearchEngine from './SearchEngine';
 
 describe('SearchEngine.semantic', () => {
@@ -13,25 +10,18 @@ describe('SearchEngine.semantic', () => {
 		await setupDatabaseAndSynchronizer(1);
 		await switchClient(1);
 
-		engine = new SearchEngine();
-		engine.setDb(db());
-
-		const provider = new TestEmbeddingProvider();
-		AiService.instance().setEmbeddingProvider(provider);
-
-		Setting.setValue('featureFlag.enableSemanticSearch', true);
+		await setUpSemanticSearch();
+		engine = SearchEngine.instance();
 	});
 
 	afterEach(async () => {
-		AiService.instance().setEmbeddingProvider(null);
-		await EmbeddingIndexer.instance().stopRunInBackground();
+		await tearDownSemanticSearch();
 	});
 
 	it('should include semantic results in general search output', async () => {
 		await Note.save({ title: 'test', body: 'letter letter letter letter' });
 
-		await EmbeddingIndexer.instance().maintenance();
-		await engine.syncTables();
+		await updateSearchIndex();
 
 		const rows = await engine.search('letters');
 		expect(rows).toHaveLength(1);
@@ -40,8 +30,7 @@ describe('SearchEngine.semantic', () => {
 	it('should not use semantic search when the user has specified a field to search in', async () => {
 		await Note.save({ title: 'test', body: 'letter letter letter body:letter' });
 
-		await EmbeddingIndexer.instance().maintenance();
-		await engine.syncTables();
+		await updateSearchIndex();
 
 		const rows = await engine.search('body:letters');
 		expect(rows).toHaveLength(0);
