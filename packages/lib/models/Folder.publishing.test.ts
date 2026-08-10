02822e709b8465e58312e6c5941bac04ad1a543c
Henry Heino <personalizedrefrigerator@gmail.com>
2026-08-10 11:59:22 -0700

    Refactor test file

--- packages/lib/models/Folder.publishing.test.ts
+++ packages/lib/models/Folder.publishing.test.ts
@@ -1,7 +1,8 @@
-import { setupDatabaseAndSynchronizer, switchClient, resourceService } from '../testing/test-utils';
+import { setupDatabaseAndSynchronizer, switchClient, resourceService, createFolderTree } from '../testing/test-utils';
 import Folder from '../models/Folder';
 import { ShareType, StateShare } from '../services/share/reducer';
 import BaseItem from './BaseItem';
+import Note from './Note';
 
 const publishedFolderShareState = (folderId: string): StateShare => ({
 	id: `share-${folderId}`,
@@ -27,24 +28,32 @@ describe('models/Folder.publishing', () => {
 	});
 
 	it('should set is_shared=1 for descendants of a published folder', async () => {
-		const root = await Folder.save({
-			title: 'root',
-		});
-		const child1 = await Folder.save({
-			title: 'sub-folder-1',
-			parent_id: root.id,
-		});
-		const child2 = await Folder.save({
-			title: 'sub-folder-2',
-			parent_id: root.id,
-		});
-		const grandChild1 = await Folder.save({
-			title: 'sub-sub-folder',
-			parent_id: child1.id,
-		});
-		const unpublished = await Folder.save({
-			title: 'not published',
-		});
+		const root = await createFolderTree('', [
+			{
+				title: 'root',
+				children: [
+					{
+						title: 'sub-folder 1',
+						children: [],
+					},
+					{
+						title: 'sub-folder 2',
+						children: [
+							{ title: 'sub-sub-folder', children: [] },
+							{ title: 'published note 1' },
+							{ title: 'published note 2' },
+						],
+					},
+					{ title: 'published note 3' },
+				],
+			},
+			{
+				title: 'unpublished folder',
+				children: [
+					{ title: 'unpublished note' },
+				],
+			},
+		]);
 
 		const shareState: StateShare[] = [
 			publishedFolderShareState(root.id),
@@ -55,10 +64,26 @@ describe('models/Folder.publishing', () => {
 			shareState,
 		);
 
-		await expectPublished(root.id);
-		await expectPublished(child1.id);
-		await expectPublished(child2.id);
-		await expectPublished(grandChild1.id);
-		await expectUnpublished(unpublished.id);
+		for (const title of [
+			'root',
+			'sub-folder 1',
+			'sub-folder 2',
+			'sub-sub-folder',
+		]) {
+			const id = (await Folder.loadByTitle(title)).id;
+			await expectPublished(id);
+		}
+		for (const title of [
+			'published note 1',
+			'published note 2',
+			'published note 3',
+		]) {
+			const id = (await Note.loadByTitle(title)).id;
+			await expectPublished(id);
+		}
+
+		// Items that are not descendants of root should not be published
+		await expectUnpublished((await Folder.loadByTitle('unpublished folder')).id);
+		await expectUnpublished((await Note.loadByTitle('unpublished note')).id);
 	});
 });
