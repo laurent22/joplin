c0654f0da8b05525bf411bebc6d1225d90a70b66
mrjo118 <jo.118@hotmail.com>
2026-08-16 02:44:58 +0100

    Improve comment

--- packages/app-desktop/gui/NoteEditor/NoteEditor.tsx
+++ packages/app-desktop/gui/NoteEditor/NoteEditor.tsx
@@ -526,7 +526,7 @@ function NoteEditorContent(props: NoteEditorProps) {
 		useCustomPdfViewer: props.useCustomPdfViewer,
 		// We need it to identify the context for which media is rendered.
 		// It is currently used to remember pdf scroll position for each attachments of each note uniquely.
-		// Keep the editor associated with the loaded form note until onBeforeLoad
+		// Use the form note object to ensure the editor is associated with the loaded form note until onBeforeLoad
 		// has flushed any pending changes from it.
 		noteId: formNote.id,
 		watchedNoteFiles: props.watchedNoteFiles,
