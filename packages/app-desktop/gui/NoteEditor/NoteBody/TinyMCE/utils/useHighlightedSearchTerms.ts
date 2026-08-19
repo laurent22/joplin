30578a4381e28886a86b7d108c2ab0527e0075e6
Henry Heino <personalizedrefrigerator@gmail.com>
2026-08-19 14:28:11 -0700

    Address CodeRabbit feedback: Guard against empty regexText

--- packages/app-desktop/gui/NoteEditor/NoteBody/TinyMCE/utils/useHighlightedSearchTerms.ts
+++ packages/app-desktop/gui/NoteEditor/NoteBody/TinyMCE/utils/useHighlightedSearchTerms.ts
@@ -23,16 +23,11 @@ const useSearchRegexes = (searchTerms: HighlightedWord[]) => {
 				text = term.value;
 			}
 
-			let regex = SearchEngine.instance().queryTermToRegex(text);
 			// Only highlight short matches at word boundaries (roughly matching the note viewer
 			// behavior)
-			if (text.length <= 2) {
-				// (?<!\w): Checks whether a non-word character is before the regex, without including it in the match
-				// (?!\w): Check whether a non-word character is after the regex, also without including it.
-				regex = `(?<!\\w)${regex}(?!\\w)`;
-			}
-			return new RegExp(regex, 'ig');
-		});
+			const regexText = SearchEngine.instance().queryTermToRegex(text, { anchorToWordBoundary: text.length <= 2 });
+			return regexText ? new RegExp(regexText, 'ig') : null;
+		}).filter(regex => !!regex);
 	}, [searchTerms]);
 };
 
