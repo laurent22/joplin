3181a416818721730fe404eeb5263359a01a35d8
Henry Heino <personalizedrefrigerator@gmail.com>
2026-08-28 11:33:33 -0700

    Revert sort order icon changes for now

--- packages/lib/models/Note.ts
+++ packages/lib/models/Note.ts
@@ -33,7 +33,7 @@ import { ShareType, StateShare } from '../services/share/reducer';
 
 export interface PreviewsOrder {
 	by: string;
-	dir: 'ASC'|'DESC';
+	dir: string;
 }
 
 export interface DuplicateOptions {
