3a782a4c9e9a8e937d0b7a805fa5f3624cc4f50d
mrjo118 <jo.118@hotmail.com>
2026-09-01 15:04:05 +0100

    Handle notes with no parent id

--- packages/app-desktop/commands/openNoteInNewWindow.ts
+++ packages/app-desktop/commands/openNoteInNewWindow.ts
@@ -6,6 +6,7 @@ import Folder from '@joplin/lib/models/Folder';
 import { createAppDefaultWindowState } from '../app.reducer';
 import Setting from '@joplin/lib/models/Setting';
 import { getDisplayParentId } from '@joplin/lib/services/trash';
+import { ALL_NOTES_FILTER_ID } from '@joplin/lib/reserved-ids';
 
 export const declaration: CommandDeclaration = {
 	name: 'openNoteInNewWindow',
@@ -22,7 +23,8 @@ export const runtime = (): CommandRuntime => {
 
 			const note = await Note.load(noteId, { fields: Note.previewFields() });
 			const parentFolder = await Folder.load(note.parent_id);
-			const folderId = note.is_conflict && !note.deleted_time ? Folder.conflictFolderId() : getDisplayParentId(note, parentFolder);
+			let folderId = note.is_conflict && !note.deleted_time ? Folder.conflictFolderId() : getDisplayParentId(note, parentFolder);
+			if (!note.is_conflict && !note.deleted_time && !parentFolder) folderId = ALL_NOTES_FILTER_ID;
 			context.dispatch({
 				type: 'WINDOW_OPEN',
 				noteId,
