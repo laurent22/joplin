2e5ff6aef4a984ea293efeb00c8fc3e34e2b8460
mrjo118 <jo.118@hotmail.com>
2026-09-01 14:38:00 +0100

    Handle nested trashed notes and deleted conflicts correctly

--- packages/app-desktop/commands/openNoteInNewWindow.ts
+++ packages/app-desktop/commands/openNoteInNewWindow.ts
@@ -5,7 +5,7 @@ import Note from '@joplin/lib/models/Note';
 import Folder from '@joplin/lib/models/Folder';
 import { createAppDefaultWindowState } from '../app.reducer';
 import Setting from '@joplin/lib/models/Setting';
-import { getTrashFolderId } from '@joplin/lib/services/trash';
+import { getDisplayParentId } from '@joplin/lib/services/trash';
 
 export const declaration: CommandDeclaration = {
 	name: 'openNoteInNewWindow',
@@ -21,7 +21,8 @@ export const runtime = (): CommandRuntime => {
 			noteId = noteId || stateUtils.selectedNoteId(context.state);
 
 			const note = await Note.load(noteId, { fields: Note.previewFields() });
-			const folderId = note.is_conflict ? Folder.conflictFolderId() : note.deleted_time ? getTrashFolderId() : note.parent_id;
+			const parentFolder = await Folder.load(note.parent_id);
+			const folderId = note.is_conflict && !note.deleted_time ? Folder.conflictFolderId() : getDisplayParentId(note, parentFolder);
 			context.dispatch({
 				type: 'WINDOW_OPEN',
 				noteId,
