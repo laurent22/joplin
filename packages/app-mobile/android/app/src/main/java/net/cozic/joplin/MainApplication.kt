3e10b196cd02d6e9206dbe18cf178b191c76f9e7
Henry Heino <personalizedrefrigerator@gmail.com>
2026-09-19 15:42:03 -0700

    Fixing Android build

--- packages/app-mobile/android/app/src/main/java/net/cozic/joplin/MainApplication.kt
+++ packages/app-mobile/android/app/src/main/java/net/cozic/joplin/MainApplication.kt
@@ -8,13 +8,9 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import net.cozic.joplin.versioninfo.SystemVersionInformationPackage
+import net.cozic.joplin.systeminfo.SystemInformationPackage
 import net.cozic.joplin.share.SharePackage
 import net.cozic.joplin.ssl.SslPackage
 
@@ -30,9 +26,6 @@ class MainApplication : Application(), ReactApplication {
                 }
             )
     }
-
-    override val reactHost: ReactHost
-        get() = ReactNativeHostWrapper.createReactHost(this.applicationContext, reactNativeHost)
 
     override fun onCreate() {
         super.onCreate()
