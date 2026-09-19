c27c80b5b5d71bdc487ab9b97fe2725244423c9c
Henry Heino <personalizedrefrigerator@gmail.com>
2026-09-19 13:01:56 -0700

    Fix iOS startup: Don't attempt to use `Platform` on startup

--- packages/react-native-saf-x/src/index.ts
+++ packages/react-native-saf-x/src/index.ts
@@ -1,29 +1,23 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules } from 'react-native';
 
 const LINKING_ERROR =
-	`The package 'react-native-saf-x' doesn't seem to be linked. Make sure: \n\n${
-		Platform.select({ ios: '- You have run \'pod install\'\n', default: '' })
-	}- You rebuilt the app after installing the package\n` +
+	'The package \'react-native-saf-x\' doesn\'t seem to be linked. Make sure: \n' +
+	'- You aren\'t attempting to use the package on iOS or web\n' +
+	'- You rebuilt the app after installing the package\n' +
 	'- You are not using Expo managed workflow\n';
 
-let SafX: SafxInterface;
-
-if (Platform.OS === 'android') {
-	SafX = (
-		NativeModules.SafX
-			? NativeModules.SafX
-			: new Proxy(
-				{},
-				{
-					get() {
-						throw new Error(LINKING_ERROR);
-					},
+const SafX = (
+	NativeModules.SafX
+		? NativeModules.SafX
+		: new Proxy(
+			{},
+			{
+				get() {
+					throw new Error(LINKING_ERROR);
 				},
-			)
-	) as SafxInterface;
-} else {
-	SafX = {} as SafxInterface;
-}
+			},
+		)
+) as SafxInterface;
 
 export type Encoding = 'utf8' | 'base64' | 'ascii';
 
