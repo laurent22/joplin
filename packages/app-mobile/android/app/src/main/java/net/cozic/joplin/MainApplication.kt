d2b46f83e380f18b691dced5734fb44473a97ef0
Henry Heino <personalizedrefrigerator@gmail.com>
2026-09-21 21:04:07 -0700

    Restore comment

--- packages/app-mobile/android/app/src/main/java/net/cozic/joplin/MainApplication.kt
+++ packages/app-mobile/android/app/src/main/java/net/cozic/joplin/MainApplication.kt
@@ -20,6 +20,7 @@ class MainApplication : Application(), ReactApplication {
             context = applicationContext,
             packageList =
                 PackageList(this).packages.apply {
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
                     add(SharePackage())
                     add(SslPackage())
                     add(SystemInformationPackage())
